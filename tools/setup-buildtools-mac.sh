21f1fff56c89d7dfca1b7babffa055e7f040d406
Max Golovanov <maxgolov@microsoft.com>
2019-11-04 14:24:13 -0800

    Remove sqlite for now

--- tools/setup-buildtools-mac.sh
+++ tools/setup-buildtools-mac.sh
@@ -26,14 +26,3 @@ sudo chown -R $(whoami) /usr/local/etc/bash_completion.d /usr/local/include /usr
 brew install cmake
 brew install wget
 brew install clang-format
-
-## Install sqlite 3.22
-#export SQLITE_PKG=sqlite-autoconf-3220000
-#wget https://www.sqlite.org/2018/$SQLITE_PKG.tar.gz -O /tmp/sqlite-snapshot.tar.gz
-#tar -xvf /tmp/sqlite-snapshot.tar.gz
-#cd $SQLITE_PKG
-#./configure && make && make install
-#cd ..
-
-## Build Google Test framework
-#./build-gtest.sh
--- tools/setup-buildtools.sh
+++ tools/setup-buildtools.sh
@@ -48,7 +48,6 @@ apt-get update -y
 apt-get install -qq automake
 apt-get install -qq libtool-bin
 apt-get install -qq cmake
-apt-get install -qq sqlite
 apt-get install -qq curl
 apt-get install -qq libcurl4-openssl-dev
 apt-get install -qq zlib1g-dev
@@ -61,17 +60,6 @@ apt-get install -qq libsqlite3-dev
 apt-get install -qq wget
 apt-get install -qq clang-format
 fi
-
-### Install sqlite 3.22
-#export SQLITE_PKG=sqlite-autoconf-3220000
-#wget https://www.sqlite.org/2018/$SQLITE_PKG.tar.gz -O /tmp/sqlite-snapshot.tar.gz
-#tar -xvf /tmp/sqlite-snapshot.tar.gz
-#cd $SQLITE_PKG
-#./configure && make && make install
-#cd ..
-
-## Build Google Test framework
-#./build-gtest.sh
 
 ## Change owner from root to current dir owner
 chown -R `stat . -c %u:%g` *
