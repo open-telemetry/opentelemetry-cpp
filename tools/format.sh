336cb4c6132f4f7d29b549d11984b7f407794f04
easy <g-easy@users.noreply.github.com>
2019-12-17 16:16:04 +1100

    Add tools/format.sh which formats all files automatically.
    Uses clang-format, cmake-format, and buildifier.

---
+++ tools/format.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+if [[ ! -e tools/format.sh ]]; then
+  echo "This tool must be run from the topmost directory." >&2
+  exit 1
+fi
+
+set -e
+
+FIND="find . -name .git -prune -o -name _deps -prune -o -name .build -prune -o"
+
+# Correct common miscapitalizations.
+sed -i 's/Open[t]elemetry/OpenTelemetry/g' $($FIND -type f -print)
+# No CRLF line endings.
+sed -i 's/\r$//' $($FIND -type f -print)
+# No trailing spaces.
+sed -i 's/ \+$//' $($FIND -type f -print)
+
+CMD=clang-format-8
+$CMD -version
+$CMD -i -style=file \
+  $($FIND -name '*.cc' -print -o -name '*.h' -print)
+
+if which cmake-format >/dev/null; then
+  echo "Running cmake-format $(cmake-format --version 2>&1)."
+  cmake-format -i $($FIND -name 'CMakeLists.txt' -print)
+else
+  echo "Can't find cmake-format. It can be installed with:"
+  echo "  pip install --user cmake_format"
+fi
+
+if which buildifier >/dev/null; then
+  echo "Running buildifier."
+  buildifier $($FIND -name WORKSPACE -print -o -name BUILD -print -o \
+    -name '*.bzl' -print)
+else
+  echo "Can't find buildifier. It can be installed with:"
+  echo "  go get github.com/bazelbuild/buildtools/buildifier"
+fi
