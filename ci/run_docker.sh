76bfba2e21316c987966d5ae8cccce01997fc153
Ryan Burn <ryan.burn@gmail.com>
2019-12-19 19:55:07 -0800

    Remove unnecessary packages

--- ci/run_docker.sh
+++ ci/run_docker.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BUILD_IMAGE=opentelemetry-cpp-build
+BUILD_IMAGE=opentelemetry-cpp-build3
 docker image inspect "$BUILD_IMAGE" &> /dev/null || {
   docker build -t "$BUILD_IMAGE" ci
 }
--- ci/setup_ci_environment.sh
+++ ci/setup_ci_environment.sh
@@ -4,24 +4,5 @@ set -e
 apt-get update 
 apt-get install --no-install-recommends --no-install-suggests -y \
                 build-essential \
-                cmake \
-                pkg-config \
-                git \
                 ca-certificates \
-                curl \
-                automake \
-                autogen \
-                autoconf \
-                libtool \
-                gnupg2 \
-                ssh \
-                wget \
-                libgtest-dev
-
-# Follows these instructions for setting up gtest
-# https://www.eriksmistad.no/getting-started-with-google-test-on-ubuntu/
-pushd /usr/src/gtest
-cmake CMakeLists.txt
-make
-cp *.a /usr/lib
-popd
+                wget
