9b0925bf407348c94e0d0d06e510df0bc931da82
Ryan <ryan.burn@gmail.com>
2019-12-22 18:52:25 -0800

    Setup minimal CI (#17)
    * Setup basic ci
    
    * Add empty README
    
    * Fix script path
    
    * Fill out README.
    
    * Typo.
    
    * Add missing script name.
    
    * Remove unnecessary packages
    
    * Use fastbuild.
    
    * Fix typo.
    
    Co-authored-by: easy <g-easy@users.noreply.github.com>

---
+++ ci/setup_cmake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+apt-get install --no-install-recommends --no-install-suggests -y \
+                cmake \
+                libgtest-dev
+
+# Follows these instructions for setting up gtest
+# https://www.eriksmistad.no/getting-started-with-google-test-on-ubuntu/
+pushd /usr/src/gtest
+cmake CMakeLists.txt
+make
+cp *.a /usr/lib
+popd
