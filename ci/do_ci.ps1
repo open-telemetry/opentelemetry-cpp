71e93db23a68804d40b90f7f5c208c2a50fa2557
Ryan Burn <ryan.burn@gmail.com>
2019-12-23 13:11:57 -0800

    Don't build shared libraries.

--- ci/do_ci.ps1
+++ ci/do_ci.ps1
@@ -13,7 +13,8 @@ switch ($action) {
 		cd "$BUILD_DIR"
 		cmake $SRC_DIR `
           -DVCPKG_TARGET_TRIPLET=x64-windows-static `
-			    "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR\scripts\buildsystems\vcpkg.cmake"
+			    "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR\scripts\buildsystems\vcpkg.cmake" `
+          -DBUILD_SHARED_LIBS=OFF
     $exit = $LASTEXITCODE
     if ($exit -ne 0) {
       exit $exit
