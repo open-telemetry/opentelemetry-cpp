9df8eb62a5c08163175b52b19109255b8f4a3fc4
Ryan <ryan.burn@gmail.com>
2020-01-27 20:07:51 -0800

    Add a Timestamp type (#29)
    * Add timestamp
    
    * Reword
    
    * Reword
    
    * Fix build for gcc48
    
    * Fix cmake tests
    
    * Update timestamp storage
    
    * Reformat
    
    * Split timestamp into a system and steady type

---
+++ api/include/opentelemetry/core/timestamp.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <algorithm>
+#include <chrono>
+#include <cstdint>
+
+namespace opentelemetry
+{
+namespace core
+{
+/**
+ * Represents a timepoint relative to the system clock epoch
+ */
+class SystemTimestamp
+{
+public:
+  SystemTimestamp() noexcept : nanos_since_epoch_{0} {}
+
+  template <class Rep, class Period>
+  explicit SystemTimestamp(const std::chrono::duration<Rep, Period> &time_since_epoch) noexcept
+      : nanos_since_epoch_{static_cast<int64_t>(
+            std::chrono::duration_cast<std::chrono::nanoseconds>(time_since_epoch).count())}
+  {}
+
+  /*implicit*/ SystemTimestamp(const std::chrono::system_clock::time_point &time_point) noexcept
+      : SystemTimestamp{time_point.time_since_epoch()}
+  {}
+
+  operator std::chrono::system_clock::time_point() const noexcept
+  {
+    return std::chrono::system_clock::time_point{
+        std::chrono::duration_cast<std::chrono::system_clock::duration>(
+            std::chrono::nanoseconds{nanos_since_epoch_})};
+  }
+
+  /**
+   * @return the amount of time that has passed since the system clock epoch
+   */
+  std::chrono::nanoseconds time_since_epoch() const noexcept
+  {
+    return std::chrono::nanoseconds{nanos_since_epoch_};
+  }
+
+private:
+  int64_t nanos_since_epoch_;
+};
+
+/**
+ * Represents a timepoint relative to the monotonic clock epoch
+ */
+class SteadyTimestamp
+{
+public:
+  SteadyTimestamp() noexcept : nanos_since_epoch_{0} {}
+
+  template <class Rep, class Period>
+  explicit SteadyTimestamp(const std::chrono::duration<Rep, Period> &time_since_epoch) noexcept
+      : nanos_since_epoch_{static_cast<int64_t>(
+            std::chrono::duration_cast<std::chrono::nanoseconds>(time_since_epoch).count())}
+  {}
+
+  /*implicit*/ SteadyTimestamp(const std::chrono::steady_clock::time_point &time_point) noexcept
+      : SteadyTimestamp{time_point.time_since_epoch()}
+  {}
+
+  operator std::chrono::steady_clock::time_point() const noexcept
+  {
+    return std::chrono::steady_clock::time_point{
+        std::chrono::duration_cast<std::chrono::steady_clock::duration>(
+            std::chrono::nanoseconds{nanos_since_epoch_})};
+  }
+
+  /**
+   * @return the amount of time that has passed since the monotonic clock epoch
+   */
+  std::chrono::nanoseconds time_since_epoch() const noexcept
+  {
+    return std::chrono::nanoseconds{nanos_since_epoch_};
+  }
+
+private:
+  int64_t nanos_since_epoch_;
+};
+}  // namespace core
+}  // namespace opentelemetry
--- api/include/opentelemetry/trace/span.h
+++ api/include/opentelemetry/trace/span.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "opentelemetry/core/timestamp.h"
+
 namespace opentelemetry
 {
 namespace trace
@@ -8,7 +10,18 @@ namespace trace
  * StartSpanOptions provides options to set properties of span at the time of starting a new span.
  */
 struct StartSpanOptions
-{};
+{
+  // Sets the start time of a span.
+  //
+  // If the start time of a span is set, timestamps from both the system clock and steady clock
+  // should be provided.
+  //
+  // Timestamps from the steady clock can be used to most accurately measure a span's
+  // duration, while timestamps from the system clock can be used to most accurately place a span's
+  // time point relative to other spans collected across a distributed system.
+  core::SystemTimestamp start_system_time;
+  core::SteadyTimestamp start_steady_time;
+};
 
 class Tracer;
 
