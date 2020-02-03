03d8fddf6a38911c74c058961a5414e78c2efde9
Ryan Burn <ryan.burn@gmail.com>
2020-02-02 22:30:31 -0800

    Add plugin example

--- api/include/opentelemetry/plugin/hook.h
+++ api/include/opentelemetry/plugin/hook.h
@@ -5,23 +5,23 @@
 
 #ifdef WIN32
 
-#  define OPENTELEMETRY_DEFINE_PLUGIN_HOOK(X)                                        \
-    extern "C" {                                                                      \
-    extern __declspec(dllexport)                                                      \
+#  define OPENTELEMETRY_DEFINE_PLUGIN_HOOK(X)                                            \
+    extern "C" {                                                                         \
+    extern __declspec(dllexport)                                                         \
         opentelemetry::plugin::OpenTelemetryHook const OpenTelemetryMakeFactoryImpl;     \
-                                                                                      \
-    __declspec(selectany)                                                             \
+                                                                                         \
+    __declspec(selectany)                                                                \
         opentelemetry::plugin::OpenTelemetryHook const OpenTelemetryMakeFactoryImpl = X; \
     }  // extern "C"
 
 #else
 
-#  define OPENTELEMETRY_DEFINE_PLUGIN_HOOK(X)                                                   \
-    extern "C" {                                                                                 \
-    __attribute(                                                                                 \
-        (weak)) extern opentelemetry::plugin::OpenTelemetryHook const OPENTELEMETRY_PLUGIN_HOOK; \
-                                                                                                 \
-    opentelemetry::plugin::OpenTelemetryHook const OPENTELEMETRY_PLUGIN_HOOK = X;                \
+#  define OPENTELEMETRY_DEFINE_PLUGIN_HOOK(X)                                                      \
+    extern "C" {                                                                                   \
+    __attribute((                                                                                  \
+        weak)) extern opentelemetry::plugin::OpenTelemetryHook const OpenTelemetryMakeFactoryImpl; \
+                                                                                                   \
+    opentelemetry::plugin::OpenTelemetryHook const OpenTelemetryMakeFactoryImpl = X;               \
     }  // extern "C"
 
 #endif
