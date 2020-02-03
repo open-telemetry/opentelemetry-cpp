7cfab5fc51c9ae5cd7eda020a3a01e8b2b037c1e
Ryan Burn <ryan.burn@gmail.com>
2020-02-02 23:54:10 -0800

    Add windows stub

--- api/include/opentelemetry/plugin/detail/dynamic_load_windows.h
+++ api/include/opentelemetry/plugin/detail/dynamic_load_windows.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <memory>
+
 namespace opentelemetry
 {
 namespace plugin
-{}  // namespace plugin
+{
+inline std::unique_ptr<Factory> LoadFactory(const char *plugin,
+                                            std::unique_ptr<char[]> &error_message) noexcept
+{
+  (void)plugin;
+  (void)error_message;
+  return nullptr;
+}
+}  // namespace plugin
 }  // namespace opentelemetry
