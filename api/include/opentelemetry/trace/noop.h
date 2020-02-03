6556bf11df95072d37b4e9314639003ad5af29f6
easy <g-easy@users.noreply.github.com>
2020-02-03 15:48:17 +1100

    Add Span API.

--- api/include/opentelemetry/trace/noop.h
+++ api/include/opentelemetry/trace/noop.h
@@ -1,5 +1,12 @@
 #pragma once
+// Please refer to provider.h for documentation on how to obtain a Tracer object.
+//
+// This file is part of the internal implementation of OpenTelemetry. Nothing in this file should be
+// used directly. Please refer to span.h and tracer.h for documentation on these interfaces.
 
+#include "opentelemetry/nostd/string_view.h"
+#include "opentelemetry/nostd/unique_ptr.h"
+#include "opentelemetry/trace/span.h"
 #include "opentelemetry/trace/tracer.h"
 
 #include <memory>
@@ -9,14 +16,24 @@ namespace opentelemetry
 namespace trace
 {
 /**
- * Noop implementation of Span.
+ * No-op implementation of Span. This class should not be used directly.
  */
 class NoopSpan final : public Span
 {
 public:
   explicit NoopSpan(const std::shared_ptr<Tracer> &tracer) noexcept : tracer_{tracer} {}
 
-  // Span
+  void AddEvent(nostd::string_view name) override {}
+
+  void AddEvent(nostd::string_view name, core::SystemTimestamp timestamp) override {}
+  void AddEvent(nostd::string_view name, core::SteadyTimestamp timestamp) override {}
+
+  void UpdateName(nostd::string_view name) override {}
+
+  void End() override {}
+
+  bool IsRecording() const override { return false; }
+
   Tracer &tracer() const noexcept override { return *tracer_; }
 
 private:
@@ -24,7 +41,7 @@ private:
 };
 
 /**
- * Noop implementation of Tracer
+ * No-op implementation of Tracer. This class should not be used directly.
  */
 class NoopTracer final : public Tracer, public std::enable_shared_from_this<NoopTracer>
 {
--- api/include/opentelemetry/trace/span.h
+++ api/include/opentelemetry/trace/span.h
@@ -1,37 +1,111 @@
 #pragma once
 
+#include <cstdint>
+
 #include "opentelemetry/core/timestamp.h"
+#include "opentelemetry/nostd/string_view.h"
 
 namespace opentelemetry
 {
 namespace trace
 {
+enum class SpanKind
+{
+  kInternal,
+  kServer,
+  kClient,
+  kProducer,
+  kConsumer,
+};
 /**
- * StartSpanOptions provides options to set properties of span at the time of starting a new span.
+ * StartSpanOptions provides options to set properties of a Span at the time of its creation
  */
 struct StartSpanOptions
 {
-  // Sets the start time of a span.
+  // Optionally sets the start time of a Span.
   //
-  // If the start time of a span is set, timestamps from both the system clock and steady clock
-  // should be provided.
+  // If the start time of a Span is set, timestamps from both the system clock and steady clock
+  // must be provided.
   //
-  // Timestamps from the steady clock can be used to most accurately measure a span's
-  // duration, while timestamps from the system clock can be used to most accurately place a span's
-  // time point relative to other spans collected across a distributed system.
+  // Timestamps from the steady clock can be used to most accurately measure a Span's
+  // duration, while timestamps from the system clock can be used to most accurately place a Span's
+  // time point relative to other Spans collected across a distributed system.
   core::SystemTimestamp start_system_time;
   core::SteadyTimestamp start_steady_time;
+
+  // TODO:
+  // Span(Context?) parent;
+  // SpanContext remote_parent;
+  // Links
+  // Attributes
+  SpanKind kind = SpanKind::kInternal;
 };
 
 class Tracer;
 
 /**
- * A span represents a single operation within a trace.
+ * A Span represents a single operation within a Trace.
+ *
+ * Spans *must* be ended by calling End(). Merely deleting a Span does not end
+ * it.
  */
 class Span
 {
 public:
+  // Note that Spans should be created using the Tracer class. Please refer to
+  // tracer.h for documentation.
+  Span()          = default;
   virtual ~Span() = default;
+
+  // Not copiable or movable.
+  Span(const Span &) = delete;
+  Span(Span &&)      = delete;
+  Span &operator=(const Span &) = delete;
+  Span &operator=(Span &&) = delete;
+
+  // TODO
+  // Sets an attribute on the Span. If the Span previously contained a mapping for
+  // the key, the old value is replaced.
+  //
+  // If an empty string is used as the value, the attribute will be silently
+  // dropped. Note: this behavior could change in the future.
+  // virtual void SetAttribute(nostd::string_view key, AttributeValue&& value) = 0;
+
+  // Adds an event to the Span.
+  virtual void AddEvent(nostd::string_view name) = 0;
+
+  // Adds an event to the Span, with a custom timestamp.
+  virtual void AddEvent(nostd::string_view name, core::SystemTimestamp timestamp) = 0;
+  virtual void AddEvent(nostd::string_view name, core::SteadyTimestamp timestamp) = 0;
+
+  // TODO
+  // Adds an event to the Span, with a custom timestamp, and attributes.
+  // virtual void AddEvent(nostd::string_view name, core::SteadyTimestamp
+  // timestamp, nostd::span<std::pair<nostd::string_view name, AttributeValue&&
+  // value>> attributes);
+
+  // TODO
+  // Sets the status of the span. The default status is OK. Only the value of the last call will be
+  // recorded, and implementations are free to ignore previous calls.
+  // virtual void SetStatus(Status status) = 0;
+
+  // Updates the name of the Span. If used, this will override the name provided
+  // during construction.
+  virtual void UpdateName(nostd::string_view name) = 0;
+
+  // Mark the end of the Span. Only the timing of the first end call for a given {@code Span} will
+  // be recorded, and implementations are free to ignore all further calls.
+  virtual void End() = 0;
+
+  // TODO
+  // virtual void End(EndSpanOptions&& opts) = 0;
+
+  // TODO
+  // SpanContext GetContext() const = 0;
+
+  // Returns true if this Span is recording tracing events (e.g. SetAttribute,
+  // AddEvent).
+  virtual bool IsRecording() const = 0;
 
   virtual Tracer &tracer() const noexcept = 0;
 };
