9cb915eac965a116d5205235295002dcc60c3067
Ryan Burn <ryan.burn@gmail.com>
2020-02-02 22:59:44 -0800

    Remove mock from sdk

--- sdk/src/opentelemetry/mock/hook.cc
+++
@@ -1,4 +0,0 @@
-#include "sdk/src/opentelemetry/mock/factory_impl.h"
-#include "opentelemetry/plugin/hook.h"
-
-OPENTELEMETRY_DEFINE_PLUGIN_HOOK(opentelemetry::sdk::mock::MakeFactoryImpl);
--- sdk/src/opentelemetry/mock/span.cc
+++
@@ -1,9 +0,0 @@
-#include "sdk/src/opentelemetry/mock/span.h"
-
-namespace opentelemetry {
-namespace sdk {
-namespace mock {
-Span::Span(std::shared_ptr<Tracer> &&tracer) noexcept : tracer_{std::move(tracer)} {}
-}  // namespace mock
-} // namespace sdk
-} // namespace opentelemetry
