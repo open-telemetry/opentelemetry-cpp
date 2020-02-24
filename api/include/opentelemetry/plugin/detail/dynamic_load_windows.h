add513fc5850fe659ff7081a7c22f79f54c34bef
Ryan Burn <ryan.burn@gmail.com>
2020-02-23 16:53:41 -0800

    Fill in windows loading code

--- api/include/opentelemetry/plugin/detail/dynamic_load_windows.h
+++ api/include/opentelemetry/plugin/detail/dynamic_load_windows.h
@@ -7,15 +7,66 @@
 #include "opentelemetry/plugin/hook.h"
 #include "opentelemetry/version.h"
 
+#include <windows.h>
+
 namespace opentelemetry
 {
 namespace plugin
 {
+class DynamicLibraryHandleWindows final : public DynamicLibraryHandle
+{
+public:
+  explicit DynamicLibraryHandleWindows(HINSTANCE handle) : handle_{handle} {}
+
+  ~DynamicLibraryHandleWindows() override { ::FreeLibrary(handle_); }
+
+private:
+  HINSTANCE handle_;
+};
+
 inline std::unique_ptr<Factory> LoadFactory(const char *plugin,
                                             std::unique_ptr<char[]> &error_message) noexcept
 {
-  detail::CopyErrorMessage("not supported yet", error_message);
-  return nullptr;
+  auto handle = ::LoadLibrary(plugin);
+  if (handle == nullptr)
+  {
+    // TODO: Set error_message
+    detail::CopyErrorMessage("Failed to load plugin", error_message);
+    return nullptr;
+  }
+
+  std::shared_ptr<DynamicLibraryHandle> library_handle{new (std::nothrow)
+                                                           DynamicLibraryHandleWindows{handle}};
+  if (library_handle == nullptr)
+  {
+    detail::CopyErrorMessage("Allocation failure", error_message);
+    return nullptr;
+  }
+
+  auto make_factory_impl = reinterpret_cast<OpenTelemetryHook *>(
+      ::GetProcAddress(handle, "OpenTelemetryMakeFactoryImpl"));
+  if (make_factory_impl == nullptr)
+  {
+    // TODO: set error_message
+    detail::CopyErrorMessage("Failed find hook", error_message);
+    return nullptr;
+  }
+  if (*make_factory_impl == nullptr)
+  {
+    detail::CopyErrorMessage("Invalid plugin hook", error_message);
+    return nullptr;
+  }
+
+  LoaderInfo loader_info;
+  nostd::unique_ptr<char[]> plugin_error_message;
+  auto factory_impl = (**make_factory_impl)(loader_info, plugin_error_message);
+  if (factory_impl == nullptr)
+  {
+    error_message = std::move(plugin_error_message);
+    return nullptr;
+  }
+  return std::unique_ptr<Factory>{new (std::nothrow)
+                                      Factory{std::move(library_handle), std::move(factory_impl)}};
 }
 }  // namespace plugin
 }  // namespace opentelemetry
