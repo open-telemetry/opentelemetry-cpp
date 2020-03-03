79b4f1d5cf546312090f734835b6a623f0d7759d
Ryan Burn <ryan.burn@gmail.com>
2020-03-02 23:43:52 -0800

    Fill out Span api

--- sdk/src/trace/span.cc
+++ sdk/src/trace/span.cc
@@ -14,6 +14,41 @@ Span::Span(std::shared_ptr<Tracer> &&tracer,
   (void)name;
   (void)options;
 }
+
+void Span::AddEvent(nostd::string_view name) noexcept
+{
+  (void)name;
+}
+
+void Span::AddEvent(nostd::string_view name, core::SystemTimestamp timestamp) noexcept
+{
+  (void)name;
+  (void)timestamp;
+}
+
+void Span::AddEvent(nostd::string_view name, core::SteadyTimestamp timestamp) noexcept
+{
+  (void)name;
+  (void)timestamp;
+}
+
+void Span::SetStatus(trace_api::CanonicalCode code, nostd::string_view description) noexcept
+{
+  (void)code;
+  (void)description;
+}
+
+void Span::UpdateName(nostd::string_view name) noexcept
+{
+  (void)name;
+}
+
+void Span::End() noexcept {}
+
+bool Span::IsRecording() const noexcept
+{
+  return true;
+}
 }  // namespace trace
 }  // namespace sdk
 }  // namespace opentelemetry
--- sdk/src/trace/span.h
+++ sdk/src/trace/span.h
@@ -4,22 +4,41 @@
 
 #include <mutex>
 
-namespace opentelemetry {
-namespace sdk {
-namespace trace {
+namespace opentelemetry
+{
+namespace sdk
+{
+namespace trace
+{
 namespace trace_api = opentelemetry::trace;
 
-class Span final : public trace_api::Span {
- public:
-   explicit Span(std::shared_ptr<Tracer> &&tracer,
-                 nostd::string_view name,
-                 const trace_api::StartSpanOptions &options) noexcept;
+class Span final : public trace_api::Span
+{
+public:
+  explicit Span(std::shared_ptr<Tracer> &&tracer,
+                nostd::string_view name,
+                const trace_api::StartSpanOptions &options) noexcept;
 
- private:
-   std::shared_ptr<Tracer> tracer_;
-   std::mutex mutex_;
-   std::unique_ptr<Recordable> recordable_;
+  void AddEvent(nostd::string_view name) noexcept override;
+
+  void AddEvent(nostd::string_view name, core::SystemTimestamp timestamp) noexcept override;
+  void AddEvent(nostd::string_view name, core::SteadyTimestamp timestamp) noexcept override;
+
+  void SetStatus(trace_api::CanonicalCode code, nostd::string_view description) noexcept override;
+
+  void UpdateName(nostd::string_view name) noexcept override;
+
+  void End() noexcept override;
+
+  bool IsRecording() const noexcept override;
+
+  trace_api::Tracer &tracer() const noexcept override { return *tracer_; }
+
+private:
+  std::shared_ptr<Tracer> tracer_;
+  std::mutex mutex_;
+  std::unique_ptr<Recordable> recordable_;
 };
-} // namespace trace
-} // namespace sdk
-} // namespace opentelemetry
+}  // namespace trace
+}  // namespace sdk
+}  // namespace opentelemetry
