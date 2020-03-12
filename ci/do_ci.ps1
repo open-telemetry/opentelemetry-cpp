366e9e092864d0e9c25d305038ab5ed0438eca20
Ryan Burn <ryan.burn@gmail.com>
2020-03-11 19:38:07 -0700

    Add otprotocol windows tests

--- ci/do_ci.ps1
+++ ci/do_ci.ps1
@@ -38,6 +38,27 @@ switch ($action) {
       exit $exit
     }
   }
+  "cmake.exporter.otprotocol.test" {
+    cd "$BUILD_DIR"
+    cmake $SRC_DIR `
+          -DVCPKG_TARGET_TRIPLET=x64-windows `
+          -DWITH_OTPROTCOL=ON `
+          "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR\scripts\buildsystems\vcpkg.cmake"
+    $exit = $LASTEXITCODE
+    if ($exit -ne 0) {
+      exit $exit
+    }
+    cmake --build .
+    $exit = $LASTEXITCODE
+    if ($exit -ne 0) {
+      exit $exit
+    }
+    ctest -C Debug
+    $exit = $LASTEXITCODE
+    if ($exit -ne 0) {
+      exit $exit
+    }
+  }
   "cmake.build_example_plugin" {
     cd "$BUILD_DIR"
     cmake $SRC_DIR `
