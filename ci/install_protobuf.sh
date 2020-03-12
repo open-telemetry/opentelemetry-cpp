092bd8d4eb4756eaee16901470a0cbc5d629381d
Ryan Burn <ryan.burn@gmail.com>
2020-03-11 18:56:54 -0700

    Fix install_protobuf.sh

--- ci/install_protobuf.sh
+++ ci/install_protobuf.sh
@@ -4,8 +4,9 @@ set -e
 
 [ -z "${PROTOBUF_VERSION}" ] && export PROTOBUF_VERSION="3.11.4"
 
+cd /
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-cpp-${PROTOBUF_VERSION}.tar.gz
-tar zxf protobuf-cpp-${PROTOBUF_VERSION}.tar.gz
+tar zxf --no-same-owner protobuf-cpp-${PROTOBUF_VERSION}.tar.gz
 cd protobuf-${PROTOBUF_VERSION}
 ./configure
 make && make install
