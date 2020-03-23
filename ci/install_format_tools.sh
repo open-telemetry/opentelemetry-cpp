deff2c66787604a846facdc9e3f7e8954f8469c0
Johannes Tax <jtax@newrelic.com>
2020-03-23 11:59:05 -0700

    Use newest available Go version

--- ci/install_format_tools.sh
+++ ci/install_format_tools.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-apt install -y clang-format-8 python3-pip golang git
+apt-get install -y software-properties-common
+add-apt-repository ppa:longsleep/golang-backports
+apt update
+
+apt install -y clang-format-8 python3-pip golang-go git
 pip3 install cmake_format==0.6.5
 go get github.com/bazelbuild/buildtools/buildifier
