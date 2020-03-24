aa4a8731d6755e8091f61c83c80deb67c5ba805e
Johannes Tax <jtax@newrelic.com>
2020-03-24 09:25:48 -0700

    Download the right thing

--- ci/install_format_tools.sh
+++ ci/install_format_tools.sh
@@ -4,4 +4,5 @@ set -e
 
 apt install -y clang-format-8 python3-pip git curl
 pip3 install cmake_format==0.6.5
-curl -o /usr/local/bin/buildifier https://github.com/bazelbuild/buildtools/releases/tag/2.2.1/buildifier
+curl -L -o /usr/local/bin/buildifier https://github.com/bazelbuild/buildtools/releases/download/2.2.1/buildifier
+chmod +x /usr/local/bin/buildifier
