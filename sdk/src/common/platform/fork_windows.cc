0cfb58344316d415e7be57234dfb3f8549dd3078
Ryan Burn <ryan.burn@gmail.com>
2020-03-25 22:14:25 -0700

    Add fork utility

---
+++ sdk/src/common/platform/fork_windows.cc
@@ -0,0 +1,16 @@
+#include "src/common/platform/fork.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace sdk {
+namespace common {
+namespace platform {
+int AtFork(void (*prepare)(), void (*parent)(), void (*child)()) noexcept {
+  (void)prepare;
+  (void)parent;
+  (void)child;
+  return 0;
+}
+} // namespace platform
+} // namespace common
+} // namespace sdk
+OPENTELEMETRY_BEGIN_NAMESPACE
