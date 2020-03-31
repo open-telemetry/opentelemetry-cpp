8aee486960c3d840f4b06764e947ff33f8dfb210
Ryan Burn <ryan.burn@gmail.com>
2020-03-31 15:29:12 -0700

    Add variant_alternative

--- api/include/opentelemetry/nostd/variant.h
+++ api/include/opentelemetry/nostd/variant.h
@@ -2,3 +2,4 @@
 
 #include "opentelemetry/nostd/detail/type_pack_element.h"
 #include "opentelemetry/nostd/detail/variant_size.h"
+#include "opentelemetry/nostd/detail/variant_alternative.h"
--- api/test/nostd/variant_test.cc
+++ api/test/nostd/variant_test.cc
@@ -5,17 +5,28 @@
 #include <gtest/gtest.h>
 
 using opentelemetry::nostd::variant;
+using opentelemetry::nostd::variant_alternative_t;
 using opentelemetry::nostd::variant_size;
 
-TEST(TypePackElementTest, IndexedType) {
+TEST(TypePackElementTest, IndexedType)
+{
   using opentelemetry::nostd::detail::type_pack_element_t;
   EXPECT_TRUE((std::is_same<type_pack_element_t<0, int, double, char>, int>::value));
   EXPECT_TRUE((std::is_same<type_pack_element_t<1, int, double, char>, double>::value));
   EXPECT_TRUE((std::is_same<type_pack_element_t<2, int, double, char>, char>::value));
 }
 
-TEST(VariantSizeTest, GetVariantSize) {
-  EXPECT_EQ(variant_size<variant<>>::value, 0); 
-  EXPECT_EQ(variant_size<variant<int>>::value, 1); 
-  EXPECT_EQ((variant_size<variant<int, double>>::value), 2); 
+TEST(VariantSizeTest, GetVariantSize)
+{
+  EXPECT_EQ(variant_size<variant<>>::value, 0);
+  EXPECT_EQ(variant_size<variant<int>>::value, 1);
+  EXPECT_EQ((variant_size<variant<int, double>>::value), 2);
+}
+
+TEST(VariantAlternativeTest, GetVariantSize)
+{
+  EXPECT_TRUE((std::is_same<variant_alternative_t<0, variant<int>>, int>::value));
+  EXPECT_TRUE((std::is_same<variant_alternative_t<1, variant<int, double>>, double>::value));
+  EXPECT_TRUE(
+      (std::is_same<variant_alternative_t<1, const variant<int, double>>, const double>::value));
 }
