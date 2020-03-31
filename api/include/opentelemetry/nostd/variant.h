cdb6281948a2bff86f2f7391f9e39605050f17c2
Ryan Burn <ryan.burn@gmail.com>
2020-03-31 15:22:12 -0700

    Add variant_size

--- api/include/opentelemetry/nostd/variant.h
+++ api/include/opentelemetry/nostd/variant.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "opentelemetry/nostd/detail/type_pack_element.h"
+#include "opentelemetry/nostd/detail/variant_size.h"
--- api/test/nostd/variant_test.cc
+++ api/test/nostd/variant_test.cc
@@ -4,11 +4,18 @@
 
 #include <gtest/gtest.h>
 
-/* using opentelemetry::nostd::variant; */
+using opentelemetry::nostd::variant;
+using opentelemetry::nostd::variant_size;
 
 TEST(TypePackElementTest, IndexedType) {
   using opentelemetry::nostd::detail::type_pack_element_t;
   EXPECT_TRUE((std::is_same<type_pack_element_t<0, int, double, char>, int>::value));
   EXPECT_TRUE((std::is_same<type_pack_element_t<1, int, double, char>, double>::value));
   EXPECT_TRUE((std::is_same<type_pack_element_t<2, int, double, char>, char>::value));
+}
+
+TEST(VariantSizeTest, GetVariantSize) {
+  EXPECT_EQ(variant_size<variant<>>::value, 0); 
+  EXPECT_EQ(variant_size<variant<int>>::value, 1); 
+  EXPECT_EQ((variant_size<variant<int, double>>::value), 2); 
 }
