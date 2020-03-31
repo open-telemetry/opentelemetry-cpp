0f7b21777759d1f7c3026ca01bd6054ba08b8087
Ryan Burn <ryan.burn@gmail.com>
2020-03-30 22:25:14 -0700

    Fill out variant backport

--- api/include/opentelemetry/nostd/utility.h
+++ api/include/opentelemetry/nostd/utility.h
@@ -53,5 +53,54 @@ size_t size(T (&array)[N]) noexcept
 {
   return N;
 }
+
+/**
+ * Back port of std::integer_sequence
+ */
+template <typename T, T... Is>
+struct integer_sequence
+{
+  using value_type = T;
+  static constexpr std::size_t size() noexcept { return sizeof...(Is); }
+};
+
+/**
+ * Back port of std::index_sequence
+ */
+template <std::size_t... Is>
+using index_sequence = integer_sequence<std::size_t, Is...>;
+
+/**
+ * Back port of std::make_index_sequence
+ */
+namespace detail {
+template<class, size_t>
+struct index_sequence_push_back {};
+
+template <size_t... Indexes, size_t I>
+struct index_sequence_push_back<index_sequence<Indexes...>, I> {
+  using type = index_sequence<Indexes..., I>;
+};
+
+template <size_t N>
+struct make_index_sequence_impl {
+  using type = typename index_sequence_push_back<typename make_index_sequence_impl<N - 1>::type,
+                                                 N - 1>::type;
+};
+
+template <>
+struct make_index_sequence_impl<0> {
+  using type = index_sequence<>;
+};
+} // namespace detail
+
+template <size_t N>
+using make_index_sequence = typename detail::make_index_sequence_impl<N>::type;
+
+/**
+ * Back port of std::index_sequence_for
+ */
+template <class... Ts>
+using index_sequence_for = make_index_sequence<sizeof...(Ts)>;
 }  // namespace nostd
 OPENTELEMETRY_END_NAMESPACE
---
+++ api/test/nostd/variant_test.cc
@@ -0,0 +1,5 @@
+#include "opentelemetry/nostd/variant.h"
+
+#include <gtest/gtest.h>
+
+/* using opentelemetry::nostd::variant; */
