73e5a44276bda37cf058d74694d67450c6cc9687
Ryan Burn <ryan.burn@gmail.com>
2020-04-03 12:09:26 -0700

    Add copyrights

--- api/include/opentelemetry/nostd/detail/find_index.h
+++ api/include/opentelemetry/nostd/detail/find_index.h
@@ -1,3 +1,11 @@
+// MPark.Variant
+//
+// Copyright Michael Park, 2015-2017
+// Copyright OpenTelemetry Authors, 2020
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file third_party/boost/LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <cstddef>
