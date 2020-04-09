3ac65d2ed58f105d241eccccce43f8419d3df34e
Ryan Burn <ryan.burn@gmail.com>
2020-04-08 21:46:54 -0700

    Fix gcc-4.8 issue

--- api/include/opentelemetry/nostd/detail/void.h
+++ api/include/opentelemetry/nostd/detail/void.h
@@ -5,10 +5,21 @@
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace nostd
 {
+namespace detail
+{
+template <class...>
+struct voider
+{
+  using type = void;
+};
+}  // namespace detail
+
 /**
  * Back port of std::void_t
+ *
+ * Note: voider workaround is required for gcc-4.8 to make SFINAE work
  */
-template <class...>
-using void_t = void;
+template <class... Tx>
+using void_t = typename detail::voider<Tx...>::type;
 }  // namespace nostd
 OPENTELEMETRY_END_NAMESPACE
