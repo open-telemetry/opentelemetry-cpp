dc35d6ea0faef371a9eda7b0395cdf72743a4d19
Ryan Burn <ryan.burn@gmail.com>
2020-04-15 22:37:51 -0700

    Add Exit test

--- sdk/test/event/libevent/dispatcher_test.cc
+++ sdk/test/event/libevent/dispatcher_test.cc
@@ -3,18 +3,30 @@
 #include <chrono>
 
 #include <gtest/gtest.h>
+using opentelemetry::sdk::event::Timer;
 using opentelemetry::sdk::event::libevent::Dispatcher;
 
-TEST(DispatcherTest, Timer) {
+TEST(DispatcherTest, Timer)
+{
   Dispatcher dispatcher;
   std::chrono::steady_clock::time_point t1, t2;
-  auto timer = dispatcher.CreateTimer([&] {
-      t2 = std::chrono::steady_clock::now();
-  });
+  auto timer = dispatcher.CreateTimer([&] { t2 = std::chrono::steady_clock::now(); });
   timer->EnableTimer(std::chrono::milliseconds{10});
   t1 = std::chrono::steady_clock::now();
   dispatcher.Run();
   auto duration = t2 - t1;
   EXPECT_TRUE(duration >= std::chrono::milliseconds{10});
   EXPECT_TRUE(duration < std::chrono::milliseconds{20});
+}
+
+TEST(DispatcherTest, Exit)
+{
+  Dispatcher dispatcher;
+  std::unique_ptr<Timer> timer;
+  auto f = [&] {
+    timer->EnableTimer(std::chrono::milliseconds{1});
+    dispatcher.Exit();
+  };
+  timer = dispatcher.CreateTimer(f);
+  dispatcher.Run();
 }
