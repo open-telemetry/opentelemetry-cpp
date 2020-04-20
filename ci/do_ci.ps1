9b62085673de71b4ebea048f501adcec4ab93540
Ryan Burn <ryan.burn@gmail.com>
2020-04-20 14:51:19 -0700

    Fix windows ci

--- ci/do_ci.ps1
+++ ci/do_ci.ps1
@@ -73,8 +73,8 @@ switch ($action) {
     cd "$BUILD_DIR"
     cmake $SRC_DIR `
           -DVCPKG_TARGET_TRIPLET=x64-windows `
-          "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR\scripts\buildsystems\vcpkg.cmake"
-          -DWITH_SDK=OFF \
+          "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR\scripts\buildsystems\vcpkg.cmake" `
+          -DWITH_SDK=OFF
     $exit = $LASTEXITCODE
     if ($exit -ne 0) {
       exit $exit
@@ -90,8 +90,8 @@ switch ($action) {
     cd "$BUILD_DIR"
     cmake $SRC_DIR `
           -DVCPKG_TARGET_TRIPLET=x64-windows `
-          "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR\scripts\buildsystems\vcpkg.cmake"
-          -DWITH_SDK=OFF \
+          "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR\scripts\buildsystems\vcpkg.cmake" `
+          -DWITH_SDK=OFF
     $exit = $LASTEXITCODE
     if ($exit -ne 0) {
       exit $exit
