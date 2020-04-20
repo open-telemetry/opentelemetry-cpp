72fd4df87bf902ca58b907eaa691395f443a1f10
Ryan Burn <ryan.burn@gmail.com>
2020-04-20 14:24:42 -0700

    Make test less flaky

--- sdk/test/event/libevent/dispatcher_test.cc
+++ sdk/test/event/libevent/dispatcher_test.cc
@@ -11,12 +11,12 @@ TEST(DispatcherTest, Timer)
   Dispatcher dispatcher;
   std::chrono::steady_clock::time_point t1, t2;
   auto timer = dispatcher.CreateTimer([&] { t2 = std::chrono::steady_clock::now(); });
-  timer->EnableTimer(std::chrono::milliseconds{10});
+  timer->EnableTimer(std::chrono::milliseconds{100});
   t1 = std::chrono::steady_clock::now();
   dispatcher.Run();
   auto duration = t2 - t1;
-  EXPECT_TRUE(duration > std::chrono::milliseconds{5});
-  EXPECT_TRUE(duration < std::chrono::milliseconds{20});
+  EXPECT_TRUE(duration > std::chrono::milliseconds{50});
+  EXPECT_TRUE(duration < std::chrono::milliseconds{200});
 }
 
 TEST(DispatcherTest, Exit)
