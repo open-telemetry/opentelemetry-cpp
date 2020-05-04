78b46e1a418552c5f717a9104ff6ae93ed1d8c2b
Johannes Tax <jtax@newrelic.com>
2020-05-04 10:53:36 -0700

    Special gcc 4.8 solution

--- sdk/include/opentelemetry/sdk/common/atomic_shared_ptr.h
+++ sdk/include/opentelemetry/sdk/common/atomic_shared_ptr.h
@@ -14,7 +14,7 @@ namespace sdk
  * specializations of std::atomic_store and std::atomic_load in all other
  * instances.
  */
-#if __cplusplus > 201703L || (__GNUC__ == 4 && (__GNUC_MINOR__ >= 8))
+#if __cplusplus > 201703L
 template <class T>
 class AtomicSharedPtr
 {
@@ -30,6 +30,27 @@ public:
 
 private:
   std::atomic<std::shared_ptr<T>> ptr_;
+};
+#elif (__GNUC__ == 4 && (__GNUC_MINOR__ >= 8))
+template <class T>
+class AtomicSharedPtr
+{
+public:
+  explicit AtomicSharedPtr(std::shared_ptr<T> ptr) noexcept
+      : ptr_{new std::shared_ptr<T>(std::move(ptr))}
+  {}
+
+  ~AtomicSharedPtr() noexcept { delete ptr_.load(std::memory_order_acquire); }
+
+  void store(const std::shared_ptr<T> &other) noexcept
+  {
+    ptr_.store(new std::shared_ptr<T>(other), std::memory_order_release);
+  }
+
+  std::shared_ptr<T> load() const noexcept { return *ptr_.load(std::memory_order_acquire); }
+
+private:
+  std::atomic<std::shared_ptr<T> *> ptr_;
 };
 #else
 template <class T>
