3212ed4db7c2b486fda8d0d18a1df024c33623a7
Ryan Burn <ryan.burn@gmail.com>
2020-05-10 17:31:04 -0700

    Add test coverage

--- sdk/test/event/async_timer/dispatcher_test.cc
+++ sdk/test/event/async_timer/dispatcher_test.cc
@@ -19,6 +19,20 @@ TEST(DispatcherTest, Timer)
   EXPECT_TRUE(duration < std::chrono::milliseconds{200});
 }
 
+TEST(DispatcherTest, ResetTimer)
+{
+  Dispatcher dispatcher;
+  std::chrono::steady_clock::time_point t1, t2;
+  auto timer = dispatcher.CreateTimer([&] { t2 = std::chrono::steady_clock::now(); });
+  timer->EnableTimer(std::chrono::milliseconds{500});
+  timer->EnableTimer(std::chrono::milliseconds{100});
+  t1 = std::chrono::steady_clock::now();
+  dispatcher.Run();
+  auto duration = t2 - t1;
+  EXPECT_TRUE(duration > std::chrono::milliseconds{50});
+  EXPECT_TRUE(duration < std::chrono::milliseconds{200});
+}
+
 TEST(DispatcherTest, TimerOrder)
 {
   Dispatcher dispatcher;
