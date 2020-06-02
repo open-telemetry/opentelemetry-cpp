4c1cd553dae79e36ad9bc93684453f083ee9c5b5
Max Golovanov <maxgolov@microsoft.com>
2020-06-02 10:56:33 -0700

    Add enum for variant types. Add support for C-string const char * and string literals.

--- api/include/opentelemetry/common/attribute_value.h
+++ api/include/opentelemetry/common/attribute_value.h
@@ -8,13 +8,15 @@
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace common
 {
-using AttributeValue = nostd::variant<bool,
-                                      int,
+using AttributeValue = nostd::variant<
+                                      bool,
+                                      int32_t,
                                       int64_t,
-                                      unsigned int,
+                                      uint32_t,
                                       uint64_t,
                                       double,
                                       nostd::string_view,
+                                      const char*,
                                       nostd::span<const bool>,
                                       nostd::span<const int>,
                                       nostd::span<const int64_t>,
@@ -22,5 +24,28 @@ using AttributeValue = nostd::variant<bool,
                                       nostd::span<const uint64_t>,
                                       nostd::span<const double>,
                                       nostd::span<const nostd::string_view>>;
+
+enum AttributeType
+{
+
+    TYPE_BOOL,
+    TYPE_INT,
+    TYPE_INT64,
+    TYPE_UINT,
+    TYPE_UINT64,
+    TYPE_DOUBLE,
+    TYPE_STRING,
+    TYPE_CSTRING,
+
+    TYPE_SPAN_BOOL,
+    TYPE_SPAN_INT,
+    TYPE_SPAN_INT64,
+    TYPE_SPAN_UINT,
+    TYPE_SPAN_UINT64,
+    TYPE_SPAN_DOUBLE,
+    TYPE_SPAN_STRING
+
+};
+
 }  // namespace common
 OPENTELEMETRY_END_NAMESPACE
