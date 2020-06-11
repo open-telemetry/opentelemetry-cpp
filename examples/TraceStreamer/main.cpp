e273b0b7f4ac834b34b56331b995e9297f016ecd
Max Golovanov <maxgolov@microsoft.com>
2020-06-11 15:44:00 -0700

    Add benchmark into sample app

--- examples/TraceStreamer/main.cpp
+++ examples/TraceStreamer/main.cpp
@@ -30,27 +30,62 @@ namespace trace = opentelemetry::trace;
 
 using M = std::map<nostd::string_view, nostd::string_view>;
 
-void test_spans()
-{
-
-  std::multimap<std::string, std::string> testParams = {
-      // Plain text key-value pairs logfile
-      {"file", "trace.log"},
-      // JSON text file
-      {"json", "trace.json"},
-
-#ifdef _WIN32
-      // JSON pipe listener (Windows)
-      {"json", "\\\\.\\pipe\\ETW-6d084bbf-6a96-44ef-83F4-0a77c9e34580"},
-      // ETW-XML listener (Windows)
-      {"ETW", "{6D084BBF-6A96-44EF-83F4-0A77C9E34580}"},
+#ifdef __has_include
+#if __has_include(<benchmark/benchmark.h>)
+#include <benchmark/benchmark.h>
+#define HAVE_BENCHMARK
+#endif
 #endif
 
-      // Console
-      {"CON", "1.0"},
-      // OutputDebugString (Visual Studio Debug Output window)
-      {"DEBUG", "1.0"}};
+#ifdef HAVE_BENCHMARK
+std::map<nostd::string_view, common::AttributeValue> createEvent(){
+  std::map<nostd::string_view, common::AttributeValue> m =
+  {
+      {"uint32Key", (uint32_t)123456},
+      {"uint64Key", (uint64_t)123456},
+      {"strKey", "someValue"}
+  };
+  return m;
+};
 
+void BM_AddEvent(benchmark::State &state)
+{
+  stream::TracerProvider tp;
+  auto tracer = tp.GetTracer("json", "NUL");
+  auto span   = tracer->StartSpan("MySpan");
+  while (state.KeepRunning())
+  {
+    benchmark::DoNotOptimize([&]
+        {
+            span->AddEvent("MyEvent", createEvent());
+        });
+  }
+  span->End();
+  tracer->Close();
+}
+BENCHMARK(BM_AddEvent);
+
+#endif
+
+std::multimap<std::string, std::string> testParams = {
+    // Plain text key-value pairs logfile
+    {"file", "trace.log"},
+    // JSON text file
+    {"json", "trace.json"},
+#ifdef _WIN32
+    // JSON pipe listener (Windows)
+    {"json", "\\\\.\\pipe\\ETW-6d084bbf-6a96-44ef-83F4-0a77c9e34580"},
+    // ETW-XML listener (Windows)
+    {"ETW", "{6D084BBF-6A96-44EF-83F4-0A77C9E34580}"},
+#endif
+    // Console
+    {"CON", "1.0"},
+    // OutputDebugString (Visual Studio Debug Output window)
+    {"DEBUG", "1.0"}
+};
+
+void test_spans()
+{
   for (auto &kv : testParams)
   {
     printf("*** Tracer(%s:%s)...\n", kv.first.c_str(), kv.second.c_str());
@@ -80,5 +115,10 @@ void test_spans()
 int main(int argc, char *argv[])
 {
   test_spans();
+
+  // Run the benchmark
+  ::benchmark::Initialize(&argc, argv);
+  ::benchmark::RunSpecifiedBenchmarks();
+
   return 0;
 }
