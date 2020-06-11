0f1e3e58fa1382b197f533fad015536662acde2c
Max Golovanov <maxgolov@microsoft.com>
2020-06-11 00:10:49 -0700

    Update build tools script for Linux

--- tools/setup-buildtools.sh
+++ tools/setup-buildtools.sh
@@ -22,13 +22,13 @@ yum -y install devtoolset-7-valgrind
 
 yum-config-manager --enable rhel-server-rhscl-7-rpms
 
-if [ `gcc --version | grep 7` == "" ]; then
-echo "*********************************************************"
-echo "*** Please make sure you start the build with gcc-7   ***"
-echo "*** > scl enable devtoolset-7 ./build.sh              ***"
-echo "*********************************************************"
-exit 3
-fi
+#if [ `gcc --version | grep 7` == "" ]; then
+#echo "*********************************************************"
+#echo "*** Please make sure you start the build with gcc-7   ***"
+#echo "*** > scl enable devtoolset-7 ./build.sh              ***"
+#echo "*********************************************************"
+#exit 3
+#fi
 
 if [ `cmake --version | grep 3` == "" ]; then
 yum -y remove cmake
@@ -59,6 +59,8 @@ apt-get install -qq libsqlite3-dev
 #apt install libsqlite3-dev
 apt-get install -qq wget
 apt-get install -qq clang-format
+apt-get install -qq libgtest-dev
+apt-get install -qq libbenchmark-dev
 fi
 
 ## Change owner from root to current dir owner
