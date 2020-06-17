9d6c5a0a5fe73d90fb3be072eb4bebba4e274a99
Brandon Kimberly <brakmb@amazon.com>
2020-06-17 11:55:42 -0400

    Moved report upload step to workflow job

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -115,7 +115,6 @@ elif [[ "$1" == "code.coverage" ]]; then
   make
   make test
   lcov --directory . --capture --output-file coverage.info
-  bash <(curl -s https://codecov.io/bash) -e yaml=".github/.codecov.yaml" -f coverage.info
   exit 0
 fi
 
