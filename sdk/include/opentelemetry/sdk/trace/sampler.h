850b463e5bbb528211c09cbbc9d3fd7c3ef136e9
Oliver Zhang <ziqizh@google.com>
2020-06-17 02:51:09 +0000

    add param and comments

--- sdk/include/opentelemetry/sdk/trace/sampler.h
+++ sdk/include/opentelemetry/sdk/trace/sampler.h
@@ -1,10 +1,13 @@
 #pragma once
 
-#include "opentelemetry/version.h"
 #include "opentelemetry/common/attribute_value.h"
+#include "opentelemetry/trace/span.h"
+#include "opentelemetry/trace/trace_id.h"
+#include "opentelemetry/trace/tracer.h"
+#include "opentelemetry/version.h"
 
-#include <memory>
 #include <map>
+#include <memory>
 #include <string>
 
 OPENTELEMETRY_BEGIN_NAMESPACE
@@ -12,15 +15,31 @@ namespace sdk
 {
 namespace trace
 {
+namespace trace_api = opentelemetry::trace;
 class Sampler
 {
 public:
+  virtual ~Sampler() = default;
   /**
-   * Initialize a new tracer.
-   * @param processor The span processor for this tracer. This must not be a
-   * nullptr.
+   * Called during Span creation to make a sampling decision.
+   *
+   * @param parentContext TODO: the parent span's SpanContext. null if this is a root
+   *     span.
+   * @param traceId the TraceId for the new Span. This will be identical to that in
+   *     the parentContext, unless this is a root span.
+   * @param name the name of the new Span.
+   * @param parentLinks TODO: the parentLinks associated with the new Span.
+   * @param spanKind the trace_api::SpanKind of the Span.
+   * @param attributes list of AttributeValue with their keys.
+   * @return sampling decision whether span should be sampled or not.
+   * @since 0.1.0
    */
-  virtual Decision shouldSample();
+
+  virtual Decision shouldSample(
+      trace_api::TraceId trace_id,
+      std::string name,
+      trace_api::SpanKind span_kind,
+      std::map<std::string, common::AttributeValue> attributes) noexcept = 0;
 
   /**
    * Returns the description of this {@code Sampler}. This may be displayed on debug pages or in the
@@ -36,6 +55,7 @@ public:
 class Decision
 {
 public:
+  virtual ~Decision() = default;
   /**
    * Return sampling decision whether span should be sampled or not.
    *
@@ -44,13 +64,15 @@ public:
   virtual bool isSampled();
 
   /**
-     * Return tags which will be attached to the span.
-     *
-     * @return attributes added to span. These attributes should be added to the span only for root
-     *     span or when sampling decision {@link #isSampled()} changes from false to true.
-     */
+   * Return tags which will be attached to the span.
+   *
+   * @return attributes added to span. These attributes should be added to the span only for root
+   *     span or when sampling decision isSampled() changes from false to true.
+   */
   virtual std::map<std::string, common::AttributeValue> getAttributes();
 };
+
+class
 }  // namespace trace
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
