d2713a0c3a1aa13c74426dd94c65688779b4bcbb
Oliver Zhang <ziqizh@google.com>
2020-06-17 19:51:41 +0000

    minor tweaks

--- sdk/include/opentelemetry/sdk/trace/sampler.h
+++ sdk/include/opentelemetry/sdk/trace/sampler.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "opentelemetry/common/attribute_value.h"
+#include "opentelemetry/nostd/span.h"
 #include "opentelemetry/nostd/string_view.h"
 #include "opentelemetry/sdk/common/atomic_shared_ptr.h"
 #include "opentelemetry/trace/span.h"
@@ -30,16 +31,16 @@ public:
   /**
    * Called during Span creation to make a sampling decision.
    *
-   * @param parent_context TODO: the parent span's SpanContext. null if this is a root
-   *     span.
-   * @param traceId the TraceId for the new Span. This will be identical to that in
+   * @param parent_context TODO: a shared pointer of the SpanContext of a parent Span. 
+   *     null if this is a root span.
+   * @param trace_id the TraceId for the new Span. This will be identical to that in
    *     the parentContext, unless this is a root span.
    * @param name the name of the new Span.
-   * @param parentLinks TODO: the parentLinks associated with the new Span.
    * @param spanKind the trace_api::SpanKind of the Span.
    * @param attributes TODO: current map is a placeholder.
    *     list of AttributeValue with their keys.
-   * @return sampling decision whether span should be sampled or not.
+   * @param links TODO: Collection of links that will be associated with the Span to be created.
+   * @return sampling result whether span should be sampled or not.
    * @since 0.1.0
    */
 
@@ -51,12 +52,10 @@ public:
       std::map<std::string, common::AttributeValue> attributes) noexcept = 0;
 
   /**
-   * Returns the description of this {@code Sampler}. This may be displayed on debug pages or in the
-   * logs.
+   * Returns the sampler name or short description with the configuration. 
+   * This may be displayed on debug pages or in the logs.
    *
-   * <p>Example: "ProbabilitySampler{0.000100}"
-   *
-   * @return the description of this {@code Sampler}.
+   * @return the description of this Sampler.
    */
   virtual std::string GetDescription() const noexcept = 0;
 };
@@ -72,20 +71,19 @@ public:
   };
   virtual ~SamplingResult() = default;
   /**
-   * Return sampling decision whether span should be sampled or not.
+   * Return sampling decision.
    *
-   * @return TODO: Decision.
+   * @return Decision.
    */
   virtual Decision GetDecision() const noexcept = 0;
 
   /**
    * Return attributes which will be attached to the span.
    *
-   * TODO: Change return value to nostd::span<std::pair<nostd::string_view, common::AttributeValue>>
-   * @return attributes added to span. These attributes should be added to the span only for root
-   *     span or when sampling decision isSampled() changes from false to true.
+   * @return the immutable list of attributes added to span.
    */
-  virtual std::map<std::string, common::AttributeValue> GetAttributes() const noexcept = 0;
+  virtual nostd::span<std::pair<nostd::string_view, common::AttributeValue>> GetAttributes() const
+      noexcept = 0;
 };
 }  // namespace trace
 }  // namespace sdk
