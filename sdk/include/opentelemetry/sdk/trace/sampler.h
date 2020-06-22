0f1e88deac48010a784c1ed5d0b9859a41281253
Oliver Zhang <ziqizh@google.com>
2020-06-22 23:31:06 +0000

    minor change

--- sdk/include/opentelemetry/sdk/trace/sampler.h
+++ sdk/include/opentelemetry/sdk/trace/sampler.h
@@ -3,10 +3,8 @@
 #include "opentelemetry/common/attribute_value.h"
 #include "opentelemetry/nostd/span.h"
 #include "opentelemetry/nostd/string_view.h"
-#include "opentelemetry/sdk/common/atomic_shared_ptr.h"
 #include "opentelemetry/trace/span.h"
 #include "opentelemetry/trace/trace_id.h"
-#include "opentelemetry/trace/tracer.h"
 #include "opentelemetry/version.h"
 
 #include <map>
@@ -19,45 +17,35 @@ namespace sdk
 namespace trace
 {
 namespace trace_api = opentelemetry::trace;
-/**
- * NOT_RECORD - IsRecording() == false,
- *    span will not be recorded and all events and attributes will be dropped.
- * RECORD - IsRecording() == true, but Sampled flag MUST NOT be set.
- * RECORD_AND_SAMPLED - IsRecording() == true AND Sampled flag` MUST be set.
- */
+
 enum class Decision
 {
+  // IsRecording() == false, span will not be recorded and all events and attributes will be
+  //      dropped.
   NOT_RECORD,
+  // IsRecording() == true, but Sampled flag MUST NOT be set.
   RECORD,
+  // IsRecording() == true AND Sampled flag` MUST be set.
   RECORD_AND_SAMPLE
-};
-
-/**
- * A placeholder for common::AttributeKeyValue
- * A key/value pair that can be used to set attributes.
- */
-struct AttributeKeyValue {
-    nostd::string_view key;
-    common::AttributeValue value;
 };
 
 struct SamplingResult
 {
   Decision decision;
-  // TODO: Change AttributeKeyValue to common::AttributeKeyValue
-  std::unique_ptr<nostd::span<AttributeKeyValue>> attributes;
+  // A set of span Attributes that will also be added to the Span
+  std::unique_ptr<std::map<std::string, openelemetry::common::AttributeValue>> attributes;
 };
 
 class Sampler
 {
 public:
-  // Placeholder
+  // TODO: Remove this placeholder with real class
   class SpanContext;
   virtual ~Sampler() = default;
   /**
    * Called during Span creation to make a sampling decision.
    *
-   * @param parent_context TODO: a shared pointer of the SpanContext of a parent Span.
+   * @param parent_context a const pointer of the SpanContext of a parent Span.
    *     null if this is a root span.
    * @param trace_id the TraceId for the new Span. This will be identical to that in
    *     the parentContext, unless this is a root span.
@@ -70,12 +58,11 @@ public:
    * @since 0.1.0
    */
 
-  virtual SamplingResult ShouldSample(
-      const SpanContext *parent_context,
-      trace_api::TraceId trace_id,
-      nostd::string_view name,
-      trace_api::SpanKind span_kind,
-      const nostd::span<AttributeKeyValue> &attributes) noexcept = 0;
+  virtual SamplingResult ShouldSample(const SpanContext *parent_context,
+                                      trace_api::TraceId trace_id,
+                                      nostd::string_view name,
+                                      trace_api::SpanKind span_kind,
+                                      const KeyValueIterable &attributes) noexcept = 0;
 
   /**
    * Returns the sampler name or short description with the configuration.
