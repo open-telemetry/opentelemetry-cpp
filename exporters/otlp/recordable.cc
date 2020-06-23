e326783fe7d505b65fa0c5c18a3cdddda0b1babc
Nadia Ciobanu <nadiaciobanu@google.com>
2020-06-23 01:02:33 +0000

    Add start time

--- exporters/otlp/recordable.cc
+++ exporters/otlp/recordable.cc
@@ -5,6 +5,12 @@ namespace exporter
 {
 namespace otlp
 {
+// void Recordable::SetIds(trace::TraceId trace_id,
+//                         trace::SpanId span_id,
+//                         trace::SpanId parent_span_id) noexcept {
+//   span_.set_trace_id(trace_id);
+// }
+
 void Recordable::AddEvent(nostd::string_view name, core::SystemTimestamp timestamp) noexcept
 {
   (void)name;
@@ -23,8 +29,14 @@ void Recordable::SetName(nostd::string_view name) noexcept
 
 void Recordable::SetStartTime(opentelemetry::core::SystemTimestamp start_time) noexcept
 {
-  span_.set_start_time_unixnano(start_time.time_since_epoch());
+  std::chrono::nanoseconds nano_time = start_time.time_since_epoch();
+  uint64_t nano_unix_time = std::chrono::duration_cast<std::chrono::milliseconds>(nano_time).count();
+  span_.set_start_time_unixnano(nano_unix_time);
 }
+
+// void Recordable::SetDuration(std::chrono::nanoseconds duration) noexcept {
+
+// }
 }  // namespace otlp
 }  // namespace exporter
 OPENTELEMETRY_END_NAMESPACE
