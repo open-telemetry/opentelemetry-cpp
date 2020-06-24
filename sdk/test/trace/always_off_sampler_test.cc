69050ed4c339743059ae81d9e7c480822d10ac34
Nick Holbrook <nholbrook@google.com>
2020-06-24 09:35:08 -0400

    Update AlwaysOffSampler unit test

--- sdk/test/trace/always_off_sampler_test.cc
+++ sdk/test/trace/always_off_sampler_test.cc
@@ -1,19 +1,19 @@
 #include "opentelemetry/sdk/trace/always_off_sampler.h"
-#include "opentelemetry/nostd/span.h"
 
 #include <gtest/gtest.h>
 
-using namespace opentelemetry::sdk::trace;
-using namespace opentelemetry::nostd;
+using opentelemetry::sdk::trace::AlwaysOffSampler;
+using opentelemetry::sdk::trace::Decision;
 
 TEST(AlwaysOffSampler, ShouldSample)
 {
 	AlwaysOffSampler sampler;
 
 	opentelemetry::trace::TraceId trace_id;
+	opentelemetry::trace::KeyValueIterableView<std::map<std::string, int>> view;
+	opentelemetry::trace::SpanKind span_kind = opentelemetry::trace::SpanKind::kInternal;
 
-	auto sampling_result = sampler.ShouldSample(
-		nullptr, trace_id, "Test", trace_api::SpanKind::kInternal, span<AttributeKeyValue>());
+	auto sampling_result = sampler.ShouldSample(nullptr, trace_id, "Test", span_kind, view);
 
 	ASSERT_EQ(Decision::NOT_RECORD, sampling_result.decision);
 	ASSERT_EQ(nullptr, sampling_result.attributes);
