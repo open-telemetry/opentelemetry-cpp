69c5c3204ea3f53274d2ae274852ddb05dd079f5
Nadia Ciobanu <nadiaciobanu@google.com>
2020-06-25 20:39:47 +0000

    Return OTLP Recordable

--- exporters/otlp/otlp_exporter.h
+++ exporters/otlp/otlp_exporter.h
@@ -18,38 +18,40 @@ class OtlpExporter final : public sdktrace::SpanExporter
 {
   std::unique_ptr<sdktrace::Recordable> MakeRecordable() noexcept
   {
-    return std::unique_ptr<sdktrace::Recordable>(new sdktrace::SpanData);
+    //return std::unique_ptr<sdktrace::Recordable>(new sdktrace::SpanData);
+    return std::unique_ptr<opentelemetry::exporter::otlp::Recordable>();
   }
 
   sdktrace::ExportResult Export(
       const nostd::span<std::unique_ptr<sdktrace::Recordable>> &spans) noexcept
   {
-    for (auto &recordable : spans)
-    {
-      auto span = std::unique_ptr<sdktrace::SpanData>(
-          static_cast<sdktrace::SpanData *>(recordable.release()));
+    // for (auto &recordable : spans)
+    // {
+    //   auto span = std::unique_ptr<sdktrace::SpanData>(
+    //       static_cast<sdktrace::SpanData *>(recordable.release()));
 
-      if (span != nullptr)
-      {
-        char trace_id[32]       = {0};
-        char span_id[16]        = {0};
-        char parent_span_id[16] = {0};
+    //   if (span != nullptr)
+    //   {
+    //     char trace_id[32]       = {0};
+    //     char span_id[16]        = {0};
+    //     char parent_span_id[16] = {0};
 
-        span->GetTraceId().ToLowerBase16(trace_id);
-        span->GetSpanId().ToLowerBase16(span_id);
-        span->GetParentSpanId().ToLowerBase16(parent_span_id);
+    //     span->GetTraceId().ToLowerBase16(trace_id);
+    //     span->GetSpanId().ToLowerBase16(span_id);
+    //     span->GetParentSpanId().ToLowerBase16(parent_span_id);
 
-        std::cout << "{"
-                  << "\n  name          : " << span->GetName()
-                  << "\n  trace_id      : " << std::string(trace_id, 32)
-                  << "\n  span_id       : " << std::string(span_id, 16)
-                  << "\n  parent_span_id: " << std::string(parent_span_id, 16)
-                  << "\n  start         : " << span->GetStartTime().time_since_epoch().count()
-                  << "\n  duration      : " << span->GetDuration().count() << "\n}"
-                  << "\n";
-      }
-    }
+    //     std::cout << "{"
+    //               << "\n  name          : " << span->GetName()
+    //               << "\n  trace_id      : " << std::string(trace_id, 32)
+    //               << "\n  span_id       : " << std::string(span_id, 16)
+    //               << "\n  parent_span_id: " << std::string(parent_span_id, 16)
+    //               << "\n  start         : " << span->GetStartTime().time_since_epoch().count()
+    //               << "\n  duration      : " << span->GetDuration().count() << "\n}"
+    //               << "\n";
+    //   }
+    // }
 
+    std::cout << "Hi" << std::endl;
     return sdktrace::ExportResult::kSuccess;
   }
 
