3088385c1ecb294ba0c62ead2f12a313eb9c1143
tz68 <tz68@cornell.edu>
2020-06-24 23:43:29 -0400

    Changed comment position, and added names to getter and setter arguments

--- api/include/opentelemetry/trace/propagation/httptextformat.h
+++ api/include/opentelemetry/trace/propagation/httptextformat.h
@@ -14,16 +14,16 @@ namespace propagation
 virtual static Context SetSpanInContext(Span span, Context &context = NULL)
 virtual static Span GetCurrentSpan(Context &context = NULL)
 
+// The HTTPTextFormat class provides an interface that enables extracting and injecting
+// context into headers of HTTP requests. HTTP frameworks and clients
+// can integrate with HTTPTextFormat by providing the object containing the
+//  headers, and a getter and setter function for the extraction and
+// injection of values, respectively.
 template <typename T>
 class HTTPTextFormat {
-    /** This class provides an interface that enables extracting and injecting
-    context into headers of HTTP requests. HTTP frameworks and clients
-    can integrate with HTTPTextFormat by providing the object containing the
-     headers, and a getter and setter function for the extraction and
-    injection of values, respectively.*/
     public:
-        using Getter = nostd::string_view(*)(nostd::string_view,nostd::string_view);
-        using Setter = void(*)(T&,nostd::string_view,nostd::string);
+        using Getter = nostd::string_view(*)(T &carrier, nostd::string_view trace_type);
+        using Setter = void(*)(T &carrier, nostd::string_view trace_type,nostd::string trace_description);
         virtual Context extract(Getter get_from_carrier, const T &carrier, Context &context)
         virtual void inject(Setter set_from_carrier, T &carrier, const Context &context)
 };
