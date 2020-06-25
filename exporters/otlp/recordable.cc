a61c01f66f41f5292df15938715eccb01b752443
Nadia Ciobanu <nadiaciobanu@google.com>
2020-06-25 17:05:10 +0000

    Address review comments

--- exporters/otlp/recordable.cc
+++ exporters/otlp/recordable.cc
@@ -9,17 +9,10 @@ void Recordable::SetIds(trace::TraceId trace_id,
                         trace::SpanId span_id,
                         trace::SpanId parent_span_id) noexcept
 {
-  const uint8_t* trace_id_data = trace_id.Id().data();
-  const uint8_t* span_id_data = span_id.Id().data();
-  const uint8_t* parent_span_id_data = parent_span_id.Id().data();
-
-  const char* trace_id_buff = (char*)trace_id_data;
-  const char* span_id_buff = (char*)span_id_data;
-  const char* parent_span_id_buff = (char*)parent_span_id_data;
-
-  span_.set_trace_id(trace_id_buff);
-  span_.set_span_id(span_id_buff);
-  span_.set_parent_span_id(parent_span_id_buff);
+  span_.set_trace_id(reinterpret_cast<const char *>(trace_id.Id().data()), trace::TraceId::kSize);
+  span_.set_span_id(reinterpret_cast<const char *>(span_id.Id().data()), trace::SpanId::kSize);
+  span_.set_parent_span_id(reinterpret_cast<const char *>(parent_span_id.Id().data()),
+                           trace::SpanId::kSize);
 }
 
 void Recordable::SetAttribute(nostd::string_view key,
@@ -48,7 +41,7 @@ void Recordable::SetName(nostd::string_view name) noexcept
 void Recordable::SetStartTime(opentelemetry::core::SystemTimestamp start_time) noexcept
 {
   std::chrono::nanoseconds nano_time = start_time.time_since_epoch();
-  uint64_t nano_unix_time = nano_time.count();
+  uint64_t nano_unix_time            = nano_time.count();
   span_.set_start_time_unixnano(nano_unix_time);
 }
 
--- exporters/otlp/recordable_test.cc
+++ exporters/otlp/recordable_test.cc
@@ -2,63 +2,72 @@
 
 #include <gtest/gtest.h>
 
-using namespace opentelemetry::exporter::otlp;
-
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace exporter
+{
+namespace otlp
+{
 TEST(Recordable, SetIds)
 {
-    Recordable rec;
+  Recordable rec;
 
-    const uint8_t trace_id_buff[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1};
-    const uint8_t span_id_buff[8] = {0,0,0,0,0,0,0,2};
-    const uint8_t parent_span_id_buff[8] = {0,0,0,0,0,0,0,3};
+  const uint8_t trace_id_buff[trace::TraceId::kSize]      = {0, 0, 0, 0, 0, 0, 0, 0,
+                                                        0, 0, 0, 0, 0, 0, 0, 1};
+  const uint8_t span_id_buff[trace::SpanId::kSize]        = {0, 0, 0, 0, 0, 0, 0, 2};
+  const uint8_t parent_span_id_buff[trace::SpanId::kSize] = {0, 0, 0, 0, 0, 0, 0, 3};
 
-    opentelemetry::nostd::span<const uint8_t, 16>trace_id_span(trace_id_buff, 16);
-    opentelemetry::nostd::span<const uint8_t, 8> span_id_span(span_id_buff, 8);
-    opentelemetry::nostd::span<const uint8_t, 8> parent_span_id_span(parent_span_id_buff, 8);
+  const trace::TraceId trace_id(
+      nostd::span<const uint8_t, trace::TraceId::kSize>(trace_id_buff, trace::TraceId::kSize));
+  const trace::SpanId span_id(
+      nostd::span<const uint8_t, trace::SpanId::kSize>(span_id_buff, trace::SpanId::kSize));
+  const trace::SpanId parent_span_id(
+      nostd::span<const uint8_t, trace::SpanId::kSize>(parent_span_id_buff, trace::SpanId::kSize));
 
-    opentelemetry::trace::TraceId trace_id(trace_id_span);
-    opentelemetry::trace::SpanId span_id(span_id_span);
-    opentelemetry::trace::SpanId parent_span_id(parent_span_id_span);
+  rec.SetIds(trace_id, span_id, parent_span_id);
 
-    rec.SetIds(trace_id, span_id, parent_span_id);
-
-    ASSERT_EQ(rec.span().trace_id(), (char*)trace_id_buff);
-    ASSERT_EQ(rec.span().span_id(), (char*)span_id_buff);
-    ASSERT_EQ(rec.span().parent_span_id(), (char*)parent_span_id_buff);
+  EXPECT_EQ(rec.span().trace_id(),
+            std::string(reinterpret_cast<const char *>(trace_id_buff), trace::TraceId::kSize));
+  EXPECT_EQ(rec.span().span_id(),
+            std::string(reinterpret_cast<const char *>(span_id_buff), trace::SpanId::kSize));
+  EXPECT_EQ(rec.span().parent_span_id(),
+            std::string(reinterpret_cast<const char *>(parent_span_id_buff), trace::SpanId::kSize));
 }
 
 TEST(Recordable, SetName)
 {
-    Recordable rec;
-    opentelemetry::nostd::string_view name = "TestSpan";
-    rec.SetName(name);
-    ASSERT_EQ(rec.span().name(), name);
+  Recordable rec;
+  nostd::string_view name = "TestSpan";
+  rec.SetName(name);
+  ASSERT_EQ(rec.span().name(), name);
 }
 
 TEST(Recordable, SetStartTime)
 {
-    Recordable rec;
-    std::chrono::system_clock::time_point start_time = std::chrono::system_clock::now();
-    opentelemetry::core::SystemTimestamp start_timestamp(start_time);
+  Recordable rec;
+  std::chrono::system_clock::time_point start_time = std::chrono::system_clock::now();
+  core::SystemTimestamp start_timestamp(start_time);
 
-    uint64_t unix_start = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                            start_time.time_since_epoch()).count();
+  uint64_t unix_start =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(start_time.time_since_epoch()).count();
 
-    rec.SetStartTime(start_timestamp);
-    ASSERT_EQ(rec.span().start_time_unixnano(), unix_start);
+  rec.SetStartTime(start_timestamp);
+  ASSERT_EQ(rec.span().start_time_unixnano(), unix_start);
 }
 
 TEST(Recordable, SetDuration)
 {
-    Recordable rec;
-    // Start time is 0
-    opentelemetry::core::SystemTimestamp start_timestamp;
+  Recordable rec;
+  // Start time is 0
+  core::SystemTimestamp start_timestamp;
 
-    std::chrono::nanoseconds duration(10);
-    uint64_t unix_end = duration.count();
+  std::chrono::nanoseconds duration(10);
+  uint64_t unix_end = duration.count();
 
-    rec.SetStartTime(start_timestamp);
-    rec.SetDuration(duration);
+  rec.SetStartTime(start_timestamp);
+  rec.SetDuration(duration);
 
-    ASSERT_EQ(rec.span().end_time_unixnano(), unix_end);
+  ASSERT_EQ(rec.span().end_time_unixnano(), unix_end);
 }
+}  // namespace otlp
+}  // namespace exporter
+OPENTELEMETRY_END_NAMESPACE
