de7c586b75ae5d8552b15b31332ca4e261fdd443
Nadia Ciobanu <nadiaciobanu@google.com>
2020-06-26 00:22:26 +0000

    Return OTLP Recordable in MakeRecordable

--- exporters/otlp/otlp_exporter.h
+++ exporters/otlp/otlp_exporter.h
@@ -13,52 +13,38 @@
 namespace trace    = opentelemetry::trace;
 namespace nostd    = opentelemetry::nostd;
 namespace sdktrace = opentelemetry::sdk::trace;
+namespace otlpexporter = opentelemetry::exporter::otlp;
 
 class OtlpExporter final : public sdktrace::SpanExporter
 {
   std::unique_ptr<sdktrace::Recordable> MakeRecordable() noexcept
   {
-    //return std::unique_ptr<sdktrace::Recordable>(new sdktrace::SpanData);
-    return std::unique_ptr<opentelemetry::exporter::otlp::Recordable>();
+    return std::unique_ptr<sdktrace::Recordable>(new opentelemetry::exporter::otlp::Recordable);
   }
 
   sdktrace::ExportResult Export(
       const nostd::span<std::unique_ptr<sdktrace::Recordable>> &spans) noexcept
   {
-    // for (auto &recordable : spans)
-    // {
-    //   auto span = std::unique_ptr<sdktrace::SpanData>(
-    //       static_cast<sdktrace::SpanData *>(recordable.release()));
+    // grpc::ClientContext context;
 
-    //   if (span != nullptr)
-    //   {
-    //     char trace_id[32]       = {0};
-    //     char span_id[16]        = {0};
-    //     char parent_span_id[16] = {0};
+    // opentelemetry::proto::collector::trace::v1::ExportTraceServiceRequest request;
+    // opentelemetry::proto::collector::trace::v1::ExportTraceServiceResponse response;
 
-    //     span->GetTraceId().ToLowerBase16(trace_id);
-    //     span->GetSpanId().ToLowerBase16(span_id);
-    //     span->GetParentSpanId().ToLowerBase16(parent_span_id);
+    // std::unique_ptr<
+    //   opentelemetry::proto::collector::trace::v1::TraceService::StubInterface>
+    //   trace_service_stub;
 
-    //     std::cout << "{"
-    //               << "\n  name          : " << span->GetName()
-    //               << "\n  trace_id      : " << std::string(trace_id, 32)
-    //               << "\n  span_id       : " << std::string(span_id, 16)
-    //               << "\n  parent_span_id: " << std::string(parent_span_id, 16)
-    //               << "\n  start         : " << span->GetStartTime().time_since_epoch().count()
-    //               << "\n  duration      : " << span->GetDuration().count() << "\n}"
-    //               << "\n";
-    //   }
-    // }
+    // trace_service_stub->Export(&context, request, &response);
 
-    std::cout << "Hi" << std::endl;
+    for (auto &recordable : spans) {
+      auto rec = std::unique_ptr<otlpexporter::Recordable>(
+          static_cast<otlpexporter::Recordable *>(recordable.release()));
+
+      std::cout << "Name: " << rec->span().name() << std::endl;
+    }
+
     return sdktrace::ExportResult::kSuccess;
   }
 
   void Shutdown(std::chrono::microseconds timeout = std::chrono::microseconds(0)) noexcept {}
-
-
-  std::unique_ptr<
-      opentelemetry::proto::collector::trace::v1::TraceService::StubInterface>
-      trace_service_stub;
 };
