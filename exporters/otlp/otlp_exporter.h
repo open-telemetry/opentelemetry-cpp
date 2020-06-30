6ff134afb72c25c3b655c31685259eac0ac37258
Nadia Ciobanu <nadiaciobanu@google.com>
2020-06-30 16:26:17 +0000

    Set IDs in exporter

--- exporters/otlp/otlp_exporter.h
+++ exporters/otlp/otlp_exporter.h
@@ -2,6 +2,7 @@
 
 #include "opentelemetry/sdk/trace/exporter.h"
 #include "opentelemetry/sdk/trace/span_data.h"
+#include "src/common/random.h"
 
 #include "opentelemetry/proto/collector/trace/v1/trace_service.pb.h"
 #include "opentelemetry/proto/collector/trace/v1/trace_service.grpc.pb.h"
@@ -18,12 +19,28 @@ namespace trace    = opentelemetry::trace;
 namespace nostd    = opentelemetry::nostd;
 namespace sdktrace = opentelemetry::sdk::trace;
 namespace otlpexporter = opentelemetry::exporter::otlp;
+namespace protocollector = opentelemetry::proto::collector::trace::v1;
+namespace prototrace = opentelemetry::proto::trace::v1;
+
+trace::TraceId GenerateRandomTraceId()
+{
+    uint8_t trace_id_buf[trace::TraceId::kSize];
+    opentelemetry::sdk::common::Random::GenerateRandomBuffer(trace_id_buf);
+    return trace::TraceId(trace_id_buf);
+}
+
+trace::SpanId GenerateRandomSpanId()
+{
+    uint8_t span_id_buf[trace::SpanId::kSize];
+    opentelemetry::sdk::common::Random::GenerateRandomBuffer(span_id_buf);
+    return trace::SpanId(span_id_buf);
+}
 
 class OtlpExporter final : public sdktrace::SpanExporter
 {
   std::unique_ptr<sdktrace::Recordable> MakeRecordable() noexcept
   {
-    return std::unique_ptr<sdktrace::Recordable>(new opentelemetry::exporter::otlp::Recordable);
+    return std::unique_ptr<sdktrace::Recordable>(new otlpexporter::Recordable);
   }
 
   sdktrace::ExportResult Export(
@@ -32,36 +49,47 @@ class OtlpExporter final : public sdktrace::SpanExporter
     std::cout << "Exporting" << std::endl;
     grpc::ClientContext context;
 
-    opentelemetry::proto::collector::trace::v1::ExportTraceServiceRequest request;
-    opentelemetry::proto::collector::trace::v1::ExportTraceServiceResponse response;
+    protocollector::ExportTraceServiceRequest request;
+    protocollector::ExportTraceServiceResponse response;
 
     std::unique_ptr<
-      opentelemetry::proto::collector::trace::v1::TraceService::StubInterface>
+      protocollector::TraceService::StubInterface>
       trace_service_stub;
     
     const std::string address = "localhost:55678";
     auto channel = grpc::CreateChannel(
         address, grpc::InsecureChannelCredentials());
     
-    trace_service_stub = \
-      opentelemetry::proto::collector::trace::v1::TraceService::NewStub(channel);
+    trace_service_stub = protocollector::TraceService::NewStub(channel);
 
-    opentelemetry::proto::trace::v1::ResourceSpans* resource_span = request.add_resource_spans();
-
+    prototrace::ResourceSpans* resource_span = request.add_resource_spans();
 
     for (auto &recordable : spans) {
-      //std::cout << "Name: " << rec->span().name() << std::endl;
 
       auto rec = std::unique_ptr<otlpexporter::Recordable>(
           static_cast<otlpexporter::Recordable *>(recordable.release()));
 
-      opentelemetry::proto::trace::v1::Span* span = resource_span->add_spans();
+      std::cout << "Name: " << rec->span().name() << std::endl;
+      // std::cout << "Start time: " << rec->span().start_time_unixnano() << std::endl;
+      // std::cout << "End time: " << rec->span().end_time_unixnano() << std::endl;
+
+      // Temporarily set IDs here until Span is updated to do it
+      rec->SetIds(GenerateRandomTraceId(), GenerateRandomSpanId(), GenerateRandomSpanId());
+
+      // std::cout << "Trace ID: " << rec->span().trace_id() << std::endl;
+      // std::cout << "Span ID: " << rec->span().span_id() << std::endl;
+      // std::cout << "Parent span ID: " << rec->span().parent_span_id() << std::endl;
+
+      prototrace::Span* span = resource_span->add_spans();
       span->CopyFrom(rec->span());
     }
 
-    trace_service_stub->Export(&context, request, &response);
+     grpc::Status status = trace_service_stub->Export(&context, request, &response);
 
-    return sdktrace::ExportResult::kSuccess;
+    if(status.ok()){
+      return sdktrace::ExportResult::kSuccess;
+    }
+    return sdktrace::ExportResult::kFailure;
   }
 
   void Shutdown(std::chrono::microseconds timeout = std::chrono::microseconds(0)) noexcept {}
