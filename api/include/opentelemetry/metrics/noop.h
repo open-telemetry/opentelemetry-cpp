c4e8d748faa56c20a2944665828e88893325ba9a
Brandon Kimberly <brakmb@amazon.com>
2020-07-03 14:58:48 -0400

    Changed unique_ptr to shared_ptr
    And change type to A/SynchronousInstrument

--- api/include/opentelemetry/metrics/noop.h
+++ api/include/opentelemetry/metrics/noop.h
@@ -1,13 +1,3 @@
-#pragma once
-
-#include "opentelemetry/metrics/meter.h"
-#include "opentelemetry/nostd/string_view.h"
-#include "opentelemetry/nostd/unique_ptr.h"
-#include "opentelemetry/trace/key_value_iterable_view.h"
-
-OPENTELEMETRY_BEGIN_NAMESPACE
-namespace metrics
-{
 /**
  * No-op implementation of Meter. This class should not be used directly.
  */
@@ -16,115 +6,115 @@ class NoopMeter : public Meter
 public:
   NoopMeter() = default;
 
-  opentelemetry::nostd::unique_ptr<DoubleCounter> NewDoubleCounter(nostd::string_view name,
+  opentelemetry::nostd::shared_ptr<SynchronousInstrument> NewDoubleCounter(nostd::string_view name,
                                                                    nostd::string_view description,
                                                                    nostd::string_view unit,
                                                                    const bool enabled) override
   {
-    return nostd::unique_ptr<DoubleCounter>{
+    return nostd::shared_ptr<SynchronousInstrument>{
         new NoopDoubleCounter(name, description, unit, enabled)};
   }
 
-  opentelemetry::nostd::unique_ptr<IntCounter> NewIntCounter(nostd::string_view name,
+  opentelemetry::nostd::shared_ptr<SynchronousInstrument> NewIntCounter(nostd::string_view name,
                                                              nostd::string_view description,
                                                              nostd::string_view unit,
                                                              const bool enabled) override
   {
-    return nostd::unique_ptr<IntCounter>{new NoopIntCounter(name, description, unit, enabled)};
+    return nostd::shared_ptr<SynchronousInstrument>{new NoopIntCounter(name, description, unit, enabled)};
   }
 
-  opentelemetry::nostd::unique_ptr<DoubleUpDownCounter> NewDoubleUpDownCounter(
+  opentelemetry::nostd::shared_ptr<SynchronousInstrument> NewDoubleUpDownCounter(
       nostd::string_view name,
       nostd::string_view description,
       nostd::string_view unit,
       const bool enabled) override
   {
-    return nostd::unique_ptr<DoubleUpDownCounter>{
+    return nostd::shared_ptr<SynchronousInstrument>{
         new NoopDoubleUpDownCounter(name, description, unit, enabled)};
   }
 
-  opentelemetry::nostd::unique_ptr<IntUpDownCounter> NewIntUpDownCounter(
+  opentelemetry::nostd::shared_ptr<SynchronousInstrument> NewIntUpDownCounter(
       nostd::string_view name,
       nostd::string_view description,
       nostd::string_view unit,
       const bool enabled) override
   {
-    return nostd::unique_ptr<IntUpDownCounter>{
+    return nostd::shared_ptr<SynchronousInstrument>{
         new NoopIntUpDownCounter(name, description, unit, enabled)};
   }
 
-  opentelemetry::nostd::unique_ptr<DoubleValueRecorder> NewDoubleValueRecorder(
+  opentelemetry::nostd::shared_ptr<SynchronousInstrument> NewDoubleValueRecorder(
       nostd::string_view name,
       nostd::string_view description,
       nostd::string_view unit,
       const bool enabled) override
   {
-    return nostd::unique_ptr<DoubleValueRecorder>{
+    return nostd::shared_ptr<SynchronousInstrument>{
         new NoopDoubleValueRecorder(name, description, unit, enabled)};
   }
 
-  opentelemetry::nostd::unique_ptr<IntValueRecorder> NewIntValueRecorder(
+  opentelemetry::nostd::shared_ptr<SynchronousInstrument> NewIntValueRecorder(
       nostd::string_view name,
       nostd::string_view description,
       nostd::string_view unit,
       const bool enabled) override
   {
-    return nostd::unique_ptr<IntValueRecorder>{
+    return nostd::shared_ptr<SynchronousInstrument>{
         new NoopIntValueRecorder(name, description, unit, enabled)};
   }
 
-  opentelemetry::nostd::unique_ptr<DoubleSumObserver> NewDoubleSumObserver(
+  opentelemetry::nostd::shared_ptr<AsynchronousInstrument> NewDoubleSumObserver(
       nostd::string_view name,
       nostd::string_view description,
       nostd::string_view unit,
       const bool enabled,
       void (*callback)(DoubleObserverResult)) override
   {
-    return nostd::unique_ptr<DoubleSumObserver>{
+    return nostd::shared_ptr<AsynchronousInstrument>{
         new NoopDoubleSumObserver(name, description, unit, enabled, callback)};
   }
 
-  opentelemetry::nostd::unique_ptr<IntSumObserver> NewIntSumObserver(
+  opentelemetry::nostd::shared_ptr<AsynchronousInstrument> NewIntSumObserver(
       nostd::string_view name,
       nostd::string_view description,
       nostd::string_view unit,
       const bool enabled,
       void (*callback)(IntObserverResult)) override
   {
-    return nostd::unique_ptr<IntSumObserver>{
+    return nostd::shared_ptr<AsynchronousInstrument>{
         new NoopIntSumObserver(name, description, unit, enabled, callback)};
   }
 
-  opentelemetry::nostd::unique_ptr<DoubleUpDownSumObserver> NewDoubleUpDownSumObserver(
+  opentelemetry::nostd::shared_ptr<AsynchronousInstrument> NewDoubleUpDownSumObserver(
       nostd::string_view name,
       nostd::string_view description,
       nostd::string_view unit,
       const bool enabled,
       void (*callback)(DoubleObserverResult)) override
   {
-    return nostd::unique_ptr<DoubleUpDownSumObserver>{
+    return nostd::shared_ptr<AsynchronousInstrument>{
         new NoopDoubleUpDownSumObserver(name, description, unit, enabled, callback)};
   }
 
-  opentelemetry::nostd::unique_ptr<IntUpDownSumObserver> NewIntUpDownSumObserver(
+  opentelemetry::nostd::shared_ptr<AsynchronousInstrument> NewIntUpDownSumObserver(
       nostd::string_view name,
       nostd::string_view description,
       nostd::string_view unit,
       const bool enabled,
       void (*callback)(IntObserverResult)) override
   {
-    return nostd::unique_ptr<IntUpDownSumObserver>{
+    return nostd::shared_ptr<AsynchronousInstrument>{
         new NoopIntUpDownSumObserver(name, description, unit, enabled, callback)};
   }
 
-  opentelemetry::nostd::unique_ptr<DoubleValueObserver> NewDoubleValueObserver(
+  opentelemetry::nostd::shared_ptr<AsynchronousInstrument> NewDoubleValueObserver(
       nostd::string_view name,
       nostd::string_view description,
       nostd::string_view unit,
       const bool enabled,
       void (*callback)(DoubleObserverResult)) override
   {
-    return nostd::unique_ptr<DoubleValueObserver>{
+    return nostd::shared_ptr<AsynchronousInstrument>{
         new NoopDoubleValueObserver(name, description, unit, enabled, callback)};
   }
 
@@ -134,6 +124,5 @@ public:
     // No-op
   }
 };
-
 }  // namespace metrics
 OPENTELEMETRY_END_NAMESPACE
