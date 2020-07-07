8780d19eace93bfb56d77b56121dd7d191c21ca4
Brandon Kimberly <brakmb@amazon.com>
2020-07-07 12:03:47 -0400

    Add RecordBatch test

--- api/test/metrics/noop_metrics_test.cc
+++ api/test/metrics/noop_metrics_test.cc
@@ -1,19 +1,23 @@
 #include "opentelemetry/metrics/noop.h"
 #include "opentelemetry/metrics/observer_result.h"
 
+#include <array>
 #include <memory>
 
 #include <gtest/gtest.h>
 
-using opentelemetry::metrics::NoopMeter;
+OPENTELEMETRY_BEGIN_NAMESPACE
+
 using opentelemetry::metrics::Meter;
+using opentelemetry::metrics::NoopMeter;
 
 void IntCallback(opentelemetry::metrics::IntObserverResult result)
 {
-  result.observe(1,"key: value");
+  result.observe(1, "key: value");
 }
 
-void DoubleCallback(opentelemetry::metrics::DoubleObserverResult result) {
+void DoubleCallback(opentelemetry::metrics::DoubleObserverResult result)
+{
   result.observe(1.0, "key: value");
 }
 
@@ -23,27 +27,49 @@ TEST(NoopTest, UseNoopMeters)
 
   auto t1 = m->NewIntCounter("Test int counter", "For testing", "Unitless", true);
   auto t2 = m->NewIntCounter("Test int counter", "For testing", "Unitless", true);
-  m->NewDoubleCounter("Test int counter", "For testing", "Unitless", true);
+  auto t3 = m->NewDoubleCounter("Test int counter", "For testing", "Unitless", true);
   m->NewIntUpDownCounter("Test int counter", "For testing", "Unitless", true);
   m->NewDoubleUpDownCounter("Test int counter", "For testing", "Unitless", true);
   m->NewIntValueRecorder("Test int counter", "For testing", "Unitless", true);
   m->NewDoubleValueRecorder("Test int counter", "For testing", "Unitless", true);
 
-  auto alpha = m->NewIntSumObserver("Test int counter", "For testing", "Unitless", true, &IntCallback);
-  auto beta = m->NewDoubleSumObserver("Test int counter", "For testing", "Unitless", true, &DoubleCallback);
-  auto gamma = m->NewIntUpDownSumObserver("Test int counter", "For testing", "Unitless", true, &IntCallback);
-  auto delta = m->NewDoubleUpDownSumObserver("Test int counter", "For testing", "Unitless", true, &DoubleCallback);
-  auto epsilon = m->NewIntValueObserver("Test int counter", "For testing", "Unitless", true, &IntCallback);
-  auto zeta = m->NewDoubleValueObserver("Test int counter", "For testing", "Unitless", true, &DoubleCallback);
+  auto alpha =
+      m->NewIntSumObserver("Test int counter", "For testing", "Unitless", true, &IntCallback);
+  auto beta =
+      m->NewDoubleSumObserver("Test int counter", "For testing", "Unitless", true, &DoubleCallback);
+  auto gamma =
+      m->NewIntUpDownSumObserver("Test int counter", "For testing", "Unitless", true, &IntCallback);
+  auto delta = m->NewDoubleUpDownSumObserver("Test int counter", "For testing", "Unitless", true,
+                                             &DoubleCallback);
+  auto epsilon =
+      m->NewIntValueObserver("Test int counter", "For testing", "Unitless", true, &IntCallback);
+  auto zeta = m->NewDoubleValueObserver("Test int counter", "For testing", "Unitless", true,
+                                        &DoubleCallback);
 
-  alpha->observe(1,"key:value");
-  beta->observe(1.0,"key:value");
-  gamma->observe(1,"key:value");
-  delta->observe(1.0,"key:value");
-  epsilon->observe(1,"key:value");
-  zeta->observe(1.0,"key:value");
+  alpha->observe(1, "key:value");
+  beta->observe(1.0, "key:value");
+  gamma->observe(1, "key:value");
+  delta->observe(1.0, "key:value");
+  epsilon->observe(1, "key:value");
+  zeta->observe(1.0, "key:value");
 
   ASSERT_NE(t1, t2);
 
-  //Test RecordBatch call
+  using M =
+  std::pair<nostd::shared_ptr<metrics::SynchronousInstrument>, nostd::variant<int, double>>;
+  M t1_pair;
+  nostd::shared_ptr<metrics::SynchronousInstrument> tmp = t1;
+  t1_pair.first                                         = tmp;
+  t1_pair.second                                        = 1;
+
+  M t3_pair;
+  nostd::shared_ptr<metrics::SynchronousInstrument> tmp2 = t3;
+  t3_pair.first                                          = tmp2;
+  t3_pair.second                                         = 2.0;
+
+  std::array<M, 2> arr{t1_pair, t3_pair};
+  nostd::span<M> span(arr);
+
+  m->RecordBatch("key: value", span);
 }
+OPENTELEMETRY_END_NAMESPACE
