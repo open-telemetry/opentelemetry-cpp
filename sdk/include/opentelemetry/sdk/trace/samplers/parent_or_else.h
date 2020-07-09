d2f979b6bd215f77e48b9a3c6661fdbb6c22bbd4
Oliver Zhang <30583106+ziqizh@users.noreply.github.com>
2020-07-09 17:42:22 -0400

    Parent-or-else Sampler (#142)

---
+++ sdk/include/opentelemetry/sdk/trace/samplers/parent_or_else.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "opentelemetry/sdk/common/atomic_shared_ptr.h"
+#include "opentelemetry/sdk/trace/sampler.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace sdk
+{
+namespace trace
+{
+namespace trace_api = opentelemetry::trace;
+
+/**
+ * A placeholder class that stores the states of a span.
+ * Will be replaced by the real SpanContext class once it is implemented.
+ */
+class Sampler::SpanContext
+{
+public:
+  inline explicit SpanContext(bool is_recording, bool sampled_flag)
+      : is_recording(is_recording), sampled_flag(sampled_flag)
+  {}
+
+  bool is_recording;
+  bool sampled_flag;
+};
+
+/**
+ * The parent or else sampler is a composite sampler. ParentOrElse(delegateSampler) either respects
+ * the parent span's sampling decision or delegates to delegateSampler for root spans.
+ */
+class ParentOrElseSampler : public Sampler
+{
+public:
+  explicit ParentOrElseSampler(std::shared_ptr<Sampler> delegate_sampler) noexcept;
+  /** The decision either respects the parent span's sampling decision or delegates to
+   * delegateSampler for root spans
+   * @return Returns NOT_RECORD always
+   */
+  SamplingResult ShouldSample(const SpanContext * parent_context,
+                              trace_api::TraceId trace_id,
+                              nostd::string_view name,
+                              trace_api::SpanKind span_kind,
+                              const trace_api::KeyValueIterable & attributes) noexcept override;
+
+  /**
+   * @return Description MUST be ParentOrElse{delegate_sampler_.getDescription()}
+   */
+  std::string GetDescription() const noexcept override;
+
+private:
+  const std::shared_ptr<Sampler> delegate_sampler_;
+  const std::string description_;
+};
+}  // namespace trace
+}  // namespace sdk
+OPENTELEMETRY_END_NAMESPACE
---
+++ sdk/src/trace/samplers/parent_or_else.cc
@@ -0,0 +1,41 @@
+#include "opentelemetry/sdk/trace/samplers/parent_or_else.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace sdk
+{
+namespace trace
+{
+ParentOrElseSampler::ParentOrElseSampler(std::shared_ptr<Sampler> delegate_sampler) noexcept
+    : delegate_sampler_(delegate_sampler),
+      description_("ParentOrElse{" + delegate_sampler->GetDescription() + "}")
+{}
+
+SamplingResult ParentOrElseSampler::ShouldSample(
+    const SpanContext *parent_context,
+    trace_api::TraceId trace_id,
+    nostd::string_view name,
+    trace_api::SpanKind span_kind,
+    const trace_api::KeyValueIterable &attributes) noexcept
+{
+  if (parent_context == nullptr)
+  {
+    // If no parent (root span) exists returns the result of the delegateSampler
+    return delegate_sampler_->ShouldSample(parent_context, trace_id, name, span_kind, attributes);
+  }
+  
+  // If parent exists:
+  if (parent_context->sampled_flag)
+  {
+    return {Decision::RECORD_AND_SAMPLE, nullptr};
+  }
+
+  return {Decision::NOT_RECORD, nullptr};
+}
+
+std::string ParentOrElseSampler::GetDescription() const noexcept
+{
+  return description_;
+}
+}  // namespace trace
+}  // namespace sdk
+OPENTELEMETRY_END_NAMESPACE
