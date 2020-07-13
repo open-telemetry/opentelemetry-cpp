af3b3a7fa75a0c51edf9d65216e358116a0fbfc8
Janet Vu <vjanet@umich.edu>
2020-07-13 21:48:19 +0000

    Make processor more thread-safe with tests

--- ext/src/zpages/tracez_processor.cc
+++ ext/src/zpages/tracez_processor.cc
@@ -5,26 +5,27 @@ namespace ext {
 namespace zpages {
 
   void TracezSpanProcessor::OnStart(opentelemetry::sdk::trace::Recordable &span) noexcept {
+    std::lock_guard<std::mutex> lock(mtx_);
     spans_.running.insert(static_cast<opentelemetry::sdk::trace::SpanData*>(&span));
   }
 
-  // TODO: Add thread safety measures
   void TracezSpanProcessor::OnEnd(std::unique_ptr<opentelemetry::sdk::trace::Recordable> &&span) noexcept {
-     if (span == nullptr) return;
-     auto span_raw = static_cast<opentelemetry::sdk::trace::SpanData*>(span.get());
-     auto span_it = spans_.running.find(span_raw);
-     if (span_it != spans_.running.end()) {
-       spans_.running.erase(span_it);
-       spans_.completed.push_back(
-           std::unique_ptr<opentelemetry::sdk::trace::SpanData>(
-               static_cast<opentelemetry::sdk::trace::SpanData*>(span.release())));
-     }
+    if (span == nullptr) return;
+    auto span_raw = static_cast<opentelemetry::sdk::trace::SpanData*>(span.get());
+    std::lock_guard<std::mutex> lock(mtx_);
+    auto span_it = spans_.running.find(span_raw);
+    if (span_it != spans_.running.end()) {
+      spans_.running.erase(span_it);
+      spans_.completed.push_back(
+          std::unique_ptr<opentelemetry::sdk::trace::SpanData>(
+              static_cast<opentelemetry::sdk::trace::SpanData*>(span.release())));
+    }
   }
 
 
-  // TODO: Add thread safety measures
   TracezSpanProcessor::CollectedSpans TracezSpanProcessor::GetSpanSnapshot() noexcept {
     CollectedSpans snapshot;
+    std::lock_guard<std::mutex> lock(mtx_);
     snapshot.running = spans_.running;
     snapshot.completed = std::move(spans_.completed);
     spans_.completed.clear();
