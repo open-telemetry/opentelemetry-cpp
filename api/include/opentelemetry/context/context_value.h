ada7c68980effd894fc5b4dee0bc8c48e257c4c6
Sam Atac <samatac@google.com>
2020-07-15 14:24:00 +0000

    Added context_value.h

---
+++ api/include/opentelemetry/context/context_value.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+
+#include "opentelemetry/nostd/span.h"
+#include "opentelemetry/nostd/string_view.h"
+#include "opentelemetry/nostd/variant.h"
+#include "opentelemetry/version.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace context
+{
+  using ContextValue = nostd::variant<bool,
+        int,
+        int64_t,
+        unsigned int,
+        uint64_t,
+        double,
+        nostd::string_view,
+        nostd::span<const bool>,
+        nostd::span<const int>,
+        nostd::span<const int64_t>,
+        nostd::span<const unsigned int>,
+        nostd::span<const uint64_t>,
+        nostd::span<const double>,
+        nostd::span<const nostd::string_view>>;
+}  // namespace context
+OPENTELEMETRY_END_NAMESPACE
