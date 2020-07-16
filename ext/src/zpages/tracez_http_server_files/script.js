9008029a50cf54071bd6ef8dd1dc88cdc5dc3627
Janet Vu <vjanet@umich.edu>
2020-07-16 18:23:53 +0000

    Add working HTML/CSS/native JS

---
+++ ext/src/zpages/tracez_http_server_files/script.js
@@ -0,0 +1,39 @@
+window.onload = loadTracezTable;
+
+const spanRow = span => "<tr><td>"
+  + span.type + " " + span.id
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td><td>"
+  + String(Math.floor(Math.random() * 150))
+  + "</td></tr>";
+
+
+function loadTracezTable () {
+  fetch("/status.json").then(res => res.json())
+    .then(data => {
+      data.forEach(span => {
+        document.getElementById("records_table")
+          .innerHTML += spanRow(span);
+        console.log(span);
+      })
+    });
+}
