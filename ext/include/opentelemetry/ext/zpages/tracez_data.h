9bd573aac35c20461925258c592c1aa091295a10
Keshav Manghat <keshavmanghat@gmail.com>
2020-07-18 01:10:20 +0000

    Added tracez_data.h header file to define structures that store data required by tracez zpages

---
+++ ext/include/opentelemetry/ext/zpages/tracez_data.h
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <string>
+
+#include "opentelemetry/nostd/span.h"
+#include "opentelemetry/nostd/string_view.h"
+#include "opentelemetry/sdk/trace/span_data.h"
+#include "opentelemetry/trace/canonical_code.h"
+#include "opentelemetry/trace/span_id.h"
+#include "opentelemetry/trace/trace_id.h"
+#include "opentelemetry/version.h"
+
+using opentelemetry::sdk::trace::SpanData;
+using opentelemetry::trace::CanonicalCode;
+using opentelemetry::trace::SpanId;
+using opentelemetry::trace::TraceId;
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace ext {
+namespace zpages {
+
+/**
+ * kMaxNumberOfSampleSpans is the maximum number of running, completed or error
+ * sample spans stored at any given time for a given span name.
+ * This limit is introduced to reduce memory usage by trimming sample spans
+ * stored.
+ */
+const int kMaxNumberOfSampleSpans = 5;
+
+/**
+ * SampleSpanData is a structure that stores some of the relevant span
+ * information for sample spans. The structure copies over data from SpanData
+ * and stores it as strings so it can readily be rendered on the frontend or
+ * converted to JSON.
+ * Defining this structure also eliminates the dependency on SpanData of
+ * running spans which is owned by span and access to which may cause race
+ * conditions.
+ * TODO: At this time SpanData that is passed into this struct's constructor
+ * does not have many attributes like span_id etc, so a lot of field's in the
+ * struct will be empty.
+ */
+struct SampleSpanData {
+  std::string span_name;
+  std::string span_id;
+  std::string trace_id;
+  std::string parent_id;
+  std::string description;
+  std::string duration;
+  SampleSpanData(SpanData &span_data) {
+    span_name = span_data.GetName().data();
+    span_id = std::string(
+        reinterpret_cast<const char *>(span_data.GetSpanId().Id().data()));
+    trace_id = std::string(
+        reinterpret_cast<const char *>(span_data.GetTraceId().Id().data()));
+    parent_id = std::string(reinterpret_cast<const char *>(
+        span_data.GetParentSpanId().Id().data()));
+    description = span_data.GetDescription().data();
+    duration = std::to_string(span_data.GetDuration().count());
+  }
+};
+
+/**
+ * TracezData is the data to be displayed for tracez zpages that is stored for
+ * each span name.
+ */
+struct TracezData {
+  /**
+   * TODO: At this time the maximum count is unknown but a larger data type
+   * might have to be used in the future to store these counts to avoid overflow
+   */
+  unsigned int running_span_count;
+  unsigned int error_span_count;
+
+  /**
+   * completed_span_count_per_latency_bucket is an array that stores the count
+   * of spans for each of the 9 latency buckets.
+   */
+  std::array<unsigned int, kLatencyBoundaries.size()>
+      completed_span_count_per_latency_bucket;
+
+  /**
+   * sample_latency_spans is an array of lists, each index of the array
+   * corresponds to a latency boundary(of which there are 9).
+   * The list in each index stores the sample spans for that latency boundary.
+   */
+  std::array<std::list<SampleSpanData>, kLatencyBoundaries.size()>
+      sample_latency_spans;
+
+  /**
+   * sample_error_spans is a list that stores the error samples for a span name.
+   */
+  std::list<SampleSpanData> sample_error_spans;
+
+  /**
+   * sample_running_spans is a list that stores the running span samples for a
+   * span name.
+   */
+  std::list<SampleSpanData> sample_running_spans;
+
+  TracezData() {
+    running_span_count = 0;
+    error_span_count = 0;
+    completed_span_count_per_latency_bucket.fill(0);
+  }
+};
+
+}  // namespace zpages
+}  // namespace ext
+OPENTELEMETRY_END_NAMESPACE
