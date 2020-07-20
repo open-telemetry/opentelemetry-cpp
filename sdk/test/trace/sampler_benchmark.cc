a974812d17be12104ea28d03c8f2b2ff11cd7880
Nick Holbrook <nholbrook@google.com>
2020-07-20 17:00:28 -0400

    Format benchmark

--- sdk/test/trace/sampler_benchmark.cc
+++ sdk/test/trace/sampler_benchmark.cc
@@ -1,4 +1,3 @@
-#include "opentelemetry/sdk/trace/tracer.h"
 #include "opentelemetry/sdk/trace/sampler.h"
 #include "opentelemetry/sdk/trace/samplers/always_off.h"
 #include "opentelemetry/sdk/trace/samplers/always_on.h"
@@ -6,6 +5,7 @@
 #include "opentelemetry/sdk/trace/samplers/probability.h"
 #include "opentelemetry/sdk/trace/simple_processor.h"
 #include "opentelemetry/sdk/trace/span_data.h"
+#include "opentelemetry/sdk/trace/tracer.h"
 
 #include <cstdint>
 
@@ -57,124 +57,124 @@ namespace
 // Sampler constructor used as a baseline to compare with other samplers
 void BM_AlwaysOffSamplerConstruction(benchmark::State &state)
 {
-	while (state.KeepRunning())
-	{
-		benchmark::DoNotOptimize(AlwaysOffSampler());
-	}
+  while (state.KeepRunning())
+  {
+    benchmark::DoNotOptimize(AlwaysOffSampler());
+  }
 }
 BENCHMARK(BM_AlwaysOffSamplerConstruction);
 
 // Sampler constructor used as a baseline to compare with other samplers
 void BM_AlwaysOnSamplerConstruction(benchmark::State &state)
 {
-	while (state.KeepRunning())
-	{
-		benchmark::DoNotOptimize(AlwaysOnSampler());
-	}
+  while (state.KeepRunning())
+  {
+    benchmark::DoNotOptimize(AlwaysOnSampler());
+  }
 }
 BENCHMARK(BM_AlwaysOnSamplerConstruction);
 
 void BM_ParentOrElseSamplerConstruction(benchmark::State &state)
 {
-	while (state.KeepRunning())
-	{
-		benchmark::DoNotOptimize(ParentOrElseSampler(std::make_shared<AlwaysOnSampler>()));
-	}
+  while (state.KeepRunning())
+  {
+    benchmark::DoNotOptimize(ParentOrElseSampler(std::make_shared<AlwaysOnSampler>()));
+  }
 }
 BENCHMARK(BM_ParentOrElseSamplerConstruction);
 
 void BM_ProbabilitySamplerConstruction(benchmark::State &state)
 {
-	while (state.KeepRunning())
-	{
-		benchmark::DoNotOptimize(ProbabilitySampler(0.01));
-	}
+  while (state.KeepRunning())
+  {
+    benchmark::DoNotOptimize(ProbabilitySampler(0.01));
+  }
 }
 BENCHMARK(BM_ProbabilitySamplerConstruction);
 
 // Sampler Helper Function
 void BenchmarkShouldSampler(Sampler &sampler, benchmark::State &state)
 {
-	opentelemetry::trace::TraceId trace_id;
-	opentelemetry::trace::SpanKind span_kind = opentelemetry::trace::SpanKind::kInternal;
+  opentelemetry::trace::TraceId trace_id;
+  opentelemetry::trace::SpanKind span_kind = opentelemetry::trace::SpanKind::kInternal;
 
-	using M = std::map<std::string, int>;
-	M m1 = {{}};
-	opentelemetry::trace::KeyValueIterableView<M> view{m1};
+  using M = std::map<std::string, int>;
+  M m1    = {{}};
+  opentelemetry::trace::KeyValueIterableView<M> view{m1};
 
-	while (state.KeepRunning())
-	{
-		benchmark::DoNotOptimize(sampler.ShouldSample(nullptr, trace_id, "", span_kind, view));
-	}
+  while (state.KeepRunning())
+  {
+    benchmark::DoNotOptimize(sampler.ShouldSample(nullptr, trace_id, "", span_kind, view));
+  }
 }
 
 // Sampler used as a baseline to compare with other samplers
 void BM_AlwaysOffSamplerShouldSample(benchmark::State &state)
 {
-	AlwaysOffSampler sampler;
+  AlwaysOffSampler sampler;
 
-	BenchmarkShouldSampler(sampler, state);
+  BenchmarkShouldSampler(sampler, state);
 }
 BENCHMARK(BM_AlwaysOffSamplerShouldSample);
 
 // Sampler used as a baseline to compare with other samplers
 void BM_AlwaysOnSamplerShouldSample(benchmark::State &state)
 {
-	AlwaysOnSampler sampler;
+  AlwaysOnSampler sampler;
 
-	BenchmarkShouldSampler(sampler, state);
+  BenchmarkShouldSampler(sampler, state);
 }
 BENCHMARK(BM_AlwaysOnSamplerShouldSample);
 
 void BM_ParentOrElseSamplerShouldSample(benchmark::State &state)
 {
-	ParentOrElseSampler sampler(std::make_shared<AlwaysOnSampler>());
+  ParentOrElseSampler sampler(std::make_shared<AlwaysOnSampler>());
 
-	BenchmarkShouldSampler(sampler, state);
+  BenchmarkShouldSampler(sampler, state);
 }
 BENCHMARK(BM_ParentOrElseSamplerShouldSample);
 
 void BM_ProbabilitySamplerShouldSample(benchmark::State &state)
 {
-	ProbabilitySampler sampler(0.01);
+  ProbabilitySampler sampler(0.01);
 
-	BenchmarkShouldSampler(sampler, state);
+  BenchmarkShouldSampler(sampler, state);
 }
 BENCHMARK(BM_ProbabilitySamplerShouldSample);
 
 // Sampler Helper Function
 void BenchmarkSpanCreation(std::shared_ptr<Sampler> sampler, benchmark::State &state)
 {
-	std::shared_ptr<std::vector<std::unique_ptr<SpanData>>> spans_received(
-		new std::vector<std::unique_ptr<SpanData>>);
+  std::shared_ptr<std::vector<std::unique_ptr<SpanData>>> spans_received(
+      new std::vector<std::unique_ptr<SpanData>>);
 
-	std::unique_ptr<SpanExporter> exporter(new MockSpanExporter(spans_received));
-	auto processor = std::make_shared<SimpleSpanProcessor>(std::move(exporter));
-	auto tracer = std::shared_ptr<opentelemetry::trace::Tracer>(new Tracer(processor, sampler));
+  std::unique_ptr<SpanExporter> exporter(new MockSpanExporter(spans_received));
+  auto processor = std::make_shared<SimpleSpanProcessor>(std::move(exporter));
+  auto tracer    = std::shared_ptr<opentelemetry::trace::Tracer>(new Tracer(processor, sampler));
 
-	while (state.KeepRunning())
-	{
-		auto span = tracer->StartSpan("span");
+  while (state.KeepRunning())
+  {
+    auto span = tracer->StartSpan("span");
 
-		span->SetAttribute("attr1", 3.1);
+    span->SetAttribute("attr1", 3.1);
 
-		span->End();
-	}
+    span->End();
+  }
 }
 
 // Test to measure performance for span creation
 void BM_SpanCreation(benchmark::State &state)
 {
-	BenchmarkSpanCreation(std::move(std::make_shared<AlwaysOnSampler>()), state);
+  BenchmarkSpanCreation(std::move(std::make_shared<AlwaysOnSampler>()), state);
 }
 BENCHMARK(BM_SpanCreation);
 
 // Test to measure performance overhead for no-op span creation
 void BM_NoopSpanCreation(benchmark::State &state)
 {
-	BenchmarkSpanCreation(std::move(std::make_shared<AlwaysOffSampler>()), state);
+  BenchmarkSpanCreation(std::move(std::make_shared<AlwaysOffSampler>()), state);
 }
 BENCHMARK(BM_NoopSpanCreation);
 
-} // namespace
+}  // namespace
 BENCHMARK_MAIN();
