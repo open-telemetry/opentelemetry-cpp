39941d3320e4be7e059df4458583f86c429411ba
Sam Atac <samatac@google.com>
2020-07-20 00:55:20 +0000

    SpanContext should be found now

--- api/include/opentelemetry/context/context_value.h
+++ api/include/opentelemetry/context/context_value.h
@@ -6,6 +6,7 @@
 #include "opentelemetry/nostd/string_view.h"
 #include "opentelemetry/nostd/variant.h"
 #include "opentelemetry/version.h"
+#include "opentelemetry/trace/span_context.h"
 
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace context
@@ -13,7 +14,7 @@ namespace context
 using ContextValue = nostd::variant<int64_t,
                                     uint64_t,
                                     double,
-                                    nostd::span<SpanContext>,
+                                    nostd::span<trace::SpanContext>,
                                     nostd::span<nostd::unique_ptr<char[]>>>;
 }  // namespace context
 OPENTELEMETRY_END_NAMESPACE
