6b34c754874393baeedadd1e23980ef3fe140679
hudsonhumphries <hudsohum@amazon.com>
2020-07-20 14:33:01 -0500

    Counter aggregator test

--- exporters/stream/test/stream_metrics_test.cc
+++ exporters/stream/test/stream_metrics_test.cc
@@ -1,13 +1,43 @@
 #include "opentelemetry/sdk/metrics/exporter.h"
 #include "opentelemetry/exporters/stream/metrics_exporter.h"
+#include "opentelemetry/sdk/metrics/record.h"
 
 #include <iostream>
 #include <gtest/gtest.h>
 
 namespace sdkmetrics = opentelemetry::sdk::metrics;
 
-TEST(StreamMetricsExporter, Shutdown)
+TEST(StreamMetricsExporter, PrintCounter)
 {
   auto exporter = std::unique_ptr<sdkmetrics::MetricsExporter> (new
       opentelemetry::exporter::metrics::StreamMetricsExporter);
+  
+  std::vector<int> vec (1,5);
+  auto record = sdkmetrics::Record("name", "description", AggregatorKind::Counter, "labels", vec);
+
+  std::vector<sdkmetrics::Record> records;
+  records.push_back(record);
+
+  // Create stringstream to redirect to
+  std::stringstream stdoutOutput;
+
+  // Save cout's buffer here
+  std::streambuf *sbuf = std::cout.rdbuf();
+
+  // Redirect cout to our stringstream buffer
+  std::cout.rdbuf(stdoutOutput.rdbuf());
+
+  exporter->Export(records);
+
+  std::cout.rdbuf(sbuf);
+
+  std::string expectedOutput = 
+  "{\n"
+  "  name        : name\n"
+  "  description : description\n"
+  "  labels      : labels\n"
+  "  sum         : 5\n"
+  "}\n"; 
+
+  ASSERT_EQ(stdoutOutput.str(),expectedOutput);
 }
