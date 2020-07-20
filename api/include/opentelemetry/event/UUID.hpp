464dcb8af6ed2958ef061ff2f3cb5da2e2c0cb94
Max Golovanov <maxgolov@microsoft.com>
2020-07-20 16:57:38 -0700

    Allow GUID format: starts with curly brace, but no dashes

--- api/include/opentelemetry/event/UUID.hpp
+++ api/include/opentelemetry/event/UUID.hpp
@@ -83,8 +83,11 @@ struct UUID
     // Convert to set of integer values
     unsigned long p0;
     unsigned int p1, p2, p3, p4, p5, p6, p7, p8, p9, p10;
-    if (11 == sscanf(str, "%08lX-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X", &p0, &p1, &p2, &p3,
-                     &p4, &p5, &p6, &p7, &p8, &p9, &p10))
+    if (
+        // Parse input with dashes
+        (11 == sscanf(str, "%08lX-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X", &p0, &p1, &p2, &p3, &p4, &p5, &p6, &p7, &p8, &p9, &p10)) ||
+        // Parse input without dashes
+        (11 == sscanf(str, "%08lX%04X%04X%02X%02X%02X%02X%02X%02X%02X%02X", &p0, &p1, &p2, &p3, &p4, &p5, &p6, &p7, &p8, &p9, &p10)))
     {
       Data1    = static_cast<uint32_t>(p0);
       Data2    = static_cast<uint16_t>(p1);
