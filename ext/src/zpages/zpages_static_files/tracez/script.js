32f93cf41beb7e95cb625707f63cf907197f8d01
Janet Vu <vjanet@umich.edu>
2020-07-21 15:57:51 +0000

    Show last fetch time, hitting dif endpts, nice css

--- ext/src/zpages/zpages_static_files/tracez/script.js
+++ ext/src/zpages/zpages_static_files/tracez/script.js
@@ -2,38 +2,65 @@ window.onload = loadTracezTable;
 
 const spanRow = span => "<tr><td>"
   + span.name
-  + "</td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 6))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 6))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 150))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 150))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 150))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 150))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 150))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 150))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 150))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 150))
-  + "</a></td><td><a href=''>"
+  + "</td><td class='click'>"
   + String(Math.floor(Math.random() * 150))
-  + "</a></td></tr>";
+  + "</td></tr>";
 
+
+const getRow = row => spanRow(row);
+
+const getRows = rows => rows.map(row => getRow(row)).join('');
 
 function loadTracezTable () {
+  fetch("/tracez/get/aggregations").then(res => res.json())
+    .then(data => {
+        document.getElementById("overview_table")
+            .innerHTML = getRows(data);
+    });
+  document.getElementById('lastUpdate').innerHTML = new Date().toLocaleString();
+}
+
+function add () {
   fetch("/status.json").then(res => res.json())
     .then(data => {
-      data.forEach(span => {
         document.getElementById("overview_table")
-          .innerHTML += spanRow(span);
-        console.log(span);
-      })
+            .getElementsByTagName('tbody')[0]
+            .innerHTML += getRows(data);
     });
+  document.getElementById('lastUpdate').innerHTML = new Date().toLocaleString();
+}
+
+function run () {
+  fetch('/tracez/get/running').then(res => res.json())
+      .then(d => console.log(d));
+}
+
+function buck () {
+  fetch('/tracez/get/bucket/1').then(res => res.json())
+      .then(d => console.log(d));
+}
+
+function err () {
+  fetch('/tracez/get/error').then(res => res.json())
+      .then(d => console.log(d));
 }
