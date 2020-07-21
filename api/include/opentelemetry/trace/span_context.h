d5581269cb967158734acc01714a20873b49abf7
TianlinZhao <tianlinzhao@google.com>
2020-07-21 12:06:20 -0400

    dependency

--- api/include/opentelemetry/trace/span_context.h
+++ api/include/opentelemetry/trace/span_context.h
@@ -34,8 +34,18 @@ class SpanContext final
 public:
   // An invalid SpanContext.
   SpanContext() noexcept : trace_state_(new TraceState) {}
-  SpanContext(SpanContext&&) {};
-  SpanContext(const SpanContext&) {};
+  SpanContext(SpanContext&& ctx) {
+    trace_id_ = ctx.trace_id();
+    span_id_ = ctx.span_id();
+    trace_flags_ = ctx.trace_flags();
+    trace_state_ = ctx.trace_state();
+  }
+  SpanContext(const SpanContext& ctx) {
+    trace_id_ = ctx.trace_id();
+    span_id_ = ctx.span_id();
+    trace_flags_ = ctx.trace_flags();
+    trace_state_ = ctx.trace_state();
+  }
   // TODO
   //
   // static SpanContext Create(TraceId traceId, SpanId spanId, TraceFlags traceFlags, TraceState
