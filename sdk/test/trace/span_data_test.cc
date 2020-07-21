777971e69e1b5297f327af61b184764d1eb0c50d
Nadia Ciobanu <nadiaciobanu@google.com>
2020-07-21 19:58:01 +0000

    Add code coverage for SpanData

--- sdk/test/trace/span_data_test.cc
+++ sdk/test/trace/span_data_test.cc
@@ -1,8 +1,8 @@
 #include "opentelemetry/sdk/trace/span_data.h"
 #include "opentelemetry/nostd/variant.h"
+#include "opentelemetry/trace/key_value_iterable_view.h"
 #include "opentelemetry/trace/span_id.h"
 #include "opentelemetry/trace/trace_id.h"
-#include "opentelemetry/trace/key_value_iterable_view.h"
 
 #include <gtest/gtest.h>
 
@@ -40,7 +40,10 @@ TEST(SpanData, Set)
   data.SetStartTime(now);
   data.SetDuration(std::chrono::nanoseconds(1000000));
   data.SetAttribute("attr1", 314159);
-  data.AddEvent("event1", now, opentelemetry::trace::KeyValueIterableView<std::map<std::string, int>>({}));
+  data.AddEvent("event1", now,
+                opentelemetry::trace::KeyValueIterableView<std::map<std::string, int>>({}));
+  data.AddLink(trace_id, span_id,
+               opentelemetry::trace::KeyValueIterableView<std::map<std::string, int>>({}));
 
   ASSERT_EQ(data.GetTraceId(), trace_id);
   ASSERT_EQ(data.GetSpanId(), span_id);
