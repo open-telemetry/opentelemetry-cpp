179216fb4d7f2ee5cae38b20967deae29a73ec20
TianlinZhao <tianlinzhao@google.com>
2020-07-22 20:18:08 -0400

    dependency

--- api/include/opentelemetry/trace/propagation/http_trace_context.h
+++ api/include/opentelemetry/trace/propagation/http_trace_context.h
@@ -36,7 +36,7 @@ namespace
 //    HttpTraceContext.inject(setter,&carrier,&context);
 //    HttpTraceContext.extract(getter,&carrier,&context);
 template <typename T>
-class HttpTraceContext : public trace::propagation::HTTPTextFormat {
+class HttpTraceContext {
     public:
         // Rules that manages how context will be extracted from carrier.
         using Getter = nostd::string_view(*)(T &carrier, nostd::string_view trace_type);
