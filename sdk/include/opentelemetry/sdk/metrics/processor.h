04327250bf76358901675ea98907d1853c87a648
hudsonhumphries <hudsohum@amazon.com>
2020-07-23 20:47:22 -0500

    update base processor

--- sdk/include/opentelemetry/sdk/metrics/processor.h
+++ sdk/include/opentelemetry/sdk/metrics/processor.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "opentelemetry/version.h"
+#include "opentelemetry/sdk/metrics/instrument.h"
+#include "opentelemetry/sdk/metrics/record.h"
 
-#include <map>
+#include <unordered_map>
 #include <string>
+
+namespace sdkmetrics = opentelemetry::sdk::metrics;
 
 OPENTELEMETRY_BEGIN_NAMESPACE
 
@@ -10,6 +14,8 @@ namespace sdk
 {
 namespace metrics
 {
+typedef std::tuple<std::string, std::string, std::string,
+          opentelemetry::metrics::InstrumentKind,> instrument_label_tuple;
 
 class Processor
 {
@@ -19,26 +25,40 @@ class Processor
     batch_map_ =  {};
   }
 
-  static Aggregator AggregatorFor(InstrumentType instrument_type)
+  std::vector<sdkmetrics::Record> CheckpointSelf() noexcept
   {
-    if(instrument_type == InstrumentType::Counter || instrument_type == InstrumentType::UpDownCounter)
+    std::vector<sdkmetrics::Record> metric_records;
+    
+    for(auto iter : batch_map_)
     {
-      return CounterAggregator();
-    }
-    else if(instrument_type == InstrumentType::SumObserver || instrument_type == InstrumentType::UpDownSumObserver)
-    {
-      return GaugeAggregator();
-    }
-    else if(instrument_type == InstrumentType::ValueRecorder || instrument_type == InstrumentType::ValueObserver)
-    {
-      return MinMaxSumCountAggreagtor();
+      instrument_label_tuple tuple = iter.first;
+      nostd::string_view name = std::get<0>(tuple);
+      nostd::string_view description = std::get<1>(tuple);
+      std::string labels = std::get<2>(tuple);
+
+      sdkmetrics::Record r(name, description, labels, iter.second);
+      metric_records.push_back(r);
     }
 
+    return metric_records;
   }
+
+  void FinishedCollection() noexcept
+  {
+    if(!stateful_)
+    {
+      batch_map_ = {};
+    }
+  }
+
+  virtual void process(sdkmetrics::Record) noexcept = 0;
+
+
+
 
 private:
   bool stateful_;
-  unordered_map<std::string, std::string> batch_map_;
+  std::unordered_map<instrument_label_tuple, sdkmetrics::AggregatorVariant> batch_map_;
 };
 
 }
