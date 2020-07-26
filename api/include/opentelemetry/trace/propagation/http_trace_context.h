f8bb13668e8306157e05a084e42ad8aff0a088b3
TianlinZhao <tianlinzhao@google.com>
2020-07-26 18:42:22 -0400

    dependency

--- api/include/opentelemetry/trace/propagation/http_trace_context.h
+++ api/include/opentelemetry/trace/propagation/http_trace_context.h
@@ -286,12 +286,12 @@ class HttpTraceContext : public HTTPTextFormat<T> {
             }
             trace::SpanContext context_from_parent_header = ExtractContextFromTraceParent(trace_parent);
             if (!context_from_parent_header.IsValid()) {
+                throw;
                 return context_from_parent_header;
             }
 
             nostd::string_view trace_state_header = getter(carrier, kTraceState);
             if (trace_state_header == "" || trace_state_header.empty()) {
-                throw;
                 return context_from_parent_header;
             }
 
