f90cb5e8d5142001b60f9bcb0701526d9b657bdb
TianlinZhao <tianlinzhao@google.com>
2020-07-26 18:37:51 -0400

    dependency

--- api/include/opentelemetry/trace/propagation/http_trace_context.h
+++ api/include/opentelemetry/trace/propagation/http_trace_context.h
@@ -277,8 +277,9 @@ class HttpTraceContext : public HTTPTextFormat<T> {
 
         static trace::SpanContext ExtractImpl(Getter getter, const T &carrier) {
             nostd::string_view trace_parent = getter(carrier, kTraceParent);
-//            std::map<std::string,std::string> c = carrier;
-//            trace_parent = nostd::string_view(c[std::string(kTraceParent)]);
+            // TODO: ask host why trace_parent's first 8 characters are incorrect
+            std::map<std::string,std::string> c = carrier;
+            trace_parent = nostd::string_view(c[std::string(kTraceParent)]);
             std::cout<<trace_parent<<std::endl;
             if (trace_parent == "") {
                 return trace::SpanContext();
