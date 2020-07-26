8487b993324db16660d7e9983ec7eb0519cee438
TianlinZhao <tianlinzhao@google.com>
2020-07-26 16:53:49 -0400

    dependency

--- api/test/trace/propagation/http_text_format_test.cc
+++ api/test/trace/propagation/http_text_format_test.cc
@@ -19,8 +19,7 @@ using namespace opentelemetry;
 
 static nostd::string_view Getter(const std::map<std::string,std::string> &carrier, nostd::string_view trace_type = "traceparent") {
     std::map<std::string,std::string> c = carrier;
-    nostd::string_view res = nostd::string_view(c[std::string(trace_type)]);
-    return res;
+    return nostd::string_view(c[std::string(trace_type)]);
 }
 
 static void Setter(std::map<std::string,std::string> &carrier, nostd::string_view trace_type = "traceparent", nostd::string_view trace_description = "") {
