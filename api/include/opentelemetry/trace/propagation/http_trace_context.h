7f70bc14827ece9e1ce41dfce4ab1e0fa8c0d08a
TianlinZhao <tianlinzhao@google.com>
2020-07-26 14:24:25 -0400

    dependency

--- api/include/opentelemetry/trace/propagation/http_trace_context.h
+++ api/include/opentelemetry/trace/propagation/http_trace_context.h
@@ -104,7 +104,7 @@ class HttpTraceContext : public HTTPTextFormat<T> {
         // TODO: need review on hex_string because trace ids are objects not string_views
 //        static void InjectImpl(Setter setter, T &carrier) {
         static void InjectImpl(Setter setter, T &carrier, const trace::SpanContext &span_context) {
-            nostd::string_view trace_parent = SpanContextToString(trace::SpanContext &span_context);
+            nostd::string_view trace_parent = SpanContextToString(span_context);
 //            setter(carrier, kTraceParent, trace_parent);
 //            if (span_context.trace_state() != NULL) {
 //                nostd::string_view trace_state = FormatTracestate(span_context.trace_state());
