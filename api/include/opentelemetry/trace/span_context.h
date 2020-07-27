f9de37f848f03baca93bc0b974fd48cba0b2fb57
TianlinZhao <tianlinzhao@google.com>
2020-07-27 00:16:32 -0400

    dependency

--- api/include/opentelemetry/trace/span_context.h
+++ api/include/opentelemetry/trace/span_context.h
@@ -43,7 +43,7 @@ public:
     remote_parent_ = is_remote;
   }
   SpanContext(SpanContext&& ctx) : trace_id_(ctx.trace_id()), span_id_(ctx.span_id()), trace_flags_(ctx.trace_flags()), trace_state_(std::move(ctx.trace_state_)) {}
-  SpanContext(const SpanContext& ctx) : trace_id_(ctx.trace_id()), span_id_(ctx.span_id()), trace_flags_(ctx.trace_flags()), trace_state_(ctx.trace_state_) {}
+  SpanContext(const SpanContext& ctx) : trace_id_(ctx.trace_id()), span_id_(ctx.span_id()), trace_flags_(ctx.trace_flags()), trace_state_(new TraceState(ctx.trace_state())) {}
 
   SpanContext &operator=(const SpanContext &ctx) {
     trace_id_ = ctx.trace_id_;
