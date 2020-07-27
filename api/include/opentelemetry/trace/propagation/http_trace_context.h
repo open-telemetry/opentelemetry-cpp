bf26fbe2cf163c97c472126ee0bca991aa369b43
TianlinZhao <tianlinzhao@google.com>
2020-07-26 20:56:56 -0400

    dependency

--- api/include/opentelemetry/trace/propagation/http_trace_context.h
+++ api/include/opentelemetry/trace/propagation/http_trace_context.h
@@ -213,7 +213,7 @@ class HttpTraceContext : public HTTPTextFormat<T> {
                       return trace::SpanContext();
 //                    return SetSpanInContext(trace::DefaultSpan.GetInvalid(), context);
                 }
-                nostd::span<char> tid{trace_id.data(),trace_id.length()};
+                nostd::span<char> tid{trace_id.data(),trace_id.size()};
 //                nostd::span<char> sid{span_id.begin(),span_id.length()};
 //                nostd::span<char> tfg{trace_flags.begin(),trace_flags.length()};
 //                TraceId trace_id_obj = TraceId(tid);
