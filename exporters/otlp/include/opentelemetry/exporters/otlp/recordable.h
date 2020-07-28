b096d8e72f2ea9c7f70d92bbd8f40a4bd9067581
Nadia Ciobanu <nadiaciobanu@google.com>
2020-07-27 19:04:38 -0700

    Remove default parameters from Recordable interface (#208)

--- exporters/otlp/include/opentelemetry/exporters/otlp/recordable.h
+++ exporters/otlp/include/opentelemetry/exporters/otlp/recordable.h
@@ -21,16 +21,12 @@ public:
   void SetAttribute(nostd::string_view key,
                     const opentelemetry::common::AttributeValue &value) noexcept override;
 
-  void AddEvent(
-      nostd::string_view name,
-      core::SystemTimestamp timestamp = core::SystemTimestamp(std::chrono::system_clock::now()),
-      const trace::KeyValueIterable &attributes =
-          trace::KeyValueIterableView<std::map<std::string, int>>({})) noexcept override;
+  void AddEvent(nostd::string_view name,
+                core::SystemTimestamp timestamp,
+                const trace::KeyValueIterable &attributes) noexcept override;
 
-  void AddLink(
-      opentelemetry::trace::SpanContext span_context,
-      const trace::KeyValueIterable &attributes =
-          trace::KeyValueIterableView<std::map<std::string, int>>({})) noexcept override;
+  void AddLink(opentelemetry::trace::SpanContext span_context,
+               const trace::KeyValueIterable &attributes) noexcept override;
 
   void SetStatus(trace::CanonicalCode code, nostd::string_view description) noexcept override;
 
