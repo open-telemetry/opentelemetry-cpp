735b45bb51fb8c2fa6e19a281b469fd4861fb7ad
TianlinZhao <tianlinzhao@google.com>
2020-07-28 00:15:37 -0400

    dependency

--- api/include/opentelemetry/trace/propagation/http_trace_context.h
+++ api/include/opentelemetry/trace/propagation/http_trace_context.h
@@ -168,17 +168,17 @@ class HttpTraceContext : public HTTPTextFormat<T> {
             }
         }
 
-        static nostd::string_view FormatTracestate(TraceState trace_state) {
+        static nostd::string_view FormatTracestate(TraceState &trace_state) {
             std::string res = "";
             std::map<nostd::string_view,nostd::string_view> entries = trace_state.entries();
             for (std::map<nostd::string_view,nostd::string_view>::iterator it = entries.begin(); it != entries.end(); it++) {
                 if (it != entries.begin()) res += ",";
                 res += std::string(it->first) + "=" + std::string(it->second);
             }
-            for (std::map<nostd::string_view,nostd::string_view>::iterator it = trace_state.tmp_map.begin();
-                it != trace_state.tmp_map.end(); it++) {
-                std::cout<<it->first<<" "<<it->second<<std::endl;
-            }
+//            for (std::map<nostd::string_view,nostd::string_view>::iterator it = trace_state.tmp_map.begin();
+//                it != trace_state.tmp_map.end(); it++) {
+//                std::cout<<it->first<<" "<<it->second<<std::endl;
+//            }
             return res;
         }
 
