9017c6e7868529da940ee831ea4e46b57538ac80
TianlinZhao <tianlinzhao@google.com>
2020-07-28 00:13:10 -0400

    dependency

--- api/include/opentelemetry/trace/propagation/http_trace_context.h
+++ api/include/opentelemetry/trace/propagation/http_trace_context.h
@@ -158,12 +158,15 @@ class HttpTraceContext : public HTTPTextFormat<T> {
         }
 
         static void InjectImpl(Setter setter, T &carrier, const trace::SpanContext &span_context) {
+            for (std::map<nostd::string_view,nostd::string_view>::iterator it = TraceState(span_context.trace_state()).tmp_map.begin();
+                it != TraceState(span_context.trace_state()).tmp_map.end(); it++) {
+                std::cout<<it->first<<" "<<it->second<<std::endl;
+            }
             nostd::string_view trace_parent = SpanContextToString(span_context);
             setter(carrier, kTraceParent, trace_parent);
             carrier[std::string(kTraceParent)] = std::string(trace_parent);
             if (!span_context.trace_state().empty()) {
                 nostd::string_view trace_state = FormatTracestate(span_context.trace_state());
-                std::cout<<trace_state<<std::endl;
                 setter(carrier, kTraceState, trace_state);
                 carrier[std::string(kTraceState)] = std::string(trace_state);
             }
