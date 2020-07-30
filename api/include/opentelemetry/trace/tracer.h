e0c8d5f3e6d3a57efbe1c3465379b8fbbb11c5f2
TianlinZhao <tianlinzhao@google.com>
2020-07-30 19:53:48 -0400

    tracer

--- api/include/opentelemetry/trace/tracer.h
+++ api/include/opentelemetry/trace/tracer.h
@@ -85,5 +85,34 @@ public:
 
   virtual void CloseWithMicroseconds(uint64_t timeout) noexcept = 0;
 };
+class DefaultTracer: public Tracer {
+public:
+  ~DefaultTracer() = default;
+
+  /**
+   * Starts a span.
+   *
+   * Optionally sets attributes at Span creation from the given key/value pairs.
+   *
+   * Attributes will be processed in order, previous attributes with the same
+   * key will be overwritten.
+   */
+  nostd::unique_ptr<Span> StartSpan(nostd::string_view name,
+                                    const KeyValueIterable &attributes,
+                                    const StartSpanOptions &options = {}) noexcept
+  {
+     return DefaultSpan::GetInvalid();
+  }
+
+  void ForceFlushWithMicroseconds(uint64_t timeout) noexcept
+  {
+    pass;
+  }
+
+  void CloseWithMicroseconds(uint64_t timeout) noexcept
+  {
+    pass;
+  }
+};
 }  // namespace trace
 OPENTELEMETRY_END_NAMESPACE
