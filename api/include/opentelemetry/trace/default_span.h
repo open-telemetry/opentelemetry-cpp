f0db27e36651b342b1bded788a4c59a3b2a6ae18
TianlinZhao <tianlinzhao@google.com>
2020-07-30 19:30:46 -0400

    tracer

--- api/include/opentelemetry/trace/default_span.h
+++ api/include/opentelemetry/trace/default_span.h
@@ -8,35 +8,6 @@
 #define pass
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace trace {
-class DefaultTracer: public Tracer {
-public:
-  ~DefaultTracer() = default;
-
-  /**
-   * Starts a span.
-   *
-   * Optionally sets attributes at Span creation from the given key/value pairs.
-   *
-   * Attributes will be processed in order, previous attributes with the same
-   * key will be overwritten.
-   */
-  nostd::unique_ptr<Span> StartSpan(nostd::string_view name,
-                                    const KeyValueIterable &attributes,
-                                    const StartSpanOptions &options = {}) noexcept
-  {
-     return DefaultSpan::GetInvalid();
-  }
-
-  void ForceFlushWithMicroseconds(uint64_t timeout) noexcept
-  {
-    pass;
-  }
-
-  void CloseWithMicroseconds(uint64_t timeout) noexcept
-  {
-    pass;
-  }
-};
 class DefaultSpan: public Span {
   public:
     // Returns an invalid span.
