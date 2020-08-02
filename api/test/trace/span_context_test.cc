0e0d344ebd0ac8ff064ee8e10b6b271d0909c0b6
TianlinZhao <tianlinzhao@google.com>
2020-08-02 15:01:39 -0400

    comply to noexcept requirements

--- api/test/trace/span_context_test.cc
+++ api/test/trace/span_context_test.cc
@@ -6,33 +6,33 @@ using opentelemetry::trace::SpanContext;
 
 TEST(SpanContextTest, IsSampled)
 {
-//  SpanContext s1(true, true);
-//
-//  ASSERT_EQ(s1.IsSampled(), true);
-//
-//  SpanContext s2(false, true);
-//
-//  ASSERT_EQ(s2.IsSampled(), false);
+  SpanContext s1(true, true);
+
+  ASSERT_EQ(s1.IsSampled(), true);
+
+  SpanContext s2(false, true);
+
+  ASSERT_EQ(s2.IsSampled(), false);
 }
 
 TEST(SpanContextTest, HasRemoteParent)
 {
-//  SpanContext s1(true, true);
-//
-//  ASSERT_EQ(s1.HasRemoteParent(), true);
-//
-//  SpanContext s2(true, false);
-//
-//  ASSERT_EQ(s2.HasRemoteParent(), false);
+  SpanContext s1(true, true);
+
+  ASSERT_EQ(s1.HasRemoteParent(), true);
+
+  SpanContext s2(true, false);
+
+  ASSERT_EQ(s2.HasRemoteParent(), false);
 }
 
 TEST(SpanContextTest, TraceFlags)
 {
-//  SpanContext s1(true, true);
-//
-//  ASSERT_EQ(s1.trace_flags().flags(), 1);
-//
-//  SpanContext s2(false, true);
-//
-//  ASSERT_EQ(s2.trace_flags().flags(), 0);
+  SpanContext s1(true, true);
+
+  ASSERT_EQ(s1.trace_flags().flags(), 1);
+
+  SpanContext s2(false, true);
+
+  ASSERT_EQ(s2.trace_flags().flags(), 0);
 }
