95b4341519862af352287f4acb72edc893a91fbc
Nadia Ciobanu <nadiaciobanu@google.com>
2020-08-04 22:33:13 +0000

    Clean up test

--- sdk/test/trace/attribute_utils_test.cc
+++ sdk/test/trace/attribute_utils_test.cc
@@ -1,5 +1,4 @@
 #include "opentelemetry/sdk/trace/attribute_utils.h"
-#include "opentelemetry/trace/key_value_iterable_view.h"
 
 #include <gtest/gtest.h>
 
@@ -13,7 +12,7 @@ TEST(AttributeMapTest, AttributesContruction)
 {
   const int kNumAttributes              = 3;
   std::string keys[kNumAttributes]      = {"attr1", "attr2", "attr3"};
-  int values[kNumAttributes]            = {4, 12, 33};
+  int values[kNumAttributes]            = {15, 24, 37};
   std::map<std::string, int> attributes = {
       {keys[0], values[0]}, {keys[1], values[1]}, {keys[2], values[2]}};
 
