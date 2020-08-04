a4d26756dcd1d95cc869092305fa6df7339dd7e3
erichsueh3 <hsuehe@amazon.com>
2020-08-04 15:42:12 -0700

    Merged ReturnCode into ExportResult, removed return_code.h, removed Shutdown() function not in Metrics specification

--- sdk/include/opentelemetry/sdk/metrics/exporter.h
+++ sdk/include/opentelemetry/sdk/metrics/exporter.h
@@ -1,38 +1,66 @@
 #pragma once
 
-#include <vector>
-
+#include <memory>
 #include "opentelemetry/sdk/metrics/record.h"
-#include "opentelemetry/sdk/metrics/return_code.h"
 #include "opentelemetry/version.h"
-
-/**
- * This class is an interface that defines all basic behaviors of an exporter.
- * This interface should be available to use for metrics exporters, and
- * the design of this interface has been based on the SpanExporter class.
- */
 
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace sdk
 {
 namespace metrics
 {
+/**
+ * ExportResult is returned as result of exporting a batch of records.
+ */
+enum class ExportResult
+{
+  /**
+   * Batch was successfully exported.
+   */
+  kSuccess = 0,
+  /**
+   * Exporting failed. The caller must not retry exporting the same batch; the
+   * batch must be dropped.
+   */
+  kFailure = 1,
+
+  /**
+   * The collection does not have enough space to receive the export batch.
+   */
+  kFailureFull = 2,
+
+  /**
+   * The export has timed out.
+   */
+  kFailureTimeout = 3,
+
+  /**
+   * The export() function was passed an invalid argument.
+   */
+  kFailureInvalidArgument = 4,
+};
+/**
+ * MetricsExporter defines the interface that protocol-specific span exporters must
+ * implement.
+ */
 class MetricsExporter
 {
 public:
-  /**
-   * Exports a batch of Metric Records.
-   * @param records: a collection of records to export
-   * @return: returns a ReturnCode detailing a success, or type of failure
-   */
-  virtual ReturnCode Export(std::vector<Record> &records) noexcept = 0;
+  virtual ~MetricsExporter() = default;
 
   /**
-   * Shuts down the exporter and does cleanup.
+   * Exports a vector of Records. This method must not be called
+   * concurrently for the same exporter instance.
+   * @param records a vector of unique pointers to metric records
    */
-  virtual void Shutdown() noexcept = 0;
+  virtual ExportResult Export(const std::vector<Record> &records) noexcept = 0;
+
+  /**
+   * In the Metrics specification, there is no Shutdown function required for exporters
+   * The Shutdown function can be implemented within exporters that wish to have one,
+   * but will not be enforced through this header
+   */
 };
 }  // namespace metrics
 }  // namespace sdk
-
 OPENTELEMETRY_END_NAMESPACE
