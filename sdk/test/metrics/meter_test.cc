ca5bc47e1b69e50f1d270b0eeb6fbd3375464ac5
Brandon Kimberly <brakmb@amazon.com>
2020-08-05 16:03:43 -0400

    Allocate meters on the stack

--- sdk/test/metrics/meter_test.cc
+++ sdk/test/metrics/meter_test.cc
@@ -1,6 +1,6 @@
 #include "opentelemetry/sdk/metrics/meter.h"
-#include <gtest/gtest.h>
 #include <future>
+#include <gtest/gtest.h>
 
 using namespace opentelemetry::sdk::metrics;
 namespace metrics_api = opentelemetry::metrics;
@@ -10,25 +10,25 @@ OPENTELEMETRY_BEGIN_NAMESPACE
 TEST(Meter, CreateSyncInstruments)
 {
   // Test that there are no errors creating synchronous instruments.
-  auto m = new Meter("Test");
+  Meter m("Test");
 
-  m->NewShortCounter("Test-short-counter", "For testing", "Unitless", true);
-  m->NewIntCounter("Test-int-counter", "For testing", "Unitless", true);
-  m->NewFloatCounter("Test-float-counter", "For testing", "Unitless", true);
-  m->NewDoubleCounter("Test-double-counter", "For testing", "Unitless", true);
+  m.NewShortCounter("Test-short-counter", "For testing", "Unitless", true);
+  m.NewIntCounter("Test-int-counter", "For testing", "Unitless", true);
+  m.NewFloatCounter("Test-float-counter", "For testing", "Unitless", true);
+  m.NewDoubleCounter("Test-double-counter", "For testing", "Unitless", true);
 
-  m->NewShortUpDownCounter("Test-short-ud-counter", "For testing", "Unitless", true);
-  m->NewIntUpDownCounter("Test-int-ud-counter", "For testing", "Unitless", true);
-  m->NewFloatUpDownCounter("Test-float-ud-counter", "For testing", "Unitless", true);
-  m->NewDoubleUpDownCounter("Test-double-ud-counter", "For testing", "Unitless", true);
+  m.NewShortUpDownCounter("Test-short-ud-counter", "For testing", "Unitless", true);
+  m.NewIntUpDownCounter("Test-int-ud-counter", "For testing", "Unitless", true);
+  m.NewFloatUpDownCounter("Test-float-ud-counter", "For testing", "Unitless", true);
+  m.NewDoubleUpDownCounter("Test-double-ud-counter", "For testing", "Unitless", true);
 
-  m->NewShortValueRecorder("Test-short-recorder", "For testing", "Unitless", true);
-  m->NewIntValueRecorder("Test-int-recorder", "For testing", "Unitless", true);
-  m->NewFloatValueRecorder("Test-float-recorder", "For testing", "Unitless", true);
-  m->NewDoubleValueRecorder("Test-double-recorder", "For testing", "Unitless", true);
+  m.NewShortValueRecorder("Test-short-recorder", "For testing", "Unitless", true);
+  m.NewIntValueRecorder("Test-int-recorder", "For testing", "Unitless", true);
+  m.NewFloatValueRecorder("Test-float-recorder", "For testing", "Unitless", true);
+  m.NewDoubleValueRecorder("Test-double-recorder", "For testing", "Unitless", true);
 }
 
-// Dummy functions for asynchronous instrument constructors
+//Dummy functions for asynchronous instrument constructors
 void ShortCallback(metrics_api::ObserverResult<short>) {}
 void IntCallback(metrics_api::ObserverResult<int>) {}
 void FloatCallback(metrics_api::ObserverResult<float>) {}
@@ -37,46 +37,42 @@ void DoubleCallback(metrics_api::ObserverResult<double>) {}
 TEST(Meter, CreateAsyncInstruments)
 {
   // Test that there are no errors when creating asynchronous instruments.
-  auto m = new Meter("Test");
+  Meter m("Test");
 
-  m->NewShortSumObserver("Test-short-sum-obs", "For testing", "Unitless", true, &ShortCallback);
-  m->NewIntSumObserver("Test-int-sum-obs", "For testing", "Unitless", true, &IntCallback);
-  m->NewFloatSumObserver("Test-float-sum-obs", "For testing", "Unitless", true, &FloatCallback);
-  m->NewDoubleSumObserver("Test-double-sum-obs", "For testing", "Unitless", true, &DoubleCallback);
+  m.NewShortSumObserver("Test-short-sum-obs", "For testing", "Unitless", true, &ShortCallback);
+  m.NewIntSumObserver("Test-int-sum-obs", "For testing", "Unitless", true, &IntCallback);
+  m.NewFloatSumObserver("Test-float-sum-obs", "For testing", "Unitless", true, &FloatCallback);
+  m.NewDoubleSumObserver("Test-double-sum-obs", "For testing", "Unitless", true, &DoubleCallback);
 
-  m->NewShortUpDownSumObserver("Test-short-ud-sum-obs", "For testing", "Unitless", true,
-                               &ShortCallback);
-  m->NewIntUpDownSumObserver("Test-int-ud-sum-obs", "For testing", "Unitless", true, &IntCallback);
-  m->NewFloatUpDownSumObserver("Test-float-ud-sum-obs", "For testing", "Unitless", true,
-                               &FloatCallback);
-  m->NewDoubleUpDownSumObserver("Test-double-ud-sum-obs", "For testing", "Unitless", true,
-                                &DoubleCallback);
+  m.NewShortUpDownSumObserver("Test-short-ud-sum-obs", "For testing", "Unitless", true, &ShortCallback);
+  m.NewIntUpDownSumObserver("Test-int-ud-sum-obs", "For testing", "Unitless", true, &IntCallback);
+  m.NewFloatUpDownSumObserver("Test-float-ud-sum-obs", "For testing", "Unitless", true, &FloatCallback);
+  m.NewDoubleUpDownSumObserver("Test-double-ud-sum-obs", "For testing", "Unitless", true, &DoubleCallback);
 
-  m->NewShortValueObserver("Test-short-val-obs", "For testing", "Unitless", true, &ShortCallback);
-  m->NewIntValueObserver("Test-int-val-obs", "For testing", "Unitless", true, &IntCallback);
-  m->NewFloatValueObserver("Test-float-val-obs", "For testing", "Unitless", true, &FloatCallback);
-  m->NewDoubleValueObserver("Test-double-val-obs", "For testing", "Unitless", true,
-                            &DoubleCallback);
+  m.NewShortValueObserver("Test-short-val-obs", "For testing", "Unitless", true, &ShortCallback);
+  m.NewIntValueObserver("Test-int-val-obs", "For testing", "Unitless", true, &IntCallback);
+  m.NewFloatValueObserver("Test-float-val-obs", "For testing", "Unitless", true, &FloatCallback);
+  m.NewDoubleValueObserver("Test-double-val-obs", "For testing", "Unitless", true, &DoubleCallback);
 }
 
 TEST(Meter, CollectSyncInstruments)
 {
   // Verify that the records returned on a call to Collect() are correct for synchronous
   // instruments.
-  auto m = new Meter("Test");
+  Meter m("Test");
 
-  ASSERT_EQ(m->Collect().size(), 0);
+  ASSERT_EQ(m.Collect().size(), 0);
 
-  auto counter = m->NewShortCounter("Test-counter", "For testing", "Unitless", true);
+  auto counter = m.NewShortCounter("Test-counter", "For testing", "Unitless", true);
 
   std::map<std::string, std::string> labels = {{"Key", "Value"}};
   auto labelkv = opentelemetry::trace::KeyValueIterableView<decltype(labels)>{labels};
 
   counter->add(1, labelkv);
 
-  std::vector<Record> res = m->Collect();
-  auto agg_var            = res[0].GetAggregator();
-  auto agg                = opentelemetry::nostd::get<0>(agg_var);
+  std::vector<Record> res = m.Collect();
+  auto agg_var = res[0].GetAggregator();
+  auto agg = opentelemetry::nostd::get<0>(agg_var);
 
   ASSERT_EQ(agg->get_checkpoint()[0], 1);
 
@@ -85,9 +81,9 @@ TEST(Meter, CollectSyncInstruments)
 
   counter->add(10, labelkv);
 
-  res     = m->Collect();
+  res = m.Collect();
   agg_var = res[0].GetAggregator();
-  agg     = opentelemetry::nostd::get<0>(agg_var);
+  agg = opentelemetry::nostd::get<0>(agg_var);
 
   ASSERT_EQ(agg->get_checkpoint()[0], 10);
 }
@@ -97,20 +93,20 @@ TEST(Meter, CollectDeletedSync)
   // Verify that calling Collect() after creating a synchronous instrument and destroying
   // the return pointer does not result in a segfault.
 
-  auto m = new Meter("Test");
+  Meter m("Test");
 
-  ASSERT_EQ(m->Collect().size(), 0);
+  ASSERT_EQ(m.Collect().size(), 0);
 
   std::map<std::string, std::string> labels = {{"Key", "Value"}};
   auto labelkv = opentelemetry::trace::KeyValueIterableView<decltype(labels)>{labels};
   {
-    auto counter = m->NewShortCounter("Test-counter", "For testing", "Unitless", true);
+    auto counter = m.NewShortCounter("Test-counter", "For testing", "Unitless", true);
     counter->add(1, labelkv);
-  }  // counter shared_ptr deleted here
+  } // counter shared_ptr deleted here
 
-  std::vector<Record> res = m->Collect();
-  auto agg_var            = res[0].GetAggregator();
-  auto agg                = opentelemetry::nostd::get<0>(agg_var);
+  std::vector<Record> res = m.Collect();
+  auto agg_var = res[0].GetAggregator();
+  auto agg = opentelemetry::nostd::get<0>(agg_var);
 
   ASSERT_EQ(agg->get_checkpoint()[0], 1);
 }
@@ -127,21 +123,20 @@ TEST(Meter, CollectAsyncInstruments)
 {
   // Verify that the records returned on a call to Collect() are correct for asynchronous
   // instruments.
-  auto m = new Meter("Test");
+  Meter m("Test");
 
-  ASSERT_EQ(m->Collect().size(), 0);
+  ASSERT_EQ(m.Collect().size(), 0);
 
-  auto sumobs =
-      m->NewShortSumObserver("Test-counter", "For testing", "Unitless", true, &ShortCallback);
+  auto sumobs = m.NewShortSumObserver("Test-counter", "For testing", "Unitless", true, &ShortCallback);
 
   std::map<std::string, std::string> labels = {{"Key", "Value"}};
   auto labelkv = opentelemetry::trace::KeyValueIterableView<decltype(labels)>{labels};
 
   sumobs->observe(1, labelkv);
 
-  std::vector<Record> res = m->Collect();
-  auto agg_var            = res[0].GetAggregator();
-  auto agg                = opentelemetry::nostd::get<0>(agg_var);
+  std::vector<Record> res = m.Collect();
+  auto agg_var = res[0].GetAggregator();
+  auto agg = opentelemetry::nostd::get<0>(agg_var);
 
   ASSERT_EQ(agg->get_checkpoint()[0], 1);
 
@@ -150,9 +145,9 @@ TEST(Meter, CollectAsyncInstruments)
 
   sumobs->observe(10, labelkv);
 
-  res     = m->Collect();
+  res = m.Collect();
   agg_var = res[0].GetAggregator();
-  agg     = opentelemetry::nostd::get<0>(agg_var);
+  agg = opentelemetry::nostd::get<0>(agg_var);
 
   ASSERT_EQ(agg->get_checkpoint()[0], 10);
 }
@@ -162,21 +157,20 @@ TEST(Meter, CollectDeletedAsync)
   // Verify that calling Collect() after creating an asynchronous instrument and destroying
   // the return pointer does not result in a segfault.
 
-  auto m = new Meter("Test");
+  Meter m("Test");
 
-  ASSERT_EQ(m->Collect().size(), 0);
+  ASSERT_EQ(m.Collect().size(), 0);
 
   std::map<std::string, std::string> labels = {{"Key", "Value"}};
   auto labelkv = opentelemetry::trace::KeyValueIterableView<decltype(labels)>{labels};
   {
-    auto sumobs =
-        m->NewShortSumObserver("Test-counter", "For testing", "Unitless", true, &Callback);
+    auto sumobs = m.NewShortSumObserver("Test-counter", "For testing", "Unitless", true, &Callback);
     sumobs->observe(1, labelkv);
-  }  // sumobs shared_ptr deleted here
+  } // sumobs shared_ptr deleted here
 
-  std::vector<Record> res = m->Collect();
-  auto agg_var            = res[0].GetAggregator();
-  auto agg                = opentelemetry::nostd::get<0>(agg_var);
+  std::vector<Record> res = m.Collect();
+  auto agg_var = res[0].GetAggregator();
+  auto agg = opentelemetry::nostd::get<0>(agg_var);
 
   ASSERT_EQ(agg->get_checkpoint()[0], 1);
 }
@@ -185,107 +179,112 @@ TEST(Meter, RecordBatch)
 {
   // This tests that RecordBatch appropriately updates the aggregators of the instruments
   // passed to the function. Short, int, float, and double data types are tested.
-  auto m = new Meter("Test");
+  Meter m("Test");
 
-  auto scounter = m->NewShortCounter("Test-scounter", "For testing", "Unitless", true);
-  auto icounter = m->NewIntCounter("Test-icounter", "For testing", "Unitless", true);
-  auto fcounter = m->NewFloatCounter("Test-fcounter", "For testing", "Unitless", true);
-  auto dcounter = m->NewDoubleCounter("Test-dcounter", "For testing", "Unitless", true);
+  auto scounter = m.NewShortCounter("Test-scounter", "For testing", "Unitless", true);
+  auto icounter = m.NewIntCounter("Test-icounter", "For testing", "Unitless", true);
+  auto fcounter = m.NewFloatCounter("Test-fcounter", "For testing", "Unitless", true);
+  auto dcounter = m.NewDoubleCounter("Test-dcounter", "For testing", "Unitless", true);
 
   std::map<std::string, std::string> labels = {{"Key", "Value"}};
   auto labelkv = opentelemetry::trace::KeyValueIterableView<decltype(labels)>{labels};
 
-  metrics_api::SynchronousInstrument<short> *sinstr_arr[] = {scounter.get()};
-  short svalues_arr[]                                     = {1};
+  metrics_api::SynchronousInstrument<short>* sinstr_arr[] =
+      {scounter.get()};
+  short svalues_arr[] = {1};
 
-  nostd::span<metrics_api::SynchronousInstrument<short> *> sinstrs{sinstr_arr};
-  nostd::span<const short, 1> svalues{svalues_arr};
+  nostd::span<metrics_api::SynchronousInstrument<short>*> sinstrs {sinstr_arr};
+  nostd::span<const short, 1> svalues {svalues_arr};
 
-  m->RecordShortBatch(labelkv, sinstrs, svalues);
-  std::vector<Record> res = m->Collect();
-  auto short_agg_var      = res[0].GetAggregator();
-  auto short_agg          = opentelemetry::nostd::get<0>(short_agg_var);
+  m.RecordShortBatch(labelkv, sinstrs, svalues);
+  std::vector<Record> res = m.Collect();
+  auto short_agg_var = res[0].GetAggregator();
+  auto short_agg = opentelemetry::nostd::get<0>(short_agg_var);
   ASSERT_EQ(short_agg->get_checkpoint()[0], 1);
 
-  metrics_api::SynchronousInstrument<int> *iinstr_arr[] = {icounter.get()};
-  int ivalues_arr[]                                     = {1};
+  metrics_api::SynchronousInstrument<int>* iinstr_arr[] =
+      {icounter.get()};
+  int ivalues_arr[] = {1};
 
-  nostd::span<metrics_api::SynchronousInstrument<int> *> iinstrs{iinstr_arr};
-  nostd::span<const int, 1> ivalues{ivalues_arr};
+  nostd::span<metrics_api::SynchronousInstrument<int>*> iinstrs {iinstr_arr};
+  nostd::span<const int, 1> ivalues {ivalues_arr};
 
-  m->RecordIntBatch(labelkv, iinstrs, ivalues);
-  res              = m->Collect();
+  m.RecordIntBatch(labelkv, iinstrs, ivalues);
+  res = m.Collect();
   auto int_agg_var = res[0].GetAggregator();
-  auto int_agg     = opentelemetry::nostd::get<1>(int_agg_var);
+  auto int_agg = opentelemetry::nostd::get<1>(int_agg_var);
   ASSERT_EQ(int_agg->get_checkpoint()[0], 1);
 
-  metrics_api::SynchronousInstrument<float> *finstr_arr[] = {fcounter.get()};
-  float fvalues_arr[]                                     = {1.0};
+  metrics_api::SynchronousInstrument<float>* finstr_arr[] =
+      {fcounter.get()};
+  float fvalues_arr[] = {1.0};
 
-  nostd::span<metrics_api::SynchronousInstrument<float> *> finstrs{finstr_arr};
-  nostd::span<const float, 1> fvalues{fvalues_arr};
+  nostd::span<metrics_api::SynchronousInstrument<float>*> finstrs {finstr_arr};
+  nostd::span<const float, 1> fvalues {fvalues_arr};
 
-  m->RecordFloatBatch(labelkv, finstrs, fvalues);
-  res                = m->Collect();
+  m.RecordFloatBatch(labelkv, finstrs, fvalues);
+  res = m.Collect();
   auto float_agg_var = res[0].GetAggregator();
-  auto float_agg     = opentelemetry::nostd::get<2>(float_agg_var);
+  auto float_agg = opentelemetry::nostd::get<2>(float_agg_var);
   ASSERT_EQ(float_agg->get_checkpoint()[0], 1.0);
 
-  metrics_api::SynchronousInstrument<double> *dinstr_arr[] = {dcounter.get()};
-  double dvalues_arr[]                                     = {1.0};
+  metrics_api::SynchronousInstrument<double>* dinstr_arr[] =
+      {dcounter.get()};
+  double dvalues_arr[] = {1.0};
 
-  nostd::span<metrics_api::SynchronousInstrument<double> *> dinstrs{dinstr_arr};
-  nostd::span<const double, 1> dvalues{dvalues_arr};
+  nostd::span<metrics_api::SynchronousInstrument<double>*> dinstrs {dinstr_arr};
+  nostd::span<const double, 1> dvalues {dvalues_arr};
 
-  m->RecordDoubleBatch(labelkv, dinstrs, dvalues);
-  res                 = m->Collect();
+  m.RecordDoubleBatch(labelkv, dinstrs, dvalues);
+  res = m.Collect();
   auto double_agg_var = res[0].GetAggregator();
-  auto double_agg     = opentelemetry::nostd::get<3>(double_agg_var);
+  auto double_agg = opentelemetry::nostd::get<3>(double_agg_var);
   ASSERT_EQ(double_agg->get_checkpoint()[0], 1.0);
 }
 
 TEST(Meter, DisableCollectSync)
 {
-  auto m                                    = new Meter("Test");
+  Meter m("Test");
   std::map<std::string, std::string> labels = {{"Key", "Value"}};
   auto labelkv = opentelemetry::trace::KeyValueIterableView<decltype(labels)>{labels};
-  auto c       = m->NewShortCounter("c", "", "", false);
+  auto c = m.NewShortCounter("c", "", "", false);
   c->add(1, labelkv);
-  ASSERT_EQ(m->Collect().size(), 0);
+  ASSERT_EQ(m.Collect().size(), 0);
 }
 
 TEST(Meter, DisableCollectAsync)
 {
-  auto m                                    = new Meter("Test");
+  Meter m("Test");
   std::map<std::string, std::string> labels = {{"Key", "Value"}};
   auto labelkv = opentelemetry::trace::KeyValueIterableView<decltype(labels)>{labels};
-  auto c       = m->NewShortValueObserver("c", "", "", false, &ShortCallback);
+  auto c = m.NewShortValueObserver("c", "", "", false, &ShortCallback);
   c->observe(1, labelkv);
-  ASSERT_EQ(m->Collect().size(), 0);
+  ASSERT_EQ(m.Collect().size(), 0);
 }
 
 TEST(MeterStringUtil, IsValid)
 {
 #if __EXCEPTIONS
-  auto m = new Meter("Test");
-  ASSERT_ANY_THROW(m->NewShortCounter("", "Empty name is invalid", " ", true));
-  ASSERT_ANY_THROW(m->NewShortCounter("1a", "Can't begin with a number", " ", true));
-  ASSERT_ANY_THROW(m->NewShortCounter(".a", "Can't begin with punctuation", " ", true));
-  ASSERT_ANY_THROW(m->NewShortCounter(" a", "Can't begin with space", " ", true));
-  ASSERT_ANY_THROW(m->NewShortCounter(
-      "te^ s=%t", "Only alphanumeric ., -, and _ characters are allowed", " ", true));
+  Meter m("Test");
+  ASSERT_ANY_THROW(m.NewShortCounter("", "Empty name is invalid", " ", true));
+  ASSERT_ANY_THROW(m.NewShortCounter("1a", "Can't begin with a number", " ", true));
+  ASSERT_ANY_THROW(m.NewShortCounter(".a", "Can't begin with punctuation", " ", true));
+  ASSERT_ANY_THROW(m.NewShortCounter(" a", "Can't begin with space", " ", true));
+  ASSERT_ANY_THROW(m.NewShortCounter("te^ s=%t", "Only alphanumeric ., -, and _ characters are allowed", " ", true));
 #endif
 }
 
 TEST(MeterStringUtil, AlreadyExists)
 {
 #if __EXCEPTIONS
-  auto m = new Meter("Test");
+  Meter m("Test");
 
-  m->NewShortCounter("a", "First instance of instrument named 'a'", "", true);
-  ASSERT_ANY_THROW(m->NewShortCounter("a", "Second (illegal) instrument named 'a'", "", true));
-  ASSERT_ANY_THROW(m->NewShortSumObserver(
-      "a", "Still illegal even though it is not a short counter", "", true, &ShortCallback));
+  m.NewShortCounter("a", "First instance of instrument named 'a'", "", true);
+  ASSERT_ANY_THROW(m.NewShortCounter("a", "Second (illegal) instrument named 'a'",
+                                      "", true));
+  ASSERT_ANY_THROW(m.NewShortSumObserver("a",
+                                          "Still illegal even though it is not a short counter",
+                                          "", true, &ShortCallback));
 #endif
 }
 OPENTELEMETRY_END_NAMESPACE
