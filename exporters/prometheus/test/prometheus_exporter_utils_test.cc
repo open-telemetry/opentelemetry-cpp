ac4488c298008bc16142bd14afc6099653aa0b10
Cunjun Wang <cunjuwan@amazon.com>
2020-08-07 11:05:14 -0400

    add copyright headers and add a new line at the end of file

--- exporters/prometheus/test/prometheus_exporter_utils_test.cc
+++ exporters/prometheus/test/prometheus_exporter_utils_test.cc
@@ -1,3 +1,19 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <map>
 #include <numeric>
 #include <string>
@@ -89,10 +105,10 @@ metric_sdk::Record get_record(const std::string &type,
                               const std::string &label,
                               std::shared_ptr<metric_sdk::Aggregator<T>> aggregator)
 {
-  std::string name = "test-" + type + "-metric-record-v_" + std::to_string(version) + ".0";
-  std::string desc = "this is a test " + type + " metric record";
-  metric_sdk::Record record(name, desc, label, aggregator);
-  return record;
+std::string name = "test-" + type + "-metric-record-v_" + std::to_string(version) + ".0";
+std::string desc = "this is a test " + type + " metric record";
+metric_sdk::Record record(name, desc, label, aggregator);
+return record;
 }
 
 TEST(PrometheusExporterUtils, TranslateToPrometheusEmptyInputReturnsEmptyCollection)
