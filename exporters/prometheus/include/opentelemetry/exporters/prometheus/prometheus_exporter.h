140efcde01008490e2e28961e708e3e5495a093f
erichsueh3 <hsuehe@amazon.com>
2020-08-07 10:51:36 -0700

    CI formatting, added copyright headers

--- exporters/prometheus/include/opentelemetry/exporters/prometheus/prometheus_exporter.h
+++ exporters/prometheus/include/opentelemetry/exporters/prometheus/prometheus_exporter.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include <memory>
@@ -36,15 +52,16 @@ public:
    * @param records: a collection of records to export
    * @return: returns a ReturnCode detailing a success, or type of failure
    */
-  sdk::metrics::ExportResult Export(const std::vector<sdk::metrics::Record> &records) noexcept override;
+  sdk::metrics::ExportResult Export(
+      const std::vector<sdk::metrics::Record> &records) noexcept override;
 
-/**
- * Shuts down the exporter and does cleanup.
- * Since Prometheus is a pull based interface, 
- * we cannot serve data remaining in the intermediate
- * collection to to client an HTTP request being sent, 
- * so we flush the data.
- */
+  /**
+   * Shuts down the exporter and does cleanup.
+   * Since Prometheus is a pull based interface,
+   * we cannot serve data remaining in the intermediate
+   * collection to to client an HTTP request being sent,
+   * so we flush the data.
+   */
   void Shutdown() noexcept;
 
   /**
