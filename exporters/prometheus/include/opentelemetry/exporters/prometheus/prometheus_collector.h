9019032028369a2d44311263dace28a5e4778a93
erichsueh3 <hsuehe@amazon.com>
2020-08-07 10:41:56 -0700

    CI formatting, added copyright header

--- exporters/prometheus/include/opentelemetry/exporters/prometheus/prometheus_collector.h
+++ exporters/prometheus/include/opentelemetry/exporters/prometheus/prometheus_collector.h
@@ -1,13 +1,29 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
 #include <memory>
 #include <mutex>
 #include <vector>
 
-#include "prometheus_exporter_utils.h"
 #include "opentelemetry/sdk/metrics/record.h"
 #include "prometheus/collectable.h"
 #include "prometheus/metric_family.h"
+#include "prometheus_exporter_utils.h"
 
 namespace prometheus_client = ::prometheus;
 namespace metric_sdk        = opentelemetry::sdk::metrics;
--- exporters/prometheus/src/prometheus_collector.cc
+++ exporters/prometheus/src/prometheus_collector.cc
@@ -1,3 +1,19 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <iostream>
 
 #include "opentelemetry/exporters/prometheus/prometheus_collector.h"
@@ -119,7 +135,8 @@ void PrometheusCollector::AddMetricData(const std::vector<sdk::metrics::Record> 
       {
         // Cannot call non const functions on const Record r
         sdk::metrics::Record c = r;
-        std::cout << "Dropped Record containing invalid aggregator with name: " + c.GetName() << std::endl;
+        std::cout << "Dropped Record containing invalid aggregator with name: " + c.GetName()
+                  << std::endl;
       }
     }
 
