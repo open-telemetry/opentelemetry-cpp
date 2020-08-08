40a71acea2340ed6d1b11fc8e0ec0eeb4a3bd282
Keshav Manghat <keshavmanghat@gmail.com>
2020-08-08 01:46:21 +0000

    Format fixes

--- examples/zpages/zpages_example.cc
+++ examples/zpages/zpages_example.cc
@@ -2,35 +2,36 @@
  * This is a basic example for zpages that helps users get familiar with how to
  * use this feature in OpenTelemetery
  */
+#include <chrono>
 #include <iostream>
 #include <string>
-#include <chrono>
 
-#include "opentelemetry/ext/zpages/zpages.h" // Required file include for zpages
+#include "opentelemetry/ext/zpages/zpages.h"  // Required file include for zpages
 
 using opentelemetry::core::SteadyTimestamp;
 
-int main(int argc, char* argv[]) {
-  
-  /** 
-   * The following line initializes zPages and starts a webserver at 
+int main(int argc, char *argv[])
+{
+
+  /**
+   * The following line initializes zPages and starts a webserver at
    * http://localhost:30000/tracez/ where spans that are created in the application
    * can be viewed.
-   * Note that the webserver is destroyed after the application ends execution. 
+   * Note that the webserver is destroyed after the application ends execution.
    */
   zPages::Initialize();
   auto tracer = opentelemetry::trace::Provider::GetTracerProvider()->GetTracer("");
-  
+
   std::cout << "This example for zPages creates a few types of spans and then "
             << "creates a span every second for the duration of the application"
             << "\n";
-  
+
   // Error span
   std::map<std::string, opentelemetry::common::AttributeValue> attribute_map;
   attribute_map["completed_search_for"] = "Unknown user";
-  tracer->StartSpan("find user",attribute_map)->SetStatus(
-      opentelemetry::trace::CanonicalCode::NOT_FOUND, "User not found");
-  
+  tracer->StartSpan("find user", attribute_map)
+      ->SetStatus(opentelemetry::trace::CanonicalCode::NOT_FOUND, "User not found");
+
   // Long time duration span
   std::map<std::string, opentelemetry::common::AttributeValue> attribute_map2;
   attribute_map2["completed_search_for"] = "John Doe";
@@ -38,17 +39,17 @@ int main(int argc, char* argv[]) {
   start.start_steady_time = SteadyTimestamp(nanoseconds(1));
   opentelemetry::trace::EndSpanOptions end;
   end.end_steady_time = SteadyTimestamp(nanoseconds(1000000000000));
-  tracer->StartSpan("find user",attribute_map2,start)->End(end);
+  tracer->StartSpan("find user", attribute_map2, start)->End(end);
 
-  
   // Running(deadlock) span
   std::map<std::string, opentelemetry::common::AttributeValue> attribute_map3;
   attribute_map3["searching_for"] = "Deleted user";
-  auto running_span = tracer->StartSpan("find user", attribute_map3);
-  
+  auto running_span               = tracer->StartSpan("find user", attribute_map3);
+
   // Create a completed span every second till user stops the loop
   std::cout << "Presss CTRL+C to stop...\n";
-  while(true){
+  while (true)
+  {
     std::this_thread::sleep_for(seconds(1));
     tracer->StartSpan("ping user")->End();
   }
