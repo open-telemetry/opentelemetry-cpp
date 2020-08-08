b4716bdc7320ada40d9b98cad750a80aa8475121
Keshav Manghat <keshavmanghat@gmail.com>
2020-08-08 01:32:54 +0000

    Format fixes

--- ext/include/opentelemetry/ext/zpages/zpages.h
+++ ext/include/opentelemetry/ext/zpages/zpages.h
@@ -1,67 +1,67 @@
 #pragma once
 
 #include <chrono>
-#include <memory>
 #include <iostream>
+#include <memory>
 
 #include "opentelemetry/ext/zpages/tracez_data_aggregator.h"
-#include "opentelemetry/ext/zpages/tracez_processor.h"
 #include "opentelemetry/ext/zpages/tracez_http_server.h"
+#include "opentelemetry/ext/zpages/tracez_processor.h"
 
-#include "opentelemetry/trace/provider.h"
-#include "opentelemetry/sdk/trace/tracer_provider.h"
 #include "opentelemetry/nostd/shared_ptr.h"
+#include "opentelemetry/sdk/trace/tracer_provider.h"
+#include "opentelemetry/trace/provider.h"
 
-using std::chrono::microseconds;
-using opentelemetry::ext::zpages::TracezSpanProcessor;
 using opentelemetry::ext::zpages::TracezDataAggregator;
 using opentelemetry::ext::zpages::TracezHttpServer;
+using opentelemetry::ext::zpages::TracezSpanProcessor;
+using std::chrono::microseconds;
 
-/** 
+/**
  * Wrapper for zPages that initializes all the components required for zPages,
  * and starts the HTTP server in the constructor and ends it in the destructor.
- * The constructor and destructor for this object is private to prevent 
+ * The constructor and destructor for this object is private to prevent
  * creation other than by calling the static function inialize. This follows the
  * meyers singelton pattern and only a single instance of the class is allowed.
  */
-class zPages {
+class zPages
+{
 public:
   /**
-   * This function is called if the user wishes to include zPages in their 
+   * This function is called if the user wishes to include zPages in their
    * application. It creates a static instance of this class.
    */
-  static void Initialize(){
-    static zPages instance;
-  }
-  
+  static void Initialize() { static zPages instance; }
+
 private:
   /**
    * Constructor is responsible for initializing the tracer, tracez processor,
    * tracez data aggregator and the tracez server. The server is also started in
    * constructor.
    */
-  zPages(){
+  zPages()
+  {
     auto tracez_processor_ = std::make_shared<TracezSpanProcessor>();
-    auto tracez_provider_ = 
-      opentelemetry::nostd::shared_ptr<opentelemetry::trace::TracerProvider>(
+    auto tracez_provider_  = opentelemetry::nostd::shared_ptr<opentelemetry::trace::TracerProvider>(
         new opentelemetry::sdk::trace::TracerProvider(tracez_processor_));
 
-    auto tracez_aggregator = std::unique_ptr<TracezDataAggregator>(
-        new TracezDataAggregator(tracez_processor_));
+    auto tracez_aggregator =
+        std::unique_ptr<TracezDataAggregator>(new TracezDataAggregator(tracez_processor_));
 
-    tracez_server_ = std::unique_ptr<TracezHttpServer>
-      (new TracezHttpServer(std::move(tracez_aggregator)));
-      
+    tracez_server_ =
+        std::unique_ptr<TracezHttpServer>(new TracezHttpServer(std::move(tracez_aggregator)));
+
     tracez_server_->start();
 
     opentelemetry::trace::Provider::SetTracerProvider(tracez_provider_);
-    
+
     // Give the server some time to set up to prevent crashes
     std::this_thread::sleep_for(setup_time_);
   }
-  
-  ~zPages(){
-    // shut down the server when the object goes out of scope(at the end of the 
+
+  ~zPages()
+  {
+    // shut down the server when the object goes out of scope(at the end of the
     // program)
     tracez_server_->stop();
   }
