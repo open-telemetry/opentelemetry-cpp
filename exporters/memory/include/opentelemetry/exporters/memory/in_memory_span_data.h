b6b2853f91c56705c2b51f358501d455df031af3
Nick Holbrook <nholbrook@google.com>
2020-08-11 11:26:59 -0400

    Move exporter to header-only implemnetation

--- exporters/memory/include/opentelemetry/exporters/memory/in_memory_span_data.h
+++ exporters/memory/include/opentelemetry/exporters/memory/in_memory_span_data.h
@@ -16,11 +16,30 @@ namespace memory
 class InMemorySpanData final
 {
 public:
-  InMemorySpanData(size_t);
+  InMemorySpanData(size_t buffer_size) : spans_received_(buffer_size) {}
 
-  void Add(std::unique_ptr<opentelemetry::sdk::trace::Recordable> data) noexcept;
+  void Add(std::unique_ptr<opentelemetry::sdk::trace::Recordable> data) noexcept
+  {
+    std::unique_ptr<opentelemetry::sdk::trace::SpanData> span_data(static_cast<opentelemetry::sdk::trace::SpanData *>(data.release()));
+    spans_received_.Add(span_data);
+  }
 
-  std::vector<std::unique_ptr<opentelemetry::sdk::trace::SpanData>> GetSpans() noexcept;
+  std::vector<std::unique_ptr<opentelemetry::sdk::trace::SpanData>> GetSpans() noexcept
+  {
+	std::vector<std::unique_ptr<opentelemetry::sdk::trace::SpanData>> res;
+
+	spans_received_.Consume(
+	    spans_received_.size(), [&](opentelemetry::sdk::common::CircularBufferRange<opentelemetry::sdk::common::AtomicUniquePtr<opentelemetry::sdk::trace::SpanData>> range) noexcept {
+	      range.ForEach([&](opentelemetry::sdk::common::AtomicUniquePtr<opentelemetry::sdk::trace::SpanData> & ptr) noexcept {
+	        std::unique_ptr<opentelemetry::sdk::trace::SpanData> swap_ptr = std::unique_ptr<opentelemetry::sdk::trace::SpanData>(nullptr);
+	        ptr.Swap(swap_ptr);
+	        res.push_back(std::unique_ptr<opentelemetry::sdk::trace::SpanData>(swap_ptr.release()));
+	        return true;
+	      });
+	    });
+
+	return res;
+  }
 
 private:
   opentelemetry::sdk::common::CircularBuffer<opentelemetry::sdk::trace::SpanData> spans_received_;
--- exporters/memory/include/opentelemetry/exporters/memory/in_memory_span_exporter.h
+++ exporters/memory/include/opentelemetry/exporters/memory/in_memory_span_exporter.h
@@ -21,17 +21,38 @@ namespace memory
 class InMemorySpanExporter final : public opentelemetry::sdk::trace::SpanExporter
 {
 public:
-  InMemorySpanExporter(size_t = MAX_BUFFER_SIZE);
+  InMemorySpanExporter(size_t buffer_size = MAX_BUFFER_SIZE)
+    : data_(new opentelemetry::exporter::memory::InMemorySpanData(buffer_size))
+  {}
 
-  std::unique_ptr<sdk::trace::Recordable> MakeRecordable() noexcept override;
+  std::unique_ptr<sdk::trace::Recordable> MakeRecordable() noexcept override
+  {
+    return std::unique_ptr<sdk::trace::Recordable>(new sdk::trace::SpanData);
+  }
 
   sdk::trace::ExportResult Export(
-      const nostd::span<std::unique_ptr<sdk::trace::Recordable>> &recordables) noexcept override;
+      const nostd::span<std::unique_ptr<sdk::trace::Recordable>> &recordables) noexcept override
+  {
+    for (auto &recordable : recordables)
+    {
+      auto span = std::unique_ptr<sdk::trace::SpanData>(
+      static_cast<sdk::trace::SpanData *>(recordable.release()));
+      if (span != nullptr)
+      {
+          data_.get()->Add(std::move(span));
+      }
+    }
+
+    return sdk::trace::ExportResult::kSuccess;
+  }
 
   void Shutdown(
       std::chrono::microseconds timeout = std::chrono::microseconds(0)) noexcept override{};
 
-  std::shared_ptr<opentelemetry::exporter::memory::InMemorySpanData> GetData() noexcept;
+  std::shared_ptr<opentelemetry::exporter::memory::InMemorySpanData> GetData() noexcept
+  {
+    return data_;
+  }
 
 private:
   std::shared_ptr<opentelemetry::exporter::memory::InMemorySpanData> data_;
