5fe65b6f471d3f446bb33892ceb9417efcbcb692
Tianlin Zhao <tianlinzhao@google.com>
2020-08-12 16:09:40 -0400

    change in tokens

--- api/include/opentelemetry/trace/default_span.h
+++ api/include/opentelemetry/trace/default_span.h
@@ -44,6 +44,8 @@ public:
 
   nostd::string_view ToString() { return "DefaultSpan"; }
 
+  void SetToken(nostd::unique_ptr<context::Token> &&token) {}
+
   DefaultSpan() = default;
 
   DefaultSpan(SpanContext span_context) { this->span_context_ = span_context; }
