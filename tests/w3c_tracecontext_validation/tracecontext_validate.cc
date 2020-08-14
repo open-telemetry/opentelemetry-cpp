cfa95f158d7893c5b6eb872e193b9279d06a7b12
Tianlin Zhao <tianlinzhao@google.com>
2020-08-14 14:54:54 -0400

    Merge branch 'master' of https://github.com/open-telemetry/opentelemetry-cpp into origin/w3ctest
    # Conflicts:
    #	api/include/opentelemetry/context/context_value.h
    #	api/include/opentelemetry/plugin/tracer.h
    #	api/include/opentelemetry/trace/span.h
    #	api/include/opentelemetry/trace/tracer.h

--- tests/w3c_tracecontext_validation/tracecontext_validate.cc
+++ tests/w3c_tracecontext_validation/tracecontext_validate.cc
@@ -1,40 +1,40 @@
 #include "tracecontext_client.h"
 #include "tracecontext_server.h"
 
+#include <algorithm>
 #include <atomic>
 #include <fstream>
 #include <iostream>
 #include <mutex>
 #include <string>
 #include <vector>
-#include <algorithm>
 #include "curl/curl.h"
 
-namespace HTTP_SERVER_NS {
+namespace HTTP_SERVER_NS
+{
 
-int main() {
-    atomic<uint32_t> reqCount{0};
+int main()
+{
+  atomic<uint32_t> reqCount{0};
 
-    HttpServer server("localhost", 5000);
-    // curl_global_init(CURL_GLOBAL_ALL);
-    //HttpClients clients = HttpClients(); // Initialize libcurl
+  HttpServer server("localhost", 5000);
+  // curl_global_init(CURL_GLOBAL_ALL);
+  // HttpClients clients = HttpClients(); // Initialize libcurl
 
-    HttpRequestCallback httpCallback{[&](HttpRequest const& req, HttpResponse& resp) {
-        return 200;
-    }};
-    server["/test"] = httpCallback;
+  HttpRequestCallback httpCallback{[&](HttpRequest const &req, HttpResponse &resp) { return 200; }};
+  server["/test"] = httpCallback;
 
-    // Start server
-    server.start();
+  // Start server
+  server.start();
 
-    // Wait for console input
-    cout << "Presss <ENTER> to stop...\n";
-    cin.get();
+  // Wait for console input
+  cout << "Presss <ENTER> to stop...\n";
+  cin.get();
 
-    // Stop server
-    server.stop();
-    cout << "Server stopped.\n";
-    //clients.~HttpClients();
+  // Stop server
+  server.stop();
+  cout << "Server stopped.\n";
+  // clients.~HttpClients();
 }
 
-}
+}  // namespace HTTP_SERVER_NS
