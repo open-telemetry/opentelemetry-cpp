49cc8ebfee9b7c75426f876d742babac8cd92f53
Tianlin Zhao <tianlinzhao@google.com>
2020-08-14 13:54:14 -0400

    w3c tests

--- tests/w3c_tracecontext_validation/tracecontext_client.h
+++ tests/w3c_tracecontext_validation/tracecontext_client.h
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <curl\curl.h>
+#include <curl.h>
 #include <stdio.h>
 #include <iterator>
 #include <string>
