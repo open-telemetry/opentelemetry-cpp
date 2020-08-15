eb9f3e595e0c44b5eb81f1ad18a12191a94473df
Tianlin Zhao <tianlinzhao@google.com>
2020-08-15 15:16:11 -0400

    fix bugs

--- api/include/opentelemetry/trace/propagation/http_trace_context.h
+++ api/include/opentelemetry/trace/propagation/http_trace_context.h
@@ -374,7 +374,7 @@ private:
     {
       if (ctr_pos != -1)
       {
-        key = trace_state_header.substr(start_pos, ctr_pos - start_pos + 1);
+        key = trace_state_header.substr(start_pos, ctr_pos - start_pos);
         val = trace_state_header.substr(ctr_pos + 1, end_pos - ctr_pos);
         std::cout<<"key: "<<key<<" val: "<<val<<std::endl;
         if (key != "") {
