3a5425bf1d93940f1d79c5240e8ce2c12b2b7afe
Tianlin Zhao <tianlinzhao@google.com>
2020-08-15 16:15:52 -0400

    fix bugs

--- api/test/trace/propagation/http_text_format_test.cc
+++ api/test/trace/propagation/http_text_format_test.cc
@@ -61,20 +61,20 @@ TEST(HTTPTextFormatTest, TraceFlagsBufferGeneration)
   EXPECT_EQ(MapHttpTraceContext::GenerateTraceFlagsFromString("00"), trace::TraceFlags());
 }
 
-//TEST(HTTPTextFormatTest, HeadersWithTraceState)
-//{
-//  const std::map<std::string, std::string> carrier = {
-//      {"traceparent", "00-4bf92f3577b34da6a3ce929d0e0e4736-0102030405060708-01"},
-//      {"tracestate", "congo=congosSecondPosition,rojo=rojosFirstPosition"}};
-//  context::Context ctx1 =
-//      context::Context("current-span", nostd::shared_ptr<trace::Span>(new trace::DefaultSpan()));
-//  context::Context ctx2                 = format.Extract(Getter, carrier, ctx1);
-//  std::map<std::string, std::string> c2 = {};
-//  format.Inject(Setter, c2, ctx2);
-//  EXPECT_EQ(c2["traceparent"], "00-4bf92f3577b34da6a3ce929d0e0e4736-0102030405060708-01");
-//  EXPECT_EQ(c2["tracestate"], "congo=congosSecondPosition,rojo=rojosFirstPosition");
-//  EXPECT_EQ(carrier.size(), c2.size());
-//}
+TEST(HTTPTextFormatTest, HeadersWithTraceState)
+{
+  const std::map<std::string, std::string> carrier = {
+      {"traceparent", "00-4bf92f3577b34da6a3ce929d0e0e4736-0102030405060708-01"},
+      {"tracestate", "congo=congosSecondPosition,rojo=rojosFirstPosition"}};
+  context::Context ctx1 =
+      context::Context("current-span", nostd::shared_ptr<trace::Span>(new trace::DefaultSpan()));
+  context::Context ctx2                 = format.Extract(Getter, carrier, ctx1);
+  std::map<std::string, std::string> c2 = {};
+  format.Inject(Setter, c2, ctx2);
+  EXPECT_EQ(c2["traceparent"], "00-4bf92f3577b34da6a3ce929d0e0e4736-0102030405060708-01");
+  EXPECT_EQ(c2["tracestate"], "congo=congosSecondPosition,rojo=rojosFirstPosition");
+  EXPECT_EQ(carrier.size(), c2.size());
+}
 
 TEST(HTTPTextFormatTest, NoTraceParentHeader)
 {
