7cb796ce8f0db40c901710b37995cb94d6508101
Tianlin Zhao <tianlinzhao@google.com>
2020-08-16 22:02:12 -0400

    w3ctest

--- ext/include/opentelemetry/ext/tracecontext_validation/tracecontext_server.h
+++ ext/include/opentelemetry/ext/tracecontext_validation/tracecontext_server.h
@@ -206,7 +206,7 @@ private:
     curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, -1);
     std::cout<<"pull 3"<<std::endl;
     res = curl_easy_perform(curl); /* ignores error */
-    if (res = CURLE_OK) {
+    if (res = CURLE_OK || res == 0) {
         std::cout<<"message delivered"<<std::endl;
     } else {
         std::cout<<"message not delivered, code "<<res<<std::endl;
