7cc367241801e4aaa15db50abde023b1831bcfe6
Tianlin Zhao <tianlinzhao@google.com>
2020-08-16 22:41:11 -0400

    w3ctest

--- ext/include/opentelemetry/ext/tracecontext_validation/tracecontext_server.h
+++ ext/include/opentelemetry/ext/tracecontext_validation/tracecontext_server.h
@@ -196,13 +196,13 @@ private:
     std::cout<<"pull 1"<<std::endl;
     CURL *curl;
     CURLcode res;
-    char *name  = curl_easy_escape(curl, arguments->name.c_str(), 0);
-    char *value = curl_easy_escape(curl, arguments->value.c_str(), 0);
+    char *name  = curl_easy_escape(curl, arguments->name.get(), 0);
+    char *value = curl_easy_escape(curl, arguments->value.get(), 0);
     std::string fields = std::string(name) + "=" + std::string(value);
 
-    std::cout<<"pull 2 - url is: "<<(arguments->url)<<std::endl;
+    std::cout<<"pull 2 - url is: "<<(arguments->url).get()<<std::endl;
     curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_URL, arguments->url.c_str());
+    curl_easy_setopt(curl, CURLOPT_URL, arguments->url);
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, fields.c_str());
     curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, -1);
     std::cout<<"pull 3"<<std::endl;
