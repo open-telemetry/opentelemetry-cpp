71036ded4467df8350ee6426be9cab659d9d7f83
Tianlin Zhao <tianlinzhao@google.com>
2020-08-16 21:29:27 -0400

    w3ctest

--- ext/include/opentelemetry/ext/tracecontext_validation/tracecontext_server.h
+++ ext/include/opentelemetry/ext/tracecontext_validation/tracecontext_server.h
@@ -91,6 +91,7 @@ private:
 
   bool ParseBody(std::string content, std::vector<std::map<std::string, std::string>> &send_list)
   {
+    std::cout<<content<<std::endl;
     char const *begin = content.c_str();
     char const *ptr   = begin;
     while (*ptr == ' ')
