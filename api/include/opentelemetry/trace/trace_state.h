78db0ecea5c50ec65b15b48c739877d160f82f45
Tianlin Zhao <tianlinzhao@google.com>
2020-08-17 12:05:01 -0400

    w3ctest

--- api/include/opentelemetry/trace/trace_state.h
+++ api/include/opentelemetry/trace/trace_state.h
@@ -96,7 +96,7 @@ public:
     nostd::unique_ptr<const char[]> CopyStringToPointer(nostd::string_view str)
     {
       nostd::unique_ptr<char[]> temp(new char[str.size() + 1]);
-      strncpy(temp.get(), str.data(), str.size());
+      memcpy(temp.get(), str.data(), str.size());
       temp.get()[str.size()] = '\0';
       return nostd::unique_ptr<const char[]>(temp.release());
     }
