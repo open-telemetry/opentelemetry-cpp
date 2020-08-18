7cf81ad7c06e0681e90ee72357b363cf952c9512
Tianlin Zhao <tianlinzhao@google.com>
2020-08-17 21:23:25 -0400

    w3ctest

--- ext/include/opentelemetry/ext/tracecontext_validation/tracecontext_server.h
+++ ext/include/opentelemetry/ext/tracecontext_validation/tracecontext_server.h
@@ -210,7 +210,7 @@ private:
     format.Inject(Setter, carrier, context);
 
     FormHeader(chunk, carrier);
-    curl_slist_append(chunk,"Thisistesting: 0001");
+    curl_slist_append(chunk,"Accept: guiwu");
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_POST, 1);
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, value.c_str());
