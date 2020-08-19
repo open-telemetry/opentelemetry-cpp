d26c6d28b144c5286a1523f6372995412833e6e2
Tianlin Zhao <tianlinzhao@google.com>
2020-08-19 17:11:59 -0400

    w3c test update

--- api/include/opentelemetry/trace/span_context.h
+++ api/include/opentelemetry/trace/span_context.h
@@ -42,7 +42,7 @@ public:
   SpanContext(TraceId trace_id,
               SpanId span_id,
               TraceFlags trace_flags,
-              TraceState &trace_state,
+              TraceState trace_state,
               bool has_remote_parent) noexcept
   {
     trace_id_.reset(&trace_id);
@@ -96,8 +96,9 @@ public:
     return SpanContext(TraceId::GetRandom(), SpanId::GetRandom(), TraceFlags::GetRandom(), TraceState(), true);
   }
 
-  static SpanContext UpdateSpanId(SpanContext span_context) {
-    return SpanContext(span_context.trace_id(), SpanId::GetRandom(), TraceFlags::GetRandom(), span_context.trace_state(),span_context.HasRemoteParent());
+  static SpanContext UpdateSpanId(SpanContext &span_context) {
+    return SpanContext(span_context, true);
+//    return SpanContext(span_context.trace_id(), SpanId::GetRandom(), TraceFlags::GetRandom(), span_context.trace_state(),span_context.HasRemoteParent());
   }
 
   bool IsSampled() const noexcept { return trace_flags_.IsSampled(); }
@@ -108,6 +109,16 @@ private:
   nostd::unique_ptr<TraceFlags> trace_flags_;
   nostd::unique_ptr<TraceState> trace_state_;  // Never nullptr.
   bool remote_parent_ = false;
+
+  SpanContext(SpanContext &span_context, bool update_span) {
+    if (update_span) {
+      trace_id_.reset(span_context.trace_id_.get());
+      span_id_.reset(span_context.span_id_.get());
+      trace_flags_.reset(span_context.trace_flags_.get());
+      trace_state_.reset(span_context.trace_state_.get());
+      remote_parent_ = span_context.remote_parent_;
+    }
+  }
 };
 
 }  // namespace trace
