dc13cbfd1d90701f4f803b5db6ba2ede1bfb6bd7
Tianlin Zhao <tianlinzhao@google.com>
2020-08-19 18:52:56 -0400

    w3c test update

--- api/include/opentelemetry/trace/trace_state.h
+++ api/include/opentelemetry/trace/trace_state.h
@@ -62,6 +62,20 @@ public:
       return *this;
     }
 
+    bool operator==(const TraceState &that) const noexcept {
+      for (const auto &entry: that.Entries()) {
+        nostd::string value;
+        Get(entry.GetKey(),value);
+        if (entry.GetValue() != value) return false;
+      }
+      for (const auto &entry: Entries()) {
+        nostd::string value;
+        that.Get(entry.GetKey(),value);
+        if (entry.GetValue() != value) return false;
+      }
+      return true;
+    }
+
     // Move contructor and assignment operator
     Entry(Entry &&other) = default;
     Entry &operator=(Entry &&other) = default;
