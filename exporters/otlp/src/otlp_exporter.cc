1a5b0a0c4a4398958fab04f3a4de3ef39a87539e
Nadia Ciobanu <nadiaciobanu@google.com>
2020-08-25 01:17:43 +0000

    Fix memory issue

--- exporters/otlp/src/otlp_exporter.cc
+++ exporters/otlp/src/otlp_exporter.cc
@@ -45,7 +45,7 @@ std::unique_ptr<proto::collector::trace::v1::TraceService::Stub> MakeServiceStub
 OtlpExporter::OtlpExporter() : OtlpExporter(OtlpExporterOptions()) {}
 
 OtlpExporter::OtlpExporter(OtlpExporterOptions options)
-    : options_(std::move(options)), trace_service_stub_(MakeServiceStub(options.endpoint))
+    : options_(options), trace_service_stub_(MakeServiceStub(options.endpoint))
 {}
 
 OtlpExporter::OtlpExporter(
