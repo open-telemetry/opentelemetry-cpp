3fb318278ab6629d0ec94666eb2945fc83d4f1ac
Tianlin Zhao <tianlinzhao@google.com>
2020-08-25 13:44:57 -0400

    resolving conflicts

--- api/include/opentelemetry/trace/span_context.h
+++ api/include/opentelemetry/trace/span_context.h
@@ -81,7 +81,6 @@ public:
 
   SpanContext &operator=(const SpanContext &ctx)
   {
-    free(this);
     SpanContext *spn_ctx =
         new SpanContext(ctx.trace_id(), ctx.span_id(), ctx.trace_flags(), ctx.HasRemoteParent());
     return *spn_ctx;
@@ -89,7 +88,6 @@ public:
 
   SpanContext &operator=(SpanContext &&ctx)
   {
-    free(this);
     SpanContext *spn_ctx =
         new SpanContext(ctx.trace_id(), ctx.span_id(), ctx.trace_flags(), ctx.HasRemoteParent());
     return *spn_ctx;
