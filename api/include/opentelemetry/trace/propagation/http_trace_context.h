431ee664fa521fefc836d0ad72704abdac2cab32
Tianlin Zhao <tianlinzhao@google.com>
2020-08-25 12:41:32 -0400

    resolving conflicts

--- api/include/opentelemetry/trace/propagation/http_trace_context.h
+++ api/include/opentelemetry/trace/propagation/http_trace_context.h
@@ -94,18 +94,18 @@ public:
   static TraceId GenerateTraceIdFromString(nostd::string_view trace_id)
   {
     int trace_id_len = kHeaderElementLengths[1];
-    uint8_t buf[trace_id_len / 2];
+    uint8_t buf[16];
     uint8_t *b_ptr = buf;
-    GenerateHexFromString(trace_id, kHeaderElementLengths[1], b_ptr);
+    GenerateHexFromString(trace_id, trace_id_len, b_ptr);
     return TraceId(buf);
   }
 
   static SpanId GenerateSpanIdFromString(nostd::string_view span_id)
   {
     int span_id_len = kHeaderElementLengths[2];
-    uint8_t buf[span_id_len / 2];
+    uint8_t buf[8];
     uint8_t *b_ptr = buf;
-    GenerateHexFromString(span_id, kHeaderElementLengths[2], b_ptr);
+    GenerateHexFromString(span_id, span_id_len, b_ptr);
     return SpanId(buf);
   }
 
