fb2c087e38eb8ee476b388bf0fa925b250050579
Max Golovanov <maxgolov@microsoft.com>
2020-08-28 03:30:25 -0700

    Add json.hpp to CI and Build Tools

--- ci/setup_cmake.sh
+++ ci/setup_cmake.sh
@@ -4,13 +4,15 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install --no-install-recommends --no-install-suggests -y \
+apt-get install --ignore-missing --no-install-recommends --no-install-suggests -y \
                 cmake \
                 libbenchmark-dev \
                 libgtest-dev \
                 zlib1g-dev \
                 sudo \
-                libcurl4-openssl-dev
+                libcurl4-openssl-dev \
+                nlohmann-json-dev \
+                nlohmann-json3-dev
 
 # Follows these instructions for setting up gtest
 # https://www.eriksmistad.no/getting-started-with-google-test-on-ubuntu/
