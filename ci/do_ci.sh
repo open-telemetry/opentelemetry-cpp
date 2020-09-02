99ca914057bdfac30b8c3a92fd6e60fe4ce84431
erichsueh3 <hsuehe@amazon.com>
2020-09-02 11:35:44 -0700

    removed usage of release9.0 of Prometheus

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -84,7 +84,23 @@ elif [[ "$1" == "cmake.c++20.stl.test" ]]; then
   make
   make test
   exit 0
-elif [[ "$1" == "cmake.legacy.test" ]]; then
+elif [[ "$1" == "cmake.exporter.prometheus.test" ]]; then
+#  export DEBIAN_FRONTEND=noninteractive
+#  apt-get update
+#  apt-get install sudo
+#  apt-get install zlib1g-dev
+#  apt-get -y install libcurl4-openssl-dev
+  cd third_party
+  git clone https://github.com/jupp0r/prometheus-cpp
+  cd prometheus-cpp
+  # git checkout v0.9.0
+  git submodule init
+  git submodule update
+  mkdir _build && cd _build
+  cmake .. -DBUILD_SHARED_LIBS=ON
+  make -j 4
+  sudo make install
+
   cd "${BUILD_DIR}"
   rm -rf *
   export BUILD_ROOT="${BUILD_DIR}"
