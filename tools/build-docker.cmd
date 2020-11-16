64bd7158e7399b02f16835e482e07c2293f1c773
Max Golovanov <maxgolov@microsoft.com>
2020-11-15 23:19:45 -0800

    Allow building with Docker

--- tools/build-docker.cmd
+++ tools/build-docker.cmd
@@ -1,12 +1,35 @@
 @echo off
 pushd %~dp0
+REM Default arguments
+set A1="/build/tools/build.sh"
+set A2=clean
 if "%~1"=="" goto help
 
+REM Image name is always the first argument
+set IMAGE_NAME=%1
+
+REM Process other optional arguments
+shift
+if NOT "%1"=="" (
+  set A1=%1
+  set A2=%2
+  set A3=%3
+  set A4=%4
+  set A5=%5
+  set A6=%6
+  set A7=%7
+  set A8=%8
+  set A9=%9
+  set A10=%10
+)
+
 WHERE choco >NUL 2>NUL
-IF %ERRORLEVEL% NEQ 0 call tools\setup-choco.cmd
+if %ERRORLEVEL% NEQ 0 (
+  echo This script requires Chocolatey to install Docker: https://chocolatey.org/
+)
 
 WHERE docker >NUL 2>NUL
-IF "%ERRORLEVEL%"=="0" goto docker_ok
+if "%ERRORLEVEL%"=="0" goto docker_ok
 choco install -y docker-desktop
 choco install -y docker-cli
 :docker_ok
@@ -14,19 +37,19 @@ choco install -y docker-cli
 docker info
 docker version
 
-set IMAGE_NAME=%1
 echo Running in container %IMAGE_NAME%
 sc query com.docker.service
 
-cd ..
-
 REM Force reinstallation of build tools
-del .buildtools 2>NUL
-echo Building docker image...
-docker build --rm -t %IMAGE_NAME% docker/%IMAGE_NAME%
+del ..\.buildtools 2>NUL
+echo Building docker image in %CD%...
+copy /Y setup-cmake.sh ..\docker\%IMAGE_NAME%\
+copy /Y setup-protobuf.sh ..\docker\%IMAGE_NAME%\
+docker build --rm -t %IMAGE_NAME% ../docker/%IMAGE_NAME%
 
+cd ..
 echo Starting build...
-docker run -it -v %CD%:/build %IMAGE_NAME% "/build/tools/build.sh" clean
+docker run -it -v %CD%:/build %IMAGE_NAME% %A1% %A2%
 
 popd
 exit
@@ -34,8 +57,13 @@ exit
 :help
 cd ..
 echo.
-echo Usage: build-docker.cmd [container_name]
+echo Usage: build-docker.cmd [image_name] [arguments...]
+echo. 
+echo Default command:
+echo. 
+echo  docker run -it -v $WORKSPACE_ROOT:/build IMAGE_NAME %A1% %A2%
 echo.
-echo Supported containers:
+echo Supported images:
+echo =================
 dir /B docker
 popd
