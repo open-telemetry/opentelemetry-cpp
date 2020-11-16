71136a58b44bbdcb828e7160f4e1acdb7a25848a
Max Golovanov <maxgolov@microsoft.com>
2020-11-15 23:20:56 -0800

    Add Windows build tools

--- tools/build-vs2015.cmd
+++ tools/build-vs2015.cmd
@@ -1,6 +1,11 @@
+REM Build with Visual Studio 2015
 set "VS_TOOLS_VERSION=vs2015"
-REM Ref. https://cmake.org/cmake/help/latest/generator/Visual%20Studio%2014%202015.html
-set "CMAKE_GEN=Visual Studio 14 2015 Win64"
+set ARCH=Win64
+if NOT "%1"=="" (
+  set ARCH=%1
+)
+set "CMAKE_GEN=Visual Studio 14 2015 %ARCH%"
 cd %~dp0
 call setup-buildtools.cmd
+REM TODO: currently we cannot build without Abseil variant for Visual Studio 2015
 call build.cmd -DWITH_ABSEIL:BOOL=ON
--- tools/setup-buildtools.cmd
+++ tools/setup-buildtools.cmd
@@ -1,12 +1,22 @@
 @echo off
-set "PATH=%PATH%;%~dp0;%~dp0\vcpkg"
+set "PATH=C:\Program Files\CMake\bin;%~dp0;%~dp0\vcpkg;%PATH%"
 pushd %~dp0
 
-REM Fail if chocolatey is not installed
-where /Q choco
-if ERRORLEVEL 1 (
-  echo This script requires chocolatey. Installation instructions: https://chocolatey.org/docs/installation
-  exit -1
+net session >nul 2>&1
+if %errorLevel% == 0 (
+  echo Running with Administrative privilege...
+  REM Fail if chocolatey is not installed
+  where /Q choco
+  if ERRORLEVEL 1 (
+    echo This script requires chocolatey. Installation instructions: https://chocolatey.org/docs/installation
+    exit -1
+  )
+  REM Install tools needed for building, but only if not installed yet
+  where /Q vswhere || choco install -y vswhere
+  where /Q cmake || choco install -y cmake
+  where /Q git || choco install -y git
+) else (
+  echo Running without Administrative privilege...
 )
 
 REM Print current Visual Studio installations detected
@@ -15,13 +25,6 @@ if ERRORLEVEL 0 (
   echo Visual Studio installations detected:
   vswhere -property installationPath
 )
-
-REM Install tools needed for building stuff
-choco install -y cmake
-choco install -y svn
-choco install -y git
-choco install -y llvm
-choco install -y zip
 
 REM Try to autodetect Visual Studio
 call "%~dp0\vcvars.cmd"
@@ -37,14 +40,16 @@ if ERRORLEVEL 1 (
   pushd .\vcpkg
   call bootstrap-vcpkg.bat
   popd
+) else (
+  echo Using existing vcpkg installation...
 )
 
-REM Install it
+REM Install dependencies
 vcpkg install gtest:x64-windows
 vcpkg install --head --overlay-ports=%~dp0\ports benchmark:x64-windows
 vcpkg install ms-gsl:x64-windows
 vcpkg install nlohmann-json:x64-windows
 vcpkg install abseil:x64-windows
-vcpkg integrate install
+vcpkg install protobuf:x64-windows
 popd
 exit /b 0
