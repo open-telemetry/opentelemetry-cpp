2568485d329827ece0dc14e28c5df3365cbfea0a
Lalit Kumar Bhasin <labhas@microsoft.com>
2020-11-19 01:37:33 +0530

    remove all files for http_server

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -169,7 +169,8 @@ elif [[ "$1" == "code.coverage" ]]; then
   make
   make test
   lcov --directory $PWD --capture --output-file coverage.info
-  lcov --remove coverage.info '*/ext/http/server/http_server.h'> tmp_coverage.info 2>/dev/null
+  # removing test http server coverage from the total coverage. We don't use this server completely.
+  lcov --remove coverage.info '*/ext/http/server/*'> tmp_coverage.info 2>/dev/null
   cp tmp_coverage.info coverage.info
   exit 0
 fi
