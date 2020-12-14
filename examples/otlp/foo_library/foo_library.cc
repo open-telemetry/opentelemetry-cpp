974ff3408ba076b889180bba008d0a7c901ab4d5
Tom Tan <Tom.Tan@microsoft.com>
2020-12-14 15:14:16 -0800

    Remove unncessary call on Span::End()

--- examples/otlp/foo_library/foo_library.cc
+++ examples/otlp/foo_library/foo_library.cc
@@ -11,13 +11,9 @@ nostd::shared_ptr<trace::Tracer> get_tracer()
   return provider->GetTracer("foo_library");
 }
 
-// TODO: Remove all calls to span->End() once context memory issue is fixed
-// (https://github.com/open-telemetry/opentelemetry-cpp/issues/287)
-
 void f1()
 {
   auto span = get_tracer()->StartSpan("f1");
-  span->End();
 }
 
 void f2()
@@ -26,7 +22,6 @@ void f2()
 
   f1();
   f1();
-  span->End();
 }
 }  // namespace
 
@@ -35,5 +30,4 @@ void foo_library()
   auto span = get_tracer()->StartSpan("library");
 
   f2();
-  span->End();
 }
