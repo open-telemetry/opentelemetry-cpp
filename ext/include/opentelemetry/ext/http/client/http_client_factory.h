ea73ca49df9f1c00d401da7a51e9ac88cc6d9f54
Lalit Kumar Bhasin <labhas@microsoft.com>
2020-12-14 13:50:21 +0530

    missing file

---
+++ ext/include/opentelemetry/ext/http/client/http_client_factory.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "opentelemetry/ext/http/client/curl/http_client_curl.h"
+#include "opentelemetry/ext/http/client/http_client.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace ext
+{
+namespace http
+{
+namespace client
+{
+
+class HttpClientFactory
+{
+public:
+#ifdef WITH_CURL
+  static std::shared_ptr<SessionManager> Create()
+  {
+
+    return std::make_shared<ext::http::client::curl::SessionManager>();
+  }
+#else
+  static std::shared_ptr<SessionManager> Create()
+  {
+    return std::shared_ptr<SessionManager>(nullptr);
+  }
+#endif
+};
+}  // namespace client
+}  // namespace http
+}  // namespace ext
+OPENTELEMETRY_END_NAMESPACE
