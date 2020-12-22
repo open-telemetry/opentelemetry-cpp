3f23ed15ab52353735f73165060085763a1d8914
Mark <markseufert1@gmail.com>
2020-12-22 16:16:38 -0500

    Update empty_attributes.h

--- sdk/include/opentelemetry/sdk/common/empty_attributes.h
+++ sdk/include/opentelemetry/sdk/common/empty_attributes.h
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #pragma once
 
 #include "opentelemetry/common/key_value_iterable_view.h"
