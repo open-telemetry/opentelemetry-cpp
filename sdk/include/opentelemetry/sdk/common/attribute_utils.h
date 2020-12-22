5721de2a515e79f811d2af27726fe6ec96bcc74c
Karen Xu <xyira@amazon.com>
2020-12-22 12:56:10 -0500

    Use attribute_utils.h from common instead of trace

--- sdk/include/opentelemetry/sdk/common/attribute_utils.h
+++ sdk/include/opentelemetry/sdk/common/attribute_utils.h
@@ -1,5 +1,22 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 
+#include <string>
 #include <unordered_map>
 #include <vector>
 #include "opentelemetry/common/attribute_value.h"
@@ -21,6 +38,12 @@ using OwnedAttributeValue = nostd::variant<bool,
                                            uint64_t,
                                            double,
                                            std::string,
+#ifdef HAVE_CSTRING_TYPE
+                                           const char *,
+#endif
+#ifdef HAVE_SPAN_BYTE
+                                           std::vector<uint8_t>,
+#endif
                                            std::vector<bool>,
                                            std::vector<int32_t>,
                                            std::vector<uint32_t>,
@@ -42,8 +65,12 @@ struct AttributeConverter
   OwnedAttributeValue operator()(double v) { return OwnedAttributeValue(v); }
   OwnedAttributeValue operator()(nostd::string_view v)
   {
-    return OwnedAttributeValue(std::string(v));
+    return OwnedAttributeValue(std::string(v.data(), v.size()));
   }
+  OwnedAttributeValue operator()(const char *s) { return OwnedAttributeValue(std::string(s)); }
+#ifdef HAVE_SPAN_BYTE
+  OwnedAttributeValue operator()(nostd::span<const uint8_t> v) { return convertSpan<uint8_t>(v); }
+#endif
   OwnedAttributeValue operator()(nostd::span<const bool> v) { return convertSpan<bool>(v); }
   OwnedAttributeValue operator()(nostd::span<const int32_t> v) { return convertSpan<int32_t>(v); }
   OwnedAttributeValue operator()(nostd::span<const uint32_t> v) { return convertSpan<uint32_t>(v); }
