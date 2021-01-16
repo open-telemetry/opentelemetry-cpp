846ad7165cabdc167af90d2d3d4ac79a7989be93
Max Golovanov <maxgolov@microsoft.com>
2021-01-15 18:47:01 -0800

    Wrong #ifdef in test

--- api/test/nostd/string_view_test.cc
+++ api/test/nostd/string_view_test.cc
@@ -68,7 +68,7 @@ TEST(StringViewTest, SubstrPortion)
 TEST(StringViewTest, SubstrOutOfRange)
 {
   string_view s = "abc123";
-#if __EXCEPTIONS || defined(HAVE_STDLIB_CPP)
+#if __EXCEPTIONS || defined(HAVE_CPP_STDLIB)
   EXPECT_THROW(s.substr(10), std::out_of_range);
 #else
   EXPECT_DEATH({ s.substr(10); }, "");
