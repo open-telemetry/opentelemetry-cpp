9e5d94c597bf9eef2b9424efbe43d45549fa60f8
Lalit Kumar Bhasin <labhas@microsoft.com>
2021-02-09 18:34:16 +0530

    fix otlp test

--- exporters/otlp/test/otlp_exporter_test.cc
+++ exporters/otlp/test/otlp_exporter_test.cc
@@ -69,9 +69,10 @@ TEST_F(OtlpExporterTestPeer, ExportIntegrationTest)
 
   auto processor = std::shared_ptr<sdk::trace::SpanProcessor>(
       new sdk::trace::SimpleSpanProcessor(std::move(exporter)));
-  auto provider = nostd::shared_ptr<trace::TracerProvider>(new sdk::trace::TracerProvider(
-      processor, opentelemetry::sdk::resource::Resource::Create({})));
-  auto tracer   = provider->GetTracer("test");
+  auto resource = opentelemetry::sdk::resource::Resource::Create({});
+  auto provider = nostd::shared_ptr<trace::TracerProvider>(
+      new sdk::trace::TracerProvider(processor, std::move(resource)));
+  auto tracer = provider->GetTracer("test");
 
   EXPECT_CALL(*mock_stub, Export(_, _, _))
       .Times(AtLeast(1))
