192f2aa5d0f02a56d23da14138c99962d5ec19df
Tom Tan <Tom.Tan@microsoft.com>
2021-02-25 10:59:38 -0800

    Add ForceFlush to TracerProvider in SDK

--- sdk/src/trace/tracer_provider.cc
+++ sdk/src/trace/tracer_provider.cc
@@ -48,6 +48,11 @@ bool TracerProvider::Shutdown() noexcept
 {
   return GetProcessor()->Shutdown();
 }
+
+bool TracerProvider::ForceFlush(std::chrono::microseconds timeout) noexcept
+{
+  return GetProcessor()->Shutdown(timeout);
+}
 }  // namespace trace
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
