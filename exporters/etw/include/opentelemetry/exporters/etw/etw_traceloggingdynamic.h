c9f1efaa6162dcd3f9d429aadcdf092a70189cbb
Max Golovanov <maxgolov@microsoft.com>
2021-03-19 21:14:40 -0700

    define HAVE_TLD only if it hasn't been defined yet

--- exporters/etw/include/opentelemetry/exporters/etw/etw_traceloggingdynamic.h
+++ exporters/etw/include/opentelemetry/exporters/etw/etw_traceloggingdynamic.h
@@ -3,6 +3,8 @@
 #ifdef __has_include
 #  if __has_include("TraceLoggingDynamic.h")
 #    include "TraceLoggingDynamic.h"
-#    define HAVE_TLD
+#    ifndef HAVE_TLD
+#      define HAVE_TLD
+#    endif
 #  endif
 #endif
