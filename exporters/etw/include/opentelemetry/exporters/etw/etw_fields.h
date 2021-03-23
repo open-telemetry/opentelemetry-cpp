4543a47c2d6d56c0bdc77f6541542a8eaacc09ee
Max Golovanov <maxgolov@microsoft.com>
2021-03-23 01:01:30 -0700

    Clean-up field names for ETW notation (common field names used for both ETW/TLD and ETW/MsgPack)

--- exporters/etw/include/opentelemetry/exporters/etw/etw_fields.h
+++ exporters/etw/include/opentelemetry/exporters/etw/etw_fields.h
@@ -14,35 +14,43 @@
  */
 #pragma once
 
+#include <unordered_map>
+
+#include "opentelemetry/exporters/etw/utils.h"
+
+/* clang-format off */
 #ifdef CUSTOM_ETW_FIELDS_H
 #  include CUSTOM_ETW_FIELDS_H
 #else
-/* clang-format off */
 
-/* Field name constants */
+/* ETW constants at envelope level that would be appended at different spot depending
+ * on what serializer used to encode the payload
+ */
 #  define ETW_FIELD_NAME            "name"                  /* Event name             */
-#  define ETW_FIELD_TYPE            "type"                  /* Event type (TLD)       */
+#  define ETW_FIELD_TYPE            "type"                  /* Event type             */
 #  define ETW_FIELD_VERSION         "ver"                   /* Event version          */
-#  define ETW_FIELD_TIME            "time"                  /* Event time             */
-#  define ETW_FIELD_KIND            "kind"                  /* Event kind (MsgPack)   */
+#  define ETW_FIELD_TIME            "time"                  /* Event time at envelope */
+#  define ETW_FIELD_KIND            "kind"                  /* Event kind             */
 
-/* Common TraceId constant to assoc events with trace parent */
-#  define ETW_FIELD_TRACE_ID        "TraceId"               /* OT Trace Id            */
+/* Common TraceId constant to associate events with a trace */
+#  define ETW_FIELD_TRACE_ID        "TraceId"               /* Trace Id               */
 
 /* Span constants */
-#  define ETW_FIELD_SPAN_ID         "SpanId"                /* OT Span Id             */
-#  define ETW_FIELD_SPAN_START      "SpanStart"             /* ETW for Span Start     */
-#  define ETW_FIELD_SPAN_END        "SpanEnd"               /* ETW for Span Start     */
-#  define ETW_FIELD_SPAN_LINKS      "SpanLinks"             /* OT Span Links array    */
-#  define ETW_FIELD_SPAN_ATTRIBS    "SpanAttributes"        /* OT Span Attribs array  */
-#  define ETW_FIELD_SPAN_PARENTID   "SpanParentId"          /* OT Span ParentId       */
-#  define ETW_FIELD_SPAN_KIND       "SpanKind"
+#  define ETW_FIELD_SPAN_ID         "SpanId"                /* SpanId                 */
+#  define ETW_FIELD_SPAN_KIND       "SpanKind"              /* SpanKind               */
+#  define ETW_FIELD_SPAN_LINKS      "Links"                 /* Span Links array       */
+#  define ETW_FIELD_SPAN_PARENTID   "ParentId"              /* Span ParentId          */
 
 /* Span option constants */
-#  define ETW_FIELD_STARTTIME       "startTime"             /* ETW event startTime    */
-#  define ETW_FIELD_DURATION        "duration"              /* OT Span or Op duration */
-#  define ETW_FIELD_SUCCESS         "success"               /* OT Span success        */
-#  define ETW_FIELD_STATUSCODE      "statusCode"            /* OT Span status code    */
+#  define ETW_FIELD_STARTTIME       "Time"                  /* Operation start time   */
+#  define ETW_FIELD_DURATION        "Duration"              /* Operation duration     */
+#  define ETW_FIELD_STATUSCODE      "SpanStatusCode"        /* OT Span status code    */
+
+/* Value constants */
+#  define ETW_VALUE_SPAN_START      "SpanStart"             /* ETW for Span Start     */
+#  define ETW_VALUE_SPAN_END        "SpanEnd"               /* ETW for Span Start     */
+#  define ETW_VALUE_SUCCESS         "Success"               /* OT Span success        */
+
+#endif
 
 /* clang-format on */
-#endif
