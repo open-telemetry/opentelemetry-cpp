e5d39c0b2c755fa6568ab4306d4394a475e610b9
ndaliya <ndaliya@cisco.com>
2021-03-29 23:58:58 +0530

    Updated test case in string_util

--- api/test/common/string_util_test.cc
+++ api/test/common/string_util_test.cc
@@ -31,7 +31,7 @@ TEST(StringUtilTest, TrimString)
     const char *expected;
   } testcases[] = {{"k1=v1", "k1=v1"},     {"k1=v1,k2=v2, k3=v3", "k1=v1,k2=v2, k3=v3"},
                    {"   k1=v1", "k1=v1"},  {"k1=v1   ", "k1=v1"},
-                   {"   k1=v1 ", "k1=v1"}, {" ", ""}};
+                   {"   k1=v1 ", "k1=v1"}, {"  ", ""}};
   for (auto &testcase : testcases)
   {
     EXPECT_EQ(StringUtil::Trim(testcase.input, 0, strlen(testcase.input) - 1), testcase.expected);
