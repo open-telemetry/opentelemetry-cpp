cb5dfdb851a7dde115c4573b16d66ece0e70793d
Josh Suereth <joshuasuereth@google.com>
2021-04-07 12:41:38 -0400

    Take 2 at fixing the builds.

--- ext/include/opentelemetry/ext/zpages/zpages.h
+++ ext/include/opentelemetry/ext/zpages/zpages.h
@@ -54,7 +54,7 @@ public:
     // GCC 4.8 can't infer the type coercion.
     std::unique_ptr<opentelemetry::sdk::trace::SpanProcessor> processor(MakeSpanProcessor().release());
     auto tracez_provider_ = opentelemetry::nostd::shared_ptr<opentelemetry::trace::TracerProvider>(
-        new opentelemetry::sdk::trace::TracerProvider(std::move(MakeSpanProcessor())));
+        new opentelemetry::sdk::trace::TracerProvider(std::move(processor)));
     opentelemetry::trace::Provider::SetTracerProvider(tracez_provider_);
   }
 
