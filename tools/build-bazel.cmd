342fd30f445099b001e18b89ed593c516536cef0
Max Golovanov <maxgolov@microsoft.com>
2021-04-28 15:45:54 -0700

    Do not exclude anything from Bazel build

--- tools/build-bazel.cmd
+++ tools/build-bazel.cmd
@@ -1,12 +1,6 @@
 pushd "%~dp0"
 set "PATH=%CD%;%PATH%"
 cd ..
-if not exist "build" (
-  mkdir build
-)
-if not exist "plugin" (
-  mkdir plugin
-)
-bazel.exe build %* -- //... //api/test/... //sdk/test/...
-bazel.exe test %* -- //... //api/test/... //sdk/test/...
+bazel.exe build %* //...
+bazel.exe test %* //...
 popd
