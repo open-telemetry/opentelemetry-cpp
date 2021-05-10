6fd34e66b541b6d833f70f6c3b635ae8527ac701
Tom Tan <Tom.Tan@microsoft.com>
2021-05-10 10:52:02 -0700

    Add nostd::monostate alias for WITH_STL and WITH_ABSEIL build

--- sdk/src/common/core.cc
+++ sdk/src/common/core.cc
@@ -1,7 +1,10 @@
-#include "opentelemetry/nostd/variant.h"
+// clang-format off
+// version.h should be included before nostd/variant.h.
 #include "opentelemetry/version.h"
+#include "opentelemetry/nostd/variant.h"
+// clang-format on
 
-#if defined(HAVE_ABSEIL)
+#if defined(HAVE_ABSEIL) && !defined(HAVE_ABSEIL_VARIANT)
 namespace absl
 {
 namespace variant_internal
