190e812f46209d6f5b21539e221733039ee5fbfd
Lalit Kumar Bhasin <labhas@microsoft.com>
2021-05-15 00:13:56 +0530

    add copyright

--- api/include/opentelemetry/trace/propagation/jaeger.h
+++ api/include/opentelemetry/trace/propagation/jaeger.h
@@ -1,5 +1,3 @@
-#pragma once
-
 // Copyright 2021, OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#pragma once
 
 #include "detail/context.h"
 #include "detail/hex.h"
--- exporters/otlp/test/otlp_exporter_test.cc
+++ exporters/otlp/test/otlp_exporter_test.cc
@@ -1,3 +1,17 @@
+// Copyright 2021, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef HAVE_CPP_STDLIB
 // Unfortunately as of 04/27/2021 the fix is NOT in the vcpkg snapshot of Google Test.
 // Remove above `#ifdef` once the GMock fix for C++20 is in the mainline.
