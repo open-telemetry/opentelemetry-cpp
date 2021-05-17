c28be737f04a50382238beb3908d298d7c1748e3
Lalit Kumar Bhasin <labhas@microsoft.com>
2021-05-17 15:44:56 +0530

    Implement Baggage propagator (#732)

--- api/include/opentelemetry/context/context_value.h
+++ api/include/opentelemetry/context/context_value.h
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 
+#include "opentelemetry/baggage/baggage.h"
 #include "opentelemetry/nostd/shared_ptr.h"
 #include "opentelemetry/nostd/span.h"
 #include "opentelemetry/nostd/unique_ptr.h"
@@ -19,6 +20,7 @@ using ContextValue = nostd::variant<nostd::monostate,
                                     uint64_t,
                                     double,
                                     nostd::shared_ptr<trace::Span>,
-                                    nostd::shared_ptr<trace::SpanContext>>;
+                                    nostd::shared_ptr<trace::SpanContext>,
+                                    nostd::shared_ptr<baggage::Baggage>>;
 }  // namespace context
 OPENTELEMETRY_END_NAMESPACE
