c983f060be9df7e63912936b2b11c2e3ceaa47b6
Lalit Kumar Bhasin <labhas@microsoft.com>
2021-05-24 02:14:50 +0530

    use scope object

--- exporters/etw/test/etw_tracer_test.cc
+++ exporters/etw/test/etw_tracer_test.cc
@@ -68,55 +68,59 @@ TEST(ETWTracer, TracerCheck)
     {"attrib1", 1},
     {"attrib2", 2}
   };
-
-  auto topSpan = tracer->StartSpan("MySpanTop");
-  std::this_thread::sleep_for (std::chrono::seconds(1));
-
-  auto outerSpan = tracer->StartSpan("MySpanL2", attribs);
-
-  // Create nested span. Note how we share the attributes here.
-  // It is Okay to either reuse/share or have your own attributes.
-  auto innerSpan = tracer->StartSpan("MySpanL3", attribs);
-
-  // Add first event
-  std::string eventName1 = "MyEvent1";
-  Properties event1 =
   {
-    {"uint32Key", (uint32_t)1234},
-    {"uint64Key", (uint64_t)1234567890},
-    {"strKey", "someValue"}
-  };
-  EXPECT_NO_THROW(outerSpan->AddEvent(eventName1, event1));
-  std::this_thread::sleep_for (std::chrono::seconds(1));
+    auto topSpan = tracer->StartSpan("MySpanTop");
+    auto topScope = tracer->WithActiveSpan(topSpan);
+    std::this_thread::sleep_for (std::chrono::seconds(1));
+    {
+      auto outerSpan = tracer->StartSpan("MySpanL2", attribs);
+      auto outerScope = tracer->WithActiveSpan(outerScope);
 
-  // Add second event
-  std::string eventName2 = "MyEvent2";
-  Properties event2 =
-  {
-    {"uint32Key", (uint32_t)9876},
-    {"uint64Key", (uint64_t)987654321},
-    {"strKey", "anotherValue"}
-  };
-  EXPECT_NO_THROW(outerSpan->AddEvent(eventName2, event2));
-  std::this_thread::sleep_for (std::chrono::seconds(2));
+      // Create nested span. Note how we share the attributes here.
+      // It is Okay to either reuse/share or have your own attributes.
+      {
+        auto innerSpan = tracer->StartSpan("MySpanL3", attribs);
+        auto innerScope = tracer->WithActiveSpan(innerSpan);
 
-  std::string eventName3= "MyEvent3";
-    Properties event3 =
-  {
-    /* Extra metadata that allows event to flow to A.I. pipeline */
-    {"metadata", "ai_event"},
-    {"uint32Key", (uint32_t)9876},
-    {"uint64Key", (uint64_t)987654321},
-    // {"int32array", {{-1,0,1,2,3}} },
-    {"tempString", getTemporaryValue() }
-  };
-  EXPECT_NO_THROW(innerSpan->AddEvent(eventName3, event3));
-  std::this_thread::sleep_for (std::chrono::seconds(1));
+        // Add first event
+        std::string eventName1 = "MyEvent1";
+        Properties event1 =
+        {
+          {"uint32Key", (uint32_t)1234},
+          {"uint64Key", (uint64_t)1234567890},
+          {"strKey", "someValue"}
+        };
+        EXPECT_NO_THROW(outerSpan->AddEvent(eventName1, event1));
+        std::this_thread::sleep_for (std::chrono::seconds(1));
 
-  EXPECT_NO_THROW(innerSpan->End());    // end innerSpan
+        // Add second event
+        std::string eventName2 = "MyEvent2";
+        Properties event2 =
+        {
+          {"uint32Key", (uint32_t)9876},
+          {"uint64Key", (uint64_t)987654321},
+          {"strKey", "anotherValue"}
+        };
+        EXPECT_NO_THROW(outerSpan->AddEvent(eventName2, event2));
+        std::this_thread::sleep_for (std::chrono::seconds(2));
 
-  EXPECT_NO_THROW(outerSpan->End());    // end outerSpan
-  EXPECT_NO_THROW(topSpan->End());      // end topSpan
+        std::string eventName3= "MyEvent3";
+        Properties event3 =
+        {
+          /* Extra metadata that allows event to flow to A.I. pipeline */
+          {"metadata", "ai_event"},
+          {"uint32Key", (uint32_t)9876},
+          {"uint64Key", (uint64_t)987654321},
+          // {"int32array", {{-1,0,1,2,3}} },
+          {"tempString", getTemporaryValue() }
+        };
+        EXPECT_NO_THROW(innerSpan->AddEvent(eventName3, event3));
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+
+      }
+
+    }
+  }
 
   EXPECT_NO_THROW(tracer->CloseWithMicroseconds(0));
 }
@@ -194,12 +198,18 @@ TEST(ETWTracer, TracerCheckMaxDecoration)
       {"enableAutoParent", true}
   });
   auto tracer = tp.GetTracer(providerName, "TLD" );
-  auto aSpan = tracer->StartSpan("A.max");
-  auto bSpan = tracer->StartSpan("B.max");
-  auto cSpan = tracer->StartSpan("C.max");
-  cSpan->End();
-  bSpan->End();
-  aSpan->End();
+  {
+    auto aSpan = tracer->StartSpan("A.max");
+    auto aScope = tracer->WithActiveSpan(aSpan);
+    {
+      auto bSpan = tracer->StartSpan("B.max");
+      auto bScope = tracer->WithActiveSpan(bSpan);
+      {
+        auto cSpan = tracer->StartSpan("C.max");
+        auto cScope = tracer->WithActiveSpan(cSpan);
+      }
+    }
+  }
   tracer->CloseWithMicroseconds(0);
 }
 
@@ -217,10 +227,13 @@ TEST(ETWTracer, TracerCheckMsgPack)
   auto tracer = tp.GetTracer(providerName, "MsgPack" );
   {
       auto aSpan = tracer->StartSpan("A.max");
+      auto aScope = tracer->WithActiveSpan(aSpan);
       {
           auto bSpan = tracer->StartSpan("B.max");
+          auto bScope = tracer->WithActiveSpan(bSpan);
           {
               auto cSpan = tracer->StartSpan("C.max");
+              auto cScope = tracer->WithActiveSpan(cSpan);
               std::string eventName = "MyMsgPackEvent";
               Properties event =
               {
@@ -229,11 +242,8 @@ TEST(ETWTracer, TracerCheckMsgPack)
                   {"strKey", "someValue"}
               };
               cSpan->AddEvent(eventName, event);
-              cSpan->End();
           }
-          bSpan->End();
       }
-      aSpan->End();
   }
   tracer->CloseWithMicroseconds(0);
 }
