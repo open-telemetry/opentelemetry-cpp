0e7b0cab3e1117e00c909d64305f74d857d65072
Max Golovanov <maxgolov@microsoft.com>
2021-06-01 15:59:24 -0700

    Adjust path

--- ext/test/http/url_parser_test.cc
+++ ext/test/http/url_parser_test.cc
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-#include "opentelemetry/ext/http/common/url_parser.h"
+#include "opentelemetry/ext/net/common/url_parser.h"
 
 #include <gtest/gtest.h>
 
