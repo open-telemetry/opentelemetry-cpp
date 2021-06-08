5f0218abb3f11da4c3f660982e22a9d4f690f26f
Max Golovanov <maxgolov@microsoft.com>
2021-06-08 14:49:37 -0700

    Use Windows 8.1 SDK and clarify how to use Win 10 SDK in a comment

--- tools/build-vs2015.cmd
+++ tools/build-vs2015.cmd
@@ -7,13 +7,17 @@ if NOT "%1"=="" (
 )
 set "CMAKE_GEN=Ninja"
 set "VCPKG_VISUAL_STUDIO_PATH=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\"
-set CMAKE_SYSTEM_VERSION=8.1
+
+REM Building with Windows SDK 8.1
 set "PATH=%ProgramFiles(x86)%\Windows Kits\8.1\bin\%ARCH%;%PATH%"
-REM set WINSDK_VERSION=10.0.19041.0
 set WINSDK_VERSION=8.1
+set CMAKE_SYSTEM_VERSION=8.1
+
+REM Replace above Windows SDK 8.1 by Windows 10 SDK if necessary.
+REM Resulting binaries may not be compatible with Windows 8.
+REM set WINSDK_VERSION=10.0.19041.0
 REM set "PATH=%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.19041.0\%ARCH%\;%PATH%"
-set VCPKG_ROOT=C:\work\vcpkg.vs2015
-REM set VCPKG_FORCE_SYSTEM_BINARIES=1
+
 cd %~dp0
 call setup-buildtools.cmd
 call build.cmd -DMSVC_TOOLSET_VERSION=140
