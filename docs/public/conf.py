ef018870b3291a41b03827455af5e34c28e291a6
Lalit Kumar Bhasin <labhas@microsoft.com>
2021-06-18 19:52:46 +0530

    rel 1.0.0.rc2

--- docs/public/conf.py
+++ docs/public/conf.py
@@ -21,7 +21,7 @@ copyright = '2021, OpenTelemetry authors'
 author = 'OpenTelemetry authors'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0-rc1'
+release = '1.0.0-rc2'
 
 # Run sphinx on subprojects and copy output
 # -----------------------------------------
