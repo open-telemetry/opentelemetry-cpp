9b1725b39a49748ef83682ae46aee69c5182dca1
Max Golovanov <maxgolov@microsoft.com>
2021-07-07 20:47:10 -0700

    Build farm for Windows (#839)

--- tools/build-vs2019.cmd
+++ tools/build-vs2019.cmd
@@ -1,15 +1,13 @@
 REM Build with Visual Studio 2017
-set "VS_TOOLS_VERSION=vs2019"
-set ARCH=Win64
+set "BUILDTOOLS_VERSION=vs2019"
+set ARCH=x64
 if NOT "%1"=="" (
   set ARCH=%1
 )
-if "%ARCH%"=="Win64" (
+if "%ARCH%"=="x64" (
   REM Parameter needed for CMake Visual Studio 2019 generator
   set CMAKE_ARCH=x64
 )
-
-set "CMAKE_GEN=Visual Studio 16 2019"
 cd %~dp0
 call setup-buildtools.cmd
 call build.cmd
