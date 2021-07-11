ddf3de172f71ff6e252d831a153038f8ecc1a1ae
Oblivion <71217171+esigo@users.noreply.github.com>
2021-07-11 16:37:48 +0200

    🐛 fix zipkin exporter compile failure

--- exporters/zipkin/include/opentelemetry/exporters/zipkin/recordable.h
+++ exporters/zipkin/include/opentelemetry/exporters/zipkin/recordable.h
@@ -40,7 +40,7 @@ public:
   void AddLink(const opentelemetry::trace::SpanContext &span_context,
                const common::KeyValueIterable &attributes) noexcept override;
 
-  void SetStatus(trace::StatusCode code, nostd::string_view description) noexcept override;
+  void SetStatus(opentelemetry::trace::StatusCode code, nostd::string_view description) noexcept override;
 
   void SetName(nostd::string_view name) noexcept override;
 
