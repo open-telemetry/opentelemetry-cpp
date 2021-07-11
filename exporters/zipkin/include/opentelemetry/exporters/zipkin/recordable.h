a97927c82aea0054364c1e2329533e1f74af4e7a
esigo <ehsan.sadroid@gmail.com>
2021-07-11 17:53:56 +0200

    fix format change

--- exporters/zipkin/include/opentelemetry/exporters/zipkin/recordable.h
+++ exporters/zipkin/include/opentelemetry/exporters/zipkin/recordable.h
@@ -40,7 +40,8 @@ public:
   void AddLink(const opentelemetry::trace::SpanContext &span_context,
                const common::KeyValueIterable &attributes) noexcept override;
 
-  void SetStatus(opentelemetry::trace::StatusCode code, nostd::string_view description) noexcept override;
+  void SetStatus(opentelemetry::trace::StatusCode code,
+                 nostd::string_view description) noexcept override;
 
   void SetName(nostd::string_view name) noexcept override;
 
