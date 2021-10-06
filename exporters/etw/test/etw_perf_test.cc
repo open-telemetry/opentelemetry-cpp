aa63e612ceff225d6bc0f45bc1054a4d33cceffd
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2021-10-05 22:03:10 -0700

    ETW Log Exporter (#1006)

--- exporters/etw/test/etw_perf_test.cc
+++ exporters/etw/test/etw_perf_test.cc
@@ -20,11 +20,12 @@ namespace
 {
 static constexpr const char *providerName = "OpenTelemetry-ETW-StressTest";
 
-static exporter::etw::TracerProviderOptions providerOptions = {{"enableTraceId", false},
-                                                               {"enableSpanId", false},
-                                                               {"enableActivityId", false},
-                                                               {"enableRelatedActivityId", false},
-                                                               {"enableAutoParent", false}};
+static exporter::etw::TelemetryProviderOptions providerOptions = {
+    {"enableTraceId", false},
+    {"enableSpanId", false},
+    {"enableActivityId", false},
+    {"enableRelatedActivityId", false},
+    {"enableAutoParent", false}};
 
 class ETWProviderStressTest
 {
