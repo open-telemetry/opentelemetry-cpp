900f46feea9a86e0e82f064855a38131353d363b
liud <xloogson@gmail.com>
2021-10-26 21:43:55 +0800

    clean code

--- exporters/otlp/src/otlp_http_exporter.cc
+++ exporters/otlp/src/otlp_http_exporter.cc
@@ -13,7 +13,6 @@
 #include "google/protobuf/message.h"
 #include "google/protobuf/reflection.h"
 #include "google/protobuf/stubs/stl_util.h"
-
 #include "opentelemetry/proto/collector/trace/v1/trace_service.pb.h"
 
 #include "opentelemetry/exporters/otlp/protobuf_include_suffix.h"
