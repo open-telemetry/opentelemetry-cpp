b312e8e3724e40eab8bc06c9523e2a22e903e2e8
Oblivion <ehsan.sadroid@gmail.com>
2021-11-13 17:57:35 +0000

    otlp grpc log export example

---
+++ examples/otlp/grpc_log_main.cc
@@ -0,0 +1,79 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#ifdef ENABLE_LOGS_PREVIEW
+#  include "opentelemetry/exporters/otlp/otlp_grpc_exporter.h"
+#  include "opentelemetry/exporters/otlp/otlp_grpc_log_exporter.h"
+#  include "opentelemetry/logs/provider.h"
+#  include "opentelemetry/sdk/logs/logger_provider.h"
+#  include "opentelemetry/sdk/logs/simple_log_processor.h"
+#  include "opentelemetry/sdk/trace/simple_processor.h"
+#  include "opentelemetry/sdk/trace/tracer_provider.h"
+#  include "opentelemetry/trace/provider.h"
+
+#  include <string>
+
+#  ifdef BAZEL_BUILD
+#    include "examples/common/logs_foo_library/foo_library.h"
+#  else
+#    include "logs_foo_library/foo_library.h"
+#  endif
+
+namespace trace    = opentelemetry::trace;
+namespace nostd    = opentelemetry::nostd;
+namespace otlp     = opentelemetry::exporter::otlp;
+namespace sdklogs  = opentelemetry::sdk::logs;
+namespace logs_api = opentelemetry::logs;
+
+namespace sdktrace = opentelemetry::sdk::trace;
+
+namespace
+{
+opentelemetry::exporter::otlp::OtlpGrpcExporterOptions opts;
+void InitTracer()
+{
+  // Create OTLP exporter instance
+  auto exporter  = std::unique_ptr<sdktrace::SpanExporter>(new otlp::OtlpGrpcExporter(opts));
+  auto processor = std::unique_ptr<sdktrace::SpanProcessor>(
+      new sdktrace::SimpleSpanProcessor(std::move(exporter)));
+  auto provider =
+      nostd::shared_ptr<trace::TracerProvider>(new sdktrace::TracerProvider(std::move(processor)));
+  // Set the global trace provider
+  trace::Provider::SetTracerProvider(provider);
+}
+
+void InitLogger()
+{
+  // Create OTLP exporter instance
+  auto exporter = std::unique_ptr<sdklogs::LogExporter>(new otlp::OtlpGrpcLogExporter(opts));
+  auto processor =
+      std::shared_ptr<sdklogs::LogProcessor>(new sdklogs::SimpleLogProcessor(std::move(exporter)));
+  auto sdkProvider = std::shared_ptr<sdklogs::LoggerProvider>(new sdklogs::LoggerProvider());
+  sdkProvider->SetProcessor(processor);
+  auto apiProvider = nostd::shared_ptr<logs_api::LoggerProvider>(sdkProvider);
+  auto provider    = nostd::shared_ptr<logs_api::LoggerProvider>(apiProvider);
+  opentelemetry::logs::Provider::SetLoggerProvider(provider);
+}
+}  // namespace
+
+int main(int argc, char *argv[])
+{
+  if (argc > 1)
+  {
+    opts.endpoint = argv[1];
+    if (argc > 2)
+    {
+      opts.use_ssl_credentials         = true;
+      opts.ssl_credentials_cacert_path = argv[2];
+    }
+  }
+  InitLogger();
+  InitTracer();
+  foo_library();
+}
+#else
+int main()
+{
+  return 0;
+}
+#endif
