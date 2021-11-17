411d6fabf4cff46481ef1de3273ce22fcaa967bf
esigo <ehsan.sadroid@gmail.com>
2021-11-17 19:29:34 +0100

    format

--- cmake/nlohmann-json.cmake
+++ cmake/nlohmann-json.cmake
@@ -27,9 +27,8 @@ ExternalProject_Add(nlohmann_json_download
 ExternalProject_Get_Property(nlohmann_json_download INSTALL_DIR)
 SET(NLOHMANN_JSON_INCLUDE_DIR ${INSTALL_DIR}/3rd_party/src/nlohmann_json_download/single_include)
 add_library(nlohmann_json_ INTERFACE)
-target_include_directories(nlohmann_json_ INTERFACE 
- "$<BUILD_INTERFACE:${NLOHMANN_JSON_INCLUDE_DIR}>"
-"$<INSTALL_INTERFACE:include>")
-#${NLOHMANN_JSON_INCLUDE_DIR})
+target_include_directories(nlohmann_json_ INTERFACE
+    "$<BUILD_INTERFACE:${NLOHMANN_JSON_INCLUDE_DIR}>"
+    "$<INSTALL_INTERFACE:include>")
 add_dependencies(nlohmann_json_ nlohmann_json_download)
 add_library(nlohmann_json::nlohmann_json ALIAS nlohmann_json_)
