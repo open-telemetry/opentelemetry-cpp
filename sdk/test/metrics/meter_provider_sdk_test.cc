6f1e8a5a015027b17b0693b878d8b69a69d9a1ad
Lalit <labhas@microsoft.com>
2021-11-20 01:26:18 -0800

    more build errors

--- sdk/test/metrics/meter_provider_sdk_test.cc
+++ sdk/test/metrics/meter_provider_sdk_test.cc
@@ -1,20 +1,21 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-#ifdef ENABLE_METRICS_PREVIEW
-#  include <gtest/gtest.h>
-
-#  include "opentelemetry/sdk/metrics/meter.h"
-#  include "opentelemetry/sdk/metrics/meter_provider.h"
+#include <gtest/gtest.h>
+#include "opentelemetry/sdk/metrics/meter.h"
+#include "opentelemetry/sdk/metrics/meter_provider.h"
 
 using namespace opentelemetry::sdk::metrics;
 
 TEST(MeterProvider, GetMeter)
 {
-  MeterProvider mp;
-  auto t1 = tf.GetMeter("test");
-  auto t2 = tf.GetMeter("test");
-  auto t3 = tf.GetMeter("different", "1.0.0");
+  std::vector<std::unique_ptr<MeterExporter>> exporters;
+  std::vector<std::unique_ptr<MeterReader>> readers;
+  std::vector<std::unique_ptr<View>> views;
+  MeterProvider mp(std::move(exporters), std::move(readers), std::move(views));
+  auto t1 = mp.GetMeter("test");
+  auto t2 = mp.GetMeter("test");
+  auto t3 = mp.GetMeter("different", "1.0.0");
   ASSERT_NE(nullptr, t1);
   ASSERT_NE(nullptr, t2);
   ASSERT_NE(nullptr, t3);
@@ -23,4 +24,3 @@ TEST(MeterProvider, GetMeter)
   ASSERT_EQ(t1, t2);
   ASSERT_EQ(t1, t3);
 }
-#endif
