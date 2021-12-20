3701a6e4c7b2672d2a11a26c8270dee857b8c171
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2021-12-20 18:49:34 +0100

    ZipkinExporter shutdown (#1153)

--- exporters/zipkin/include/opentelemetry/exporters/zipkin/zipkin_exporter.h
+++ exporters/zipkin/include/opentelemetry/exporters/zipkin/zipkin_exporter.h
@@ -82,10 +82,7 @@ public:
    * @param timeout an optional timeout, default to max.
    */
   bool Shutdown(
-      std::chrono::microseconds timeout = std::chrono::microseconds::max()) noexcept override
-  {
-    return true;
-  }
+      std::chrono::microseconds timeout = std::chrono::microseconds::max()) noexcept override;
 
 private:
   void InitializeLocalEndpoint();
