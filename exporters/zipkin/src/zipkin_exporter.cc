8cf00e9f728a559ebfe98d288a6cb67a7f0f2752
Oblivion <ehsan.sadroid@gmail.com>
2022-01-01 14:32:31 +0000

    CI Windows

--- exporters/zipkin/src/zipkin_exporter.cc
+++ exporters/zipkin/src/zipkin_exporter.cc
@@ -1,6 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+#define _WINSOCKAPI_  // stops including winsock.h
 #include "opentelemetry/exporters/zipkin/zipkin_exporter.h"
 #include <mutex>
 #include "opentelemetry/exporters/zipkin/recordable.h"
