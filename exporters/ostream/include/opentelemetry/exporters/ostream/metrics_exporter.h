ab81e0e555ce5c85927ea021b12afbb90e3a19f5
esigo <ehsan.sadroid@gmail.com>
2022-02-06 21:11:40 +0100

    revert old metric

--- exporters/ostream/include/opentelemetry/exporters/ostream/metrics_exporter.h
+++ exporters/ostream/include/opentelemetry/exporters/ostream/metrics_exporter.h
@@ -2,16 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #pragma once
-#ifndef ENABLE_METRICS_PREVIEW
+#ifdef ENABLE_METRICS_PREVIEW
 
 #  include <iostream>
 #  include <string>
-#  include "opentelemetry/common/spin_lock_mutex.h"
-#  include "opentelemetry/nostd/span.h"
-#  include "opentelemetry/sdk/metrics/instruments.h"
-#  include "opentelemetry/sdk/metrics/metric_exporter.h"
-#  include "opentelemetry/sdk/metrics/recordable.h"
-#  include "opentelemetry/version.h"
+#  include "opentelemetry/sdk/_metrics/aggregator/exact_aggregator.h"
+#  include "opentelemetry/sdk/_metrics/aggregator/gauge_aggregator.h"
+#  include "opentelemetry/sdk/_metrics/aggregator/histogram_aggregator.h"
+#  include "opentelemetry/sdk/_metrics/exporter.h"
+#  include "opentelemetry/sdk/_metrics/record.h"
 
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace exporter
@@ -20,39 +19,146 @@ namespace metrics
 {
 
 /**
- * The OStreamMetricExporter exports record data through an ostream
+ * The OStreamMetricsExporter exports record data through an ostream
  */
-class OStreamMetricExporter final : public opentelemetry::sdk::metrics::MetricExporter
+class OStreamMetricsExporter final : public opentelemetry::sdk::metrics::MetricsExporter
 {
 public:
   /**
-   * Create an OStreamMetricExporter. This constructor takes in a reference to an ostream that the
+   * Create an OStreamMetricsExporter. This constructor takes in a reference to an ostream that the
    * export() function will send span data into.
    * The default ostream is set to stdout
    */
-  explicit OStreamMetricExporter(std::ostream &sout = std::cout) noexcept;
+  explicit OStreamMetricsExporter(std::ostream &sout = std::cout) noexcept;
 
-  /**
-   * Export
-   * @param records a span of unique pointers to metrics recordables
-   */
   sdk::common::ExportResult Export(
-      const nostd::span<std::unique_ptr<opentelemetry::sdk::metrics::Recordable>> &records) noexcept
-      override;
-
-  /**
-   * Shut down the exporter.
-   * @param timeout an optional timeout, the default timeout of 0 means that no
-   * timeout is applied.
-   * @return return the status of this operation
-   */
-  bool Shutdown(std::chrono::microseconds timeout = std::chrono::microseconds(0)) noexcept override;
+      const std::vector<opentelemetry::sdk::metrics::Record> &records) noexcept override;
 
 private:
   std::ostream &sout_;
-  bool is_shutdown_ = false;
-  mutable opentelemetry::common::SpinLockMutex lock_;
-  bool isShutdown() const noexcept;
+
+  /**
+   * Send specific data from the given AggregatorVariant based on what AggregatorKind
+   * it is holding. Each Aggregator holds data differently, so each have their own
+   * custom printing.
+   */
+  template <typename T>
+  void PrintAggregatorVariant(opentelemetry::sdk::metrics::AggregatorVariant value)
+  {
+    auto agg     = nostd::get<std::shared_ptr<opentelemetry::sdk::metrics::Aggregator<T>>>(value);
+    auto aggKind = agg->get_aggregator_kind();
+
+    if (!agg)
+      return;
+    switch (aggKind)
+    {
+      case opentelemetry::sdk::metrics::AggregatorKind::Counter: {
+        sout_ << "\n  sum         : " << agg->get_checkpoint()[0];
+      }
+      break;
+      case opentelemetry::sdk::metrics::AggregatorKind::MinMaxSumCount: {
+        auto mmsc = agg->get_checkpoint();
+        sout_ << "\n  min         : " << mmsc[0] << "\n  max         : " << mmsc[1]
+              << "\n  sum         : " << mmsc[2] << "\n  count       : " << mmsc[3];
+      }
+      break;
+      case opentelemetry::sdk::metrics::AggregatorKind::Gauge: {
+        auto timestamp = agg->get_checkpoint_timestamp();
+
+        sout_ << "\n  last value  : " << agg->get_checkpoint()[0]
+              << "\n  timestamp   : " << std::to_string(timestamp.time_since_epoch().count());
+      }
+      break;
+      case opentelemetry::sdk::metrics::AggregatorKind::Exact: {
+        // TODO: Find better way to print quantiles
+        if (agg->get_quant_estimation())
+        {
+          sout_ << "\n  quantiles   : "
+                << "[0: " << agg->get_quantiles(0) << ", "
+                << ".25: " << agg->get_quantiles(.25) << ", "
+                << ".50: " << agg->get_quantiles(.50) << ", "
+                << ".75: " << agg->get_quantiles(.75) << ", "
+                << "1: " << agg->get_quantiles(1) << ']';
+        }
+        else
+        {
+          auto vec    = agg->get_checkpoint();
+          size_t size = vec.size();
+          size_t i    = 1;
+
+          sout_ << "\n  values      : " << '[';
+
+          for (auto val : vec)
+          {
+            sout_ << val;
+            if (i != size)
+              sout_ << ", ";
+            i++;
+          }
+          sout_ << ']';
+        }
+      }
+      break;
+      case opentelemetry::sdk::metrics::AggregatorKind::Histogram: {
+        auto boundaries = agg->get_boundaries();
+        auto counts     = agg->get_counts();
+
+        size_t boundaries_size = boundaries.size();
+        size_t counts_size     = counts.size();
+
+        sout_ << "\n  buckets     : " << '[';
+
+        for (size_t i = 0; i < boundaries_size; i++)
+        {
+          sout_ << boundaries[i];
+
+          if (i != boundaries_size - 1)
+            sout_ << ", ";
+        }
+        sout_ << ']';
+
+        sout_ << "\n  counts      : " << '[';
+        for (size_t i = 0; i < counts_size; i++)
+        {
+          sout_ << counts[i];
+
+          if (i != counts_size - 1)
+            sout_ << ", ";
+        }
+        sout_ << ']';
+      }
+      break;
+      case opentelemetry::sdk::metrics::AggregatorKind::Sketch: {
+        auto boundaries = agg->get_boundaries();
+        auto counts     = agg->get_counts();
+
+        size_t boundaries_size = boundaries.size();
+        size_t counts_size     = counts.size();
+
+        sout_ << "\n  buckets     : " << '[';
+
+        for (size_t i = 0; i < boundaries_size; i++)
+        {
+          sout_ << boundaries[i];
+
+          if (i != boundaries_size - 1)
+            sout_ << ", ";
+        }
+        sout_ << ']';
+
+        sout_ << "\n  counts      : " << '[';
+        for (size_t i = 0; i < counts_size; i++)
+        {
+          sout_ << counts[i];
+
+          if (i != counts_size - 1)
+            sout_ << ", ";
+        }
+        sout_ << ']';
+      }
+      break;
+    }
+  }
 };
 }  // namespace metrics
 }  // namespace exporter
--- exporters/ostream/src/metrics_exporter.cc
+++ exporters/ostream/src/metrics_exporter.cc
@@ -1,11 +1,8 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-#ifndef ENABLE_METRICS_PREVIEW
+#ifdef ENABLE_METRICS_PREVIEW
 #  include "opentelemetry/exporters/ostream/metrics_exporter.h"
-#  include "opentelemetry/sdk/metrics/aggregation/default_aggregation.h"
-#  include "opentelemetry/sdk/metrics/aggregation/histogram_aggregation.h"
-#  include "opentelemetry/sdk_config.h"
 
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace exporter
@@ -13,38 +10,47 @@ namespace exporter
 namespace metrics
 {
 
-OStreamMetricExporter::OStreamMetricExporter(std::ostream &sout) noexcept : sout_(sout) {}
+OStreamMetricsExporter::OStreamMetricsExporter(std::ostream &sout) noexcept : sout_(sout) {}
 
-sdk::common::ExportResult OStreamMetricExporter::Export(
-    const nostd::span<std::unique_ptr<opentelemetry::sdk::metrics::Recordable>> &records) noexcept
+sdk::common::ExportResult OStreamMetricsExporter::Export(
+    const std::vector<sdk::metrics::Record> &records) noexcept
 {
-  if (isShutdown())
-  {
-    OTEL_INTERNAL_LOG_ERROR("[OStream Metric] Exporting "
-                            << records.size() << " records(s) failed, exporter is shutdown");
-    return sdk::common::ExportResult::kFailure;
-  }
-
-  for (auto &record : records)
+  for (auto record : records)
   {
     sout_ << "{"
-          << "\n  name        : " << record->GetName();
+          << "\n  name        : " << record.GetName()
+          << "\n  description : " << record.GetDescription()
+          << "\n  labels      : " << record.GetLabels();
+
+    auto aggregator = record.GetAggregator();
+
+    /**
+     * Unpack the AggregatorVariant from the record so we can pass the data type to
+     * PrintAggregatorVariant to unpack the Aggregator from the variant.
+     */
+    if (nostd::holds_alternative<std::shared_ptr<opentelemetry::sdk::metrics::Aggregator<int>>>(
+            aggregator))
+    {
+      PrintAggregatorVariant<int>(aggregator);
+    }
+    else if (nostd::holds_alternative<
+                 std::shared_ptr<opentelemetry::sdk::metrics::Aggregator<short>>>(aggregator))
+    {
+      PrintAggregatorVariant<short>(aggregator);
+    }
+    else if (nostd::holds_alternative<
+                 std::shared_ptr<opentelemetry::sdk::metrics::Aggregator<double>>>(aggregator))
+    {
+      PrintAggregatorVariant<double>(aggregator);
+    }
+    else if (nostd::holds_alternative<
+                 std::shared_ptr<opentelemetry::sdk::metrics::Aggregator<float>>>(aggregator))
+    {
+      PrintAggregatorVariant<float>(aggregator);
+    }
     sout_ << "\n}\n";
   }
   return sdk::common::ExportResult::kSuccess;
-}
-
-bool OStreamMetricExporter::Shutdown(std::chrono::microseconds timeout) noexcept
-{
-  const std::lock_guard<opentelemetry::common::SpinLockMutex> locked(lock_);
-  is_shutdown_ = true;
-  return true;
-}
-
-bool OStreamMetricExporter::isShutdown() const noexcept
-{
-  const std::lock_guard<opentelemetry::common::SpinLockMutex> locked(lock_);
-  return is_shutdown_;
 }
 
 }  // namespace metrics
