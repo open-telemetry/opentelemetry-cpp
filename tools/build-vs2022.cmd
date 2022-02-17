3a9d0870bc846f79f97c81796ce70440d944aa88
Tom Tan <Tom.Tan@microsoft.com>
2022-02-17 00:10:19 -0800

    Detect vs2022 as build environment (#1212)

--- tools/build-vs2022.cmd
+++ tools/build-vs2022.cmd
@@ -1,11 +1,11 @@
-REM Build with Visual Studio 2019
-set "BUILDTOOLS_VERSION=vs2019"
+REM Build with Visual Studio 2022
+set "BUILDTOOLS_VERSION=vs2022"
 set ARCH=x64
 if NOT "%1"=="" (
   set ARCH=%1
 )
 if "%ARCH%"=="x64" (
-  REM Parameter needed for CMake Visual Studio 2019 generator
+  REM Parameter needed for CMake Visual Studio 2022 generator
   set CMAKE_ARCH=x64
 )
 cd %~dp0
