8e900d571aae8d64305c0c170cd10147f2e1aea0
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-02-26 11:49:21 +0100

    review comments

--- ci/setup_thrift.ps1
+++ ci/setup_thrift.ps1
@@ -2,12 +2,11 @@ $ErrorActionPreference = "Stop"
 trap { $host.SetShouldExit(1) }
 
 # TODO: consider moving this to .gitmodules
-git clone https://github.com/Microsoft/vcpkg.git
+git clone --depth=1 -b 2020.04-2702-g5568f110b https://github.com/Microsoft/vcpkg.git
 Push-Location -Path vcpkg
 $VCPKG_DIR=(Get-Item -Path ".\").FullName
 setx VCPKG_DIR (Get-Item -Path ".\").FullName
 ./bootstrap-vcpkg.bat
-./vcpkg integrate install
 
 # boost needed for thrift
 ./vcpkg install boost-predef[core]:x64-windows boost-locale[core]:x64-windows boost-numeric-conversion[core]:x64-windows boost-scope-exit[core]:x64-windows  openssl:x64-windows
