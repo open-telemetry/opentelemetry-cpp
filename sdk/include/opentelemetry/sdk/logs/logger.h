0322126f7f7d4decddd91408a4185d6438eb9103
owentou <owentou@tencent.com>
2022-03-05 01:00:24 +0800

    Fix comment
    Signed-off-by: owentou <owentou@tencent.com>

--- sdk/include/opentelemetry/sdk/logs/logger.h
+++ sdk/include/opentelemetry/sdk/logs/logger.h
@@ -67,9 +67,9 @@ private:
   // The name of this logger
   std::string logger_name_;
 
+  // order of declaration is important here - instrumentation library should destroy after
+  // logger-context.
   std::unique_ptr<instrumentationlibrary::InstrumentationLibrary> instrumentation_library_;
-
-  // The logger context of this Logger. Uses a weak_ptr to avoid cyclic dependency issues the with
   std::shared_ptr<LoggerContext> context_;
 };
 
