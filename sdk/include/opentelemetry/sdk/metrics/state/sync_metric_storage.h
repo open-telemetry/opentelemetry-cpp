5ab4f75d85bd95ae93d730c6b905a78880ce6e19
Lalit <labhas@microsoft.com>
2022-03-18 00:05:03 -0700

    comments

--- sdk/include/opentelemetry/sdk/metrics/state/sync_metric_storage.h
+++ sdk/include/opentelemetry/sdk/metrics/state/sync_metric_storage.h
@@ -100,7 +100,7 @@ public:
     opentelemetry::common::SystemTimestamp last_collection_ts = sdk_start_ts;
     auto aggregation_temporarily = collector->GetAggregationTemporarily();
 
-    // Add the current delta metrics to unreported metrics for all the collectors,
+    // Add the current delta metrics to `unreported metrics stash` for all the collectors,
     // this will also empty the delta metrics hashmap, and make it available for
     // recordings
     std::shared_ptr<AttributesHashMap> delta_metrics = std::move(attributes_hashmap_);
@@ -109,8 +109,9 @@ public:
       unreported_metrics_[col.get()].push_back(delta_metrics);
     }
 
-    // Get the unreported metrics for the `collector` since last collection, this will
-    // also cleanup the unreported metrics. Return early if there is no unreported metrics
+    // Get the unreported metrics for the `collector` from `unreported metrics stash`
+    // since last collection, this will also cleanup the unreported metrics for `collector`
+    // from the stash.
     auto present = unreported_metrics_.find(collector);
     if (present == unreported_metrics_.end())
     {
@@ -119,7 +120,7 @@ public:
     }
     auto unreported_list = std::move(unreported_metrics_[collector]);
 
-    // Iterate over the unreporter metrics for `collector` and do merge
+    // Iterate over the unreporter metrics for `collector` and store result in `merged_metrics`
     std::unique_ptr<AttributesHashMap> merged_metrics;
     for (auto &agg_hashmap : unreported_list)
     {
@@ -128,16 +129,25 @@ public:
             auto agg = merged_metrics->Get(attributes);
             if (agg)
             {
-              merged_attributes->Set(attributes, agg->Merge(aggregation));
+              merged_metrics->Set(attributes, agg->Merge(aggregation));
             }
             else
             {
-              merged_attributes->Set(attributes,
-                                     DefaultAggregation::CreateAggregation(instrument_descriptor_));
+              merged_metrics->Set(attributes,
+                                  DefaultAggregation::CreateAggregation(instrument_descriptor_));
             }
             return true;
           });
     }
+
+    // Get the last reported metrics for the `collector` from `last reported metrics` stash
+    //   - If the aggregation_temporarily for the collector is cummulative
+    //       - Merge the last reported metrics with unreported metrics (which is in merged_metrics),
+    //           Final result of merge would be in merged_metrics.
+    //       - Move the final merge to the `last reported metrics` stash.
+    //   - If the aggregation_temporarily is delta
+    //       - Store the unreported metrics for `collector`(which is in merged_mtrics) to
+    //          `last reported metrics` stash.
 
     auto reported = last_reported_metrics_.find(collector);
     if (reported != last_reported_metrics_.end())
@@ -148,29 +158,30 @@ public:
       {
         // merge current delta to previous cummulative
         last_aggr_hashmap->GetAllEnteries(
-            [&merged_attributes, this](const MetricAttributes &attributes,
-                                       Aggregation &aggregation) {
-              auto agg = merged_attributes->Get(attributes);
+            [&merged_metrics, this](const MetricAttributes &attributes, Aggregation &aggregation) {
+              auto agg = merged_metrics->Get(attributes);
               if (agg)
               {
-                merged_attributes->Set(attributes, agg->Merge(aggregation));
+                merged_metrics->Set(attributes, agg->Merge(aggregation));
               }
               else
               {
-                merged_attributes->Set(
-                    attributes, DefaultAggregation::CreateAggregation(instrument_descriptor_));
+                merged_metrics->Set(attributes,
+                                    DefaultAggregation::CreateAggregation(instrument_descriptor_));
               }
               return true;
             });
       }
       last_reported_metrics_[collector] =
-          LastReportedMetrics{std::move(merged_attributes), collection_ts};
+          LastReportedMetrics{std::move(merged_metrics), collection_ts};
     }
     else
     {
-      last_reported_metrics_.insert(std::make_pair(
-          collector, LastReportedMetrics{std::move(merged_attributes), collection_ts}));
+      last_reported_metrics_.insert(
+          std::make_pair(collector, LastReportedMetrics{std::move(merged_metrics), collection_ts}));
     }
+
+    // Generate the MetricData from the final merged_metrics, and invoke callback over it.
 
     AttributesHashMap *result_to_export = (last_reported_metrics_[collector]).attributes_map.get();
     MetricData metric_data;
@@ -195,9 +206,13 @@ public:
 private:
   InstrumentDescriptor instrument_descriptor_;
   AggregationType aggregation_type_;
+
+  // hashmap to maintain the metrics for delta collection (i.e, collection since last Collect call)
   std::unique_ptr<AttributesHashMap> attributes_hashmap_;
+  // unreported metrics stash for all the collectors
   std::unordered_map<CollectorHandle *, std::list<std::shared_ptr<AttributesHashMap>>>
       unreported_metrics_;
+  // last reported metrics stash for all the collectors.
   std::unordered_map<CollectorHandle *, LastReportedMetrics> last_reported_metrics_;
   const AttributesProcessor *attributes_processor_;
   std::function<std::unique_ptr<Aggregation>()> create_default_aggregation_;
