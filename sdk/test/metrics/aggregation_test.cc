6819f0c847cf769af67135b6a4a1212ac848f58f
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2022-03-31 16:03:25 -0700

    histogram merge and diff

--- sdk/test/metrics/aggregation_test.cc
+++ sdk/test/metrics/aggregation_test.cc
@@ -90,6 +90,37 @@ TEST(Aggregation, LongHistogramAggregation)
   EXPECT_EQ(histogram_data.count_, 4);
   EXPECT_EQ(histogram_data.counts_[3], 2);
   EXPECT_EQ(histogram_data.counts_[8], 1);
+
+  // Merge
+  LongHistogramAggregation aggr1;
+  aggr1.Aggregate(1l, {});
+  aggr1.Aggregate(11l, {});
+  aggr1.Aggregate(26l, {});
+
+  LongHistogramAggregation aggr2;
+  aggr2.Aggregate(2l, {});
+  aggr2.Aggregate(3l, {});
+  aggr2.Aggregate(13l, {});
+  aggr2.Aggregate(28l, {});
+  aggr2.Aggregate(105l, {});
+
+  auto aggr3     = aggr1.Merge(aggr2);
+  histogram_data = nostd::get<HistogramPointData>(aggr3->ToPoint());
+
+  EXPECT_EQ(histogram_data.count_, 8);      // 3 each from aggr1 and aggr2
+  EXPECT_EQ(histogram_data.counts_[1], 3);  // 1, 2, 3
+  EXPECT_EQ(histogram_data.counts_[3], 2);  // 11, 13
+  EXPECT_EQ(histogram_data.counts_[4], 2);  // 25, 28
+  EXPECT_EQ(histogram_data.counts_[7], 1);  // 105
+
+  // Diff
+  auto aggr4     = aggr1.Diff(aggr2);
+  histogram_data = nostd::get<HistogramPointData>(aggr4->ToPoint());
+  EXPECT_EQ(histogram_data.count_, 2);      // aggr2:5 - aggr1:3
+  EXPECT_EQ(histogram_data.counts_[1], 1);  // aggr2(2, 3) - aggr1(1)
+  EXPECT_EQ(histogram_data.counts_[3], 0);  // aggr2(13) - aggr1(11)
+  EXPECT_EQ(histogram_data.counts_[4], 0);  // aggr2(28) - aggr1(25)
+  EXPECT_EQ(histogram_data.counts_[7], 1);  // aggr2(105) - aggr1(0)
 }
 
 TEST(Aggregation, DoubleHistogramAggregation)
@@ -116,5 +147,36 @@ TEST(Aggregation, DoubleHistogramAggregation)
   EXPECT_EQ(histogram_data.counts_[3], 2);
   EXPECT_EQ(histogram_data.counts_[8], 1);
   EXPECT_EQ(nostd::get<double>(histogram_data.sum_), 377);
+
+  // Merge
+  DoubleHistogramAggregation aggr1;
+  aggr1.Aggregate(1.0, {});
+  aggr1.Aggregate(11.0, {});
+  aggr1.Aggregate(25.1, {});
+
+  DoubleHistogramAggregation aggr2;
+  aggr2.Aggregate(2.0, {});
+  aggr2.Aggregate(3.0, {});
+  aggr2.Aggregate(13.0, {});
+  aggr2.Aggregate(28.1, {});
+  aggr2.Aggregate(105.0, {});
+
+  auto aggr3     = aggr1.Merge(aggr2);
+  histogram_data = nostd::get<HistogramPointData>(aggr3->ToPoint());
+
+  EXPECT_EQ(histogram_data.count_, 8);      // 3 each from aggr1 and aggr2
+  EXPECT_EQ(histogram_data.counts_[1], 3);  // 1.0, 2.0, 3.0
+  EXPECT_EQ(histogram_data.counts_[3], 2);  // 11.0, 13.0
+  EXPECT_EQ(histogram_data.counts_[4], 2);  // 25.1, 28.1
+  EXPECT_EQ(histogram_data.counts_[7], 1);  // 105.0
+
+  // Diff
+  auto aggr4     = aggr1.Diff(aggr2);
+  histogram_data = nostd::get<HistogramPointData>(aggr4->ToPoint());
+  EXPECT_EQ(histogram_data.count_, 2);      // aggr2:5 - aggr1:3
+  EXPECT_EQ(histogram_data.counts_[1], 1);  // aggr2(2.0, 3.0) - aggr1(1.0)
+  EXPECT_EQ(histogram_data.counts_[3], 0);  // aggr2(13.0) - aggr1(11.0)
+  EXPECT_EQ(histogram_data.counts_[4], 0);  // aggr2(28.1) - aggr1(25.1)
+  EXPECT_EQ(histogram_data.counts_[7], 1);  // aggr2(105.0) - aggr1(0)
 }
 #endif
