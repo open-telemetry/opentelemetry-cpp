df44c963a313cc310b61157887ceebbd1f443232
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-04-16 17:27:27 +0200

    metrics histogram example

--- examples/common/metrics_foo_library/foo_library.cc
+++ examples/common/metrics_foo_library/foo_library.cc
@@ -2,24 +2,43 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #ifndef ENABLE_METRICS_PREVIEW
+#  include "foo_library.h"
 #  include <chrono>
+#  include <list>
 #  include <thread>
 #  include "opentelemetry/metrics/provider.h"
 
 namespace nostd       = opentelemetry::nostd;
 namespace metrics_api = opentelemetry::metrics;
 
-void foo_library(const std::string &name)
+void foo_library::counter_example(const std::string &name)
 {
-  // Get the Meter from the MeterProvider
   auto provider                               = metrics_api::Provider::GetMeterProvider();
   nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter(name, "1.2.0");
   auto double_counter                         = meter->CreateDoubleCounter(name);
-  double_counter->Add(28.5);
-  std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  double_counter->Add(3.14);
-  std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  double_counter->Add(23.5);
-  std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+
+  while (true)
+  {
+    double val = (rand() % 700) + 1.1;
+    double_counter->Add(val);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
 }
+
+void foo_library::histogram_example(const std::string &name)
+{
+  auto provider                               = metrics_api::Provider::GetMeterProvider();
+  nostd::shared_ptr<metrics_api::Meter> meter = provider->GetMeter(name, "1.2.0");
+  auto histogram_counter                      = meter->CreateDoubleHistogram(name);
+  auto context                                = opentelemetry::context::Context{};
+  std::map<std::string, std::string> labels   = {{"key", "value"}};
+  auto labelkv = opentelemetry::common::KeyValueIterableView<decltype(labels)>{labels};
+  while (true)
+  {
+    double val = (rand() % 700) + 1.1;
+    histogram_counter->Record(val, labelkv, context);
+    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+  }
+}
+
 #endif
--- examples/metrics_simple/metrics_ostream.cc
+++ examples/metrics_simple/metrics_ostream.cc
@@ -44,21 +44,49 @@ void initMetrics(const std::string &name)
       new metric_sdk::MeterProvider(std::move(exporters)));
   auto p = std::static_pointer_cast<metric_sdk::MeterProvider>(provider);
   p->AddMetricReader(std::move(reader));
+
+  // counter view
   std::unique_ptr<metric_sdk::InstrumentSelector> instrument_selector{
       new metric_sdk::InstrumentSelector(metric_sdk::InstrumentType::kCounter, name)};
   std::unique_ptr<metric_sdk::MeterSelector> meter_selector{
       new metric_sdk::MeterSelector(name, version, schema)};
-  std::unique_ptr<metric_sdk::View> view{
+  std::unique_ptr<metric_sdk::View> sum_view{
       new metric_sdk::View{name, "description", metric_sdk::AggregationType::kSum}};
-  p->AddView(std::move(instrument_selector), std::move(meter_selector), std::move(view));
+  p->AddView(std::move(instrument_selector), std::move(meter_selector), std::move(sum_view));
+
+  // histogram view
+  std::unique_ptr<metric_sdk::InstrumentSelector> histogram_instrument_selector{
+      new metric_sdk::InstrumentSelector(metric_sdk::InstrumentType::kHistogram, name)};
+  std::unique_ptr<metric_sdk::MeterSelector> histogram_meter_selector{
+      new metric_sdk::MeterSelector(name, version, schema)};
+  std::unique_ptr<metric_sdk::View> histogram_view{
+      new metric_sdk::View{name, "description", metric_sdk::AggregationType::kHistogram}};
+  p->AddView(std::move(histogram_instrument_selector), std::move(histogram_meter_selector),
+             std::move(histogram_view));
   metrics_api::Provider::SetMeterProvider(provider);
 }
 }  // namespace
-int main()
+
+int main(int argc, char **argv)
 {
+  if (argc != 2)
+  {
+    std::puts("invalid number of command line arguments");
+    return 0;
+  }
+
+  std::string example_type(argv[1]);
   std::string name{"ostream_metric_example"};
   initMetrics(name);
-  foo_library(name);
+
+  if (example_type == "counter")
+  {
+    foo_library::counter_example(name);
+  }
+  else
+  {
+    foo_library::histogram_example(name);
+  }
 }
 #else
 int main() {}
