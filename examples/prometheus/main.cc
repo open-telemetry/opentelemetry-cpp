7807dc04f431058f344a6a8bfebd611b28bdc0c0
Oblivion <ehsan.sadroid@gmail.com>
2022-04-25 18:00:34 +0000

    comments

--- examples/prometheus/main.cc
+++ examples/prometheus/main.cc
@@ -18,78 +18,96 @@
 #    include "metrics_foo_library/foo_library.h"
 #  endif
 
-namespace metric_sdk      = opentelemetry::sdk::metrics;
-namespace nostd           = opentelemetry::nostd;
-namespace common          = opentelemetry::common;
-namespace exportermetrics = opentelemetry::exporter::metrics;
-namespace metrics_api     = opentelemetry::metrics;
+namespace metrics_sdk      = opentelemetry::sdk::metrics;
+namespace nostd            = opentelemetry::nostd;
+namespace common           = opentelemetry::common;
+namespace metrics_exporter = opentelemetry::exporter::metrics;
+namespace metrics_api      = opentelemetry::metrics;
 
 namespace
 {
 
-void initMetrics(const std::string &name)
+void initMetrics(const std::string &name, const std::string &addr)
 {
-  std::string addr{"localhost:8080"};
+  metrics_exporter::PrometheusExporterOptions opts;
+  if (!addr.empty())
+  {
+    opts.url = addr;
+  }
   std::puts("PrometheusExporter example program running ...");
 
-  std::unique_ptr<metric_sdk::MetricExporter> exporter{
-      new exportermetrics::PrometheusExporter(addr)};
-  std::vector<std::unique_ptr<metric_sdk::MetricExporter>> exporters;
+  std::unique_ptr<metrics_sdk::MetricExporter> exporter{
+      new metrics_exporter::PrometheusExporter(opts)};
+  std::vector<std::unique_ptr<metrics_sdk::MetricExporter>> exporters;
 
   std::string version{"1.2.0"};
   std::string schema{"https://opentelemetry.io/schemas/1.2.0"};
 
   // Initialize and set the global MeterProvider
-  metric_sdk::PeriodicExportingMetricReaderOptions options;
+  metrics_sdk::PeriodicExportingMetricReaderOptions options;
   options.export_interval_millis = std::chrono::milliseconds(1000);
   options.export_timeout_millis  = std::chrono::milliseconds(500);
-  std::unique_ptr<metric_sdk::MetricReader> reader{
-      new metric_sdk::PeriodicExportingMetricReader(std::move(exporter), options)};
+  std::unique_ptr<metrics_sdk::MetricReader> reader{
+      new metrics_sdk::PeriodicExportingMetricReader(std::move(exporter), options)};
   auto provider = std::shared_ptr<metrics_api::MeterProvider>(
-      new metric_sdk::MeterProvider(std::move(exporters)));
-  auto p = std::static_pointer_cast<metric_sdk::MeterProvider>(provider);
+      new metrics_sdk::MeterProvider(std::move(exporters)));
+  auto p = std::static_pointer_cast<metrics_sdk::MeterProvider>(provider);
   p->AddMetricReader(std::move(reader));
 
   // counter view
-  std::unique_ptr<metric_sdk::InstrumentSelector> instrument_selector{
-      new metric_sdk::InstrumentSelector(metric_sdk::InstrumentType::kCounter, name)};
-  std::unique_ptr<metric_sdk::MeterSelector> meter_selector{
-      new metric_sdk::MeterSelector(name, version, schema)};
-  std::unique_ptr<metric_sdk::View> sum_view{
-      new metric_sdk::View{name, "description", metric_sdk::AggregationType::kSum}};
+  std::string counter_name = name + "_counter";
+  std::unique_ptr<metrics_sdk::InstrumentSelector> instrument_selector{
+      new metrics_sdk::InstrumentSelector(metrics_sdk::InstrumentType::kCounter, counter_name)};
+  std::unique_ptr<metrics_sdk::MeterSelector> meter_selector{
+      new metrics_sdk::MeterSelector(name, version, schema)};
+  std::unique_ptr<metrics_sdk::View> sum_view{
+      new metrics_sdk::View{name, "description", metrics_sdk::AggregationType::kSum}};
   p->AddView(std::move(instrument_selector), std::move(meter_selector), std::move(sum_view));
 
   // histogram view
-  std::unique_ptr<metric_sdk::InstrumentSelector> histogram_instrument_selector{
-      new metric_sdk::InstrumentSelector(metric_sdk::InstrumentType::kHistogram, name)};
-  std::unique_ptr<metric_sdk::MeterSelector> histogram_meter_selector{
-      new metric_sdk::MeterSelector(name, version, schema)};
-  std::unique_ptr<metric_sdk::View> histogram_view{
-      new metric_sdk::View{name, "description", metric_sdk::AggregationType::kHistogram}};
+  std::string histogram_name = name + "_histogram";
+  std::unique_ptr<metrics_sdk::InstrumentSelector> histogram_instrument_selector{
+      new metrics_sdk::InstrumentSelector(metrics_sdk::InstrumentType::kHistogram, histogram_name)};
+  std::unique_ptr<metrics_sdk::MeterSelector> histogram_meter_selector{
+      new metrics_sdk::MeterSelector(name, version, schema)};
+  std::unique_ptr<metrics_sdk::View> histogram_view{
+      new metrics_sdk::View{name, "description", metrics_sdk::AggregationType::kHistogram}};
   p->AddView(std::move(histogram_instrument_selector), std::move(histogram_meter_selector),
              std::move(histogram_view));
   metrics_api::Provider::SetMeterProvider(provider);
 }
 }  // namespace
+
 int main(int argc, char **argv)
 {
-  if (argc != 2)
+  std::string example_type;
+  std::string addr{"localhost:8080"};
+  if (argc >= 2)
   {
-    std::puts("invalid number of command line arguments");
-    return 0;
+    example_type = argv[1];
+  }
+  if (argc > 2)
+  {
+    addr = argv[2];
   }
 
-  std::string example_type(argv[1]);
-  std::string name{"prometheus_example"};
-  initMetrics(name);
+  std::string name{"prometheus_metric_example"};
+  initMetrics(name, addr);
 
   if (example_type == "counter")
   {
     foo_library::counter_example(name);
   }
-  else
+  else if (example_type == "histogram")
   {
     foo_library::histogram_example(name);
+  }
+  else
+  {
+    std::thread counter_example{&foo_library::counter_example, name};
+    std::thread histogram_example{&foo_library::histogram_example, name};
+    counter_example.join();
+    histogram_example.join();
   }
 }
 #else
