a4fd8583a4cfc1aafc6c5b68ad23ff80a04d5917
Lalit <labhas@microsoft.com>
2022-06-07 23:28:16 -0700

    more changes

--- sdk/include/opentelemetry/sdk/metrics/state/observable_registry.h
+++ sdk/include/opentelemetry/sdk/metrics/state/observable_registry.h
@@ -6,7 +6,10 @@
 
 #  include "opentelemetry/sdk/metrics/async_instruments.h"
 
-#  include <unordered_set>>
+#  include <memory>
+#  include <unordered_set>
+#  include <vector>
+
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace sdk
 {
@@ -15,12 +18,34 @@ namespace metrics
 
 struct ObservableCallbackRecord
 {
-  ObservableCallbackPtr callback_;
-  std::unordered_set
+  ObservableCallbackPtr callback;
+  ObservableInstrument *instrument;
 };
 
 class ObservableRegistry
-{};
+{
+public:
+  void AddCallback(ObservableCallbackPtr callback, ObservableInstrument *instrument)
+  {
+    // TBD - Check if existing
+    auto record =
+        std::unique_ptr<ObservableCallbackRecord>(ObservableCallbackRecord{callback, instrument});
+    callbacks_.push_back(std::move(record));
+  }
+
+  void RemoveCallback(ObservableCallbackPtr callback, ObservableInstrument *instrument)
+  {
+    auto new_end = std::remove_if(
+        callbacks_.begin(), callbacks_.end(),
+        [callback, instrument](const std::unique_ptr<ObservableCallbackRecord> &record) {
+          return record->callback == callback && record->instrument == instrument;
+        });
+    callbacks_.erase(new_end, callbacks_.end());
+  }
+
+private:
+  std::vector<std::unique_ptr<ObservableCallbackRecord>> callbacks_;
+};
 
 }  // namespace metrics
 }  // namespace sdk
