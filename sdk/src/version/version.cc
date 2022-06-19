9a5a4f0e3878282e37c04b707f4949c4a0269920
Tom Tan <Tom.Tan@microsoft.com>
2022-06-19 09:32:50 -0700

    Prepare v1.4.1 patch release

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -1,5 +1,5 @@
 // Please DONOT touch this file.
-// Any changes done here would be overwritten during release/build.
+// Any changes done here would be overwritten by pre-commit git hook
 
 #include "opentelemetry/sdk/version/version.h"
 
@@ -8,17 +8,17 @@ namespace sdk
 {
 namespace version
 {
-const int MAJOR_VERSION     = 1;
-const int MINOR_VERSION     = 4;
-const int PATCH_VERSION     = 0;
-const char *PRE_RELEASE     = "";
-const char *BUILD_METADATA  = "";
-const int COUNT_NEW_COMMITS = 0;
-const char *BRANCH          = "";
-const char *COMMIT_HASH     = "";
-const char *SHORT_VERSION   = "";
-const char *FULL_VERSION    = "";
-const char *BUILD_DATE      = "";
-}  // namespace version
-}  // namespace sdk
+    const int MAJOR_VERSION = 1;
+    const int MINOR_VERSION = 4;
+    const int PATCH_VERSION = 1;
+    const char* PRE_RELEASE = "NONE";
+    const char* BUILD_METADATA = "NONE";
+    const int COUNT_NEW_COMMITS = 20;
+    const char* BRANCH = "pre_release_1.4.1";
+    const char* COMMIT_HASH = "7f4eaac3c296d5198e5b4be3a2222ade85551759";
+    const char* SHORT_VERSION = "1.4.0";
+    const char* FULL_VERSION = "1.4.0-NONE-NONE-20-pre_release_1.4.1-7f4eaac3c296d5198e5b4be3a2222ade85551759";
+    const char* BUILD_DATE = "Sun Jun 19 16:34:17 UTC 2022";
+}
+}
 OPENTELEMETRY_END_NAMESPACE
