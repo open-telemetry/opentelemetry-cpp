528dd1f24d9cd2808c2774dd3d2505b49472dd09
yuhelu <yuhelu@tencent.com>
2022-06-27 11:27:40 +0800

    Fix trace kIsSampled flag set incorrect

--- sdk/src/trace/tracer.cc
+++ sdk/src/trace/tracer.cc
@@ -64,17 +64,17 @@ nostd::shared_ptr<trace_api::Span> Tracer::StartSpan(
 
   auto sampling_result = context_->GetSampler().ShouldSample(parent_context, trace_id, name,
                                                              options.kind, attributes, links);
-  auto trace_flags     = sampling_result.decision == Decision::DROP
-                         ? trace_api::TraceFlags{}
-                         : trace_api::TraceFlags{trace_api::TraceFlags::kIsSampled};
+  auto trace_flags     = sampling_result.IsSampled()
+                             ? trace_api::TraceFlags{trace_api::TraceFlags::kIsSampled}
+                             : trace_api::TraceFlags{};
 
-  auto span_context = std::unique_ptr<trace_api::SpanContext>(new trace_api::SpanContext(
-      trace_id, span_id, trace_flags, false,
-      sampling_result.trace_state ? sampling_result.trace_state
-                                  : is_parent_span_valid ? parent_context.trace_state()
-                                                         : trace_api::TraceState::GetDefault()));
+  auto span_context = std::unique_ptr<trace_api::SpanContext>(
+      new trace_api::SpanContext(trace_id, span_id, trace_flags, false,
+                                 sampling_result.trace_state ? sampling_result.trace_state
+                                 : is_parent_span_valid      ? parent_context.trace_state()
+                                                        : trace_api::TraceState::GetDefault()));
 
-  if (sampling_result.decision == Decision::DROP)
+  if (!sampling_result.IsRecording())
   {
     // create no-op span with valid span-context.
 
