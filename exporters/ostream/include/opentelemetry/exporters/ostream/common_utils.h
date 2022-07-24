e5cf017fa99dc750c38e1f3f3109b4c08aeba69c
Ákos Hadnagy <akos.hadnagy@gmail.com>
2022-07-24 10:03:18 +0200

    Inline print_value() in ostream exporter (#1512)

--- exporters/ostream/include/opentelemetry/exporters/ostream/common_utils.h
+++ exporters/ostream/include/opentelemetry/exporters/ostream/common_utils.h
@@ -61,7 +61,8 @@ private:
 
 #endif
 
-void print_value(const opentelemetry::sdk::common::OwnedAttributeValue &value, std::ostream &sout)
+inline void print_value(const opentelemetry::sdk::common::OwnedAttributeValue &value,
+                        std::ostream &sout)
 {
 #if __cplusplus < 201402L
   opentelemetry::nostd::visit(OwnedAttributeValueVisitor(sout), value);
