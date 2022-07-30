be4a7a2b0dce8eec304d5c1d736a80cfef0c7d65
Tom Tan <Tom.Tan@microsoft.com>
2022-07-29 22:46:48 -0700

    Format version.cc from SDK

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -8,17 +8,18 @@ namespace sdk
 {
 namespace version
 {
-    const int MAJOR_VERSION = 1;
-    const int MINOR_VERSION = 5;
-    const int PATCH_VERSION = 0;
-    const char* PRE_RELEASE = "NONE";
-    const char* BUILD_METADATA = "NONE";
-    const int COUNT_NEW_COMMITS = 25;
-    const char* BRANCH = "pre_release_1.5.0";
-    const char* COMMIT_HASH = "0015475adc9aa559cd65ea814c08ec574cda141a";
-    const char* SHORT_VERSION = "1.5.0";
-    const char* FULL_VERSION = "1.5.0-NONE-NONE-25-pre_release_1.5.0-0015475adc9aa559cd65ea814c08ec574cda141a";
-    const char* BUILD_DATE = "Fri Jul 29 23:20:17 UTC 2022";
-}
-}
+const int MAJOR_VERSION     = 1;
+const int MINOR_VERSION     = 5;
+const int PATCH_VERSION     = 0;
+const char *PRE_RELEASE     = "NONE";
+const char *BUILD_METADATA  = "NONE";
+const int COUNT_NEW_COMMITS = 25;
+const char *BRANCH          = "pre_release_1.5.0";
+const char *COMMIT_HASH     = "0015475adc9aa559cd65ea814c08ec574cda141a";
+const char *SHORT_VERSION   = "1.5.0";
+const char *FULL_VERSION =
+    "1.5.0-NONE-NONE-25-pre_release_1.5.0-0015475adc9aa559cd65ea814c08ec574cda141a";
+const char *BUILD_DATE = "Fri Jul 29 23:20:17 UTC 2022";
+}  // namespace version
+}  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
