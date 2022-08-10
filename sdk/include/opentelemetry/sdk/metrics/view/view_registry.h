45037ac2a8861176d04d2ea27abde6a2cc3c2888
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-08-10 18:41:39 +0200

    Fix default Metric view name (#1515)

--- sdk/include/opentelemetry/sdk/metrics/view/view_registry.h
+++ sdk/include/opentelemetry/sdk/metrics/view/view_registry.h
@@ -64,7 +64,7 @@ public:
     // return default view if none found;
     if (!found)
     {
-      static View view("otel-default-view");
+      static const View view("");
       if (!callback(view))
       {
         return false;
