76999571551e6e9b3d76374e80fc8602089df087
Marc Alff <marc.alff@oracle.com>
2022-08-31 22:16:43 +0200

    WIP, added bazel build

--- api/test/singleton/component_b.h
+++ api/test/singleton/component_b.h
@@ -1,4 +1,8 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+// component_b is compiled with visibility("hidden"),
+// so make the entry point visible
+
+__attribute__((visibility("default")))
 void do_something_in_b();
