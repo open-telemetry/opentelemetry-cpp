38649b4fd53879fe6a1e85bea7c2b362ca1204fe
Marc Alff <marc.alff@oracle.com>
2022-09-01 10:39:20 +0200

    cleanup

--- api/test/singleton/component_b.h
+++ api/test/singleton/component_b.h
@@ -1,7 +1,4 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-// component_b is compiled with visibility("hidden"),
-// so make the entry point visible
-
-__attribute__((visibility("default"))) void do_something_in_b();
+void do_something_in_b();
