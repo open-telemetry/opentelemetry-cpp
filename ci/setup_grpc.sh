6ad45a9a69171b409514d4bc3105e507832368fe
Ehsan Saei <71217171+esigo@users.noreply.github.com>
2022-09-10 18:30:17 +0200

    gRPC latest release test

--- ci/setup_grpc.sh
+++ ci/setup_grpc.sh
@@ -8,18 +8,23 @@ export DEBIAN_FRONTEND=noninteractive
 old_grpc_version='v1.33.2'
 new_grpc_version='v1.43.2'
 gcc_version_for_new_grpc='5.1'
+std_version='11'
 install_grpc_version=${new_grpc_version}
 grpc_version='v1.39.0'
 install_dir='/usr/local/'
 usage() { echo "Usage: $0 [-v <gcc-version>] [-i <install_dir>"] 1>&2; exit 1;}
 
-while getopts ":v:i:" o; do
+while getopts ":v:i:r:" o; do
     case "${o}" in
         v)
             gcc_version=${OPTARG}
             ;;
         i)
             install_dir=${OPTARG}
+            ;;
+        r)
+            install_grpc_version=${OPTARG}
+            std_version='14'
             ;;
         *)
             usage
