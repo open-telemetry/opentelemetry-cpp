6c984815ab65e47edaba000baf6093019b564f9b
Tom Tan <Tom.Tan@microsoft.com>
2022-09-22 12:20:16 -0700

    Fix format

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -8,18 +8,18 @@ namespace sdk
 {
 namespace version
 {
-const int MAJOR_VERSION = 1;
-const int MINOR_VERSION = 6;
-const int PATCH_VERSION = 1;
-const char* PRE_RELEASE = "NONE";
-const char* BUILD_METADATA = "NONE";
+const int MAJOR_VERSION     = 1;
+const int MINOR_VERSION     = 6;
+const int PATCH_VERSION     = 1;
+const char *PRE_RELEASE     = "NONE";
+const char *BUILD_METADATA  = "NONE";
 const int COUNT_NEW_COMMITS = 27;
-const char* BRANCH = "pre_release_1.6.1";
-const char* COMMIT_HASH = "38012fab34faa50d5020959b68eba77ac485b508";
-const char* SHORT_VERSION = "1.6.1";
-const char* FULL_VERSION =
+const char *BRANCH          = "pre_release_1.6.1";
+const char *COMMIT_HASH     = "38012fab34faa50d5020959b68eba77ac485b508";
+const char *SHORT_VERSION   = "1.6.1";
+const char *FULL_VERSION =
     "1.6.0-NONE-NONE-27-pre_release_1.6.1-38012fab34faa50d5020959b68eba77ac485b508";
-const char* BUILD_DATE = "Thu Sep 22 18:05:21 UTC 2022";
+const char *BUILD_DATE = "Thu Sep 22 18:05:21 UTC 2022";
 }  // namespace version
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
