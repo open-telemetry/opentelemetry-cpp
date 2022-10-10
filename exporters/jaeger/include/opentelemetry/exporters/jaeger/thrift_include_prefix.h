01834ac79dd45ccca296c664963bb83e5666fdf7
Marc Alff <marc.alff@oracle.com>
2022-10-10 21:35:44 +0200

    Warning cleanup for Thrift-gen

---
+++ exporters/jaeger/include/opentelemetry/exporters/jaeger/thrift_include_prefix.h
@@ -0,0 +1,11 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// This file may be include multiple times, do not add #pragma once here
+
+#if defined(__GNUC__) && !defined(__clang__) && !defined(__apple_build_version__)
+#  if (__GNUC__ * 100 + __GNUC_MINOR__ * 10) >= 460
+#    pragma GCC diagnostic push
+#  endif
+#  pragma GCC diagnostic ignored "-Wsuggest-override"
+#endif
