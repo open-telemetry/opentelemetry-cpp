52f23975fef1e8546e2261f4227a1b820a5f2d05
Oblivion <ehsan.sadroid@gmail.com>
2022-10-28 19:49:01 +0000

    format

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -19,7 +19,7 @@ const char *COMMIT_HASH     = "dd7e257b6de71eeaf9e3149530962301705b9a0d";
 const char *SHORT_VERSION   = "1.7.0";
 const char *FULL_VERSION =
     "1.7.0-NONE-NONE-35-pre_release_1.7.0-dd7e257b6de71eeaf9e3149530962301705b9a0d";
-const char* BUILD_DATE = "Fri 28 Oct 2022 06:22:43 PM UTC";
+const char *BUILD_DATE = "Fri 28 Oct 2022 06:22:43 PM UTC";
 }  // namespace version
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
