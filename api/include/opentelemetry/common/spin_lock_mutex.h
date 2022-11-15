d9abb5304261dd363657c75055d85afaf64fb132
Gabriel Schwab <gschwab@lyolinlst7.ansys.com>
2022-11-15 12:32:42 -0500

    include immintrin.h when __INTEL_COMPILER defined

--- api/include/opentelemetry/common/spin_lock_mutex.h
+++ api/include/opentelemetry/common/spin_lock_mutex.h
@@ -16,8 +16,10 @@
 #  define _WINSOCKAPI_  // stops including winsock.h
 #  include <windows.h>
 #elif defined(__i386__) || defined(__x86_64__)
-#  if defined(__clang__) || defined(__INTEL_COMPILER)
+#  if defined(__clang__) 
 #    include <emmintrin.h>
+#  elif defined(__INTEL_COMPILER)
+#    include <immintrin.h>
 #  endif
 #endif
 
