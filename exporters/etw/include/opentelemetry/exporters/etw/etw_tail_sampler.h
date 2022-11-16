fe3435ce313d7cd48d6c787494feacd4ed174a55
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2022-11-16 13:03:32 -0800

    add file

---
+++ exporters/etw/include/opentelemetry/exporters/etw/etw_tail_sampler.h
@@ -0,0 +1,35 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+#include "opentelemetry/trace/span.h"
+#include "opentelemetry/sdk/trace/sampler.h"
+
+#pragma once
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace exporter
+{
+namespace etw
+{
+    
+class TailSampler
+{
+public:
+    // convert to etw span if required for getters on span.
+    // auto etw_span = static_cast<const opentelemetry::exporter::etw::Span*>(&span);
+    // Decision based on 
+    //      Span::GetStatus()
+    //      Span::GetProperties()
+    virtual opentelemetry::sdk::trace::SamplingResult ShouldSample(const opentelemetry::trace::Span &span) noexcept = 0;
+};
+
+class AlwaysOnTailSampler : public TailSampler
+{
+    public:
+    opentelemetry::sdk::trace::SamplingResult ShouldSample(const opentelemetry::trace::Span &span) noexcept override
+    {
+        return  { opentelemetry::sdk::trace::Decision::RECORD_AND_SAMPLE };
+    }
+};
+
+}  // namespace etw
+}  // namespace exporter
+OPENTELEMETRY_END_NAMESPACE
