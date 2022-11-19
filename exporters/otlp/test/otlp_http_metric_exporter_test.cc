71f8fd24d73afaadf513a41b9f391593ca743fd8
Oblivion <ehsan.sadroid@gmail.com>
2022-11-19 15:06:05 +0000

    otlp_http_metric_exporter_test leak

--- exporters/otlp/test/otlp_http_metric_exporter_test.cc
+++ exporters/otlp/test/otlp_http_metric_exporter_test.cc
@@ -23,6 +23,7 @@
 #include "opentelemetry/sdk/metrics/instruments.h"
 #include "opentelemetry/sdk/resource/resource.h"
 
+#include <google/protobuf/message_lite.h>
 #include <gtest/gtest.h>
 #include "gmock/gmock.h"
 
@@ -845,6 +846,7 @@ TEST_F(OtlpHttpMetricExporterTestPeer, ConfigJsonBytesMappingTest)
   opts.json_bytes_mapping = JsonBytesMappingKind::kHex;
   std::unique_ptr<OtlpHttpMetricExporter> exporter(new OtlpHttpMetricExporter(opts));
   EXPECT_EQ(GetOptions(exporter).json_bytes_mapping, JsonBytesMappingKind::kHex);
+  google::protobuf::ShutdownProtobufLibrary();
 }
 
 #ifndef NO_GETENV
