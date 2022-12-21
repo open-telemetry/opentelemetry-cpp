6d68105dc96b25f798420d0dc49d5875bc8a622d
github-action-benchmark <github@users.noreply.github.com>
2022-12-21 02:32:45 +0000

    add OpenTelemetry-cpp exporters Benchmark (googlecpp) benchmark result for 066c2f39272b47766ad39a030e597f980f03eedb

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1671589963939,
+  "lastUpdate": 1671589965554,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-cpp",
   "entries": {
     "OpenTelemetry-cpp api Benchmark": [
@@ -98272,6 +98272,54 @@ window.BENCHMARK_DATA = {
             "value": 2990745.9165991805,
             "unit": "ns/iter",
             "extra": "iterations: 246\ncpu: 627439.0243902439 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "066c2f39272b47766ad39a030e597f980f03eedb",
+          "message": "Bump actions/stale from 6 to 7 (#1878)\n\nBumps [actions/stale](https://github.com/actions/stale) from 6 to 7.\r\n- [Release notes](https://github.com/actions/stale/releases)\r\n- [Changelog](https://github.com/actions/stale/blob/main/CHANGELOG.md)\r\n- [Commits](https://github.com/actions/stale/compare/v6...v7)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: actions/stale\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-major\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2022-12-20T18:20:29-08:00",
+          "tree_id": "daeba1a9e216f76e90555831a94737cddaea06e4",
+          "url": "https://github.com/open-telemetry/opentelemetry-cpp/commit/066c2f39272b47766ad39a030e597f980f03eedb"
+        },
+        "date": 1671589961072,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_OtlpExporterEmptySpans",
+            "value": 115.08067774909554,
+            "unit": "ns/iter",
+            "extra": "iterations: 1219000\ncpu: 114.81460213289581 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_OtlpExporterSparseSpans",
+            "value": 171.96676704321015,
+            "unit": "ns/iter",
+            "extra": "iterations: 801000\ncpu: 171.96841448189758 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_OtlpExporterDenseSpans",
+            "value": 627.7898771572957,
+            "unit": "ns/iter",
+            "extra": "iterations: 226000\ncpu: 627.7438053097343 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_otlp_grpc_with_collector",
+            "value": 1434675.2466107698,
+            "unit": "ns/iter",
+            "extra": "iterations: 446\ncpu: 322891.25560538116 ns\nthreads: 1"
           }
         ]
       }
