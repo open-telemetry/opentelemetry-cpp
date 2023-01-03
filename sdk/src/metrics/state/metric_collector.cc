711b2ba42d672f0a0ab072ce4d7a9d46af6cb0b8
Alexey Shakula <ashakula@riverbed.com>
2023-01-03 08:38:40 -0500

    cosmetic [code format]

--- sdk/src/metrics/state/metric_collector.cc
+++ sdk/src/metrics/state/metric_collector.cc
@@ -40,13 +40,13 @@ bool MetricCollector::Collect(
   ResourceMetrics resource_metrics;
   meter_context_->ForEachMeter([&](std::shared_ptr<Meter> meter) noexcept {
     auto collection_ts = std::chrono::system_clock::now();
-    auto metric_data = meter->Collect(this, collection_ts);
-    if (!metric_data.empty ())
+    auto metric_data   = meter->Collect(this, collection_ts);
+    if (!metric_data.empty())
     {
       ScopeMetrics scope_metrics;
-      scope_metrics.metric_data_ = std::move (metric_data);
+      scope_metrics.metric_data_ = std::move(metric_data);
       scope_metrics.scope_       = meter->GetInstrumentationScope();
-      resource_metrics.scope_metric_data_.emplace_back(std::move (scope_metrics));
+      resource_metrics.scope_metric_data_.emplace_back(std::move(scope_metrics));
     }
     return true;
   });
