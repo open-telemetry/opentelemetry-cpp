92806cd08a0d932fee45ecb61714b94b70225795
owent <admin@owent.net>
2023-01-13 15:29:17 +0800

    Remove unused headers and `EmitEvent` API.
    Signed-off-by: owent <admin@owent.net>

--- api/include/opentelemetry/logs/event_logger.h
+++ api/include/opentelemetry/logs/event_logger.h
@@ -8,24 +8,14 @@
 #  include <map>
 #  include <vector>
 
-#  include "opentelemetry/common/attribute_value.h"
-#  include "opentelemetry/common/key_value_iterable.h"
-#  include "opentelemetry/common/key_value_iterable_view.h"
 #  include "opentelemetry/common/macros.h"
-#  include "opentelemetry/common/timestamp.h"
 #  include "opentelemetry/logs/log_record.h"
 #  include "opentelemetry/logs/logger.h"
 #  include "opentelemetry/logs/logger_type_traits.h"
 #  include "opentelemetry/logs/severity.h"
 #  include "opentelemetry/nostd/shared_ptr.h"
-#  include "opentelemetry/nostd/span.h"
 #  include "opentelemetry/nostd/string_view.h"
-#  include "opentelemetry/nostd/type_traits.h"
 #  include "opentelemetry/nostd/unique_ptr.h"
-#  include "opentelemetry/trace/span_context.h"
-#  include "opentelemetry/trace/span_id.h"
-#  include "opentelemetry/trace/trace_flags.h"
-#  include "opentelemetry/trace/trace_id.h"
 #  include "opentelemetry/version.h"
 
 OPENTELEMETRY_BEGIN_NAMESPACE
@@ -66,30 +56,6 @@ public:
    *
    * @param event_name Event name
    * @param log_record Log record
-   * @tparam args Arguments
-   */
-  template <class... ArgumentType>
-  void EmitEvent(nostd::string_view event_name,
-                 nostd::unique_ptr<LogRecord> &&log_record,
-                 ArgumentType &&... args)
-  {
-    if (!log_record)
-    {
-      return;
-    }
-
-    IgnoreTraitResult(
-        detail::LogRecordSetterTrait<typename std::decay<ArgumentType>::type>::template Set(
-            log_record.get(), std::forward<ArgumentType>(args))...);
-
-    EmitEvent(event_name, std::move(log_record));
-  }
-
-  /**
-   * Emit a event Log Record object with arguments
-   *
-   * @param event_name Event name
-   * @param log_record Log record
    */
   template <class... ArgumentType>
   void EmitEvent(Severity severity, nostd::string_view event_name, ArgumentType &&... args)
@@ -101,7 +67,12 @@ public:
     }
 
     log_record->SetSeverity(severity);
-    EmitEvent(event_name, std::move(log_record), std::forward<ArgumentType>(args)...);
+
+    IgnoreTraitResult(
+        detail::LogRecordSetterTrait<typename std::decay<ArgumentType>::type>::template Set(
+            log_record.get(), std::forward<ArgumentType>(args))...);
+
+    EmitEvent(event_name, std::move(log_record));
   }
 
 private:
