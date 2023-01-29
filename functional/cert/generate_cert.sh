f6fff95db85789737db3a73530085e34f6913ec9
Marc Alff <marc.alff@oracle.com>
2023-01-29 23:04:43 +0100

    Func test, continued.

--- functional/cert/generate_cert.sh
+++ functional/cert/generate_cert.sh
@@ -11,13 +11,21 @@ which cfssljson
 cfssl version
 cfssljson -version
 
+echo "Generating CA"
 cfssl genkey -initca ca_csr.json | cfssljson -bare ca
+
+echo "Generating CLIENT CERT"
 cfssl gencert -ca ca.pem -ca-key ca-key.pem client_csr.json | cfssljson -bare client_cert
+echo "Generating SERVER CERT"
 cfssl gencert -ca ca.pem -ca-key ca-key.pem server_csr.json | cfssljson -bare server_cert
 
-cfssl genkey -initca ca_csr_b.json | cfssljson -bare ca
-cfssl gencert -ca ca_b.pem -ca-key ca-key_b.pem client_csr_b.json | cfssljson -bare client_cert_b
-cfssl gencert -ca ca_b.pem -ca-key ca-key_b.pem server_csr_b.json | cfssljson -bare server_cert_b
+echo "Generating CA B"
+cfssl genkey -initca ca_csr_b.json | cfssljson -bare ca_b
+
+echo "Generating CLIENT CERT B"
+cfssl gencert -ca ca_b.pem -ca-key ca_b-key.pem client_csr_b.json | cfssljson -bare client_cert_b
+echo "Generating SERVER CERT B"
+cfssl gencert -ca ca_b.pem -ca-key ca_b-key.pem server_csr_b.json | cfssljson -bare server_cert_b
 
 echo "UNREADABLE" > unreadable.pem
 chmod 0 unreadable.pem
