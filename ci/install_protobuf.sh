beb6aa81ee324e9b2376822863c6260545d6931b
Marc Alff <marc.alff@oracle.com>
2023-01-29 21:06:53 +0100

    Protobuf tar name

--- ci/install_protobuf.sh
+++ ci/install_protobuf.sh
@@ -9,8 +9,8 @@ set -e
 
 cd /
 wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-cpp-3.${PROTOBUF_VERSION}.tar.gz
-tar zxf protobuf-cpp-${PROTOBUF_VERSION}.tar.gz --no-same-owner
-cd protobuf-${PROTOBUF_VERSION}
+tar zxf protobuf-cpp-3.${PROTOBUF_VERSION}.tar.gz --no-same-owner
+cd protobuf-3.${PROTOBUF_VERSION}
 ./configure
 make -j $(nproc) && make install
 ldconfig
