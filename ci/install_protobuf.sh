63a53e1219cd335ba621767e9ed9b4c72df97a80
Marc Alff <marc.alff@oracle.com>
2023-01-31 10:18:16 +0100

    Restored make -j, with the proper syntax : $(nproc)

--- ci/install_protobuf.sh
+++ ci/install_protobuf.sh
@@ -38,5 +38,5 @@ wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/p
 tar zxf protobuf-cpp-${CPP_PROTOBUF_VERSION}.tar.gz --no-same-owner
 cd protobuf-${CPP_PROTOBUF_VERSION}
 ./configure
-make && make install
+make -j $(nproc) && make install
 ldconfig
