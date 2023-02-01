a801f6ddea326028cce2cf149f4fb6a6ace4baa5
Lalit <labhas@microsoft.com>
2023-01-31 22:55:22 -0800

    sum aggregation

--- sdk/test/metrics/histogram_aggregation_benchmark.cc
+++ sdk/test/metrics/histogram_aggregation_benchmark.cc
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-#include <benchmark/benchmark.h>
+#include "common.h"
 
 #include "opentelemetry/sdk/metrics/data/point_data.h"
 #include "opentelemetry/sdk/metrics/meter.h"
@@ -10,55 +10,12 @@
 #include "opentelemetry/sdk/metrics/metric_reader.h"
 #include "opentelemetry/sdk/metrics/push_metric_exporter.h"
 
+#include <benchmark/benchmark.h>
 #include <random>
 
 using namespace opentelemetry;
 using namespace opentelemetry::sdk::instrumentationscope;
 using namespace opentelemetry::sdk::metrics;
-
-class MockMetricExporter : public PushMetricExporter
-{
-public:
-  MockMetricExporter() = default;
-  opentelemetry::sdk::common::ExportResult Export(
-      const ResourceMetrics & /* records */) noexcept override
-  {
-    return opentelemetry::sdk::common::ExportResult::kSuccess;
-  }
-
-  AggregationTemporality GetAggregationTemporality(
-      InstrumentType /* instrument_type */) const noexcept override
-  {
-    return AggregationTemporality::kCumulative;
-  }
-
-  bool ForceFlush(std::chrono::microseconds /* timeout */) noexcept override { return true; }
-
-  bool Shutdown(std::chrono::microseconds /* timeout */) noexcept override { return true; }
-};
-
-class MockMetricReader : public MetricReader
-{
-public:
-  MockMetricReader(std::unique_ptr<PushMetricExporter> exporter) : exporter_(std::move(exporter)) {}
-  AggregationTemporality GetAggregationTemporality(
-      InstrumentType instrument_type) const noexcept override
-  {
-    return exporter_->GetAggregationTemporality(instrument_type);
-  }
-  virtual bool OnForceFlush(std::chrono::microseconds /* timeout */) noexcept override
-  {
-    return true;
-  }
-  virtual bool OnShutDown(std::chrono::microseconds /* timeout */) noexcept override
-  {
-    return true;
-  }
-  virtual void OnInitialized() noexcept override {}
-
-private:
-  std::unique_ptr<PushMetricExporter> exporter_;
-};
 
 namespace
 {
