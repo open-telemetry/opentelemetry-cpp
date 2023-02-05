c4c8ee053f68d052f2287dd15dffcf42cc9e98a8
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2023-02-05 10:12:09 -0800

    Convert Prometheus Exporter to Pull MetricReader (#1953)

--- exporters/prometheus/test/exporter_utils_test.cc
+++ exporters/prometheus/test/exporter_utils_test.cc
@@ -83,67 +83,44 @@ void assert_histogram(prometheus_client::MetricFamily &metric,
 
 TEST(PrometheusExporterUtils, TranslateToPrometheusEmptyInputReturnsEmptyCollection)
 {
-  auto translated = PrometheusExporterUtils::TranslateToPrometheus(
-      std::vector<std::unique_ptr<metric_sdk::ResourceMetrics>>{});
+  metric_sdk::ResourceMetrics metrics_data = {};
+  auto translated = PrometheusExporterUtils::TranslateToPrometheus(metrics_data);
   ASSERT_EQ(translated.size(), 0);
 }
 
 TEST(PrometheusExporterUtils, TranslateToPrometheusIntegerCounter)
 {
-  std::vector<std::unique_ptr<metric_sdk::ResourceMetrics>> collection;
 
-  collection.emplace_back(new metric_sdk::ResourceMetrics{CreateSumPointData()});
+  metric_sdk::ResourceMetrics metrics_data = CreateSumPointData();
 
-  auto translated = PrometheusExporterUtils::TranslateToPrometheus(collection);
-  ASSERT_EQ(translated.size(), collection.size());
+  auto translated = PrometheusExporterUtils::TranslateToPrometheus(metrics_data);
+  ASSERT_EQ(translated.size(), 1);
 
   auto metric1          = translated[0];
   std::vector<int> vals = {10};
   assert_basic(metric1, "library_name", "description", prometheus_client::MetricType::Counter, 1,
                vals);
-
-  collection.emplace_back(new metric_sdk::ResourceMetrics{CreateSumPointData()});
-
-  translated = PrometheusExporterUtils::TranslateToPrometheus(collection);
-  ASSERT_EQ(translated.size(), collection.size());
-
-  auto metric2 = translated[1];
-  assert_basic(metric2, "library_name", "description", prometheus_client::MetricType::Counter, 1,
-               vals);
 }
 
 TEST(PrometheusExporterUtils, TranslateToPrometheusIntegerLastValue)
 {
-  std::vector<std::unique_ptr<metric_sdk::ResourceMetrics>> collection;
+  metric_sdk::ResourceMetrics metrics_data = CreateLastValuePointData();
 
-  collection.emplace_back(new metric_sdk::ResourceMetrics{CreateLastValuePointData()});
-
-  auto translated = PrometheusExporterUtils::TranslateToPrometheus(collection);
-  ASSERT_EQ(translated.size(), collection.size());
+  auto translated = PrometheusExporterUtils::TranslateToPrometheus(metrics_data);
+  ASSERT_EQ(translated.size(), 1);
 
   auto metric1          = translated[0];
   std::vector<int> vals = {10};
   assert_basic(metric1, "library_name", "description", prometheus_client::MetricType::Gauge, 1,
                vals);
-
-  collection.emplace_back(new metric_sdk::ResourceMetrics{CreateLastValuePointData()});
-
-  translated = PrometheusExporterUtils::TranslateToPrometheus(collection);
-  ASSERT_EQ(translated.size(), collection.size());
-
-  auto metric2 = translated[1];
-  assert_basic(metric2, "library_name", "description", prometheus_client::MetricType::Gauge, 1,
-               vals);
 }
 
 TEST(PrometheusExporterUtils, TranslateToPrometheusHistogramNormal)
 {
-  std::vector<std::unique_ptr<metric_sdk::ResourceMetrics>> collection;
+  metric_sdk::ResourceMetrics metrics_data = CreateHistogramPointData();
 
-  collection.emplace_back(new metric_sdk::ResourceMetrics{CreateHistogramPointData()});
-
-  auto translated = PrometheusExporterUtils::TranslateToPrometheus(collection);
-  ASSERT_EQ(translated.size(), collection.size());
+  auto translated = PrometheusExporterUtils::TranslateToPrometheus(metrics_data);
+  ASSERT_EQ(translated.size(), 1);
 
   auto metric              = translated[0];
   std::vector<double> vals = {3, 900.5, 4};
