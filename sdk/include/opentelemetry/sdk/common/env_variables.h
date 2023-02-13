98de43bb08a3d52a798ad1b820384e558dcaf389
Marc Alff <marc.alff@oracle.com>
2023-02-13 21:26:39 +0100

    Fixes #1859
    [EXPORTER] Boolean environment variables not parsed per the spec

--- sdk/include/opentelemetry/sdk/common/env_variables.h
+++ sdk/include/opentelemetry/sdk/common/env_variables.h
@@ -3,7 +3,9 @@
 
 #pragma once
 
+#include <chrono>
 #include <string>
+
 #include "opentelemetry/version.h"
 
 OPENTELEMETRY_BEGIN_NAMESPACE
@@ -11,6 +13,31 @@ namespace sdk
 {
 namespace common
 {
+
+/**
+  Read a boolean environment variable.
+  @param env_var_name Environment variable name
+  @param [out] value Variable value, if it exists
+  @return true if the variable exists
+*/
+bool GetBoolEnvironmentVariable(const char *env_var_name, bool &value);
+
+/**
+  Read a duration environment variable.
+  @param env_var_name Environment variable name
+  @param [out] value Variable value, if it exists
+  @return true if the variable exists
+*/
+bool GetDurationEnvironmentVariable(const char *env_var_name,
+                                    std::chrono::system_clock::duration &value);
+
+/**
+  Read a string environment variable.
+  @param env_var_name Environment variable name
+  @param [out] value Variable value, if it exists
+  @return true if the variable exists
+*/
+bool GetStringEnvironmentVariable(const char *env_var_name, std::string &value);
 
 #if defined(_MSC_VER)
 inline int setenv(const char *name, const char *value, int)
@@ -24,30 +51,6 @@ inline int unsetenv(const char *name)
 }
 #endif
 
-// Returns the env variable set.
-inline const std::string GetEnvironmentVariable(const char *env_var_name)
-{
-#if !defined(NO_GETENV)
-  const char *endpoint_from_env = nullptr;
-#  if defined(_MSC_VER)
-  // avoid calling std::getenv which is deprecated in MSVC.
-  size_t required_size = 0;
-  getenv_s(&required_size, nullptr, 0, env_var_name);
-  std::unique_ptr<char[]> endpoint_buffer;
-  if (required_size > 0)
-  {
-    endpoint_buffer = std::unique_ptr<char[]>{new char[required_size]};
-    getenv_s(&required_size, endpoint_buffer.get(), required_size, env_var_name);
-    endpoint_from_env = endpoint_buffer.get();
-  }
-#  else
-  endpoint_from_env = std::getenv(env_var_name);
-#  endif  // defined(_MSC_VER)
-  return endpoint_from_env == nullptr ? std::string{} : std::string{endpoint_from_env};
-#else
-  return std::string{};
-#endif  // !defined(NO_GETENV)
-}
 }  // namespace common
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
