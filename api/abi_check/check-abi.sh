1072c2388af6e4bf368f0af7bb9534d228aa7972
Marc Alff <marc.alff@oracle.com>
2023-02-23 18:33:14 +0100

    cleanup

--- api/abi_check/check-abi.sh
+++ api/abi_check/check-abi.sh
@@ -15,11 +15,21 @@ export PATH=${INSTALL_PREFIX}/bin:$PATH
 
 export CXX_ABI_FLAGS="-g -Og"
 
+echo "Compiling abi_check_baggage.cc ..."
+
 ${CXX} ${CXX_ABI_FLAGS} -I ../include -c abi_check_baggage.cc -o abi_check_baggage.o
+
+echo "Compiling abi_check_trace.cc ..."
+
 ${CXX} ${CXX_ABI_FLAGS} -I ../include -c abi_check_trace.cc -o abi_check_trace.o
 
-abi-dumper -lver latest abi_check_trace.o -o abi_check_trace-vlatest.dump
+echo "Dumping abi_check_baggage.o ..."
+
 abi-dumper -lver latest abi_check_baggage.o -o abi_check_baggage-vlatest.dump
+
+echo "Dumping abi_check_trace.o ..."
+
+abi-dumper -lver latest abi_check_trace.o -o abi_check_trace-vlatest.dump
 
 #
 # Do not fail CI,
@@ -28,8 +38,13 @@ abi-dumper -lver latest abi_check_baggage.o -o abi_check_baggage-vlatest.dump
 
 set +e
 
-abi-compliance-checker -l trace --old abi_check_trace-v1.0.0.dump --new abi_check_trace-vlatest.dump
+echo "Checking compliance for abi_check_baggage ..."
 
 abi-compliance-checker -l baggage --old abi_check_baggage-v1.0.0.dump --new abi_check_baggage-vlatest.dump
 
+echo "Checking compliance for abi_check_trace ..."
+
+abi-compliance-checker -l trace --old abi_check_trace-v1.0.0.dump --new abi_check_trace-vlatest.dump
+
+# Return OK to CI
 echo "Done"
