33dfbedb252cb9ca7a14bd5f3cd2e266fff469e1
Marc Alff <marc.alff@oracle.com>
2023-03-03 22:53:44 +0100

    Fixed warning about sprintf.

--- api/test/core/version_test.cc
+++ api/test/core/version_test.cc
@@ -8,8 +8,8 @@
 TEST(VersionTest, Consistency)
 {
   char expected[10];
-  sprintf(expected, "%d.%d.%d", OPENTELEMETRY_VERSION_MAJOR, OPENTELEMETRY_VERSION_MINOR,
-          OPENTELEMETRY_VERSION_PATCH);
+  snprintf(expected, sizeof(expected), "%d.%d.%d", OPENTELEMETRY_VERSION_MAJOR,
+           OPENTELEMETRY_VERSION_MINOR, OPENTELEMETRY_VERSION_PATCH);
 
   std::string actual = OPENTELEMETRY_VERSION;
   EXPECT_EQ(actual, expected);
