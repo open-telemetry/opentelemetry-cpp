aa700f4f85db59abb78979a2a66b5353acfeb8f3
Tom Tan <Tom.Tan@microsoft.com>
2023-03-06 12:30:02 -0800

    Prepare v1.8.3 release

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -11,18 +11,18 @@ namespace sdk
 {
 namespace version
 {
-const int major_version     = 1;
-const int minor_version     = 8;
-const int patch_version     = 2;
-const char *pre_release     = "NONE";
-const char *build_metadata  = "NONE";
-const int count_new_commits = 37;
-const char *branch          = "pre_release_1.8.2";
-const char *commit_hash     = "435ce60f233b6718aaa04bb7068dd641b536299b";
-const char *short_version   = "1.8.2";
-const char *full_version =
-    "1.8.2-NONE-NONE-37-pre_release_1.8.2-435ce60f233b6718aaa04bb7068dd641b536299b";
-const char *build_date = "Tue 31 Jan 2023 04:01:10 PM UTC";
+const int major_version = 1;
+const int minor_version = 8;
+const int patch_version = 3;
+const char* pre_release = "NONE";
+const char* build_metadata = "NONE";
+const int count_new_commits = 33;
+const char* branch = "pre_release_1.8.3";
+const char* commit_hash = "267c28ff22c5c380eb1897f35995961223f43a72";
+const char* short_version = "1.8.3";
+const char* full_version =
+    "1.8.2-NONE-NONE-33-pre_release_1.8.3-267c28ff22c5c380eb1897f35995961223f43a72";
+const char* build_date = "Mon Mar 6 20:46:20 UTC 2023";
 }  // namespace version
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
