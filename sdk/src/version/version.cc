89d4e0278490b72aad6734e7e0c25386afdbd2a9
Tom Tan <Tom.Tan@microsoft.com>
2023-03-07 13:21:18 -0800

    Update versions.cc

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -16,13 +16,13 @@ const int minor_version     = 8;
 const int patch_version     = 3;
 const char *pre_release     = "NONE";
 const char *build_metadata  = "NONE";
-const int count_new_commits = 33;
+const int count_new_commits = 43;
 const char *branch          = "pre_release_1.8.3";
-const char *commit_hash     = "267c28ff22c5c380eb1897f35995961223f43a72";
+const char *commit_hash     = "d011e407306991be4f0580b59d5975aecdc86aca";
 const char *short_version   = "1.8.3";
 const char *full_version =
-    "1.8.2-NONE-NONE-33-pre_release_1.8.3-267c28ff22c5c380eb1897f35995961223f43a72";
-const char *build_date = "Mon Mar 6 20:46:20 UTC 2023";
+    "1.8.2-NONE-NONE-43-pre_release_1.8.3-d011e407306991be4f0580b59d5975aecdc86aca";
+const char *build_date = "Tue Mar  7 21:15:06 UTC 2023";
 }  // namespace version
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
