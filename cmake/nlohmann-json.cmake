c214b36f956823fab9aae85cac5c056d32b627d6
Pasaoglu, Cengizhan (DI FA CTR SVC PRC3) <cengizhan.pasaoglu@siemens.com>
2023-03-29 21:57:21 +0200

    Another elseif case for found CMake target of nlohmann JSON

--- cmake/nlohmann-json.cmake
+++ cmake/nlohmann-json.cmake
@@ -33,6 +33,8 @@ find_package(nlohmann_json QUIET)
 set(nlohmann_json_clone FALSE)
 if(nlohmann_json_FOUND)
   message(STATUS "nlohmann::json dependency satisfied by: package")
+elseif(TARGET nlohmann_json) 
+  message("nlohmann_json was already added as a third party CMake target!")
 elseif(EXISTS ${PROJECT_SOURCE_DIR}/.git
        AND EXISTS
            ${PROJECT_SOURCE_DIR}/third_party/nlohmann-json/CMakeLists.txt)
