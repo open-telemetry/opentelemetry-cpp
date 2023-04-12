b2913ce493075f11d3891c1dc97d908b2aac4d14
Marc Alff <marc.alff@oracle.com>
2023-04-12 12:11:25 +0200

    Prepare release 1.9.0 (#2089)

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -12,17 +12,17 @@ namespace sdk
 namespace version
 {
 const int major_version     = 1;
-const int minor_version     = 8;
-const int patch_version     = 3;
+const int minor_version     = 9;
+const int patch_version     = 0;
 const char *pre_release     = "NONE";
 const char *build_metadata  = "NONE";
-const int count_new_commits = 43;
-const char *branch          = "pre_release_1.8.3";
-const char *commit_hash     = "d011e407306991be4f0580b59d5975aecdc86aca";
-const char *short_version   = "1.8.3";
+const int count_new_commits = 22;
+const char *branch          = "pre_release_1.9.0";
+const char *commit_hash     = "de0a42cecdb9f2d6932f864e67578769e3400a75";
+const char *short_version   = "1.9.0";
 const char *full_version =
-    "1.8.2-NONE-NONE-43-pre_release_1.8.3-d011e407306991be4f0580b59d5975aecdc86aca";
-const char *build_date = "Tue Mar  7 21:15:06 UTC 2023";
+    "1.9.0-NONE-NONE-22-pre_release_1.9.0-de0a42cecdb9f2d6932f864e67578769e3400a75";
+const char *build_date = "Wed 12 Apr 2023 10:06:17 AM UTC";
 }  // namespace version
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
