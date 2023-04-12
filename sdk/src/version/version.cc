bc545056206dee77459a4a7a95c39a326d0b72d6
Marc Alff <marc.alff@oracle.com>
2023-04-12 12:35:01 +0200

    Fix sdk/src/version/version.cc

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -16,13 +16,13 @@ const int minor_version     = 9;
 const int patch_version     = 0;
 const char *pre_release     = "NONE";
 const char *build_metadata  = "NONE";
-const int count_new_commits = 22;
+const int count_new_commits = 23;
 const char *branch          = "pre_release_1.9.0";
-const char *commit_hash     = "de0a42cecdb9f2d6932f864e67578769e3400a75";
+const char *commit_hash     = "b2913ce493075f11d3891c1dc97d908b2aac4d14";
 const char *short_version   = "1.9.0";
 const char *full_version =
-    "1.9.0-NONE-NONE-22-pre_release_1.9.0-de0a42cecdb9f2d6932f864e67578769e3400a75";
-const char *build_date = "Wed 12 Apr 2023 10:06:17 AM UTC";
+    "1.9.0-NONE-NONE-23-pre_release_1.9.0-b2913ce493075f11d3891c1dc97d908b2aac4d14";
+const char *build_date = "Wed 12 Apr 2023 10:33:02 AM UTC";
 }  // namespace version
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
