c23b5cad4be80bcd001b548caa53c9b3b22a1c12
dbolduc <dbolduc@google.com>
2023-08-18 14:26:55 -0400

    add CI test

---
+++ ci/verify_packages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+
+set -ex
+
+apt-get install pkg-config
+
+PKG_CONFIG_PATH="/usr/local/lib64/pkgconfig:${PKG_CONFIG_PATH:-}"
+
+for library in api common logs metrics resources trace version; do
+  pkg-config --validate opentelemetry_${library} --print-errors
+done
