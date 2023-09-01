b16e370bed3046a32ad72bb40b897129fc52d4fc
Dimiter 'malkia' Stanev <malkia@gmail.com>
2023-09-01 15:03:11 -0700

    testing

--- otel_sdk_build.cmd
+++ otel_sdk_build.cmd
@@ -8,8 +8,7 @@ set BAZEL_VC_FULL_VERSION=
 for /F "usebackq" %%i in (`where bazel`) do set __BAZEL__=%%i
 if "%__BAZEL__%"=="" goto:no-bazel
 
-rem Python311 is something we need to solve better here!
-set PATH=c:\windows\system32;c:\python311;c:\program files\python312;
+set PATH=c:\windows\system32;c:\python310
 pushd "%~dp0"
 
 rem Note that this builds (through the magic of force_debug/release/reldeb) all configurations (unlike tests).
@@ -17,9 +16,9 @@ rem Note that `otel_sdk.zip` is built here for the default fastdbg (e.g. when no
 "%__BAZEL__%" build --//:with_dll=true ... || goto:error
 
 rem We can't test dbg, fastbuild and opt at the same time, as done above ^^^ (no config "transition" possible when doing testing (AFAIK))
-"%__BAZEL__%" test --//:with_dll=true --test_size_filters=small,medium,large,enormous --test_timeout_filters=short,moderate,long,eternal --test_verbose_timeout_warnings -c dbg ... || goto:error
-"%__BAZEL__%" test --//:with_dll=true --test_size_filters=small,medium,large,enormous --test_timeout_filters=short,moderate,long,eternal --test_verbose_timeout_warnings -c fastbuild ... || goto:error
-"%__BAZEL__%" test --//:with_dll=true --test_size_filters=small,medium,large,enormous --test_timeout_filters=short,moderate,long,eternal --test_verbose_timeout_warnings -c opt ... || goto:error
+"%__BAZEL__%" test -k --//:with_dll=true --test_size_filters=small,medium,large,enormous --test_timeout_filters=short,moderate,long,eternal --test_verbose_timeout_warnings -c dbg ... || goto:error
+"%__BAZEL__%" test -k --//:with_dll=true --test_size_filters=small,medium,large,enormous --test_timeout_filters=short,moderate,long,eternal --test_verbose_timeout_warnings -c fastbuild ... || goto:error
+"%__BAZEL__%" test -k --//:with_dll=true --test_size_filters=small,medium,large,enormous --test_timeout_filters=short,moderate,long,eternal --test_verbose_timeout_warnings -c opt ... || goto:error
 
 for /F "usebackq" %%i in (`"%__BAZEL__%" info execution_root 2^>nul`) do set __ROOT__=%%i
 for /F "usebackq" %%i in (`"%__BAZEL__%" cquery --//:with_dll^=true otel_sdk_zip --output^=files 2^>nul`) do set __ZIP__=%%i
