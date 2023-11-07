3f3ba7cb890c845be71104ded109d683556186b2
Marc Alff <marc.alff@oracle.com>
2023-11-07 21:35:48 +0100

    Revert "Added link tests for windows DLL."
    This reverts commit 544707ed98ebb1438031da12e354ab8c1d6aa1ce.

--- ext/src/dll/dllmain.cc
+++ ext/src/dll/dllmain.cc
@@ -9,19 +9,6 @@
 #include <opentelemetry/logs/logger_provider.h>
 #include <opentelemetry/logs/provider.h>
 
-#include <opentelemetry/exporters/otlp/otlp_grpc_exporter_factory.h>
-#include <opentelemetry/exporters/otlp/otlp_grpc_exporter_options.h>
-#include <opentelemetry/exporters/otlp/otlp_grpc_log_record_exporter_factory.h>
-#include <opentelemetry/exporters/otlp/otlp_grpc_log_record_exporter_options.h>
-#include <opentelemetry/exporters/otlp/otlp_grpc_metric_exporter_factory.h>
-#include <opentelemetry/exporters/otlp/otlp_grpc_metric_exporter_options.h>
-#include <opentelemetry/exporters/otlp/otlp_http_exporter_factory.h>
-#include <opentelemetry/exporters/otlp/otlp_http_exporter_options.h>
-#include <opentelemetry/exporters/otlp/otlp_http_log_record_exporter_factory.h>
-#include <opentelemetry/exporters/otlp/otlp_http_log_record_exporter_options.h>
-#include <opentelemetry/exporters/otlp/otlp_http_metric_exporter_factory.h>
-#include <opentelemetry/exporters/otlp/otlp_http_metric_exporter_options.h>
-
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
 {
   UNREFERENCED_PARAMETER(hInstance);
@@ -29,58 +16,4 @@ extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpRes
   UNREFERENCED_PARAMETER(lpReserved);
 
   return TRUE;
-}
-
-extern std::unique_ptr<opentelemetry::sdk::trace::SpanExporter> test_otlp_http_trace()
-{
-  opentelemetry::exporter::otlp::OtlpHttpExporterOptions options;
-  options.url = "your endpoint";
-
-  auto e = opentelemetry::exporter::otlp::OtlpHttpExporterFactory::Create(options);
-  return e;
-}
-
-extern std::unique_ptr<opentelemetry::sdk::metrics::PushMetricExporter> test_otlp_http_metrics()
-{
-  opentelemetry::exporter::otlp::OtlpHttpMetricExporterOptions options;
-  options.url = "your endpoint";
-
-  auto e = opentelemetry::exporter::otlp::OtlpHttpMetricExporterFactory::Create(options);
-  return e;
-}
-
-extern std::unique_ptr<opentelemetry::sdk::logs::LogRecordExporter> test_otlp_http_logs()
-{
-  opentelemetry::exporter::otlp::OtlpHttpLogRecordExporterOptions options;
-  options.url = "your endpoint";
-
-  auto e = opentelemetry::exporter::otlp::OtlpHttpLogRecordExporterFactory::Create(options);
-  return e;
-}
-
-extern std::unique_ptr<opentelemetry::sdk::trace::SpanExporter> test_otlp_grpc_trace()
-{
-  opentelemetry::exporter::otlp::OtlpGrpcExporterOptions options;
-  options.endpoint = "your endpoint";
-
-  auto e = opentelemetry::exporter::otlp::OtlpGrpcExporterFactory::Create(options);
-  return e;
-}
-
-extern std::unique_ptr<opentelemetry::sdk::metrics::PushMetricExporter> test_otlp_grpc_metrics()
-{
-  opentelemetry::exporter::otlp::OtlpGrpcMetricExporterOptions options;
-  options.endpoint = "your endpoint";
-
-  auto e = opentelemetry::exporter::otlp::OtlpGrpcMetricExporterFactory::Create(options);
-  return e;
-}
-
-extern std::unique_ptr<opentelemetry::sdk::logs::LogRecordExporter> test_otlp_grpc_logs()
-{
-  opentelemetry::exporter::otlp::OtlpGrpcLogRecordExporterOptions options;
-  options.endpoint = "your endpoint";
-
-  auto e = opentelemetry::exporter::otlp::OtlpGrpcLogRecordExporterFactory::Create(options);
-  return e;
 }
