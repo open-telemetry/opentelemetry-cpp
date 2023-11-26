9bb9c951b3403b7499ad7c6b2e7753f2e8ebfa0a
Dimiter 'malkia' Stanev <malkia@gmail.com>
2023-11-26 00:02:26 -0800

    cleanup

--- api/include/opentelemetry/trace/noop.h
+++ api/include/opentelemetry/trace/noop.h
@@ -28,7 +28,7 @@ namespace trace
 /**
  * No-op implementation of Span. This class should not be used directly.
  */
-class OPENTELEMETRY_EXPORT NoopSpan final : public Span
+class /* OPENTELEMETRY_EXPORT */ NoopSpan final : public Span
 {
 public:
   explicit NoopSpan(const std::shared_ptr<Tracer> &tracer) noexcept
@@ -85,7 +85,7 @@ private:
 /**
  * No-op implementation of Tracer.
  */
-class OPENTELEMETRY_EXPORT NoopTracer final : public Tracer,
+class /* OPENTELEMETRY_EXPORT */ NoopTracer final : public Tracer,
                                               public std::enable_shared_from_this<NoopTracer>
 {
 public:
@@ -110,7 +110,7 @@ public:
 /**
  * No-op implementation of a TracerProvider.
  */
-class OPENTELEMETRY_EXPORT NoopTracerProvider final : public trace::TracerProvider
+class /* OPENTELEMETRY_EXPORT */ NoopTracerProvider final : public trace::TracerProvider
 {
 public:
   NoopTracerProvider() noexcept
