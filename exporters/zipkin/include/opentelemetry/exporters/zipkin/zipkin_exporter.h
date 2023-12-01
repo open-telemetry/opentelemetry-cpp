8f1e81f646dd9e09d7b1d06e826d343962945d2c
Tom Tan <Tom.Tan@microsoft.com>
2023-11-30 16:47:00 -0800

    Wrap min and max as function call in parenthesises

--- exporters/zipkin/include/opentelemetry/exporters/zipkin/zipkin_exporter.h
+++ exporters/zipkin/include/opentelemetry/exporters/zipkin/zipkin_exporter.h
@@ -55,14 +55,14 @@ public:
    * @return return true when all data are exported, and false when timeout
    */
   bool ForceFlush(
-      std::chrono::microseconds timeout = std::chrono::microseconds::max()) noexcept override;
+      std::chrono::microseconds timeout = (std::chrono::microseconds::max)()) noexcept override;
 
   /**
    * Shut down the exporter.
    * @param timeout an optional timeout, default to max.
    */
   bool Shutdown(
-      std::chrono::microseconds timeout = std::chrono::microseconds::max()) noexcept override;
+      std::chrono::microseconds timeout = (std::chrono::microseconds::max)()) noexcept override;
 
 private:
   void InitializeLocalEndpoint();
