0c82bb999beaecc2f5989efa969fc128fae0b53e
Tom Tan <Tom.Tan@microsoft.com>
2023-12-07 14:34:59 -0800

    [BUILD] Accept path list in OPENTELEMETRY_EXTERNAL_COMPONENT_PATH

--- cmake/opentelemetry-build-external-component.cmake
+++ cmake/opentelemetry-build-external-component.cmake
@@ -3,8 +3,8 @@
 
 # Enable building external components through otel-cpp build
 # The config options are
-#  - OPENTELEMETRY_EXTERNAL_COMPONENT_PATH - Setting local path of the external
-# component as env variable
+#  - OPENTELEMETRY_EXTERNAL_COMPONENT_PATH - Setting local paths of the external
+# component as env variable. Multiple paths can be set by separating them with.
 #  - OPENTELEMETRY_EXTERNAL_COMPONENT_URL Setting github-repo of external component
 #  as env variable
 
@@ -13,13 +13,16 @@
 if(OPENTELEMETRY_EXTERNAL_COMPONENT_PATH)
   # Add custom component path to build tree and consolidate binary artifacts in
   # current project binary output directory.
-  add_subdirectory(${OPENTELEMETRY_EXTERNAL_COMPONENT_PATH}
-                   ${PROJECT_BINARY_DIR}/external)
+  foreach(DIR IN LSITS OPENTELEMETRY_EXTERNAL_COMPONENT_PATH)
+    add_subdirectory(${DIR} ${PROJECT_BINARY_DIR}/external)
+  endforeach()
 elseif(DEFINED ENV{OPENTELEMETRY_EXTERNAL_COMPONENT_PATH})
   # Add custom component path to build tree and consolidate binary artifacts in
   # current project binary output directory.
-  add_subdirectory($ENV{OPENTELEMETRY_EXTERNAL_COMPONENT_PATH}
-                   ${PROJECT_BINARY_DIR}/external)
+  set(OPENTELEMETRY_EXTERNAL_COMPONENT_PATH_VAR $ENV{OPENTELEMETRY_EXTERNAL_COMPONENT_PATH})
+  foreach(DIR IN LSITS OPENTELEMETRY_EXTERNAL_COMPONENT_PATH_VAR)
+    add_subdirectory(${DIR} ${PROJECT_BINARY_DIR}/external)
+  endforeach()
 elseif(DEFINED $ENV{OPENTELEMETRY_EXTERNAL_COMPONENT_URL})
   # This option requires CMake 3.11+: add standard remote repo to build tree.
   include(FetchContent)
