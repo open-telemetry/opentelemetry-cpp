3c769653ee7ee269690945b41ff2d49da686b6d8
Tom Tan <totan@microsoft.com>
2023-12-13 21:55:49 -0800

    Update chagnelog

--- api/test/nostd/string_view_test.cc
+++ api/test/nostd/string_view_test.cc
@@ -71,7 +71,7 @@ TEST(StringViewTest, SubstrPortion)
 TEST(StringViewTest, SubstrOutOfRange)
 {
   string_view s = "abc123";
-#if __EXCEPTIONS || ((defined(OPENTELEMETRY_STL_VERSION) && (OPENTELEMETRY_STL_VERSION >= 2020)))
+#if __EXCEPTIONS || (defined(OPENTELEMETRY_STL_VERSION) && (OPENTELEMETRY_STL_VERSION >= 2020))
   EXPECT_THROW((void)s.substr(10), std::out_of_range);
 #else
   EXPECT_DEATH({ s.substr(10); }, "");
