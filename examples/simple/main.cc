d1dbf8f981f194047f3f05e55bea5cf93f84634f
Mats Taraldsvik <mats.taraldsvik@gmail.com>
2024-01-02 12:31:30 +0100

    Update main.cc

--- examples/simple/main.cc
+++ examples/simple/main.cc
@@ -8,42 +8,17 @@
 #include "opentelemetry/sdk/trace/tracer_provider_factory.h"
 #include "opentelemetry/trace/provider.h"
 
-//#ifdef BAZEL_BUILD
-//#  include "examples/common/foo_library/foo_library.h"
-//#else
-//#  include "foo_library/foo_library.h"
-//#endif
-
-namespace trace_api      = opentelemetry::trace;
-namespace trace_sdk      = opentelemetry::sdk::trace;
+namespace trace_api = opentelemetry::trace;
+namespace trace_sdk = opentelemetry::sdk::trace;
 namespace trace_exporter = opentelemetry::exporter::trace;
 
-namespace
-{
-void InitTracer()
-{
-  auto exporter  = trace_exporter::OStreamSpanExporterFactory::Create();
-  auto processor = trace_sdk::SimpleSpanProcessorFactory::Create(std::move(exporter));
-  std::shared_ptr<opentelemetry::trace::TracerProvider> provider =
-      trace_sdk::TracerProviderFactory::Create(std::move(processor));
+int main() {
+	auto exporter = trace_exporter::OStreamSpanExporterFactory::Create();
+	auto processor = trace_sdk::SimpleSpanProcessorFactory::Create(std::move(exporter));
+	std::shared_ptr<opentelemetry::trace::TracerProvider> provider = trace_sdk::TracerProviderFactory::Create(std::move(processor));
 
-  // Set the global trace provider
-  trace_api::Provider::SetTracerProvider(provider);
-}
+	// Set the global trace provider
+	trace_api::Provider::SetTracerProvider(provider);
 
-void CleanupTracer()
-{
-  std::shared_ptr<opentelemetry::trace::TracerProvider> none;
-  trace_api::Provider::SetTracerProvider(none);
-}
-}  // namespace
-
-int main()
-{
-  // Removing this line will leave the default noop TracerProvider in place.
-  InitTracer();
-
-  // foo_library();
-
-  CleanupTracer();
+	return 0;
 }
