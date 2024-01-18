697794d098af57cb582c19dd51bda79d2d446ed1
bcsgh <33939446+bcsgh@users.noreply.github.com>
2024-01-18 10:48:28 -0800

    Make formatting happy.

--- api/include/opentelemetry/version.h
+++ api/include/opentelemetry/version.h
@@ -7,9 +7,16 @@
 #include "opentelemetry/detail/preprocessor.h"
 
 #if defined(OPENTELEMETRY_STL_VERSION)
-#  if (!defined(_MSVC_LANG) && OPENTELEMETRY_STL_VERSION > (__cplusplus/100)) || \
-      (defined(_MSVC_LANG) && OPENTELEMETRY_STL_VERSION > (_MSVC_LANG/100))
-#    pragma message OPENTELEMETRY_STRINGIFY(OPENTELEMETRY_STL_VERSION) " vs. " OPENTELEMETRY_STRINGIFY(__cplusplus)
+#  if (!defined(_MSVC_LANG) && OPENTELEMETRY_STL_VERSION > (__cplusplus / 100)) || \
+      (defined(_MSVC_LANG) && OPENTELEMETRY_STL_VERSION > (_MSVC_LANG / 100))
+#    if defined(_MSVC_LANG)
+#      pragma message OPENTELEMETRY_STRINGIFY( \
+          OPENTELEMETRY_STL_VERSION) " vs. " OPENTELEMETRY_STRINGIFY(_MSVC_LANG)
+#    else
+#      pragma message OPENTELEMETRY_STRINGIFY( \
+          OPENTELEMETRY_STL_VERSION) " vs. " OPENTELEMETRY_STRINGIFY( \
+          __cplusplus)
+#    endif
 #    error "OPENTELEMETRY_STL_VERSION is set to a version newer than the curent C++ version."
 #  endif
 #endif
