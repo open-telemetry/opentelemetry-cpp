b92dad2e2ba75447b8acb9c1c13ce26fcc19fe5e
Mats Taraldsvik <mats.taraldsvik@gmail.com>
2024-01-19 07:35:25 +0100

    only sdk and exporters folders should be searched for lib exports

--- ext/src/dll/make_def.ps1
+++ ext/src/dll/make_def.ps1
@@ -5,4 +5,4 @@ param ($dumpbin, $bindir, $inputfile, $targetfile)
    
  "LIBRARY opentelemetry_cpp`r`nEXPORTS`r`n" > $targetfile
     
- Get-ChildItem -Verbose -Path $bindir/*.lib -Recurse | % { & "$dumpbin" /SYMBOLS $_ | Select-String -Pattern @(Get-Content -Verbose -Path "$inputfile" | Where-Object { $_.Trim() -ne '' } | % { "External\s+\|\s+(\?+[0-9]?$_[^\s]*)\s+\((.*)\)$" }) | % { "; $($_.matches.groups[2])`r`n$($_.matches.groups[1])" } >> $targetfile }
+ Get-ChildItem -Verbose -Path $bindir/sdk/*,$bindir/exporters/* -Include *.lib -Recurse | % { & "$dumpbin" /SYMBOLS $_ | Select-String -Pattern @(Get-Content -Verbose -Path "$inputfile" | Where-Object { $_.Trim() -ne '' } | % { "External\s+\|\s+(\?+[0-9]?$_[^\s]*)\s+\((.*)\)$" }) | % { "; $($_.matches.groups[2])`r`n$($_.matches.groups[1])" } >> $targetfile }
