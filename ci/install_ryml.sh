172e212c54253b1059347ab398edfbe09f5fd7ce
Marc Alff <marc.alff@oracle.com>
2024-02-18 01:01:59 +0100

    CI

--- ci/install_ryml.sh
+++ ci/install_ryml.sh
@@ -5,7 +5,7 @@
 
 set -ex
 export DEBIAN_FRONTEND=noninteractive
-[ -z "${RYML_VERSION}" ] && export RYML_VERSION="0.5.0"
+[ -z "${RYML_VERSION}" ] && export RYML_VERSION="v0.5.0"
 
 BUILD_DIR=/tmp/
 INSTALL_DIR=/usr/local/
