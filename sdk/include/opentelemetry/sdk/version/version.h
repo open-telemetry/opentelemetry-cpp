1b53e05eb939910d981924367451e4f948178c66
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-02-27 11:10:22 -0800

    make it a patch release

--- sdk/include/opentelemetry/sdk/version/version.h
+++ sdk/include/opentelemetry/sdk/version/version.h
@@ -5,7 +5,7 @@
 
 #include "opentelemetry/detail/preprocessor.h"
 
-#define OPENTELEMETRY_SDK_VERSION "1.15.0"
+#define OPENTELEMETRY_SDK_VERSION "1.14.2"
 
 #include "opentelemetry/version.h"
 
--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -12,12 +12,12 @@ namespace sdk
 namespace version
 {
 const int major_version    = 1;
-const int minor_version    = 15;
-const int patch_version    = 0;
+const int minor_version    = 14;
+const int patch_version    = 2;
 const char *pre_release    = "NONE";
 const char *build_metadata = "NONE";
-const char *short_version  = "1.15.0";
-const char *full_version   = "1.15.0-NONE-NONE";
+const char *short_version  = "1.14.2";
+const char *full_version   = "1.14.2-NONE-NONE";
 const char *build_date     = "Tue Feb 27 18:25:51 UTC 2024";
 }  // namespace version
 }  // namespace sdk
