d76710a5a7cb26e901568065c9a658ab6ee97941
Tom Tan <totan@microsoft.com>
2024-03-26 16:11:17 -0700

    One more macro guard

--- sdk/include/opentelemetry/sdk/metrics/meter.h
+++ sdk/include/opentelemetry/sdk/metrics/meter.h
@@ -17,9 +17,9 @@
 #include "opentelemetry/nostd/unique_ptr.h"
 #include "opentelemetry/sdk/common/attributemap_hash.h"
 #include "opentelemetry/sdk/instrumentationscope/instrumentation_scope.h"
-#include "opentelemetry/sdk/metrics/aggregation/aggregation_config.h"
 
 #if ENABLE_METRICS_EXEMPLAR_PREVIEW
+#include "opentelemetry/sdk/metrics/aggregation/aggregation_config.h"
 #include "opentelemetry/sdk/metrics/exemplar/aligned_histogram_bucket_exemplar_reservoir.h"
 #include "opentelemetry/sdk/metrics/exemplar/reservoir.h"
 #include "opentelemetry/sdk/metrics/exemplar/simple_fixed_size_exemplar_reservoir.h"
