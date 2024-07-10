f9d636fb76d7085e8dfaff3747b3b012d7c1a50f
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-07-10 13:59:38 -0700

    template specialization for const char *, and varous string type tests

--- sdk/test/metrics/attributes_hashmap_test.cc
+++ sdk/test/metrics/attributes_hashmap_test.cc
@@ -143,3 +143,32 @@ TEST(AttributesHashMap, HashWithKeyValueIterable)
   // Expect the hashes to be the same because the content is the same
   EXPECT_EQ(hash1, hash3);
 }
+
+
+TEST(AttributesHashMap, HashConsistencyAcrossStringTypes)
+{
+  const char* c_str = "teststring";
+  std::string std_str = "teststring";
+  nostd::string_view nostd_str_view = "teststring";
+  #if __cplusplus >= 201703L
+  std::string_view std_str_view = "teststring";
+  #endif
+
+  size_t hash_c_str = 0;
+  size_t hash_std_str = 0;
+  size_t hash_nostd_str_view = 0;
+  size_t hash_std_str_view = 0;
+
+  opentelemetry::sdk::common::GetHash(hash_c_str, c_str);
+  opentelemetry::sdk::common::GetHash(hash_std_str, std_str);
+  opentelemetry::sdk::common::GetHash(hash_nostd_str_view, nostd_str_view);
+  #if __cplusplus >= 201703L
+  opentelemetry::sdk::common::GetHash(hash_std_str_view, std_str_view);
+  #endif
+
+  EXPECT_EQ(hash_c_str, hash_std_str);
+  EXPECT_EQ(hash_c_str, hash_nostd_str_view);
+  #if __cplusplus >= 201703L
+  EXPECT_EQ(hash_c_str, hash_std_str_view);
+  #endif
+}
