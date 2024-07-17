d8ae09e53d6988a063364c86a8362053432a3f9f
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-07-17 07:00:20 -0700

    [Metrics SDK] Fix hash calculation for nostd::string  (#2999)
    * add test to validate cardinaity limit
    
    * format
    
    * warning
    
    * Format
    
    * maint mode CI
    
    * modify test to reproduce the issue
    
    * Format
    
    * remove vscode settings
    
    * remove redundant test
    
    * remove unused code
    
    * fix to calculate hash on string_view
    
    * remove iostream
    
    * template specialization for const char *, and varous string type tests
    
    * unused variable warning
    
    * more warnings
    
    * format
    
    * fix use-after-stack-scope
    
    * format
    
    * another try
    
    * format
    
    ---------
    
    Co-authored-by: Tom Tan <Tom.Tan@microsoft.com>

--- sdk/test/metrics/attributes_hashmap_test.cc
+++ sdk/test/metrics/attributes_hashmap_test.cc
@@ -13,7 +13,6 @@ namespace nostd = opentelemetry::nostd;
 
 TEST(AttributesHashMap, BasicTests)
 {
-
   // Empty map
   AttributesHashMap hash_map;
   EXPECT_EQ(hash_map.Size(), 0);
@@ -72,4 +71,79 @@ TEST(AttributesHashMap, BasicTests)
         return true;
       });
   EXPECT_EQ(count, hash_map.Size());
+}
+
+std::string make_unique_string(const char *str)
+{
+  return std::string(str);
+}
+
+TEST(AttributesHashMap, HashWithKeyValueIterable)
+{
+  std::string key1   = make_unique_string("k1");
+  std::string value1 = make_unique_string("v1");
+  std::string key2   = make_unique_string("k2");
+  std::string value2 = make_unique_string("v2");
+  std::string key3   = make_unique_string("k3");
+  std::string value3 = make_unique_string("v3");
+
+  // Create mock KeyValueIterable instances with the same content but different variables
+  std::map<std::string, std::string> attributes1({{key1, value1}, {key2, value2}});
+  std::map<std::string, std::string> attributes2({{key1, value1}, {key2, value2}});
+  std::map<std::string, std::string> attributes3({{key1, value1}, {key2, value2}, {key3, value3}});
+
+  // Create a callback that filters "k3" key
+  auto is_key_filter_k3_callback = [](nostd::string_view key) {
+    if (key == "k3")
+    {
+      return false;
+    }
+    return true;
+  };
+  // Calculate hash
+  size_t hash1 = opentelemetry::sdk::common::GetHashForAttributeMap(
+      opentelemetry::common::KeyValueIterableView<std::map<std::string, std::string>>(attributes1),
+      is_key_filter_k3_callback);
+  size_t hash2 = opentelemetry::sdk::common::GetHashForAttributeMap(
+      opentelemetry::common::KeyValueIterableView<std::map<std::string, std::string>>(attributes2),
+      is_key_filter_k3_callback);
+
+  size_t hash3 = opentelemetry::sdk::common::GetHashForAttributeMap(
+      opentelemetry::common::KeyValueIterableView<std::map<std::string, std::string>>(attributes3),
+      is_key_filter_k3_callback);
+
+  // Expect the hashes to be the same because the content is the same
+  EXPECT_EQ(hash1, hash2);
+  // Expect the hashes to be the same because the content is the same
+  EXPECT_EQ(hash1, hash3);
+}
+
+TEST(AttributesHashMap, HashConsistencyAcrossStringTypes)
+{
+  const char *c_str                 = "teststring";
+  std::string std_str               = "teststring";
+  nostd::string_view nostd_str_view = "teststring";
+#if __cplusplus >= 201703L
+  std::string_view std_str_view = "teststring";
+#endif
+
+  size_t hash_c_str          = 0;
+  size_t hash_std_str        = 0;
+  size_t hash_nostd_str_view = 0;
+#if __cplusplus >= 201703L
+  size_t hash_std_str_view = 0;
+#endif
+
+  opentelemetry::sdk::common::GetHash(hash_c_str, c_str);
+  opentelemetry::sdk::common::GetHash(hash_std_str, std_str);
+  opentelemetry::sdk::common::GetHash(hash_nostd_str_view, nostd_str_view);
+#if __cplusplus >= 201703L
+  opentelemetry::sdk::common::GetHash(hash_std_str_view, std_str_view);
+#endif
+
+  EXPECT_EQ(hash_c_str, hash_std_str);
+  EXPECT_EQ(hash_c_str, hash_nostd_str_view);
+#if __cplusplus >= 201703L
+  EXPECT_EQ(hash_c_str, hash_std_str_view);
+#endif
 }
