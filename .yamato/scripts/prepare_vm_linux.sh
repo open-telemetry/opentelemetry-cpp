7148965e482ea1ccd66f424ace11c2e1448ac2c6
chp9-u <63417401+chp9-u@users.noreply.github.co>
2024-08-09 14:01:28 -0400

    Fixed clang command line

--- .yamato/scripts/prepare_vm_linux.sh
+++ .yamato/scripts/prepare_vm_linux.sh
@@ -6,11 +6,12 @@ set -euxo pipefail
 # ===================================
 .yamato/bee steve internal-unpack public 7za-linux-x64/904b71ca3f79_0d0f2cf7ea23d00d2ba5d151c735ef9f03ed99a9e451c6dcf3a198cbedf69861.zip 7z
 export BEE_INTERNAL_STEVEDORE_7ZA=7z/7za
-.yamato/bee steve internal-unpack public clang-centos-x64-9/9a8bc1d-7.7.1908_a100ab2e73351d22fc99337de9cbb4644f0bcfdfa0e21b7b2670573232db7539.7z TC
+.yamato/bee steve internal-unpack public toolchain-llvm-centos-amd64/9.0.1-7.7.1908_9a2f0527b0c16f2524d24e372c7ebb8735e2e85ec99b6914f2a0a00c1e74d34d.7z TC
 .yamato/bee steve internal-unpack public linux-sysroot-amd64/glibc2.17_21db091a7ba6f278ece53e4afe50f840f2ba704112aaed9562fbd86421e4ce3b.7z sysroot
-echo -e '#!/bin/bash\nexec PWD/ls /clang --sysroot="PWD/sysroot" --gcc-toolchain="PWD/sysroot/usr" -I=/usr/include/c++/11 -target x86_64-glibc2.17-linux-gnu $*' > clang
+echo -e '#!/bin/bash\nexec PWD/TC/bin/clang --sysroot="PWD/sysroot" --gcc-toolchain="PWD/sysroot/usr" -I=/usr/include/c++/9.1.0 -target x86_64-glibc2.17-linux-gnu $*' > clang
 sed -i -e "s|PWD|`pwd`|g" clang
 chmod +x clang
+export COMPILATION_MODE=opt # defaults to fastbuild
 export CC=`pwd`/clang
 export CXX=`pwd`/clang
 
