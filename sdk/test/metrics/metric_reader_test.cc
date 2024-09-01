7f631d90035ab0e05a8498f399e3993ff532df73
Punya Biswal <punya.biswal@gmail.com>
2024-09-01 11:01:39 -0400

    Update MetricProducer interface to match spec
    Following https://github.com/open-telemetry/opentelemetry-specification/blob/v1.36.0/specification/metrics/sdk.md#metricproducer.
    
    Updates #1831

--- sdk/test/metrics/metric_reader_test.cc
+++ sdk/test/metrics/metric_reader_test.cc
@@ -25,5 +25,5 @@ TEST(MetricReaderTest, BasicTests)
   std::shared_ptr<MeterContext> meter_context2(new MeterContext());
   std::shared_ptr<MetricProducer> metric_producer{
       new MetricCollector(meter_context2.get(), std::move(metric_reader2))};
-  metric_producer->Collect([](ResourceMetrics & /* metric_data */) { return true; });
+  metric_producer->Produce();
 }
