b0732da5dcdc0d972115249d6a15917a3f6cd8c2
Pranav Sharma <sharmapranav@google.com>
2024-10-30 21:39:58 +0000

    Add TrimString function

---
+++ api/test/trace/propagation/detail/string_test.cc
@@ -0,0 +1,13 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#include <gtest/gtest.h>
+#include <stdint.h>
+#include <string>
+
+#include "opentelemetry/nostd/string_view.h"
+
+TEST(StringTest, TrimStrings)
+{
+  //TODO(psx95): Add tests
+}
