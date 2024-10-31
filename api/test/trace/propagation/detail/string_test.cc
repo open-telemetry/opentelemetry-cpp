ffab2ee2407e9b619edc5fd6f7c69fa626283140
Pranav Sharma <sharmapranav@google.com>
2024-10-31 21:55:00 +0000

    Replace parameterized test with standard one

--- api/test/trace/propagation/detail/string_test.cc
+++ api/test/trace/propagation/detail/string_test.cc
@@ -7,70 +7,60 @@
 
 #include "opentelemetry/nostd/string_view.h"
 
-struct SplitStringTestData
+using namespace opentelemetry;
+
+// struct SplitStringTestData
+//{
+//   opentelemetry::nostd::string_view input;
+//   char separator;
+//   size_t max_count;
+//   std::vector<opentelemetry::nostd::string_view> splits;
+//   size_t expected_number_strings;
+// };
+//
+// const SplitStringTestData split_string_test_cases[] = {
+//     {"foo,bar,baz", ',', 4, std::vector<opentelemetry::nostd::string_view>{"foo", "bar", "baz"},
+//     3},
+//     {"foo,bar,baz,foobar", ',', 4,
+//      std::vector<opentelemetry::nostd::string_view>{"foo", "bar", "baz", "foobar"}, 4},
+//     {"foo,bar,baz,foobar", '.', 4,
+//      std::vector<opentelemetry::nostd::string_view>{"foo,bar,baz,foobar"}, 1},
+//     {"foo,bar,baz,", ',', 4,
+//      std::vector<opentelemetry::nostd::string_view>{"foo", "bar", "baz", ""}, 4},
+//     {"foo,bar,baz,", ',', 2, std::vector<opentelemetry::nostd::string_view>{"foo", "bar"}, 2},
+//     {"foo ,bar, baz ", ',', 4,
+//      std::vector<opentelemetry::nostd::string_view>{"foo ", "bar", " baz "}, 3},
+//     {"foo ,bar, baz ", ',', 4,
+//      std::vector<opentelemetry::nostd::string_view>{"foo ", "bar", " baz "}, 3},
+//     {"00-0af7651916cd43dd8448eb211c80319c-00f067aa0ba902b7-01", '-', 4,
+//      std::vector<opentelemetry::nostd::string_view>{"00", "0af7651916cd43dd8448eb211c80319c",
+//                                                     "00f067aa0ba902b7", "01"},
+//      4},
+// };
+//
+//
+// TEST(StringTest, SplitStringTest)
+//{
+//   for (auto &test_param : split_string_test_cases) {
+//     std::vector<opentelemetry::nostd::string_view> fields{};
+//     fields.reserve(test_param.expected_number_strings);
+//     size_t got_splits_num = opentelemetry::trace::propagation::detail::SplitString(
+//         test_param.input, test_param.separator, fields.data(), test_param.max_count);
+//
+//     // Assert on the output
+//     EXPECT_EQ(got_splits_num, test_param.expected_number_strings);
+//     for (size_t i = 0; i < got_splits_num; i++)
+//     {
+//       // Checks for resulting strings in-order
+//       EXPECT_EQ(fields[i], test_param.splits[i]);
+//     }
+//   }
+// }
+
+TEST(StringTest, SimpleTest)
 {
-  opentelemetry::nostd::string_view input;
-  char separator;
-  size_t max_count;
-  std::vector<opentelemetry::nostd::string_view> splits;
-  size_t expected_number_strings;
-
-  // When googletest registers parameterized tests, it uses this method to format the parameters.
-  // The default implementation prints hex dump of all bytes in the object. If there is any padding
-  // in these bytes, valgrind reports this as a warning - "Use of uninitialized bytes".
-  // See https://github.com/google/googletest/issues/3805.
-  friend void PrintTo(const SplitStringTestData& data, std::ostream* os) {
-    std::stringstream ss;
-    for (auto it = data.splits.begin(); it != data.splits.end(); it++) {
-      if (it != data.splits.begin()) {
-        ss << ",";
-      }
-      ss << *it;
-    }
-    *os << "(" << data.input << "," << data.separator << "," << data.max_count << "," << ss.str() << "," << data.expected_number_strings << ")";
-  }
-};
-
-const SplitStringTestData split_string_test_cases[] = {
-    {"foo,bar,baz", ',', 4, std::vector<opentelemetry::nostd::string_view>{"foo", "bar", "baz"}, 3},
-    {"foo,bar,baz,foobar", ',', 4,
-     std::vector<opentelemetry::nostd::string_view>{"foo", "bar", "baz", "foobar"}, 4},
-    {"foo,bar,baz,foobar", '.', 4,
-     std::vector<opentelemetry::nostd::string_view>{"foo,bar,baz,foobar"}, 1},
-    {"foo,bar,baz,", ',', 4,
-     std::vector<opentelemetry::nostd::string_view>{"foo", "bar", "baz", ""}, 4},
-    {"foo,bar,baz,", ',', 2, std::vector<opentelemetry::nostd::string_view>{"foo", "bar"}, 2},
-    {"foo ,bar, baz ", ',', 4,
-     std::vector<opentelemetry::nostd::string_view>{"foo ", "bar", " baz "}, 3},
-    {"foo ,bar, baz ", ',', 4,
-     std::vector<opentelemetry::nostd::string_view>{"foo ", "bar", " baz "}, 3},
-    {"00-0af7651916cd43dd8448eb211c80319c-00f067aa0ba902b7-01", '-', 4,
-     std::vector<opentelemetry::nostd::string_view>{"00", "0af7651916cd43dd8448eb211c80319c",
-                                                    "00f067aa0ba902b7", "01"},
-     4},
-};
-
-// Test fixture
-class SplitStringTestFixture : public ::testing::TestWithParam<SplitStringTestData>
-{};
-
-TEST_P(SplitStringTestFixture, SplitsAsExpected)
-{
-  SplitStringTestData test_param = GetParam();
-  std::vector<opentelemetry::nostd::string_view> fields{};
-  fields.reserve(test_param.expected_number_strings);
-  size_t got_splits_num = opentelemetry::trace::propagation::detail::SplitString(
-      test_param.input, test_param.separator, fields.data(), test_param.max_count);
-
-  // Assert on the output
-  EXPECT_EQ(got_splits_num, test_param.expected_number_strings);
-  for (size_t i = 0; i < got_splits_num; i++)
-  {
-    // Checks for resulting strings in-order
-    EXPECT_EQ(fields[i], test_param.splits[i]);
-  }
+  nostd::string_view input = "foo,bar,baz";
+  std::array<nostd::string_view, 4> fields{};
+  size_t got_splits = ::trace::propagation::detail::SplitString(input, ',', fields.data(), 4);
+  EXPECT_EQ(3, got_splits);
 }
-
-INSTANTIATE_TEST_SUITE_P(SplitStringTestCases,
-                         SplitStringTestFixture,
-                         ::testing::ValuesIn(split_string_test_cases));
