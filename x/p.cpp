0c6bf4473697d97673f1150b13ce75cd3369c265
Dimiter 'malkia' Stanev <malkia@gmail.com>
2024-11-17 11:31:52 -0800

    small example of using the proxy

---
+++ x/p.cpp
@@ -0,0 +1,18 @@
+#include <memory>
+#include <opentelemetry/exporters/otlp/otlp_grpc_forward_proxy.h>
+
+int main()
+{
+    using namespace opentelemetry::exporter::otlp;
+    
+    OtlpGrpcMetricExporterOptions metricExporterOptions;
+    metricExporterOptions.endpoint = "otel.ct.activision.com:4317";
+
+    auto proxy{ std::make_unique<OtlpGrpcForwardProxy>() };
+
+    proxy->AddListenAddress("0.0.0.0:4317");
+    proxy->RegisterMetricExporter(metricExporterOptions);
+    proxy->Start();
+    proxy->Wait();
+    return 0;    
+}
