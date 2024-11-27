d103cdceb755303b0c8adbcacf7f951bbb0bf27b
Dimiter 'malkia' Stanev <malkia@gmail.com>
2024-11-26 22:36:05 -0800

    cleanup

--- x/proxy.h
+++
@@ -1,8 +0,0 @@
-struct ProxyOptions {
-    bool enable_health_check{ true };
-    bool enable_reflection{ true };
-    bool insecure_credentials{ true };
-    std::string listen_address{ "0.0.0.0:44317" };
-    std::string forward_address{ "motel.cat.fact.com:4317" };
-
-}
