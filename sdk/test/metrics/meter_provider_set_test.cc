5e6411c09b026ad443cce80ac9595cbf88f10445
Marc Alff <marc.alff@oracle.com>
2025-02-04 20:44:43 +0100

    include-what-you-use cleanup

--- sdk/test/metrics/meter_provider_set_test.cc
+++ sdk/test/metrics/meter_provider_set_test.cc
@@ -4,7 +4,6 @@
 #include <gtest/gtest.h>
 #include <stdlib.h>
 
-#include "opentelemetry/metrics/meter_provider.h"
 #include "opentelemetry/metrics/noop.h"
 #include "opentelemetry/metrics/provider.h"
 #include "opentelemetry/nostd/shared_ptr.h"
