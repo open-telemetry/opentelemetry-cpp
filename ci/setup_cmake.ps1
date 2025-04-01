ef957507cf046c63e73c442f4a620b2f1f919227
Douglas Barker <doug.barker@gmail.com>
2025-04-01 12:20:45 -0600

    pin cmake on bazel builds. force the cmake downgrade on windows

--- ci/setup_cmake.ps1
+++ ci/setup_cmake.ps1
@@ -7,9 +7,9 @@ trap { $host.SetShouldExit(1) }
 if (-not $env:CMAKE_VERSION) { $env:CMAKE_VERSION = "3.31.6" }
 $CMAKE_VERSION = $env:CMAKE_VERSION
 
-choco uninstall cmake -y
+choco uninstall cmake cmake.install -y --remove-dependencies --skip-autouninstaller --force --no-progress
 
 Write-Host "Installing CMake version $CMAKE_VERSION ..."
-choco install cmake --version=$CMAKE_VERSION -y
+choco install cmake --version=$CMAKE_VERSION --allow-downgrade -y --force --no-progress
 
 cmake --version
