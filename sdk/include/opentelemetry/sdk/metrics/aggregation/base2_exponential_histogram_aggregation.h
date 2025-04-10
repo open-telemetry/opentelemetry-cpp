de046031209abdd9aafbd9c0e571084f3496a5b6
Felipe C. Dos Santos <felipe.santos.k@gmail.com>
2025-04-10 00:20:26 +0000

    Fix iwyu warning

--- sdk/include/opentelemetry/sdk/metrics/aggregation/base2_exponential_histogram_aggregation.h
+++ sdk/include/opentelemetry/sdk/metrics/aggregation/base2_exponential_histogram_aggregation.h
@@ -3,16 +3,17 @@
 
 #pragma once
 
+#include <stdint.h>
 #include <memory>
-#include <mutex>
 
 #include "opentelemetry/common/spin_lock_mutex.h"
-#include "opentelemetry/sdk/metrics/aggregation/aggregation.h"
 #include "opentelemetry/sdk/metrics/aggregation/aggregation_config.h"
+#include "opentelemetry/sdk/metrics/aggregation/aggregation.h"
 #include "opentelemetry/sdk/metrics/aggregation/base2_exponential_histogram_indexer.h"
 #include "opentelemetry/sdk/metrics/data/circular_buffer.h"
 #include "opentelemetry/sdk/metrics/data/metric_data.h"
 #include "opentelemetry/sdk/metrics/data/point_data.h"
+#include "opentelemetry/version.h"
 
 OPENTELEMETRY_BEGIN_NAMESPACE
 namespace sdk
