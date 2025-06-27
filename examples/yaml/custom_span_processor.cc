c0f37aba821a863aa3c4dcb8314c655191775873
Marc Alff <marc.alff@oracle.com>
2025-06-27 14:46:48 +0200

    cleanup

--- examples/yaml/custom_span_processor.cc
+++ examples/yaml/custom_span_processor.cc
@@ -1,6 +1,8 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+#include <utility>
+
 #include "opentelemetry/sdk/common/global_log_handler.h"
 #include "opentelemetry/sdk/trace/recordable.h"
 #include "opentelemetry/sdk/trace/span_data.h"
