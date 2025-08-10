4710d0fd7afce41dd14acc2ff69e43b92acf7dfa
Douglas Barker <doug.barker@gmail.com>
2025-08-10 12:25:58 -0600

    update ci scripts to use the test_common/cmake cache variable scripts

--- ci/do_ci.ps1
+++ ci/do_ci.ps1
@@ -110,9 +110,7 @@ switch ($action) {
   "cmake.maintainer.test" {
     cd "$BUILD_DIR"
     cmake $SRC_DIR `
-      -DWITH_OTLP_GRPC=ON `
-      -DWITH_OTLP_HTTP=ON `
-      -DWITH_OTLP_RETRY_PREVIEW=ON `
+      "-C $SRC_DIR/test_common/cmake/all-options-abiv1-preview.cmake" `
       -DOTELCPP_MAINTAINER_MODE=ON `
       -DWITH_NO_DEPRECATED_CODE=ON `
       -DVCPKG_TARGET_TRIPLET=x64-windows `
@@ -135,11 +133,9 @@ switch ($action) {
   "cmake.maintainer.cxx20.stl.test" {
     cd "$BUILD_DIR"
     cmake $SRC_DIR `
+      "-C $SRC_DIR/test_common/cmake/all-options-abiv1-preview.cmake" `
       -DWITH_STL=CXX20 `
       -DCMAKE_CXX_STANDARD=20 `
-      -DWITH_OTLP_GRPC=ON `
-      -DWITH_OTLP_HTTP=ON `
-      -DWITH_OTLP_RETRY_PREVIEW=ON `
       -DOTELCPP_MAINTAINER_MODE=ON `
       -DWITH_NO_DEPRECATED_CODE=ON `
       -DVCPKG_TARGET_TRIPLET=x64-windows `
@@ -162,13 +158,9 @@ switch ($action) {
   "cmake.maintainer.abiv2.test" {
     cd "$BUILD_DIR"
     cmake $SRC_DIR `
-      -DWITH_OTLP_GRPC=ON `
-      -DWITH_OTLP_HTTP=ON `
-      -DWITH_OTLP_RETRY_PREVIEW=ON `
+      "-C $SRC_DIR/test_common/cmake/all-options-abiv2-preview.cmake" `
       -DOTELCPP_MAINTAINER_MODE=ON `
       -DWITH_NO_DEPRECATED_CODE=ON `
-      -DWITH_ABI_VERSION_1=OFF `
-      -DWITH_ABI_VERSION_2=ON `
       -DVCPKG_TARGET_TRIPLET=x64-windows `
       "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake"
     $exit = $LASTEXITCODE
@@ -210,11 +202,8 @@ switch ($action) {
   "cmake.exporter.otprotocol.test" {
     cd "$BUILD_DIR"
     cmake $SRC_DIR `
-      -DVCPKG_TARGET_TRIPLET=x64-windows `
-      -DWITH_OTLP_GRPC=ON `
-      -DWITH_OTLP_HTTP=ON `
-      -DWITH_OTLP_RETRY_PREVIEW=ON `
-      -DWITH_OTPROTCOL=ON `
+      "-C $SRC_DIR/test_common/cmake/all-options-abiv1-preview.cmake" `
+      -DVCPKG_TARGET_TRIPLET=x64-windows `      
       "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake"
     $exit = $LASTEXITCODE
     if ($exit -ne 0) {
@@ -257,9 +246,9 @@ switch ($action) {
   "cmake.exporter.otprotocol.with_async_export.test" {
     cd "$BUILD_DIR"
     cmake $SRC_DIR `
+      "-C $SRC_DIR/test_common/cmake/all-options-abiv1-preview.cmake" `
       -DVCPKG_TARGET_TRIPLET=x64-windows `
       -DWITH_ASYNC_EXPORT_PREVIEW=ON `
-      -DWITH_OTPROTCOL=ON `
       "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake"
     $exit = $LASTEXITCODE
     if ($exit -ne 0) {
@@ -342,27 +331,8 @@ switch ($action) {
     cmake $SRC_DIR `
       $CMAKE_OPTIONS `
       "-DCMAKE_INSTALL_PREFIX=$INSTALL_TEST_DIR" `
-      -DWITH_ABI_VERSION_1=OFF `
-      -DWITH_ABI_VERSION_2=ON `
+      "-C $SRC_DIR/test_common/cmake/all-options-abiv2-preview.cmake" `
       -DWITH_GSL=ON `
-      -DWITH_THREAD_INSTRUMENTATION_PREVIEW=ON `
-      -DWITH_METRICS_EXEMPLAR_PREVIEW=ON `
-      -DWITH_ASYNC_EXPORT_PREVIEW=ON `
-      -DWITH_OTLP_GRPC_SSL_MTLS_PREVIEW=ON `
-      -DWITH_OTLP_GRPC_CREDENTIAL_PREVIEW=ON `
-      -DWITH_OTLP_RETRY_PREVIEW=ON `
-      -DWITH_OTLP_GRPC=ON `
-      -DWITH_OTLP_HTTP=ON `
-      -DWITH_OTLP_FILE=ON `
-      -DWITH_OTLP_HTTP_COMPRESSION=ON `
-      -DWITH_HTTP_CLIENT_CURL=ON `
-      -DWITH_PROMETHEUS=ON `
-      -DWITH_ZIPKIN=ON `
-      -DWITH_ELASTICSEARCH=ON `
-      -DWITH_ETW=ON `
-      -DWITH_EXAMPLES=ON `
-      -DWITH_EXAMPLES_HTTP=ON `
-      -DBUILD_W3CTRACECONTEXT_TEST=ON `
       -DOPENTELEMETRY_INSTALL=ON
 
     $exit = $LASTEXITCODE
@@ -447,12 +417,7 @@ switch ($action) {
     cmake $SRC_DIR `
       $CMAKE_OPTIONS `
       "-DCMAKE_INSTALL_PREFIX=$INSTALL_TEST_DIR" `
-      -DWITH_ABI_VERSION_1=ON `
-      -DWITH_ABI_VERSION_2=OFF `
-      -DWITH_THREAD_INSTRUMENTATION_PREVIEW=ON `
-      -DWITH_METRICS_EXEMPLAR_PREVIEW=ON `
-      -DWITH_ASYNC_EXPORT_PREVIEW=ON `
-      -DWITH_ETW=ON `
+      "-C $SRC_DIR/test_common/cmake/all-options-abiv1-preview.cmake" `
       -DOPENTELEMETRY_INSTALL=ON `
       -DWITH_OTLP_GRPC_SSL_MTLS_PREVIEW=OFF `
       -DWITH_OTLP_GRPC_CREDENTIAL_PREVIEW=OFF `
