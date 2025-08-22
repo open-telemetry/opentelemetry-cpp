f676c98340bba333dbdbc3df8c813548c07f44aa
Marc Alff <marc.alff@oracle.com>
2025-08-22 22:55:20 +0200

    code cleanup, functional tests

--- functional/configuration/run_test.sh
+++ functional/configuration/run_test.sh
@@ -6,7 +6,6 @@
 set -e
 
 # To run tests in a local dev environment:
-# - make sure docker is running
 # - set BUILD_DIR to the top level build directory,
 
 [ -z "${BUILD_DIR}" ] && export BUILD_DIR="${HOME}/build"
