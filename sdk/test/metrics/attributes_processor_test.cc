09abd1755626ad7b3c04d07180ce5af932bcde1c
nikhilbhatia08 <bhatianikhil551@gmail.com>
2025-09-11 01:24:49 +0530

    test changes

--- sdk/test/metrics/attributes_processor_test.cc
+++ sdk/test/metrics/attributes_processor_test.cc
@@ -57,12 +57,13 @@ TEST(AttributesProcessor, FilteringExcludeAttributesProcessor)
   const int kNumFilterAttributes               = 3;
   std::unordered_map<std::string, bool> filter = {
       {"attr2", true}, {"attr4", true}, {"attr6", true}};
-  const int kNumAttributes              = 6;
-  std::string keys[kNumAttributes]      = {"attr1", "attr2", "attr3", "attr4", "attr5", "attr6"};
-  int values[kNumAttributes]            = {10, 20, 30, 40, 50, 60};
-  std::map<std::string, int> attributes = {{keys[0], values[0]}, {keys[1], values[1]},
-                                           {keys[2], values[2]}, {keys[3], values[3]},
-                                           {keys[4], values[4]}, {keys[5], values[5]}};
+  const int kNumAttributes              = 7;
+  std::string keys[kNumAttributes]      = {"attr1", "attr2", "attr3", "attr4",
+                                           "attr5", "attr6", "attr7"};
+  int values[kNumAttributes]            = {10, 20, 30, 40, 50, 60, 70};
+  std::map<std::string, int> attributes = {
+      {keys[0], values[0]}, {keys[1], values[1]}, {keys[2], values[2]}, {keys[3], values[3]},
+      {keys[4], values[4]}, {keys[5], values[5]}, {keys[6], values[6]}};
   FilteringExcludeAttributesProcessor attributes_processor(filter);
   opentelemetry::common::KeyValueIterableView<std::map<std::string, int>> iterable(attributes);
   auto filtered_attributes = attributes_processor.process(iterable);
@@ -70,7 +71,8 @@ TEST(AttributesProcessor, FilteringExcludeAttributesProcessor)
   {
     EXPECT_TRUE(filter.find(e.first) == filter.end());
   }
-  EXPECT_EQ(filter.size(), kNumFilterAttributes);
+  int expected_filtered_attributes_size = 4;
+  EXPECT_EQ(filtered_attributes.size(), expected_filtered_attributes_size);
 }
 
 TEST(AttributesProcessor, FilteringExcludeAllAttributesProcessor)
@@ -86,5 +88,5 @@ TEST(AttributesProcessor, FilteringExcludeAllAttributesProcessor)
   FilteringExcludeAttributesProcessor attributes_processor(filter);
   opentelemetry::common::KeyValueIterableView<std::map<std::string, int>> iterable(attributes);
   auto filtered_attributes = attributes_processor.process(iterable);
-  EXPECT_EQ(filter.size(), kNumFilterAttributes);
+  EXPECT_EQ(filtered_attributes.size(), kNumAttributes);
 }
