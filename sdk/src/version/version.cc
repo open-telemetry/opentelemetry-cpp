ea1f0d61ce5baa5584b097266bf133d1f31e3607
Marc Alff <marc.alff@oracle.com>
2025-09-26 02:04:33 +0200

    [RELEASE] Release opentelemetry-cpp v1.23.0 (#3660)

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -14,11 +14,11 @@ namespace version
 const int major_version    = 1;
 const int minor_version    = 23;
 const int patch_version    = 0;
-const char *pre_release    = "dev";
+const char *pre_release    = "";
 const char *build_metadata = "none";
 const char *short_version  = "1.23.0";
-const char *full_version   = "1.23.0-dev";
-const char *build_date     = "MAIN BRANCH";
+const char *full_version   = "1.23.0";
+const char *build_date     = "Thu Sep 25 07:47:31 PM UTC 2025";
 }  // namespace version
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
