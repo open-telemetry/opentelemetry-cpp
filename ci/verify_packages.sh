1fe198c04b3e89b3729cfc9d8abd5b94243a69df
Harish <140232061+perhapsmaple@users.noreply.github.com>
2026-01-30 21:40:04 +0530

    Update CI verification script
    Signed-off-by: Harish <140232061+perhapsmaple@users.noreply.github.com>

--- ci/verify_packages.sh
+++ ci/verify_packages.sh
@@ -7,6 +7,34 @@ set -ex
 
 PKG_CONFIG_PATH="/usr/local/lib64/pkgconfig:${PKG_CONFIG_PATH:-}"
 
-for library in api common logs metrics resources trace version; do
+validate_package() {
+  local package=$1
+  if pkg-config --exists "${package}" 2>/dev/null; then
+    pkg-config --validate "${package}" --print-errors
+  fi
+}
+
+# Core packages
+for library in api common logs metrics resources trace version configuration; do
   pkg-config --validate opentelemetry_${library} --print-errors
+done
+
+# ostream exporter
+for variant in span span_builder metrics metrics_builder logs logs_builder; do
+  validate_package "opentelemetry_exporter_ostream_${variant}"
+done
+
+# OTLP exporters
+validate_package "opentelemetry_otlp_recordable"
+validate_package "opentelemetry_exporter_otlp_builder_utils"
+
+for protocol in grpc http file; do
+  for variant in client "" builder log log_builder metrics metric_builder; do
+    validate_package "opentelemetry_exporter_otlp_${protocol}_${variant}"
+  done
+done
+
+# Other exporters
+for exporter in prometheus prometheus_builder zipkin_trace elasticsearch_logs in_memory in_memory_metric etw; do
+  validate_package "opentelemetry_exporter_${exporter}"
 done
