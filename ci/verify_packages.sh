cb5528dca2c263451f6f92966b6ebf2680c38438
Harish <140232061+perhapsmaple@users.noreply.github.com>
2026-02-04 22:50:47 +0530

    [INSTALL] Add pkg-config support for exporters (#3829)
    Signed-off-by: Harish <140232061+perhapsmaple@users.noreply.github.com>
    Co-authored-by: Tom Tan <Tom.Tan@microsoft.com>
    Co-authored-by: Tom Tan <lilotom@gmail.com>
    Co-authored-by: Lalit Kumar Bhasin <lalit_fin@yahoo.com>

--- ci/verify_packages.sh
+++ ci/verify_packages.sh
@@ -7,6 +7,45 @@ set -ex
 
 PKG_CONFIG_PATH="/usr/local/lib64/pkgconfig:${PKG_CONFIG_PATH:-}"
 
+validate_package() {
+  local package=$1
+  if pkg-config --exists "${package}" 2>/dev/null; then
+    pkg-config --validate "${package}" --print-errors
+  fi
+}
+
+# Core packages
 for library in api common logs metrics resources trace version; do
   pkg-config --validate opentelemetry_${library} --print-errors
 done
+
+# ostream exporter
+for variant in span span_builder metrics metrics_builder logs logs_builder; do
+  validate_package "opentelemetry_exporter_ostream_${variant}"
+done
+
+# OTLP exporters
+validate_package "opentelemetry_otlp_recordable"
+validate_package "opentelemetry_exporter_otlp_builder_utils"
+
+# OTLP gRPC exporter
+validate_package "opentelemetry_exporter_otlp_grpc"
+for variant in client builder log log_builder metrics metric_builder; do
+  validate_package "opentelemetry_exporter_otlp_grpc_${variant}"
+done
+
+# OTLP HTTP and File exporters
+for protocol in http file; do
+  validate_package "opentelemetry_exporter_otlp_${protocol}"
+  for variant in client builder log log_builder metric metric_builder; do
+    validate_package "opentelemetry_exporter_otlp_${protocol}_${variant}"
+  done
+done
+
+# Other exporters
+for exporter in prometheus prometheus_builder zipkin_trace elasticsearch_logs in_memory in_memory_metric; do
+  validate_package "opentelemetry_exporter_${exporter}"
+done
+
+# Optional features
+validate_package "opentelemetry_configuration"
