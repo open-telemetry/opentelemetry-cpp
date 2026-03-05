3d35b54e493255d8ee1cf477ebe4a92bdf7050a8
Marc Alff <marc.alff@oracle.com>
2026-03-05 10:08:07 +0100

    [CONFIGURATION] File configuration - spec stability (#3862)

--- sdk/src/configuration/yaml_configuration_parser.cc
+++ sdk/src/configuration/yaml_configuration_parser.cc
@@ -30,7 +30,7 @@ std::unique_ptr<Configuration> YamlConfigurationParser::ParseFile(const std::str
 
   if (input_file.empty())
   {
-    static std::string env_var_name("OTEL_EXPERIMENTAL_CONFIG_FILE");
+    static std::string env_var_name("OTEL_CONFIG_FILE");
     std::string env_var;
     const bool env_exists =
         sdk::common::GetStringEnvironmentVariable(env_var_name.c_str(), env_var);
