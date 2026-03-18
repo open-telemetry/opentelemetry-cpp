c34524721dfc1f18654ca5d299f2d84699818fdb
Douglas Barker <doug.barker@gmail.com>
2026-03-17 23:12:36 -0400

    Fix install test failure with c++14

--- install/test/src/test_resource_detectors.cc
+++ install/test/src/test_resource_detectors.cc
@@ -6,21 +6,28 @@
 #include <opentelemetry/resource_detectors/container_detector.h>
 #include <opentelemetry/resource_detectors/env_entity_detector.h>
 #include <opentelemetry/resource_detectors/process_detector.h>
+#include <memory>
 
 TEST(ResourceDetectorsInstall, ContainerResourceDetector)
 {
-  ASSERT_NO_THROW(auto detector = opentelemetry::resource_detector::ContainerResourceDetector();
-                  auto resource = detector.Detect());
-}
-
-TEST(ResourceDetectorsInstall, ProcessResourceDetector)
-{
-  ASSERT_NO_THROW(auto detector = opentelemetry::resource_detector::ProcessResourceDetector();
-                  auto resource = detector.Detect());
+  std::unique_ptr<opentelemetry::sdk::resource::ResourceDetector> detector =
+      std::make_unique<opentelemetry::resource_detector::ContainerResourceDetector>();
+  ASSERT_TRUE(detector != nullptr);
+  ASSERT_NO_THROW(auto resource = detector->Detect());
 }
 
 TEST(ResourceDetectorsInstall, EnvEntityDetector)
 {
-  ASSERT_NO_THROW(auto detector = opentelemetry::resource_detector::EnvEntityDetector();
-                  auto resource = detector.Detect());
+  std::unique_ptr<opentelemetry::sdk::resource::ResourceDetector> detector =
+      std::make_unique<opentelemetry::resource_detector::EnvEntityDetector>();
+  ASSERT_TRUE(detector != nullptr);
+  ASSERT_NO_THROW(auto resource = detector->Detect());
+}
+
+TEST(ResourceDetectorsInstall, ProcessResourceDetector)
+{
+  std::unique_ptr<opentelemetry::sdk::resource::ResourceDetector> detector =
+      std::make_unique<opentelemetry::resource_detector::ProcessResourceDetector>();
+  ASSERT_TRUE(detector != nullptr);
+  ASSERT_NO_THROW(auto resource = detector->Detect());
 }
