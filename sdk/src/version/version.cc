898d9b9a913b50e48bf0020529c05e6167d7a8bd
Marc Alff <marc.alff@oracle.com>
2026-03-19 17:26:33 +0100

    Release date

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -18,7 +18,7 @@ const char *pre_release    = "";
 const char *build_metadata = "none";
 const char *short_version  = "1.26.0";
 const char *full_version   = "1.26.0";
-const char *build_date     = "Wed Mar 18 11:10:50 PM UTC 2026";
+const char *build_date     = "Thu Mar 19 04:25:52 PM UTC 2026";
 }  // namespace version
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
