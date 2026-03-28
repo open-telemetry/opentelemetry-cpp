4428c0e52c59970970fa16fe83b0ed9baf37ff30
Harish <140232061+perhapsmaple@users.noreply.github.com>
2026-03-28 11:08:32 +0530

    Explicitly cast the pointers to void*
    Signed-off-by: Harish <140232061+perhapsmaple@users.noreply.github.com>

--- sdk/test/common/empty_attributes_test.cc
+++ sdk/test/common/empty_attributes_test.cc
@@ -15,5 +15,5 @@ TEST(EmptyAttributesTest, TestMemory)
 {
   auto attributes1 = opentelemetry::sdk::GetEmptyAttributes();
   auto attributes2 = opentelemetry::sdk::GetEmptyAttributes();
-  EXPECT_EQ(memcmp(&attributes1, &attributes2, sizeof(attributes1)), 0);  // NOLINT
+  EXPECT_EQ(memcmp(static_cast<void *>(&attributes1), static_cast<void *>(&attributes2), sizeof(attributes1)), 0);  // NOLINT
 }
