5c6a4b70a122544b6bd7d67573835c85284573f2
Marc Alff <marc.alff@oracle.com>
2026-05-13 16:20:45 +0200

    [RELEASE] Bump main branch to 1.28.0-dev

--- sdk/src/version/version.cc
+++ sdk/src/version/version.cc
@@ -12,18 +12,18 @@ namespace sdk
 namespace version
 {
 const int major_version    = 1;
-const int minor_version    = 27;
+const int minor_version    = 28;
 const int patch_version    = 0;
-const char *pre_release    = "";
+const char *pre_release    = "dev";
 const char *build_metadata = "none";
-const char *short_version  = "1.27.0";
-const char *full_version   = "1.27.0";
+const char *short_version  = "1.28.0";
+const char *full_version   = "1.28.0-dev";
 /**
  * Release date.
  * For published releases: YYYY-MM-DD
  * For -dev releases: empty string
  */
-const char *build_date = "2026-05-13";
+const char *build_date = "";
 }  // namespace version
 }  // namespace sdk
 OPENTELEMETRY_END_NAMESPACE
