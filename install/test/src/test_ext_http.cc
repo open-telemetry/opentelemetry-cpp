235c5965516939f267c0fba733c9d983cdab5c9f
Lukasz Towarek <lukasz.towarek@gmail.com>
2026-05-14 17:58:10 +0000

    Install http_client

---
+++ install/test/src/test_ext_http.cc
@@ -0,0 +1,13 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#include <gtest/gtest.h>
+
+#include <opentelemetry/ext/http/client/http_client.h>
+
+TEST(ExtHttpInstall, Headers)
+{
+  using opentelemetry::ext::http::client::HttpClient;
+  using opentelemetry::ext::http::client::HttpClientSync;
+  SUCCEED();
+}
