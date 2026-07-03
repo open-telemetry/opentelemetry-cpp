fc024e87116e72e0a1b93148c59049dcbe138e64
Douglas Barker <doug.barker@gmail.com>
2026-07-03 10:46:42 -0400

    Adds a message if the external component is added. Clean up comments to remove env var support

--- cmake/opentelemetry-build-external-component.cmake
+++ cmake/opentelemetry-build-external-component.cmake
@@ -13,11 +13,12 @@ endfunction()
 # Enable building external components through otel-cpp build
 # The config options are
 #  - OPENTELEMETRY_EXTERNAL_COMPONENT_PATH - Setting local paths of the external
-# component as env variable. Multiple paths can be set by separating them with.
+# component. Multiple paths can be set by separating them with.
 
 # Add custom vendor component from local path
-# Prefer CMake option, then env variable, then URL.
+# CMake option.
 if(OPENTELEMETRY_EXTERNAL_COMPONENT_PATH)
+  message(STATUS "Building external components from local path(s): ${OPENTELEMETRY_EXTERNAL_COMPONENT_PATH}")
   # Add custom component path to build tree and consolidate binary artifacts in
   # current project binary output directory.
   foreach(DIR IN LISTS OPENTELEMETRY_EXTERNAL_COMPONENT_PATH)
