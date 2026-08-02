6a54829f1daee0f0b51f91424ae2fa3e73703641
ayush-that <ayush1337@hotmail.com>
2026-08-03 00:41:11 +0530

    Include probability_sampler_configuration.h directly in sdk_builder.cc

--- sdk/src/configuration/sdk_builder.cc
+++ sdk/src/configuration/sdk_builder.cc
@@ -101,6 +101,7 @@
 #include "opentelemetry/sdk/configuration/parent_based_sampler_configuration.h"
 #include "opentelemetry/sdk/configuration/periodic_metric_reader_builder.h"
 #include "opentelemetry/sdk/configuration/periodic_metric_reader_configuration.h"
+#include "opentelemetry/sdk/configuration/probability_sampler_configuration.h"
 #include "opentelemetry/sdk/configuration/prometheus_pull_metric_exporter_builder.h"
 #include "opentelemetry/sdk/configuration/prometheus_pull_metric_exporter_configuration.h"
 #include "opentelemetry/sdk/configuration/propagator_configuration.h"
