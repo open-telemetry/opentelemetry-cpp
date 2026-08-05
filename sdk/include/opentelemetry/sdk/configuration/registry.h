5a5fbfd01a9868f55a8579cc7852180f83a5ac96
Doug Barker <3782873+dbarker@users.noreply.github.com>
2026-08-05 09:09:04 -0400

    [CONFIGURATION] Add SDK component builder interfaces to the registry (#4358)

--- sdk/include/opentelemetry/sdk/configuration/registry.h
+++ sdk/include/opentelemetry/sdk/configuration/registry.h
@@ -16,16 +16,33 @@ namespace configuration
 {
 
 // Forward declarations
+class AlwaysOffSamplerBuilder;
+class AlwaysOnSamplerBuilder;
+class BatchLogRecordProcessorBuilder;
+class BatchSpanProcessorBuilder;
+class ComposableAlwaysOffSamplerBuilder;
+class ComposableAlwaysOnSamplerBuilder;
+class ComposableParentThresholdSamplerBuilder;
+class ComposableProbabilitySamplerBuilder;
+class ComposableRuleBasedSamplerBuilder;
 class ConsoleLogRecordExporterBuilder;
 class ConsolePushMetricExporterBuilder;
 class ConsoleSpanExporterBuilder;
+class ContainerResourceDetectorBuilder;
 class ExtensionLogRecordExporterBuilder;
 class ExtensionLogRecordProcessorBuilder;
+class ExtensionMetricProducerBuilder;
 class ExtensionPullMetricExporterBuilder;
 class ExtensionPushMetricExporterBuilder;
+class ExtensionResourceDetectorBuilder;
 class ExtensionSamplerBuilder;
 class ExtensionSpanExporterBuilder;
 class ExtensionSpanProcessorBuilder;
+class HostResourceDetectorBuilder;
+class JaegerRemoteSamplerBuilder;
+class LoggerConfiguratorBuilder;
+class MeterConfiguratorBuilder;
+class OpenCensusMetricProducerBuilder;
 class OtlpFileLogRecordExporterBuilder;
 class OtlpFilePushMetricExporterBuilder;
 class OtlpFileSpanExporterBuilder;
@@ -35,9 +52,18 @@ class OtlpGrpcSpanExporterBuilder;
 class OtlpHttpLogRecordExporterBuilder;
 class OtlpHttpPushMetricExporterBuilder;
 class OtlpHttpSpanExporterBuilder;
+class ParentBasedSamplerBuilder;
 class PeriodicMetricReaderBuilder;
+class ProbabilitySamplerBuilder;
+class ProcessResourceDetectorBuilder;
 class PrometheusPullMetricExporterBuilder;
+class PullMetricReaderBuilder;
+class ServiceResourceDetectorBuilder;
+class SimpleLogRecordProcessorBuilder;
+class SimpleSpanProcessorBuilder;
 class TextMapPropagatorBuilder;
+class TraceIdRatioBasedSamplerBuilder;
+class TracerConfiguratorBuilder;
 
 class OPENTELEMETRY_EXPORT Registry
 {
@@ -120,12 +146,27 @@ public:
   void SetPrometheusPullMetricExporterBuilder(
       std::unique_ptr<PrometheusPullMetricExporterBuilder> &&builder);
 
+  const OpenCensusMetricProducerBuilder *GetOpenCensusMetricProducerBuilder() const
+  {
+    return open_census_metric_producer_builder_.get();
+  }
+
+  void SetOpenCensusMetricProducerBuilder(
+      std::unique_ptr<OpenCensusMetricProducerBuilder> &&builder);
+
   const PeriodicMetricReaderBuilder *GetPeriodicMetricReaderBuilder() const
   {
     return periodic_metric_reader_builder_.get();
   }
 
   void SetPeriodicMetricReaderBuilder(std::unique_ptr<PeriodicMetricReaderBuilder> &&builder);
+
+  const PullMetricReaderBuilder *GetPullMetricReaderBuilder() const
+  {
+    return pull_metric_reader_builder_.get();
+  }
+
+  void SetPullMetricReaderBuilder(std::unique_ptr<PullMetricReaderBuilder> &&builder);
 
   const OtlpHttpLogRecordExporterBuilder *GetOtlpHttpLogRecordBuilder() const
   {
@@ -154,6 +195,178 @@ public:
   }
 
   void SetConsoleLogRecordBuilder(std::unique_ptr<ConsoleLogRecordExporterBuilder> &&builder);
+
+  /* Samplers. */
+
+  const AlwaysOnSamplerBuilder *GetAlwaysOnSamplerBuilder() const
+  {
+    return always_on_sampler_builder_.get();
+  }
+
+  void SetAlwaysOnSamplerBuilder(std::unique_ptr<AlwaysOnSamplerBuilder> &&builder);
+
+  const AlwaysOffSamplerBuilder *GetAlwaysOffSamplerBuilder() const
+  {
+    return always_off_sampler_builder_.get();
+  }
+
+  void SetAlwaysOffSamplerBuilder(std::unique_ptr<AlwaysOffSamplerBuilder> &&builder);
+
+  const TraceIdRatioBasedSamplerBuilder *GetTraceIdRatioBasedSamplerBuilder() const
+  {
+    return trace_id_ratio_based_sampler_builder_.get();
+  }
+
+  void SetTraceIdRatioBasedSamplerBuilder(
+      std::unique_ptr<TraceIdRatioBasedSamplerBuilder> &&builder);
+
+  const ProbabilitySamplerBuilder *GetProbabilitySamplerBuilder() const
+  {
+    return probability_sampler_builder_.get();
+  }
+
+  void SetProbabilitySamplerBuilder(std::unique_ptr<ProbabilitySamplerBuilder> &&builder);
+
+  const ParentBasedSamplerBuilder *GetParentBasedSamplerBuilder() const
+  {
+    return parent_based_sampler_builder_.get();
+  }
+
+  void SetParentBasedSamplerBuilder(std::unique_ptr<ParentBasedSamplerBuilder> &&builder);
+
+  const JaegerRemoteSamplerBuilder *GetJaegerRemoteSamplerBuilder() const
+  {
+    return jaeger_remote_sampler_builder_.get();
+  }
+
+  void SetJaegerRemoteSamplerBuilder(std::unique_ptr<JaegerRemoteSamplerBuilder> &&builder);
+
+  /* Composable samplers. */
+
+  const ComposableAlwaysOnSamplerBuilder *GetComposableAlwaysOnSamplerBuilder() const
+  {
+    return composable_always_on_sampler_builder_.get();
+  }
+
+  void SetComposableAlwaysOnSamplerBuilder(
+      std::unique_ptr<ComposableAlwaysOnSamplerBuilder> &&builder);
+
+  const ComposableAlwaysOffSamplerBuilder *GetComposableAlwaysOffSamplerBuilder() const
+  {
+    return composable_always_off_sampler_builder_.get();
+  }
+
+  void SetComposableAlwaysOffSamplerBuilder(
+      std::unique_ptr<ComposableAlwaysOffSamplerBuilder> &&builder);
+
+  const ComposableProbabilitySamplerBuilder *GetComposableProbabilitySamplerBuilder() const
+  {
+    return composable_probability_sampler_builder_.get();
+  }
+
+  void SetComposableProbabilitySamplerBuilder(
+      std::unique_ptr<ComposableProbabilitySamplerBuilder> &&builder);
+
+  const ComposableParentThresholdSamplerBuilder *GetComposableParentThresholdSamplerBuilder() const
+  {
+    return composable_parent_threshold_sampler_builder_.get();
+  }
+
+  void SetComposableParentThresholdSamplerBuilder(
+      std::unique_ptr<ComposableParentThresholdSamplerBuilder> &&builder);
+
+  const ComposableRuleBasedSamplerBuilder *GetComposableRuleBasedSamplerBuilder() const
+  {
+    return composable_rule_based_sampler_builder_.get();
+  }
+
+  void SetComposableRuleBasedSamplerBuilder(
+      std::unique_ptr<ComposableRuleBasedSamplerBuilder> &&builder);
+
+  /* Processors. */
+
+  const BatchSpanProcessorBuilder *GetBatchSpanProcessorBuilder() const
+  {
+    return batch_span_processor_builder_.get();
+  }
+
+  void SetBatchSpanProcessorBuilder(std::unique_ptr<BatchSpanProcessorBuilder> &&builder);
+
+  const SimpleSpanProcessorBuilder *GetSimpleSpanProcessorBuilder() const
+  {
+    return simple_span_processor_builder_.get();
+  }
+
+  void SetSimpleSpanProcessorBuilder(std::unique_ptr<SimpleSpanProcessorBuilder> &&builder);
+
+  const BatchLogRecordProcessorBuilder *GetBatchLogRecordProcessorBuilder() const
+  {
+    return batch_log_record_processor_builder_.get();
+  }
+
+  void SetBatchLogRecordProcessorBuilder(std::unique_ptr<BatchLogRecordProcessorBuilder> &&builder);
+
+  const SimpleLogRecordProcessorBuilder *GetSimpleLogRecordProcessorBuilder() const
+  {
+    return simple_log_record_processor_builder_.get();
+  }
+
+  void SetSimpleLogRecordProcessorBuilder(
+      std::unique_ptr<SimpleLogRecordProcessorBuilder> &&builder);
+
+  /* Configurators. */
+
+  const TracerConfiguratorBuilder *GetTracerConfiguratorBuilder() const
+  {
+    return tracer_configurator_builder_.get();
+  }
+
+  void SetTracerConfiguratorBuilder(std::unique_ptr<TracerConfiguratorBuilder> &&builder);
+
+  const MeterConfiguratorBuilder *GetMeterConfiguratorBuilder() const
+  {
+    return meter_configurator_builder_.get();
+  }
+
+  void SetMeterConfiguratorBuilder(std::unique_ptr<MeterConfiguratorBuilder> &&builder);
+
+  const LoggerConfiguratorBuilder *GetLoggerConfiguratorBuilder() const
+  {
+    return logger_configurator_builder_.get();
+  }
+
+  void SetLoggerConfiguratorBuilder(std::unique_ptr<LoggerConfiguratorBuilder> &&builder);
+
+  /* Resource detectors. */
+
+  const ContainerResourceDetectorBuilder *GetContainerResourceDetectorBuilder() const
+  {
+    return container_resource_detector_builder_.get();
+  }
+
+  void SetContainerResourceDetectorBuilder(
+      std::unique_ptr<ContainerResourceDetectorBuilder> &&builder);
+
+  const HostResourceDetectorBuilder *GetHostResourceDetectorBuilder() const
+  {
+    return host_resource_detector_builder_.get();
+  }
+
+  void SetHostResourceDetectorBuilder(std::unique_ptr<HostResourceDetectorBuilder> &&builder);
+
+  const ProcessResourceDetectorBuilder *GetProcessResourceDetectorBuilder() const
+  {
+    return process_resource_detector_builder_.get();
+  }
+
+  void SetProcessResourceDetectorBuilder(std::unique_ptr<ProcessResourceDetectorBuilder> &&builder);
+
+  const ServiceResourceDetectorBuilder *GetServiceResourceDetectorBuilder() const
+  {
+    return service_resource_detector_builder_.get();
+  }
+
+  void SetServiceResourceDetectorBuilder(std::unique_ptr<ServiceResourceDetectorBuilder> &&builder);
 
   /* Extension points */
 
@@ -207,6 +420,19 @@ public:
       const std::string &name,
       std::unique_ptr<ExtensionLogRecordProcessorBuilder> &&builder);
 
+  const ExtensionMetricProducerBuilder *GetExtensionMetricProducerBuilder(
+      const std::string &name) const;
+
+  void SetExtensionMetricProducerBuilder(const std::string &name,
+                                         std::unique_ptr<ExtensionMetricProducerBuilder> &&builder);
+
+  const ExtensionResourceDetectorBuilder *GetExtensionResourceDetectorBuilder(
+      const std::string &name) const;
+
+  void SetExtensionResourceDetectorBuilder(
+      const std::string &name,
+      std::unique_ptr<ExtensionResourceDetectorBuilder> &&builder);
+
 private:
   std::unique_ptr<OtlpHttpSpanExporterBuilder> otlp_http_span_builder_;
   std::unique_ptr<OtlpGrpcSpanExporterBuilder> otlp_grpc_span_builder_;
@@ -219,11 +445,42 @@ private:
   std::unique_ptr<ConsolePushMetricExporterBuilder> console_metric_builder_;
   std::unique_ptr<PrometheusPullMetricExporterBuilder> prometheus_metric_builder_;
   std::unique_ptr<PeriodicMetricReaderBuilder> periodic_metric_reader_builder_;
+  std::unique_ptr<PullMetricReaderBuilder> pull_metric_reader_builder_;
 
   std::unique_ptr<OtlpHttpLogRecordExporterBuilder> otlp_http_log_record_builder_;
   std::unique_ptr<OtlpGrpcLogRecordExporterBuilder> otlp_grpc_log_record_builder_;
   std::unique_ptr<OtlpFileLogRecordExporterBuilder> otlp_file_log_record_builder_;
   std::unique_ptr<ConsoleLogRecordExporterBuilder> console_log_record_builder_;
+
+  std::unique_ptr<AlwaysOnSamplerBuilder> always_on_sampler_builder_;
+  std::unique_ptr<AlwaysOffSamplerBuilder> always_off_sampler_builder_;
+  std::unique_ptr<TraceIdRatioBasedSamplerBuilder> trace_id_ratio_based_sampler_builder_;
+  std::unique_ptr<ProbabilitySamplerBuilder> probability_sampler_builder_;
+  std::unique_ptr<ParentBasedSamplerBuilder> parent_based_sampler_builder_;
+  std::unique_ptr<JaegerRemoteSamplerBuilder> jaeger_remote_sampler_builder_;
+
+  std::unique_ptr<ComposableAlwaysOnSamplerBuilder> composable_always_on_sampler_builder_;
+  std::unique_ptr<ComposableAlwaysOffSamplerBuilder> composable_always_off_sampler_builder_;
+  std::unique_ptr<ComposableProbabilitySamplerBuilder> composable_probability_sampler_builder_;
+  std::unique_ptr<ComposableParentThresholdSamplerBuilder>
+      composable_parent_threshold_sampler_builder_;
+  std::unique_ptr<ComposableRuleBasedSamplerBuilder> composable_rule_based_sampler_builder_;
+
+  std::unique_ptr<BatchSpanProcessorBuilder> batch_span_processor_builder_;
+  std::unique_ptr<SimpleSpanProcessorBuilder> simple_span_processor_builder_;
+  std::unique_ptr<BatchLogRecordProcessorBuilder> batch_log_record_processor_builder_;
+  std::unique_ptr<SimpleLogRecordProcessorBuilder> simple_log_record_processor_builder_;
+
+  std::unique_ptr<TracerConfiguratorBuilder> tracer_configurator_builder_;
+  std::unique_ptr<MeterConfiguratorBuilder> meter_configurator_builder_;
+  std::unique_ptr<LoggerConfiguratorBuilder> logger_configurator_builder_;
+
+  std::unique_ptr<OpenCensusMetricProducerBuilder> open_census_metric_producer_builder_;
+
+  std::unique_ptr<ContainerResourceDetectorBuilder> container_resource_detector_builder_;
+  std::unique_ptr<HostResourceDetectorBuilder> host_resource_detector_builder_;
+  std::unique_ptr<ProcessResourceDetectorBuilder> process_resource_detector_builder_;
+  std::unique_ptr<ServiceResourceDetectorBuilder> service_resource_detector_builder_;
 
   std::map<std::string, std::unique_ptr<TextMapPropagatorBuilder>> propagator_builders_;
   std::map<std::string, std::unique_ptr<ExtensionSamplerBuilder>> sampler_builders_;
@@ -237,6 +494,9 @@ private:
       log_record_exporter_builders_;
   std::map<std::string, std::unique_ptr<ExtensionLogRecordProcessorBuilder>>
       log_record_processor_builders_;
+  std::map<std::string, std::unique_ptr<ExtensionMetricProducerBuilder>> metric_producer_builders_;
+  std::map<std::string, std::unique_ptr<ExtensionResourceDetectorBuilder>>
+      resource_detector_builders_;
 };
 
 }  // namespace configuration
