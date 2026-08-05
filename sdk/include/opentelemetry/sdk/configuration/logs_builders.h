9846e83d17ccdbfca142256be4c42696cb787631
Douglas Barker <doug.barker@gmail.com>
2026-08-05 15:07:39 -0400

    add sdk logs component builders

---
+++ sdk/include/opentelemetry/sdk/configuration/logs_builders.h
@@ -0,0 +1,19 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "opentelemetry/sdk/configuration/registry.h"
+#include "opentelemetry/version.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace sdk
+{
+namespace configuration
+{
+
+void RegisterDefaultLogsBuilders(Registry *registry);
+
+}  // namespace configuration
+}  // namespace sdk
+OPENTELEMETRY_END_NAMESPACE
