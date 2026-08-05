d077f5f166c71c15e5eafec35019bba9d22d7476
Douglas Barker <doug.barker@gmail.com>
2026-08-05 15:12:41 -0400

    add registry factory

---
+++ sdk/src/configuration/registry_factory.cc
@@ -0,0 +1,30 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#include "opentelemetry/sdk/configuration/registry_factory.h"
+
+#include <memory>
+
+#include "opentelemetry/sdk/configuration/logs_builders.h"
+#include "opentelemetry/sdk/configuration/metrics_builders.h"
+#include "opentelemetry/sdk/configuration/registry.h"
+#include "opentelemetry/sdk/configuration/trace_builders.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace sdk
+{
+namespace configuration
+{
+
+std::shared_ptr<Registry> RegistryFactory::Create()
+{
+  auto registry = std::make_shared<Registry>();
+  RegisterDefaultTraceBuilders(registry.get());
+  RegisterDefaultMetricsBuilders(registry.get());
+  RegisterDefaultLogsBuilders(registry.get());
+  return registry;
+}
+
+}  // namespace configuration
+}  // namespace sdk
+OPENTELEMETRY_END_NAMESPACE
