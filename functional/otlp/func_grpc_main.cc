10883ba6a680f75f25d22f76a54c4ee1a932cecf
Douglas Barker <doug.barker@gmail.com>
2026-08-07 13:16:03 -0400

    [CI] disable gRPC cert_not_found test until the upstream fix is released

--- functional/otlp/func_grpc_main.cc
+++ functional/otlp/func_grpc_main.cc
@@ -345,8 +345,10 @@ static int test_client_key_unreadable();
 static int test_mtls_ok();
 #endif  // ENABLE_OTLP_GRPC_SSL_MTLS_PREVIEW
 
+// TODO: re-enable "cert-not-found" once gRPC releases grpc/grpc#42608 (OpenSSL NO_ATEXIT fix for
+// grpc/grpc#38539).
 static const test_case all_tests[] = {{"basic", test_basic},
-                                      {"cert-not-found", test_cert_not_found},
+                                      // {"cert-not-found", test_cert_not_found},
                                       {"cert-invalid", test_cert_invalid},
                                       {"cert-unreadable", test_cert_unreadable},
 #ifdef ENABLE_OTLP_GRPC_SSL_MTLS_PREVIEW
