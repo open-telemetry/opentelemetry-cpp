0245e4a63ba19ad3fdbeaaa1442fc20c16b0f476
Pradeep <pradeep211005@gmail.com>
2026-08-15 16:47:29 +0530

    Call new functions in Detect()

--- resource_detectors/src/process_detector.cc
+++ resource_detectors/src/process_detector.cc
@@ -39,7 +39,17 @@ opentelemetry::sdk::resource::Resource ProcessResourceDetector::Detect() noexcep
     std::string executable_path = opentelemetry::resource_detector::detail::GetExecutablePath(pid);
     if (!executable_path.empty())
     {
-      attributes[semconv::process::kProcessExecutablePath] = std::move(executable_path);
+      attributes[semconv::process::kProcessExecutablePath] = executable_path;
+
+      // process.executable.name is the basename of the executable path.
+      std::size_t sep = executable_path.find_last_of("/\\");
+      std::string executable_name = (sep == std::string::npos)
+                                        ? executable_path
+                                        : executable_path.substr(sep + 1);
+      if (!executable_name.empty())
+      {
+        attributes[semconv::process::kProcessExecutableName] = std::move(executable_name);
+      }
     }
   }
   catch (const ::std::exception &ex)
@@ -63,6 +73,50 @@ opentelemetry::sdk::resource::Resource ProcessResourceDetector::Detect() noexcep
   {
     OTEL_INTERNAL_LOG_ERROR("[Process Resource Detector] "
                             << "Error extracting command with arguments: " << ex.what());
+  }
+
+  try
+  {
+    std::string creation_time =
+        opentelemetry::resource_detector::detail::GetProcessCreationTime(pid);
+    if (!creation_time.empty())
+    {
+      attributes[semconv::process::kProcessCreationTime] = std::move(creation_time);
+    }
+  }
+  catch (const std::exception &ex)
+  {
+    OTEL_INTERNAL_LOG_ERROR("[Process Resource Detector] "
+                            << "Error extracting process creation time: " << ex.what());
+  }
+
+  try
+  {
+    std::string owner = opentelemetry::resource_detector::detail::GetProcessOwner(pid);
+    if (!owner.empty())
+    {
+      attributes[semconv::process::kProcessOwner] = std::move(owner);
+    }
+  }
+  catch (const std::exception &ex)
+  {
+    OTEL_INTERNAL_LOG_ERROR("[Process Resource Detector] "
+                            << "Error extracting process owner: " << ex.what());
+  }
+
+  try
+  {
+    std::string build_id =
+        opentelemetry::resource_detector::detail::GetExecutableBuildIdHtlhash(pid);
+    if (!build_id.empty())
+    {
+      attributes[semconv::process::kProcessExecutableBuildIdHtlhash] = std::move(build_id);
+    }
+  }
+  catch (const std::exception &ex)
+  {
+    OTEL_INTERNAL_LOG_ERROR("[Process Resource Detector] "
+                            << "Error computing executable build id (htlhash): " << ex.what());
   }
 
   return ResourceDetector::Create(attributes);
