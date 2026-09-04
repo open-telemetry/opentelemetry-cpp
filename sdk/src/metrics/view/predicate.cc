d94341fafb442a7c71458ffe2c3ec93bf41dff00
Yiğitcan Öztürk <318303091+yigitcan-ozturk@users.noreply.github.com>
2026-09-04 20:16:27 +0300

    refactor: address wildcard view selector review feedback

---
+++ sdk/src/metrics/view/predicate.cc
@@ -0,0 +1,84 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#include "opentelemetry/sdk/metrics/view/predicate.h"
+
+#include <regex>
+#include <string>
+
+#include "opentelemetry/common/macros.h"
+#include "opentelemetry/sdk/common/global_log_handler.h"
+#include "src/common/wildcard_match.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace sdk
+{
+namespace metrics
+{
+
+class PatternPredicate::Impl
+{
+public:
+  explicit Impl(opentelemetry::nostd::string_view pattern)
+#if OPENTELEMETRY_HAVE_WORKING_REGEX
+      : reg_key_{std::string{pattern.data(), pattern.size()}}
+#else
+      : reg_key_{pattern.data(), pattern.size()}
+#endif
+  {}
+
+#if OPENTELEMETRY_HAVE_WORKING_REGEX
+  std::regex reg_key_;
+#else
+  std::string reg_key_;
+#endif
+};
+
+PatternPredicate::PatternPredicate(opentelemetry::nostd::string_view pattern)
+    : impl_{new Impl(pattern)}
+{}
+
+PatternPredicate::~PatternPredicate() = default;
+
+bool PatternPredicate::Match(opentelemetry::nostd::string_view str) const noexcept
+{
+#if OPENTELEMETRY_HAVE_WORKING_REGEX
+  return std::regex_match(str.data(), str.data() + str.size(), impl_->reg_key_);
+#else
+  OTEL_INTERNAL_LOG_ERROR(
+      "PatternPredicate::Match - failed. std::regex not fully supported for this compiler.");
+  return false;
+#endif
+}
+
+WildcardPredicate::WildcardPredicate(opentelemetry::nostd::string_view pattern)
+    : pattern_{pattern.data(), pattern.size()}
+{}
+
+bool WildcardPredicate::Match(opentelemetry::nostd::string_view str) const noexcept
+{
+  return opentelemetry::sdk::common::WildcardMatch(pattern_, str);
+}
+
+ExactPredicate::ExactPredicate(opentelemetry::nostd::string_view pattern)
+    : pattern_{pattern.data(), pattern.size()}
+{}
+
+bool ExactPredicate::Match(opentelemetry::nostd::string_view str) const noexcept
+{
+  return pattern_ == str;
+}
+
+bool MatchEverythingPattern::Match(opentelemetry::nostd::string_view /* str */) const noexcept
+{
+  return true;
+}
+
+bool MatchNothingPattern::Match(opentelemetry::nostd::string_view /* str */) const noexcept
+{
+  return false;
+}
+
+}  // namespace metrics
+}  // namespace sdk
+OPENTELEMETRY_END_NAMESPACE
