215edcc72a32182bd0622d6dee8eba96e2a92fab
Yiğitcan Öztürk <318303091+yigitcan-ozturk@users.noreply.github.com>
2026-09-04 20:16:42 +0300

    refactor: address wildcard view selector review feedback

--- sdk/test/metrics/view_registry_test.cc
+++ sdk/test/metrics/view_registry_test.cc
@@ -18,22 +18,34 @@
 using namespace opentelemetry::sdk::metrics;
 using namespace opentelemetry::sdk::instrumentationscope;
 
-TEST(InstrumentSelector, NameFilterUsesWildcardMatching)
+TEST(InstrumentSelector, NameFilterSupportsExplicitWildcardMatching)
 {
-  InstrumentSelector star_selector{InstrumentType::kCounter, "my_counter*", ""};
+  InstrumentSelector star_selector{
+      InstrumentType::kCounter, "my_counter*", "", PredicateType::kWildcard};
   EXPECT_TRUE(star_selector.GetNameFilter()->Match("my_counter"));
   EXPECT_TRUE(star_selector.GetNameFilter()->Match("my_counter_one"));
   EXPECT_TRUE(star_selector.GetNameFilter()->Match("my_counter_two"));
   EXPECT_FALSE(star_selector.GetNameFilter()->Match("other_counter"));
 
-  InstrumentSelector question_selector{InstrumentType::kCounter, "my_counter_?", ""};
+  InstrumentSelector question_selector{
+      InstrumentType::kCounter, "my_counter_?", "", PredicateType::kWildcard};
   EXPECT_TRUE(question_selector.GetNameFilter()->Match("my_counter_1"));
   EXPECT_FALSE(question_selector.GetNameFilter()->Match("my_counter_12"));
 
-  InstrumentSelector literal_selector{InstrumentType::kCounter, "my.counter*", ""};
+  InstrumentSelector literal_selector{
+      InstrumentType::kCounter, "my.counter*", "", PredicateType::kWildcard};
   EXPECT_TRUE(literal_selector.GetNameFilter()->Match("my.counter_one"));
   EXPECT_FALSE(literal_selector.GetNameFilter()->Match("myXcounter_one"));
 }
+
+#if OPENTELEMETRY_HAVE_WORKING_REGEX
+TEST(InstrumentSelector, NameFilterDefaultsToPatternMatching)
+{
+  InstrumentSelector selector{InstrumentType::kCounter, "my\\.counter.*", ""};
+  EXPECT_TRUE(selector.GetNameFilter()->Match("my.counter_one"));
+  EXPECT_FALSE(selector.GetNameFilter()->Match("myXcounter_one"));
+}
+#endif
 
 TEST(ViewRegistry, FindViewsEmptyRegistry)
 {
