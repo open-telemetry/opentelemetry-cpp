32713835bf6cadb1a9dd407d030582e08be19ef7
Deniz Ariyan <denizariyan@gmail.com>
2026-09-13 15:51:52 +0200

    [CODE HEALTH] Prevent std::out_of_range escaping noexcept substr callers

--- api/test/common/string_util_test.cc
+++ api/test/common/string_util_test.cc
@@ -3,8 +3,10 @@
 
 #include <gtest/gtest.h>
 #include <cstring>
+#include <ostream>
 #include <string>
 
+#include <opentelemetry/common/macros.h>
 #include <opentelemetry/common/string_util.h>
 
 // ------------------------- StringUtil class tests ---------------------------------
@@ -56,3 +58,69 @@ TEST(StringUtilTest, TrimStringOutOfRange)
 {
   EXPECT_EQ(StringUtil::Trim("x", 2, 1), "");
 }
+
+struct SubstrTestData
+{
+  const char *input;
+  size_t pos;
+  size_t count;
+
+  friend void PrintTo(const SubstrTestData &data, std::ostream *os)
+  {
+    *os << "(\"" << data.input << "\"," << data.pos << "," << data.count << ")";
+  }
+};
+
+const SubstrTestData substr_in_range_test_cases[] = {
+    {"k1=v1", 0, std::string::npos},
+    {"k1=v1", 3, std::string::npos},
+    {"k1=v1", 0, 2},
+    {"k1=v1", 3, 100},
+    {"k1=v1", 2, 0},
+    {"k1=v1", 5, std::string::npos},
+    {"x", 0, std::string::npos},
+    {"x", 1, std::string::npos},
+    {"", 0, std::string::npos},
+    {"", 0, 0},
+};
+
+using SubstrTestFixture = ::testing::TestWithParam<SubstrTestData>;
+
+TEST_P(SubstrTestFixture, MatchesStdSubstr)
+{
+  const SubstrTestData test_param = GetParam();
+  const std::string input(test_param.input);
+
+  EXPECT_EQ(StringUtil::Substr(input, test_param.pos, test_param.count),
+            input.substr(test_param.pos, test_param.count));
+}
+
+INSTANTIATE_TEST_SUITE_P(SubstrTestCases,
+                         SubstrTestFixture,
+                         ::testing::ValuesIn(substr_in_range_test_cases));
+
+const SubstrTestData substr_out_of_range_test_cases[] = {
+    {"k1=v1", 6, std::string::npos},
+    {"k1=v1", 100, 2},
+    {"k1=v1", std::string::npos, std::string::npos},
+    {"x", 2, std::string::npos},
+    {"", 1, std::string::npos},
+};
+
+using SubstrOutOfRangeTestFixture = ::testing::TestWithParam<SubstrTestData>;
+
+TEST_P(SubstrOutOfRangeTestFixture, HandlesPosPastTheEnd)
+{
+  const SubstrTestData test_param = GetParam();
+
+#if OPENTELEMETRY_HAVE_EXCEPTIONS
+  EXPECT_EQ(StringUtil::Substr(test_param.input, test_param.pos, test_param.count), "");
+#else
+  // Without exceptions the underlying calls std::terminate(), so helper cannot recover.
+  EXPECT_DEATH({ StringUtil::Substr(test_param.input, test_param.pos, test_param.count); }, "");
+#endif
+}
+
+INSTANTIATE_TEST_SUITE_P(SubstrOutOfRangeTestCases,
+                         SubstrOutOfRangeTestFixture,
+                         ::testing::ValuesIn(substr_out_of_range_test_cases));
