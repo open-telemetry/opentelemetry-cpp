b72a588d5926d6593831a51ca0e7bd5e57fd2afe
Om Kulkarni <kulkarniom7057@gmail.com>
2026-09-22 08:52:54 +0530

    Add missing nostd/unique_ptr.h include for IWYU

--- sdk/test/logs/logger_sdk_test.cc
+++ sdk/test/logs/logger_sdk_test.cc
@@ -22,6 +22,7 @@
 #include "opentelemetry/logs/severity.h"
 #include "opentelemetry/nostd/shared_ptr.h"
 #include "opentelemetry/nostd/string_view.h"
+#include "opentelemetry/nostd/unique_ptr.h"
 #include "opentelemetry/nostd/variant.h"
 #include "opentelemetry/sdk/instrumentationscope/instrumentation_scope.h"
 #include "opentelemetry/sdk/instrumentationscope/scope_configurator.h"
